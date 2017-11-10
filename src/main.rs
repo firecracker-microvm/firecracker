3c27ec879e00ade320d530630939e4c615d03327
Alexandru Branciog <branciog@amazon.com>
2017-11-10 08:22:13 +0100

    add kernel file and kernel cmdline arguments
    Signed-off-by: Alexandru Branciog <branciog@amazon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+#[macro_use(crate_version, crate_authors)]
+extern crate clap;
 extern crate vmm;
 
+use clap::{App, Arg};
 use vmm::boot_kernel;
+use vmm::machine::MachineCfg;
+
 
 fn main() {
-    boot_kernel();
+    let matches = App::new("firecracker")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Launch a microvm.")
+        .arg(Arg::with_name("kernel")
+            .short("k")
+            .long("kernel")
+            .value_name("FILE")
+            .help("Filename of the kernel (vmlinux.bin)")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("kernel_cmdline")
+            .long("kernel-cmdline")
+            .help("The kernel's command line")
+            .takes_value(true))
+        .get_matches();
+
+    let mut cfg = MachineCfg::new();
+    cfg.populate(matches).expect("parsing arguments failed");
+
+    boot_kernel(&cfg);
 }
