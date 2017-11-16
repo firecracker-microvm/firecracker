2b1b1c55a3db7cca3c7873f80adffd040b991c78
Constantin Musca <mcostin@amazon.com>
2017-11-16 19:30:06 +0200

    Propagate the vmm errors and simplify MachineCfg
    Also, put the kernel_path in the config (not the fd) as this is passed
    as configuration option.
    
    Signed-off-by: Constantin Musca <mcostin@amazon.com>

--- vmm/src/machine.rs
+++ vmm/src/machine.rs
@@ -1,29 +1,28 @@
 /// Machine configuration specifics
 
-use std::path::Path;
-use std::fs::File;
 use std::io;
+use std::path::PathBuf;
+
 use clap::ArgMatches;
 
 pub struct MachineCfg {
-    kernel_fd: Option<File>,
-    kernel_cmdline: Option<String>,
+    pub kernel_path: Option<PathBuf>,
+    pub kernel_cmdline: Option<String>,
 }
 
 impl MachineCfg {
     pub fn new() -> Self {
         MachineCfg {
-            kernel_fd: None,
+            kernel_path: None,
             kernel_cmdline: Some(String::from(
                 "console=ttyS0,115200n8 init=/init tsc=reliable no_timer_check cryptomgr.notests",
             )),
         }
     }
 
-    pub fn populate(&mut self, matches: ArgMatches) -> Result<(), io::Error> {
-        if let Some(value) = matches.value_of("kernel") {
-            let path = Path::new(value);
-            self.kernel_fd = Some(File::open(&path)?);
+    pub fn populate(&mut self, matches: ArgMatches) -> io::Result<()> {
+        if let Some(value) = matches.value_of("kernel_path") {
+            self.kernel_path = Some(PathBuf::from(value));
         }
 
         if let Some(value) = matches.value_of("kernel_cmdline") {
@@ -32,13 +31,4 @@ impl MachineCfg {
 
         Ok(())
     }
-
-    pub fn kernel_fd(&self) -> &Option<File> {
-        &self.kernel_fd
-    }
-
-    pub fn kernel_cmdline(&self) -> &Option<String> {
-        &self.kernel_cmdline
-    }
-
 }
