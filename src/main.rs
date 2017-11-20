90000db6e0247b2ded2519c13eb8037ba17bfe8c
Constantin Musca <mcostin@amazon.com>
2017-11-20 17:28:15 +0200

    root: fix style by running cargo fmt
    Signed-off-by: Constantin Musca <mcostin@amazon.com>

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,20 @@ fn main() {
         .version(crate_version!())
         .author(crate_authors!())
         .about("Launch a microvm.")
-        .arg(Arg::with_name("kernel_path")
-            .short("k")
-            .long("kernel-path")
-            .help("The kernel's file path (vmlinux.bin)")
-            .required(true)
-            .takes_value(true))
-        .arg(Arg::with_name("kernel_cmdline")
-            .long("kernel-cmdline")
-            .help("The kernel's command line")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("kernel_path")
+                .short("k")
+                .long("kernel-path")
+                .help("The kernel's file path (vmlinux.bin)")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("kernel_cmdline")
+                .long("kernel-cmdline")
+                .help("The kernel's command line")
+                .takes_value(true),
+        )
         .get_matches();
 
     let mut cfg = MachineCfg::new();
