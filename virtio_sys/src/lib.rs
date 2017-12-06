6c3f497e5506e5b2ca5be23d837bcf8d90c2d39e
Alexandru Agache <aagch@amazon.com>
2017-12-06 15:34:59 +0200

    virtio_sys: added new crate
    Added parts of the virtio_sys crate from crosvm, which are used by the
    virtio-net code. This crate  will probably be further developed as we
    also introduce vhost devices.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

---
+++ virtio_sys/src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+pub mod virtio_net;
