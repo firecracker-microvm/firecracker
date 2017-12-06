fdba4b6eeaa74dc57d03434374b69209146785a1
Alexandru Agache <aagch@amazon.com>
2017-12-06 18:11:18 +0200

    firecracker: added virtio blk and net dev support
    The firecracker app now supports additional parameters that allow one
    block device and/or one net device to be attached to the VM. Block
    devices are backed by the specified file. Net devices communicate with
    the host via a TAP device that is created with the specified ip address
    and subnet mask (the subnet mask defaults to /24).
    
    Also added code for attaching blk and net virtio devices based on the
    machine configuration, and for responding to MmioRead and MmioWrite
    cpu exit events.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use(crate_version, crate_authors)]
 extern crate clap;
 
+extern crate devices;
 extern crate sys_util;
 extern crate vmm;
 
@@ -11,7 +12,6 @@ use clap::{App, Arg};
 use sys_util::syslog;
 use vmm::boot_kernel;
 use vmm::machine::MachineCfg;
-
 
 fn main() {
     if let Err(e) = syslog::init() {
@@ -52,10 +52,40 @@ fn main() {
                 .help("Number of VCPUs")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("root_blk_file")
+                .short("r")
+                .long("root-blk")
+                .help("File to serve as root block device")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("host_ip")
+                .long("host-ip")
+                .help("IPv4 address of the host interface")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("subnet_mask")
+                .long("subnet-mask")
+                .default_value("255.255.255.0")
+                .help("Subnet mask for the IP address of host interface")
+                .takes_value(true)
+        )
+        /*
+        The mac address option is not currently implemented; the L2 addresses for both the
+        host interface and the guest interface take some implicit (possibly random) values
+        .arg(
+            Arg::with_name("mac_address")
+                .long("mac-addr")
+                .help("MAC address for the VM")
+                .takes_value(true)
+        )*/
         .get_matches();
 
     let kernel_path : Option<PathBuf> = cmd_arguments.value_of("kernel_path").map(|s| PathBuf::from(s));
 
+    //unwrap should not panic because kernel_cmdline has a default value
     let kernel_cmdline = String::from(cmd_arguments.value_of("kernel_cmdline").unwrap());
 
     let vcpu_count = match cmd_arguments
@@ -80,8 +110,28 @@ fn main() {
         }
     };
 
-    let cfg = MachineCfg::new(kernel_path, kernel_cmdline, vcpu_count, mem_size,
-                              None , None, "255.255.255.0".parse().unwrap());
+    let root_blk_file = cmd_arguments.value_of("root_blk_file").map(|s| PathBuf::from(s));
+
+    //fixme print some message when the Ipv4Addrs cannot be parsed
+    let host_ip = cmd_arguments
+        .value_of("host_ip")
+        .map(|x| x.parse().unwrap());
+
+    let subnet_mask = cmd_arguments
+        .value_of("subnet_mask")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let cfg = MachineCfg::new(
+        kernel_path,
+        kernel_cmdline,
+        vcpu_count,
+        mem_size,
+        root_blk_file,
+        host_ip,
+        subnet_mask,
+    );
 
     boot_kernel(&cfg).expect("cannot boot kernel");
 }
