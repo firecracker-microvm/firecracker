d695064b8a9e221970ae5e13fcd81c8e8c9bd634
Alexandru Agache <aagch@amazon.com>
2017-12-15 10:36:30 +0200

    devices: added virtio block device support
    Added virtio block device support, based on code from crosvm.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- devices/src/virtio/mod.rs
+++ devices/src/virtio/mod.rs
@@ -4,9 +4,11 @@
 
 //! Implements virtio devices, queues, and transport mechanisms.
 
+mod block;
 mod mmio;
 mod queue;
 
+pub use self::block::*;
 pub use self::mmio::*;
 pub use self::queue::*;
 
@@ -16,6 +18,8 @@ const DEVICE_DRIVER_OK: u32 = 0x04;
 const DEVICE_FEATURES_OK: u32 = 0x08;
 const DEVICE_FAILED: u32 = 0x80;
 
+// Types taken from linux/virtio_ids.h
+const TYPE_BLOCK: u32 = 2;
 const INTERRUPT_STATUS_USED_RING: u32 = 0x1;
 
 /// Offset from the base MMIO address of a virtio device used by the guest to notify the device of
