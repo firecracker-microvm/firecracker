7697c82f0e5ee2c785da72ba7c88d475751266ad
Diana Popa <dpopa@amazon.com>
2018-02-20 15:26:47 +0000

    virtio-devices: adding vsock as a virtio device
    Moreover, the ActivateResult was moved from mmio module
    to the main module as this is not mmio specific and it
    could also be used for other types of devices in the future.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- devices/src/virtio/mod.rs
+++ devices/src/virtio/mod.rs
@@ -3,16 +3,22 @@
 // found in the LICENSE file.
 
 //! Implements virtio devices, queues, and transport mechanisms.
+use std;
+use sys_util::Error as SysError;
+use std::io::Error as IOError;
+use self::vhost::Error as VhostBackendError;
 
 pub mod block;
 mod mmio;
 pub mod net;
 mod queue;
+pub mod vhost;
 
 pub use self::block::*;
 pub use self::mmio::*;
 pub use self::net::*;
 pub use self::queue::*;
+pub use self::vhost::vsock::*;
 
 const DEVICE_ACKNOWLEDGE: u32 = 0x01;
 const DEVICE_DRIVER: u32 = 0x02;
@@ -23,9 +29,21 @@ const DEVICE_FAILED: u32 = 0x80;
 // Types taken from linux/virtio_ids.h
 const TYPE_NET: u32 = 1;
 const TYPE_BLOCK: u32 = 2;
+const TYPE_VSOCK: u32 = 19;
 
 const INTERRUPT_STATUS_USED_RING: u32 = 0x1;
 
 /// Offset from the base MMIO address of a virtio device used by the guest to notify the device of
 /// queue events.
 pub const NOTIFY_REG_OFFSET: u32 = 0x50;
+
+#[derive(Debug)]
+pub enum ActivateError {
+    EventFd(SysError),
+    TryClone(SysError),
+    EpollCtl(IOError),
+    BadActivate,
+    BadVhostActivate(VhostBackendError),
+}
+
+pub type ActivateResult = std::result::Result<(), ActivateError>;
