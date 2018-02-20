1f3a90f260b89d74e79aef1890b68b091163d3c4
Diana Popa <dpopa@amazon.com>
2018-02-20 14:08:58 +0000

    virtio-vhost: Implement virtio protocol for vhost based devices
    Adding support for agnostical handling of events for any vhost-based device.
    The vsock module implements the virtio-vsock specific transport
    protocol making use of the event handler.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

---
+++ devices/src/virtio/vhost/vsock.rs
@@ -0,0 +1,261 @@
+use super::*;
+use super::handle::*;
+use super::super::{ActivateError, ActivateResult};
+use super::super::{Queue, VirtioDevice, TYPE_VSOCK};
+use vhost_backend::Vsock as VhostVsockFd;
+use vhost_backend::Vhost;
+use sys_util::{EventFd, GuestMemory};
+use vhost_sys::vhost::*;
+use virtio_sys::virtio_config::*;
+use virtio_sys::virtio_ring::*;
+
+use epoll;
+use byteorder::{ByteOrder, LittleEndian};
+use std::sync::Arc;
+use std::sync::atomic::AtomicUsize;
+use std::os::unix::io::AsRawFd;
+
+const QUEUE_SIZE: u16 = 256;
+const NUM_QUEUES: usize = 3;
+const QUEUE_SIZES: &'static [u16] = &[QUEUE_SIZE; NUM_QUEUES];
+
+impl std::convert::From<super::Error> for ActivateError {
+    fn from(error: super::Error) -> Self {
+        ActivateError::BadVhostActivate(error)
+    }
+}
+
+pub struct Vsock {
+    worker_kill_evt: Option<EventFd>,
+    kill_evt: Option<EventFd>,
+    vsock_fd: Option<VhostVsockFd>,
+    cid: u64,
+    interrupt: Option<EventFd>,
+    avail_features: u64,
+    acked_features: u64,
+    epoll_config: VhostEpollConfig,
+}
+
+impl Vsock {
+    /// Create a new virtio-vsock device with the given VM cid.
+    pub fn new(cid: u64, mem: &GuestMemory, epoll_config: VhostEpollConfig) -> Result<Vsock> {
+        let kill_evt = EventFd::new().map_err(Error::CreateKillEventFd)?;
+        let fd = VhostVsockFd::new(mem).map_err(Error::VhostOpen)?;
+
+        let avail_features = 1 << VIRTIO_F_NOTIFY_ON_EMPTY | 1 << VIRTIO_RING_F_INDIRECT_DESC
+            | 1 << VIRTIO_RING_F_EVENT_IDX | 1 << VHOST_F_LOG_ALL
+            | 1 << VIRTIO_F_ANY_LAYOUT | 1 << VIRTIO_F_VERSION_1;
+
+        Ok(Vsock {
+            worker_kill_evt: Some(kill_evt.try_clone().map_err(Error::CloneKillEventFd)?),
+            kill_evt: Some(kill_evt),
+            vsock_fd: Some(fd),
+            cid,
+            interrupt: Some(EventFd::new().map_err(Error::VhostIrqCreate)?),
+            avail_features: avail_features,
+            acked_features: 0,
+            epoll_config,
+        })
+    }
+
+    pub fn new_for_testing(cid: u64, features: u64, epoll_config: VhostEpollConfig) -> Vsock {
+        Vsock {
+            worker_kill_evt: None,
+            kill_evt: None,
+            vsock_fd: None,
+            cid: cid,
+            interrupt: None,
+            avail_features: features,
+            acked_features: 0,
+            epoll_config,
+        }
+    }
+
+    pub fn acked_features(&self) -> u64 {
+        self.acked_features
+    }
+}
+
+impl Drop for Vsock {
+    fn drop(&mut self) {
+        // Only kill the child if it claimed its eventfd.
+        if self.worker_kill_evt.is_none() {
+            if let Some(ref kill_evt) = self.kill_evt {
+                // Ignore the result because there is nothing we can do about it.
+                let _ = kill_evt.write(1);
+            }
+        }
+    }
+}
+
+impl VirtioDevice for Vsock {
+    fn device_type(&self) -> u32 {
+        TYPE_VSOCK
+    }
+
+    fn queue_max_sizes(&self) -> &[u16] {
+        QUEUE_SIZES
+    }
+
+    fn features(&self, page: u32) -> u32 {
+        match page {
+            // Get the lower 32-bits of the features bitfield.
+            0 => self.avail_features as u32,
+            // Get the upper 32-bits of the features bitfield.
+            1 => (self.avail_features >> 32) as u32,
+            _ => {
+                warn!(
+                    "vsock: virtio-vsock got request for features page: {}",
+                    page
+                );
+                0u32
+            }
+        }
+    }
+
+    fn ack_features(&mut self, page: u32, value: u32) {
+        let mut v = match page {
+            0 => value as u64,
+            1 => (value as u64) << 32,
+            _ => {
+                warn!(
+                    "vsock: virtio-vsock device cannot ack unknown feature page: {}",
+                    page
+                );
+                0u64
+            }
+        };
+
+        // Check if the guest is ACK'ing a feature that we didn't claim to have.
+        let unrequested_features = v & !self.avail_features;
+        if unrequested_features != 0 {
+            warn!("vsock: virtio-vsock got unknown feature ack: {:x}", v);
+
+            // Don't count these features as acked.
+            v &= !unrequested_features;
+        }
+        self.acked_features |= v;
+    }
+
+    fn read_config(&self, offset: u64, data: &mut [u8]) {
+        match offset {
+            0 if data.len() == 8 => LittleEndian::write_u64(data, self.cid),
+            0 if data.len() == 4 => LittleEndian::write_u32(data, (self.cid & 0xffffffff) as u32),
+            4 if data.len() == 4 => {
+                LittleEndian::write_u32(data, ((self.cid >> 32) & 0xffffffff) as u32)
+            }
+            _ => warn!(
+                "vsock: virtio-vsock received invalid read request of {} bytes at offset {}",
+                data.len(),
+                offset
+            ),
+        }
+    }
+
+
+
+    fn activate(
+        &mut self,
+        _: GuestMemory,
+        interrupt_evt: EventFd,
+        interrupt_status: Arc<AtomicUsize>,
+        queues: Vec<Queue>,
+        queue_evts: Vec<EventFd>,
+    ) -> ActivateResult {
+        if queues.len() != NUM_QUEUES || queue_evts.len() != NUM_QUEUES {
+            error!(
+                "vsock: virtio-vsock expected {} queues, got {}",
+                NUM_QUEUES,
+                queues.len()
+            );
+            return Err(ActivateError::BadActivate);
+        }
+
+        if let Some(vsock_fd) = self.vsock_fd.take() {
+            if let Some(interrupt) = self.interrupt.take() {
+                if let Some(kill_evt) = self.worker_kill_evt.take() {
+                    let kill_raw_fd = kill_evt.as_raw_fd();
+                    let cid = self.cid;
+
+                    // The third vq is an event-only vq that is not handled by the vhost
+                    // subsystem (but still needs to exist).  Split it off here.
+                    let vhost_queues = queues[..2].to_vec();
+
+                    // Preliminary setup for vhost net.
+                    vsock_fd.set_owner().map_err(Error::VhostSetOwner)?;
+
+                    let avail_features = vsock_fd.get_features().map_err(Error::VhostGetFeatures)?;
+                    let features: u64 = self.acked_features & avail_features;
+                    vsock_fd
+                        .set_features(features)
+                        .map_err(Error::VhostSetFeatures)?;
+
+                    vsock_fd.set_mem_table().map_err(Error::VhostSetMemTable)?;
+
+                    for (queue_index, ref queue) in vhost_queues.iter().enumerate() {
+                        vsock_fd
+                            .set_vring_num(queue_index, queue.get_max_size())
+                            .map_err(Error::VhostSetVringNum)?;
+
+                        vsock_fd
+                            .set_vring_addr(
+                                QUEUE_SIZES[queue_index],
+                                queue.actual_size(),
+                                queue_index,
+                                0,
+                                queue.desc_table,
+                                queue.used_ring,
+                                queue.avail_ring,
+                                None,
+                            )
+                            .map_err(Error::VhostSetVringAddr)?;
+                        vsock_fd
+                            .set_vring_base(queue_index, 0)
+                            .map_err(Error::VhostSetVringBase)?;
+                        vsock_fd
+                            .set_vring_call(queue_index, &interrupt)
+                            .map_err(Error::VhostSetVringCall)?;
+                        vsock_fd
+                            .set_vring_kick(queue_index, &queue_evts[queue_index])
+                            .map_err(Error::VhostSetVringKick)?;
+                    }
+
+                    let handler = VhostEpollHandler::new(
+                        vsock_fd,
+                        interrupt_status,
+                        interrupt_evt,
+                        interrupt,
+                    );
+
+                    // vsock specific ioctl setup for running device
+                    handler.get_device().set_guest_cid(cid).map_err(Error::VhostVsockSetCid)?;
+                    handler.get_device().start().map_err(Error::VhostVsockStart)?;
+
+                    let queue_evt_raw_fd = handler.get_queue_evt();
+                    //channel should be open and working
+                    self.epoll_config
+                        .get_sender()
+                        .send(Box::new(handler))
+                        .unwrap();
+
+                    epoll::ctl(
+                        self.epoll_config.get_raw_epoll_fd(),
+                        epoll::EPOLL_CTL_ADD,
+                        queue_evt_raw_fd,
+                        epoll::Event::new(epoll::EPOLLIN, self.epoll_config.get_queue_evt_token()),
+                    ).map_err(ActivateError::EpollCtl)?;
+
+                    epoll::ctl(
+                        self.epoll_config.get_raw_epoll_fd(),
+                        epoll::EPOLL_CTL_ADD,
+                        kill_raw_fd,
+                        epoll::Event::new(epoll::EPOLLIN, self.epoll_config.get_kill_token()),
+                    ).map_err(ActivateError::EpollCtl)?;
+
+                    return Ok(());
+                }
+            }
+        }
+        Err(ActivateError::BadActivate)
+    }
+}
