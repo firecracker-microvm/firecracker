d5fc6b32f3105da18bd215f2282a7a1bd68b98f1
Diana Popa <dpopa@amazon.com>
2018-02-20 13:27:32 +0000

    virtio_sys: autogenerate virtio-vsock related headers
    The virtio transport protocol over the vsock device
    needs additional linux kernels autogenerated: bindgen v0.32.1, linux v4.13.
    
    bindgen include/uapi/linux/virtio_*name*.h -o virtio_*name*.rs \
    	--no-rustfmt-bindings \
    	--with-derive-default \
    	--blacklist-type s8 \
    	--blacklist-type u8 \
    	--blacklist-type s16 \
    	--blacklist-type u16 \
    	--blacklist-type s32 \
    	--blacklist-type u32 \
    	--blacklist-type s64 \
    	--blacklist-type u64 \
    	-- -Iinclude
    rustup run nightly-2018-01-08 rustfmt virtio_*name*.rs
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- virtio_sys/src/lib.rs
+++ virtio_sys/src/lib.rs
@@ -1,5 +1,11 @@
 // Copyright 2017 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
 pub mod virtio_net;
+pub mod virtio_config;
+pub mod virtio_ring;
+pub mod virtio_ids;
