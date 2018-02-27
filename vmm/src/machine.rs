30bc41ecb14c8895b61e09ee27112a53f7e71f18
Andreea Florescu <fandree@amazon.com>
2018-02-27 15:52:16 +0200

    vm config: add structure for configuring the VM
    Added VirtualMachineConfiguration structure that contains
    the configurable parameters of the virtual machine (nr of vcpu
    and memory size).
    Added a function for setting the VM configuration that is used
    from main and will also be used to setup the machine from the API.
    Removed vcpu_count and mem_size from MachineCfg.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- vmm/src/machine.rs
+++ vmm/src/machine.rs
@@ -4,8 +4,6 @@ use std::net::Ipv4Addr;
 use std::path::PathBuf;
 
 pub struct MachineCfg {
-    pub vcpu_count: u8,         //has default value
-    pub mem_size: usize,        //has default value
     pub root_blk_file: Option<PathBuf>,
     pub host_ip: Option<Ipv4Addr>,
     pub subnet_mask: Ipv4Addr, //has default value
@@ -14,16 +12,12 @@ pub struct MachineCfg {
 
 impl MachineCfg {
     pub fn new(
-        vcpu_count: u8,
-        mem_size: usize,
         root_blk_file: Option<PathBuf>,
         host_ip: Option<Ipv4Addr>,
         subnet_mask: Ipv4Addr,
         vsock_guest_cid: Option<u64>,
     ) -> Self {
         MachineCfg {
-            vcpu_count,
-            mem_size,
             root_blk_file,
             host_ip,
             subnet_mask,
