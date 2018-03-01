e21d1f0c889d3cb8fa44d490a6707bad9e0005f9
Alexandru Agache <aagch@amazon.com>
2018-03-02 00:35:16 +0200

    vmm: refactored device_config
    Refactored the device_config module, by creating a folder with the same
    name, and renaming the old device_config.rs file to device_config/
    drive.rs, because it mostly contained drive related functionality. The
    network device related content has been moved to device_config/net.rs.
    Each different class of devices will have its own submodule now, and the
    elements which are relevant to the outside  will be pub used from
    device_config/mod.rs.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- vmm/src/device_config/drive.rs
+++ vmm/src/device_config/drive.rs
@@ -1,16 +1,9 @@
-use std::collections::linked_list::{self, LinkedList};
-use std::mem;
+use std::collections::LinkedList;
 use std::path::PathBuf;
 
-use std::rc::Rc;
 use std::result;
 
-use api_server::request::sync::{DriveDescription, DriveError, Error as SyncError,
-                                NetworkInterfaceBody, OkStatus as SyncOkStatus};
-use net_util::{Tap, TapError};
-
-// TODO: I think this module should be broken up into multiple files, one for each of drives,
-// network interfaces, and vsocks (and limiters maybe at some point).
+use api_server::request::sync::{DriveDescription, DriveError};
 
 type Result<T> = result::Result<T, DriveError>;
 
@@ -95,73 +88,6 @@ impl BlockDeviceConfigs {
         }
 
         Ok(())
-    }
-}
-
-pub struct NetworkInterfaceConfig {
-    // The request body received from the API side.
-    _body: NetworkInterfaceBody,
-    // We extract the id from the body and hold it as a reference counted String. This should
-    // come in handy later on, when we'll need the id to appear in a number of data structures
-    // to implement efficient lookup, update, deletion, etc.
-    id: Rc<String>,
-    // We open the tap that will be associated with the virtual device as soon as the PUT request
-    // arrives from the API. We want to see if there are any errors associated with the operation,
-    // and if so, we want to report the failure back to the API caller immediately. This is an
-    // option, because the inner value will be moved to the actual virtio net device before boot.
-    pub tap: Option<Tap>,
-}
-
-impl NetworkInterfaceConfig {
-    pub fn try_from_body(mut body: NetworkInterfaceBody) -> result::Result<Self, TapError> {
-        let id = Rc::new(mem::replace(&mut body.iface_id, String::new()));
-
-        // TODO: rework net_util stuff such that references would suffice here, instead
-        // of having to move things around.
-        let tap = Tap::open_named(body.host_dev_name.as_str())?;
-
-        Ok(NetworkInterfaceConfig {
-            _body: body,
-            id,
-            tap: Some(tap),
-        })
-    }
-
-    pub fn id_as_str(&self) -> &str {
-        self.id.as_str()
-    }
-
-    pub fn take_tap(&mut self) -> Option<Tap> {
-        self.tap.take()
-    }
-}
-
-pub struct NetworkInterfaceConfigs {
-    // We use just a list for now, since we only add interfaces as this point.
-    if_list: LinkedList<NetworkInterfaceConfig>,
-}
-
-impl NetworkInterfaceConfigs {
-    pub fn new() -> Self {
-        NetworkInterfaceConfigs {
-            if_list: LinkedList::new(),
-        }
-    }
-
-    pub fn put(&mut self, body: NetworkInterfaceBody) -> result::Result<SyncOkStatus, SyncError> {
-        let cfg = NetworkInterfaceConfig::try_from_body(body).map_err(SyncError::OpenTap)?;
-
-        for x in self.if_list.iter() {
-            if x.id_as_str() == cfg.id_as_str() {
-                return Err(SyncError::UpdateNotImplemented);
-            }
-        }
-        self.if_list.push_back(cfg);
-        Ok(SyncOkStatus::Created)
-    }
-
-    pub fn iter_mut(&mut self) -> linked_list::IterMut<NetworkInterfaceConfig> {
-        self.if_list.iter_mut()
     }
 }
 
