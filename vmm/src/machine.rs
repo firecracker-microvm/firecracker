2768d2a8c8c6adec4d3e0e792275420273a135d9
Diana Popa <dpopa@amazon.com>
2018-03-01 13:10:24 +0000

    MachineCfg: Implementing Default trait
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- vmm/src/machine.rs
+++ vmm/src/machine.rs
@@ -3,11 +3,24 @@
 use std::net::Ipv4Addr;
 use std::path::PathBuf;
 
+pub const DEFAULT_SUBNET_MASK: &str = "255.255.255.0";
+
 pub struct MachineCfg {
     pub root_blk_file: Option<PathBuf>,
     pub host_ip: Option<Ipv4Addr>,
     pub subnet_mask: Ipv4Addr, //has default value
     pub vsock_guest_cid: Option<u64>,
+}
+
+impl Default for MachineCfg {
+    fn default() -> Self {
+        MachineCfg {
+            root_blk_file: None,
+            host_ip: None,
+            subnet_mask: DEFAULT_SUBNET_MASK.parse().unwrap(),
+            vsock_guest_cid: None,
+        }
+    }
 }
 
 impl MachineCfg {
