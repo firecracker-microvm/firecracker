9dc4fa16bcfb8ecd8a688dd103084368dcb775d6
Andreea Florescu <fandree@amazon.com>
2018-03-06 11:55:57 +0200

    vmm: fix drive test error
    delete_dummy_path was failing because it could not find
    the remove_file function. Import std::fs::self.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- vmm/src/device_config/drive.rs
+++ vmm/src/device_config/drive.rs
@@ -94,9 +94,9 @@ impl BlockDeviceConfigs {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
+    use std::fs::{self, File};
 
-    // Helper function for creating a dummy file
+   // Helper function for creating a dummy file
     // The filename has to be unique among all tests because the tests are run on many threads
     fn create_dummy_path(filename: String) -> PathBuf {
         let _file = File::create(filename.clone());
@@ -105,7 +105,7 @@ mod tests {
 
     // Helper function for deleting a dummy file
     fn delete_dummy_path(filename: String) {
-        let _rs = std::fs::remove_file(filename);
+        let _rs = fs::remove_file(filename);
     }
 
     #[test]
