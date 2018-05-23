e5534ed4d7b91d4794fb9070e336301886282aa1
Andreea Florescu <fandree@amazon.com>
2018-05-23 17:51:48 +0300

    cpuid: group defines by leaf & register
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- x86_64/src/cpuid.rs
+++ x86_64/src/cpuid.rs
@@ -1,26 +1,55 @@
 use kvm::CpuId;
 use std::result;
 
+// Basic CPUID Information
+mod leaf_0x1 {
+    pub mod ebx {
+        pub const APICID_SHIFT: u32 = 24; // The (fixed) default APIC ID.
+        pub const CLFLUSH_SIZE_SHIFT: u32 = 8; // Bytes flushed when executing CLFLUSH.
+        pub const CPU_COUNT_SHIFT: u32 = 16; // The logical processor count.
+    }
+
+    pub mod ecx {
+        pub const TSC_DEADLINE_TIMER_SHIFT: u32 = 24;
+        // Flag to be set when the cpu is running on a hypervisor.
+        pub const HYPERVISOR_SHIFT: u32 = 31;
+    }
+
+    pub mod edx {
+        pub const HTT_SHIFT: u32 = 28; // Hyper Threading Enabled.
+    }
+}
+
+// Deterministic Cache Parameters Leaf
+mod leaf_0x4 {
+    pub mod eax {
+        pub const CACHE_LEVEL: u32 = 5;
+        pub const MAX_ADDR_IDS_SHARING_CACHE: u32 = 14;
+        pub const MAX_ADDR_IDS_IN_PACKAGE: u32 = 26;
+    }
+}
+
+// Thermal and Power Management Leaf
+mod leaf_0x6 {
+    pub mod ecx {
+        pub const EPB_SHIFT: u32 = 3; // "Energy Performance Bias" bit.
+    }
+}
+
+// Extended Topology Leaf
+#[allow(non_snake_case)]
+mod leaf_0xb {
+    pub const LEVEL_TYPE_INVALID: u32 = 0;
+    pub const LEVEL_TYPE_THREAD: u32 = 1;
+    pub const LEVEL_TYPE_CORE: u32 = 2;
+    pub mod ecx {
+        pub const LEVEL_TYPE_SHIFT: u32 = 8; // Shift for setting level type for leaf 11
+    }
+}
+
 // constants for setting the fields of kvm_cpuid2 structures
 // CPUID bits in ebx, ecx, and edx.
 const EBX_CLFLUSH_CACHELINE: u32 = 8; // Flush a cache line size.
-const EBX_CLFLUSH_SIZE_SHIFT: u32 = 8; // Bytes flushed when executing CLFLUSH.
-const EBX_CPU_COUNT_SHIFT: u32 = 16; // The logical processor count .
-const EBX_APICID_SHIFT: u32 = 24; // The (fixed) default APIC ID.
-const ECX_EPB_SHIFT: u32 = 3; // "Energy Performance Bias" bit.
-const ECX_TSC_DEADLINE_TIMER_SHIFT: u32 = 24;
-const ECX_HYPERVISOR_SHIFT: u32 = 31; // Flag to be set when the cpu is running on a hypervisor.
-const ECX_LEVEL_TYPE_SHIFT: u32 = 8; // Shift for setting level type for leaf 11
-const EDX_HTT_SHIFT: u32 = 28; // Hyper Threading Enabled.
-
-// Deterministic Cache Parameters Leaf
-const EAX_CACHE_LEVEL: u32 = 5;
-const EAX_MAX_ADDR_IDS_SHARING_CACHE: u32 = 14;
-const EAX_MAX_ADDR_IDS_IN_PACKAGE: u32 = 26;
-
-const LEAFBH_LEVEL_TYPE_INVALID: u32 = 0;
-const LEAFBH_LEVEL_TYPE_THREAD: u32 = 1;
-const LEAFBH_LEVEL_TYPE_CORE: u32 = 2;
 
 // The APIC ID shift in leaf 0xBh specifies the number of bits to shit the x2APIC ID to get a
 // unique topology of the next level. This allows 64 logical processors/package.
@@ -56,24 +85,24 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
             0x1 => {
                 // X86 hypervisor feature
                 if entry.index == 0 {
-                    entry.ecx |= 1 << ECX_TSC_DEADLINE_TIMER_SHIFT;
-                    entry.ecx |= 1 << ECX_HYPERVISOR_SHIFT;
+                    entry.ecx |= 1 << leaf_0x1::ecx::TSC_DEADLINE_TIMER_SHIFT;
+                    entry.ecx |= 1 << leaf_0x1::ecx::HYPERVISOR_SHIFT;
                 }
-                entry.ebx = ((cpu_id as u32) << EBX_APICID_SHIFT) as u32
-                    | (EBX_CLFLUSH_CACHELINE << EBX_CLFLUSH_SIZE_SHIFT);
-                entry.ebx |= max_addr_cpu << EBX_CPU_COUNT_SHIFT;
+                entry.ebx = ((cpu_id as u32) << leaf_0x1::ebx::APICID_SHIFT) as u32
+                    | (EBX_CLFLUSH_CACHELINE << leaf_0x1::ebx::CLFLUSH_SIZE_SHIFT);
+                entry.ebx |= max_addr_cpu << leaf_0x1::ebx::CPU_COUNT_SHIFT;
                 // Make sure that Hyperthreading is disabled
-                entry.edx &= !(1 << EDX_HTT_SHIFT);
+                entry.edx &= !(1 << leaf_0x1::edx::HTT_SHIFT);
                 // Enable Hyperthreading for even vCPU count so you don't end up with
                 // an even and > 1 number of sibilings
                 if cpu_count > 1 && cpu_count % 2 == 0 {
-                    entry.edx |= 1 << EDX_HTT_SHIFT;
+                    entry.edx |= 1 << leaf_0x1::edx::HTT_SHIFT;
                 }
             }
             0x4 => {
                 // Deterministic Cache Parameters Leaf
                 // Only use the last 3 bits of EAX[5:32] because the level is encoded in EAX[5:7]
-                let cache_level = (entry.eax >> EAX_CACHE_LEVEL) & (0b111 as u32);
+                let cache_level = (entry.eax >> leaf_0x4::eax::CACHE_LEVEL) & (0b111 as u32);
                 match cache_level {
                     // L1 & L2 Cache
                     1 | 2 => {
@@ -81,10 +110,10 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                         // when you only have 1 vcpu because there are no other threads on
                         // the machine to share the data/instruction cache
                         // This sets EAX[25:14]
-                        entry.eax &= !(0b111111111111 << EAX_MAX_ADDR_IDS_SHARING_CACHE);
+                        entry.eax &= !(0b111111111111 << leaf_0x4::eax::MAX_ADDR_IDS_SHARING_CACHE);
                         if cpu_count > 1 {
                             // Hyperthreading is enabled by default for vcpu_count > 2
-                            entry.eax |= 1 << EAX_MAX_ADDR_IDS_SHARING_CACHE;
+                            entry.eax |= 1 << leaf_0x4::eax::MAX_ADDR_IDS_SHARING_CACHE;
                         }
                     }
                     // L3 Cache
@@ -93,9 +122,10 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                         // when you only have 1 vcpu because there are no other threads on
                         // the machine to share the data/instruction cache
                         // This sets EAX[25:14]
-                        entry.eax &= !(0b111111111111 << EAX_MAX_ADDR_IDS_SHARING_CACHE);
+                        entry.eax &= !(0b111111111111 << leaf_0x4::eax::MAX_ADDR_IDS_SHARING_CACHE);
                         if cpu_count > 1 {
-                            entry.eax |= ((cpu_count - 1) as u32) << EAX_MAX_ADDR_IDS_SHARING_CACHE;
+                            entry.eax |= ((cpu_count - 1) as u32)
+                                << leaf_0x4::eax::MAX_ADDR_IDS_SHARING_CACHE;
                         }
                     }
                     _ => (),
@@ -105,15 +135,16 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                 // should be the same on all cache levels
                 // set this to 0 because there is only 1 core available for vcpu_count <= 2
                 // This sets EAX[31:26]
-                entry.eax &= !(0b111111 << EAX_MAX_ADDR_IDS_IN_PACKAGE);
+                entry.eax &= !(0b111111 << leaf_0x4::eax::MAX_ADDR_IDS_IN_PACKAGE);
                 if cpu_count > 2 {
                     // we have HT enabled by default, so we will have cpu_count/2 cores in package
-                    entry.eax |= (((cpu_count >> 1) - 1) as u32) << EAX_MAX_ADDR_IDS_IN_PACKAGE;
+                    entry.eax |=
+                        (((cpu_count >> 1) - 1) as u32) << leaf_0x4::eax::MAX_ADDR_IDS_IN_PACKAGE;
                 }
             }
             0x6 => {
                 // Clear X86 EPB feature.  No frequency selection in the hypervisor.
-                entry.ecx &= !(1 << ECX_EPB_SHIFT);
+                entry.ecx &= !(1 << leaf_0x6::ecx::EPB_SHIFT);
             }
             0xB => {
                 // Hide the actual topology of the underlying host
@@ -126,7 +157,8 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                             // Set the numbers of logical processors to 1
                             entry.ebx = 1;
                             // There are no hyperthreads for 1 VCPU, set the level type = 2 (Core)
-                            entry.ecx = LEAFBH_LEVEL_TYPE_CORE << ECX_LEVEL_TYPE_SHIFT;
+                            entry.ecx =
+                                leaf_0xb::LEVEL_TYPE_CORE << leaf_0xb::ecx::LEVEL_TYPE_SHIFT;
                         } else {
                             // To get the next level APIC ID, shift right with 1 because we have
                             // maximum 2 hyperthreads per core that can be represented with 1 bit
@@ -134,7 +166,8 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                             // 2 logical cores at this level
                             entry.ebx = 2;
                             // enforce this level to be of type thread
-                            entry.ecx = LEAFBH_LEVEL_TYPE_THREAD << ECX_LEVEL_TYPE_SHIFT;
+                            entry.ecx =
+                                leaf_0xb::LEVEL_TYPE_THREAD << leaf_0xb::ecx::LEVEL_TYPE_SHIFT;
                         }
                     }
                     1 => {
@@ -145,11 +178,11 @@ pub fn filter_cpuid(cpu_id: u8, cpu_count: u8, kvm_cpuid: &mut CpuId) -> Result<
                             entry.ebx = 0;
                             // ECX[7:0] = entry.index; ECX[15:8] = 0 (Invalid Level)
                             entry.ecx = (entry.index as u32)
-                                | (LEAFBH_LEVEL_TYPE_INVALID << ECX_LEVEL_TYPE_SHIFT);
+                                | (leaf_0xb::LEVEL_TYPE_INVALID << leaf_0xb::ecx::LEVEL_TYPE_SHIFT);
                         } else {
                             entry.ebx = cpu_count as u32;
                             entry.ecx = (entry.index as u32)
-                                | (LEAFBH_LEVEL_TYPE_CORE << ECX_LEVEL_TYPE_SHIFT);
+                                | (leaf_0xb::LEVEL_TYPE_CORE << leaf_0xb::ecx::LEVEL_TYPE_SHIFT);
                         }
                     }
                     level => {
@@ -208,9 +241,9 @@ mod tests {
             index: 0,
             flags: 1,
             eax: 0,
-            ebx: 1,                                              // nr of hyperthreads/core
-            ecx: LEAFBH_LEVEL_TYPE_CORE << ECX_LEVEL_TYPE_SHIFT, // ECX[15:8] = 2 (Core Level)
-            edx: 0,                                              // EDX = APIC ID = 0
+            ebx: 1, // nr of hyperthreads/core
+            ecx: leaf_0xb::LEVEL_TYPE_CORE << leaf_0xb::ecx::LEVEL_TYPE_SHIFT, // ECX[15:8] = 2 (Core Level)
+            edx: 0,                                                            // EDX = APIC ID = 0
             padding: [0, 0, 0],
         };
         assert!(entries.contains(&leaf11_index0));
