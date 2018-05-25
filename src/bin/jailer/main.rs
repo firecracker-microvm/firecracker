1806765afa5f5289666371c7a41bc110b98f52f9
Filippo Sironi <sironi@amazon.de>
2018-05-25 12:53:50 +0200

    jailer: Add initial implementation of the firecracker jailer
    At the moment, the application isn't compatible with firecracker yet. It
    simply takes an id and an exec file. When jailing firecracker, the
    expectation is that the id will be the instance id while the exec file
    is an absolute path to the firecracker executable.
    
    The id will later be used to create the "environment". The environment
    is made up of the cgroup plus a run directory containing the API socket
    - which will be created before hand - and the chroot.
    
    Signed-off-by: Filippo Sironi <sironi@amazon.de>

---
+++ src/bin/jailer/main.rs
@@ -0,0 +1,69 @@
+use std::env::args;
+use std::fs::metadata;
+use std::path::Path;
+use std::process::exit;
+
+fn print_usage() {
+    println!("Usage: jailer [OPTION] ID EXEC-FILE\n");
+    println!("Example: jailer 73 /usr/bin/firecracker\n");
+    println!("Options:");
+    println!("    -h, --help    display this text and exit");
+}
+
+struct Args {
+    id: String,
+    exec_file: String,
+}
+
+fn parse_args() -> Args {
+    let mut id = String::from("");
+    let mut exec_file = String::from("");
+    let args: Vec<String> = args().collect();
+    for arg in &args[1..] {
+        match arg.as_ref() {
+            "-h" => {
+                print_usage();
+                exit(0);
+            },
+            _ => {
+                if id.is_empty() {
+                    id = arg.to_string();
+                } else if exec_file.is_empty() {
+                    exec_file = arg.to_string();
+                } else {
+                    println!("failed to parse the command line arguments");
+                    exit(1);
+                }
+            },
+        }
+    }
+    Args { id: id, exec_file: exec_file }
+}
+
+fn validate_args(args: &Args) {
+    if args.id.is_empty() {
+        println!("id is missing");
+        exit(1);
+    }
+    if args.exec_file.is_empty() {
+        println!("exec-file is missing");
+        exit(1);
+    }
+    let metadata = match metadata(Path::new(&args.exec_file)) {
+        Ok(c) => c,
+        Err(_) => {
+            println!("failed to get the metadata for {}", args.exec_file);
+            exit(1);
+        },
+    };
+    if metadata.is_file() == false {
+        println!("{} is not a file", args.exec_file);
+        exit(1);
+    }
+}
+
+fn main() {
+    let args = parse_args();
+    validate_args(&args);
+    exit(1);
+}
