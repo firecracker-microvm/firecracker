aa1c0215331af7e6fa70e08f8f6ad8d7f0e12ea0
Alexandru Agache <aagch@amazon.com>
2018-06-13 13:37:29 +0300

    main: detect when running jailed
    Firecracker will consider it's running in a jail whenever the "--jailed"
    command line parameter is present. We register this using a global variable
    from data_model (an AtomicBool, which allows convenient access through
    a global non-mut static). This boolean will be used when obtaining file
    descriptors for /dev/kvm, /dev/net/tun, and the listening socket, since
    the jailer will provide those when present (since Firecracker can no
    longer call open or bind on its own).
    
    Removed (at least for now) the unit test for main(), because it was very
    hard to make it work properly with the new jailing detection (without
    doing something completely artificial, like not checking if Firecracker
    is jailed when #[cfg(test)] applies).
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- data_model/src/lib.rs
+++ data_model/src/lib.rs
@@ -3,3 +3,8 @@ extern crate serde;
 extern crate serde_derive;
 
 pub mod vm;
+
+use std::sync::atomic::{AtomicBool, ATOMIC_BOOL_INIT};
+
+// ATOMIC_BOOL_INIT = false
+pub static FIRECRACKER_IS_JAILED: AtomicBool = ATOMIC_BOOL_INIT;
