6ce3dc94b3fca14be4bc3efc42dfc4d9c4ec4c68
Alexandru Agache <aagch@amazon.com>
2018-06-13 13:08:54 +0300

    jailer: added run() method
    The run() method from jailer/src/lib.rs initiates the jailing process
    based on the received JailerArgs, and is called from the jailer binary.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- jailer/src/lib.rs
+++ jailer/src/lib.rs
@@ -1,14 +1,26 @@
 extern crate libc;
 extern crate regex;
 
-use std::ffi::OsStr;
-use std::fs::{canonicalize, metadata};
-use std::io;
-use std::path::PathBuf;
-use std::result;
+extern crate sys_util;
 
 mod cgroup;
 mod env;
+
+use std::ffi::OsStr;
+use std::fs::{canonicalize, metadata};
+use std::io;
+use std::os::unix::io::AsRawFd;
+use std::os::unix::net::UnixListener;
+use std::path::PathBuf;
+use std::result;
+
+use env::Env;
+
+pub const KVM_FD: i32 = 3;
+pub const DEV_NET_TUN_FD: i32 = 4;
+pub const LISTENER_FD: i32 = 5;
+
+const SOCKET_FILE_NAME: &str = "api.socket";
 
 #[derive(Debug)]
 pub enum Error {
@@ -25,9 +37,16 @@ pub enum Error {
     Gid(String),
     Metadata(PathBuf, io::Error),
     NotAFile(PathBuf),
+    OpenDevKvm(sys_util::Error),
+    OpenDevNetTun(sys_util::Error),
     ReadLine(PathBuf, io::Error),
     RegEx(regex::Error),
     Uid(String),
+    UnexpectedKvmFd(i32),
+    UnexpectedDevNetTunFd(i32),
+    UnexpectedListenerFd(i32),
+    UnixListener(io::Error),
+    UnsetCloexec(sys_util::Error),
     Write(PathBuf, io::Error),
 }
 
@@ -70,4 +89,64 @@ impl<'a> JailerArgs<'a> {
             .file_name()
             .ok_or_else(|| Error::FileName(self.exec_file_path.clone()))
     }
+}
+
+pub fn run(args: JailerArgs) -> Result<()> {
+    // We open /dev/kvm, /dev/tun, and create the listening socket. These file descriptors will be
+    // passed on to Firecracker post exec, and used as file descriptors 3, 4, and 5, respectively.
+
+    // TODO: use dup2 to make sure we're actually getting 3, 4, and 5?
+
+    // TODO: can a malicious guest that takes over firecracker use its access to the KVM fd to
+    // starve  the host of resources? (cgroups should take care of that, but do they currently?)
+
+    // Safe because we use a constant nul-terminated string and verify the result. We should
+    // get our fd = 3 here.
+    let ret = unsafe { libc::open("/dev/kvm\0".as_ptr() as *const libc::c_char, libc::O_RDWR) };
+    if ret < 0 {
+        return Err(Error::OpenDevKvm(sys_util::Error::last()));
+    }
+    if ret != KVM_FD {
+        return Err(Error::UnexpectedKvmFd(ret));
+    }
+
+    // TODO: is RDWR required for /dev/tun (most likely)?
+    // Safe because we use a constant nul-terminated string and verify the result. We should
+    // get our fd = 4 here.
+    let ret = unsafe {
+        libc::open(
+            "/dev/net/tun\0".as_ptr() as *const libc::c_char,
+            libc::O_RDWR | libc::O_NONBLOCK,
+        )
+    };
+    if ret < 0 {
+        return Err(Error::OpenDevNetTun(sys_util::Error::last()));
+    }
+    if ret != DEV_NET_TUN_FD {
+        return Err(Error::UnexpectedDevNetTunFd(ret));
+    }
+
+    let env = Env::new(args)?;
+
+    // We should get our fd = 5 here.
+    let listener = UnixListener::bind(env.chroot_dir().join(SOCKET_FILE_NAME))
+        .map_err(|e| Error::UnixListener(e))?;
+
+    let listener_fd = listener.as_raw_fd();
+    if listener_fd != LISTENER_FD {
+        return Err(Error::UnexpectedListenerFd(listener_fd));
+    }
+
+    // It turns out Rust is so safe, it opens everything with CLOSE_ON_EXEC.
+
+    // TODO: So as of today (20180612), FD_CLOEXEC is the only file descriptor flag, so setting
+    // flags to 0 should clear that and only that. Maybe at some point it would make sense to
+    // get the flags first, clear FD_CLOEXEC, and set the resulting flags.
+
+    // This is safe because we know the fd is valid.
+    if unsafe { libc::fcntl(listener_fd, libc::F_SETFD, 0) } < 0 {
+        return Err(Error::UnsetCloexec(sys_util::Error::last()));
+    }
+
+    env.run()
 }
