e4574eb727a51c6b38c90bbd42bbf7ca909d6264
Alexandra Iordache <aghecen@amazon.com>
2018-06-15 14:39:35 +0300

    Use the release jailer in the integration test
    The integration test for seccomp builds a mini jailer. The release
    binary should be run instead of the debug one.
    Aligned the mini jailer code generated by the test.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- tests/security/test_seccomp.py
+++ tests/security/test_seccomp.py
@@ -11,22 +11,22 @@ def tmp_jailer():
     JAILER_SRCDIR = path.normpath(path.join(getcwd(), '../src/bin/demo_jailer/'))
     """ Source directory for a new binary that ingests the `seccomp` crate. """
 
-    JAILER_BIN = path.normpath(path.join(getcwd(), '../target/x86_64-unknown-linux-musl/debug/demo_jailer'))
+    JAILER_BIN = path.normpath(path.join(getcwd(), '../target/x86_64-unknown-linux-musl/release/demo_jailer'))
     """ Name of the mini jailer binary. """
 
     makedirs(JAILER_SRCDIR)
     with open(path.join(JAILER_SRCDIR, 'main.rs'), 'w') as jailer_src:
         jailer_src.write("""
-extern crate seccomp;
-use std::env::args;
-use std::os::unix::process::CommandExt;
-use std::process::{Command, Stdio};
-fn main() {
-    let args: Vec<String> = args().collect();
-    let exec_file = &args[1];
-    seccomp::setup_seccomp().unwrap();
-    Command::new(exec_file).stdin(Stdio::inherit()).stdout(Stdio::inherit()).stderr(Stdio::inherit()).exec();
-}
+            extern crate seccomp;
+            use std::env::args;
+            use std::os::unix::process::CommandExt;
+            use std::process::{Command, Stdio};
+            fn main() {
+                let args: Vec<String> = args().collect();
+                let exec_file = &args[1];
+                seccomp::setup_seccomp().unwrap();
+                Command::new(exec_file).stdin(Stdio::inherit()).stdout(Stdio::inherit()).stderr(Stdio::inherit()).exec();
+            }
             """)
     yield JAILER_BIN
 
@@ -42,7 +42,7 @@ def test_seccomp_ls(tmp_jailer):
     JAILED_BIN = '/bin/ls'
     """ Path to the `ls` binary, which attempts to execute `SYS_access`, blacklisted for Firecracker."""
 
-    run(['cargo', 'build', '--bin', 'demo_jailer'])
+    run(['cargo', 'build', '--release', '--bin', 'demo_jailer'])
     assert(path.exists(tmp_jailer))
     """ Compile the mini jailer. """
 
