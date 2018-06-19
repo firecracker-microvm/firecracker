85a50fc7312e68402590e6a32999e8e1088d2ac0
Alexandru Agache <aagch@amazon.com>
2018-06-19 15:25:17 +0300

    jailer: fixed cgroup config inheritance
    We had a potential race condition, which could arise if a jailer process
    created a .../firecracker/<id> cgroup subfolder, but did not get to
    populate cgroup config files such as cpuset.mems. Now, when we attempt to
    inherit the value in such a file from the parent (the "firecracker"
    folder), we repeat the process for the grand parent whenever an empty
    line is encountered.
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- jailer/src/cgroup.rs
+++ jailer/src/cgroup.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::ffi::OsStr;
-use std::fs::{self, File, OpenOptions};
-use std::io::{BufRead, BufReader, Write};
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 use std::process;
 
@@ -9,51 +9,117 @@ use regex::Regex;
 
 use super::{Error, Result};
 
+const CONTROLLER_CPU: &str = "cpu";
+
+const CONTROLLER_CPUSET: &str = "cpuset";
+const CPUSET_CPUS: &str = "cpuset.cpus";
+const CPUSET_MEMS: &str = "cpuset.mems";
+
+const CONTROLLER_PIDS: &str = "pids";
+
 // The list of cgroup controllers we're interested in.
-const CONTROLLERS: [&'static str; 3] = ["cpu", "cpuset", "pids"];
+const CONTROLLERS: [&'static str; 3] = [CONTROLLER_CPU, CONTROLLER_CPUSET, CONTROLLER_PIDS];
 const PROC_MOUNTS: &str = "/proc/mounts";
 
 pub struct Cgroup {
     tasks_files: Vec<PathBuf>,
 }
 
-// This is called writeln_special because we have to use this rather convoluted way of writing
-// to avoid getting all sorts of weird errors. I would be nice to know why that happens.
-fn writeln_special<T, V>(file_path: T, value: V) -> Result<()>
+// It's called writeln_special because we have to use this rather convoluted way of writing
+// to special cgroup files, to avoid getting errors. It would be nice to know why that happens :-s
+fn writeln_special<T, V>(file_path: &T, value: V) -> Result<()>
 where
     T: AsRef<Path>,
     V: ::std::fmt::Display,
 {
-    // Open does not work here, or so it seemed at one point :-s
-    let mut f = File::create(file_path.as_ref())
-        .map_err(|e| Error::FileCreate(PathBuf::from(file_path.as_ref()), e))?;
-
-    // For some reason, using writeln!(f, "{}", pid) doesn't work :(
-    let mut bytes = format!("{}\n", value).into_bytes();
-    f.write_all(bytes.as_mut())
+    fs::write(file_path, format!("{}\n", value))
         .map_err(|e| Error::Write(PathBuf::from(file_path.as_ref()), e))
 }
 
-fn inherit_from_parent(path: &mut PathBuf, what: &'static str) -> Result<()> {
-    // This unwrap() is safe because path_buf is guaranteed to have a parent.
-    let mut parent_what = PathBuf::from(path.parent().unwrap());
-    parent_what.push(what);
+fn readln_special<T: AsRef<Path>>(file_path: &T) -> Result<String> {
+    let mut line = fs::read_to_string(file_path)
+        .map_err(|e| Error::ReadToString(PathBuf::from(file_path.as_ref()), e))?;
 
-    let f = OpenOptions::new()
-        .read(true)
-        .open(&parent_what)
-        .map_err(|e| Error::FileOpen(parent_what.clone(), e))?;
+    // Remove the newline character at the end (if any).
+    line.pop();
 
-    let mut line = String::new();
-    BufReader::new(f)
-        .read_line(&mut line)
-        .map_err(|e| Error::ReadLine(parent_what.clone(), e))?;
+    Ok(line)
+}
 
-    path.push(what);
+// If we call inherit_from_parent_aux(.../A/B/C, file, condition), the following will happen:
+// 1) If .../A/B/C/file does not exist, or if .../A/B/file does not exist, return an error.
+// 2) If .../A/B/file is not empty, write the first line of .../A/B/file into .../A/B/C/file
+// and return.
+// 3) Otherwise, if condition == true, call inherit_from_parent_aux(.../A/B, file, false).
+// 4) If .../A/B/file is no longer empty, write the first line of .../A/B/file into
+// .../A/B/C/file, and return.
+// 5) Otherwise, return an error.
+
+// How is this helpful? When creating cgroup folders for the jailer Firecracker instance, the jailer
+// will create a hierarchy that looks like <cgroup_base>/firecracker/<id>. Depending on each
+// particular cgroup controller, <cgroup_base> contains a number of configuration files. These are
+// not actually present on a disk; they are special files exposed by the controller, and they
+// usually contain a single line with some configuration value(s). When the "firecracker" and <id>
+// subfolders are created, configuration files with the same name appear automatically in the new
+// folders, but their contents are not always automatically populated. Moreover,
+// if <cgroup_base>/firecracker/some_file is empty, then we cannot have a non-empty file with
+// at <cgroup_base>/firecracker/<id>/some_file. The inherit_from_parent function (which is based
+// on the following helper function) helps with propagating the values.
+
+// There is also a potential race condition mentioned below. Here is what it refers to: let's say we
+// start multiple jailer processes, and one of them calls
+// inherit_from_parent_aux(/A/firecracker/id1, file, true), and hits case number 3) from the list
+// above, thus recursively calling inherit_from_parent_aux(/A/firecracker, file, false). It's
+// entirely possible there was another process in the exact same situations, and that process
+// gets to write something to /A/firecracker/file first. In this case, the recursive call made by
+// the first process to inherit_from_parent_aux(/A/firecracker, file, false) may fail when writing
+// to /A/firecracker/file, but we can still continue, because step 4) only cares about the file
+// no longer being empty, regardless of who actually got to populated its contents.
+
+fn inherit_from_parent_aux(
+    path: &mut PathBuf,
+    file_name: &'static str,
+    retry_one_level_up: bool,
+) -> Result<()> {
+    // The function with_file_name() replaces the last component of a path with the given name.
+    let parent_file = path.with_file_name(file_name);
+
+    let mut line = readln_special(&parent_file)?;
+
+    if line.is_empty() {
+        if retry_one_level_up {
+            // We have to borrow "parent" from "parent_file" as opposed to "path", because then
+            // we wouldn't be able to mutably borrow path at the end of this function (at least not
+            // according to how the Rust borrow checker operates right now :-s)
+            let parent = parent_file
+                .parent()
+                .ok_or_else(|| Error::MissingParent(parent_file.clone()))?;
+
+            // Trying to avoid the race condition described above. We don't care about the result,
+            // because we check once more if line.is_empty() after the end of this block.
+            let _ = inherit_from_parent_aux(&mut parent.to_path_buf(), file_name, false);
+            line = readln_special(&parent_file)?;
+        }
+
+        if line.is_empty() {
+            return Err(Error::CgroupInheritFromParent(
+                path.to_path_buf(),
+                file_name,
+            ));
+        }
+    }
+
+    path.push(file_name);
     writeln_special(&path, &line)?;
     path.pop();
 
     Ok(())
+}
+
+// The path reference is &mut here because we do a push to get the destination file name. However,
+// a pop follows shortly after (see fn inherit_from_parent_aux), reverting to the original value.
+fn inherit_from_parent(path: &mut PathBuf, file_name: &'static str) -> Result<()> {
+    inherit_from_parent_aux(path, file_name, true)
 }
 
 impl Cgroup {
@@ -98,7 +164,7 @@ impl Cgroup {
             }
         }
 
-        // This is a just sanity check.
+        // This is just a sanity check.
         assert_eq!(keys_len, CONTROLLERS.len());
 
         // We now both create the cgroup subfolders, and fill the tasks_files vector.
@@ -106,32 +172,30 @@ impl Cgroup {
 
         for (controller, mut path_buf) in found_controllers.drain() {
             path_buf.push(exec_file_name);
-
-            // TODO: fix this racy thing.
-            if controller == "cpuset" && !path_buf.exists() {
-                fs::create_dir_all(&path_buf).map_err(|e| Error::CreateDir(path_buf.clone(), e))?;
-
-                inherit_from_parent(&mut path_buf, "cpuset.mems")?;
-                inherit_from_parent(&mut path_buf, "cpuset.cpus")?;
-            }
-
             path_buf.push(id);
 
             fs::create_dir_all(&path_buf).map_err(|e| Error::CreateDir(path_buf.clone(), e))?;
 
-            if controller == "cpuset" {
+            // For now, the jailer is only populating configuration values for the cpuset
+            // controller, related to the cpu cores we are allowed to run on, and the numa node we
+            // want to restrict to. The jailer only creates the folder hierarchy for other cgroups,
+            // and the customer has to provide any desired configuration explicitly (if any).
+
+            if controller == CONTROLLER_CPUSET {
+                inherit_from_parent(&mut path_buf, CPUSET_CPUS)?;
+
+                // TODO: this does make an unnecessary write, as we change the value of the
+                // "cpuset.mems" file again at the end of the for block. Maybe fix this sometime.
+                inherit_from_parent(&mut path_buf, CPUSET_MEMS)?;
+
                 // Enforce NUMA node restriction.
-                path_buf.push("cpuset.mems");
+                path_buf.push(CPUSET_MEMS);
                 writeln_special(&path_buf, numa_node)?;
                 path_buf.pop();
-
-                // Inherit cpuset
-                inherit_from_parent(&mut path_buf, "cpuset.cpus")?;
             }
 
             // And now add "tasks" to get the path of the corresponding tasks file.
             path_buf.push("tasks");
-
             if !tasks_files.contains(&path_buf) {
                 tasks_files.push(path_buf);
             }
