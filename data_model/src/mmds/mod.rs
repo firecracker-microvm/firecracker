19ff7a87b152c323446c108278ff9054625430ae
Andreea Florescu <fandree@amazon.com>
2018-07-23 18:01:54 +0300

    mmds: added PATCH method
    The mmds data is updated when a patch request is sent
    by merging the data received as body in the request
    with the existing MMDS data. Merging is done using
    the merge function from the json_patch crate.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- data_model/src/mmds/mod.rs
+++ data_model/src/mmds/mod.rs
@@ -1,3 +1,4 @@
+use json_patch::merge;
 use serde_json::Value;
 
 /// The MMDS is the Microvm Metadata Service represented as an untyped json.
@@ -30,6 +31,10 @@ impl MMDS {
         self.is_initialized = true;
     }
 
+    pub fn patch_data(&mut self, patch_data: Value) {
+        merge(&mut self.data_store, &patch_data);
+    }
+
     pub fn get_data_str(&self) -> String {
         if self.data_store.is_null() {
             return String::from("{}");
@@ -48,12 +53,17 @@ mod test {
         let mut mmds = MMDS::default();
         assert_eq!(mmds.is_initialized(), false);
 
-        let mmds_json = "{\"meta-data\":{\"iam\":\"dummy\"},\"user-data\":\"1522850095\"}";
+        let mut mmds_json = "{\"meta-data\":{\"iam\":\"dummy\"},\"user-data\":\"1522850095\"}";
 
         mmds.put_data(serde_json::from_str(mmds_json).unwrap());
         assert_eq!(mmds.is_initialized(), true);
 
         assert_eq!(mmds.get_data_str(), mmds_json);
-    }
 
+        // update the user-data field add test that patch works as expected
+        let patch_json = "{\"user-data\":\"10\"}";
+        mmds.patch_data(serde_json::from_str(patch_json).unwrap());
+        mmds_json = "{\"meta-data\":{\"iam\":\"dummy\"},\"user-data\":\"10\"}";
+        assert_eq!(mmds.get_data_str(), mmds_json);
+    }
 }
