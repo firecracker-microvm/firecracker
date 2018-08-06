1f6d0ac506440f4819d4888ca96e6ec3b6a20bf7
Andreea Florescu <fandree@amazon.com>
2018-08-06 15:38:45 +0300

    micro_http: added minimal implementation for HTTP/1.0
    micro_http is to be used by MMDS and has the following restrictions:
     * supported method: GET.
     * supported protocol version: HTTP/1.0
     * available status codes: 200, 400, 500 and 501.
     * for incoming requests, only the request line is parsed. The
       request line contains the method, request URI and HTTP version.
       Everything else is ignored.
     * when creating responses, the only headers set are Content-Length
       and Content-Type.
     * supported media type: "text/plain". All entity bodies are assumed
       to be text/plain.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

---
+++ micro_http/src/common/headers.rs
@@ -0,0 +1,93 @@
+use std::collections::HashMap;
+
+use ascii::{COLON, CRLF, SP};
+
+#[derive(Eq, Hash, PartialEq)]
+pub enum Header {
+    ContentLength,
+    ContentType,
+}
+
+impl Header {
+    fn raw(&self) -> &'static [u8] {
+        match self {
+            Header::ContentLength => b"Content-Length",
+            Header::ContentType => b"Content-Type",
+        }
+    }
+}
+
+pub struct Headers {
+    headers: HashMap<Header, String>,
+}
+
+impl Headers {
+    pub fn default() -> Headers {
+        return Headers {
+            headers: HashMap::new(),
+        };
+    }
+
+    pub fn add(&mut self, header: Header, value: String) {
+        self.headers.insert(header, value);
+    }
+
+    pub fn raw(&self) -> Vec<u8> {
+        let mut response = Vec::new();
+
+        for (key, val) in &self.headers {
+            let header = [key.raw(), COLON, SP, val.clone().as_bytes(), CRLF].concat();
+            response = [response, header].concat()
+        }
+
+        return response;
+    }
+}
+
+pub enum MediaType {
+    PlainText,
+}
+
+impl MediaType {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            MediaType::PlainText => "text/plain",
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default() {
+        assert!(Headers::default().headers == HashMap::new());
+    }
+
+    #[test]
+    fn test_add_headers() {
+        let mut headers = Headers::default();
+
+        headers.add(Header::ContentType, "text/plain".to_string());
+        headers.add(Header::ContentLength, "120".to_string());
+
+        assert!(headers.headers.contains_key(&Header::ContentType));
+        assert_eq!(
+            headers.headers.get(&Header::ContentType).unwrap(),
+            &"text/plain".to_string()
+        );
+        assert!(headers.headers.contains_key(&Header::ContentLength));
+        assert_eq!(
+            headers.headers.get(&Header::ContentLength).unwrap(),
+            &"120".to_string()
+        );
+
+        // Test that adding a Header with the same key, updates the value.
+        headers.add(Header::ContentLength, "130".to_string());
+        assert_eq!(
+            headers.headers.get(&Header::ContentLength).unwrap(),
+            &"130".to_string()
+        );
+    }
+}
---
+++ micro_http/src/common/mod.rs
@@ -0,0 +1,54 @@
+pub mod headers;
+
+pub mod ascii {
+    pub const CRLF: &[u8] = b"\r\n";
+    pub const COLON: &[u8] = b":";
+    pub const LF: &[u8] = b"\n";
+    pub const SP: &[u8] = b" ";
+}
+
+pub struct Body {
+    body: Vec<u8>,
+}
+
+impl Body {
+    pub fn new(body: String) -> Self {
+        Body {
+            body: body.into_bytes(),
+        }
+    }
+
+    pub fn raw(&self) -> &[u8] {
+        return self.body.as_slice();
+    }
+
+    pub fn len(&self) -> usize {
+        return self.body.len();
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Method {
+    Get,
+}
+
+impl Method {
+    pub fn raw(&self) -> &'static [u8] {
+        match self {
+            Method::Get => b"GET",
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Version {
+    Http10,
+}
+
+impl Version {
+    pub fn raw(&self) -> &'static [u8] {
+        match self {
+            Version::Http10 => b"HTTP/1.0",
+        }
+    }
+}
