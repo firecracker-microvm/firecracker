af18e51b46c258d03e605ab535a497a1e5997e76
Radu Matei Lăcraru <ral@amazon.com>
2018-08-14 18:37:50 +0300

    tests: write seccomp integration tests
    Signed-off-by: Radu Matei Lăcraru <ral@amazon.com>

--- tests/security/test_seccomp.py
+++ tests/security/test_seccomp.py
@@ -80,3 +80,96 @@ def test_seccomp_ls(test_session_root_path, tmp_jailer):
     # The seccomp filters should send SIGSYS (31) to the binary. `ls` doesn't
     # handle it, so it will exit with error.
     assert outcome.returncode != 0
+
+
+@pytest.fixture
+def tmp_advanced_seccomp_binaries(test_session_root_path):
+    """
+    Build binaries required for the advanced seccomp tests.
+
+    Build `demo_advanced_jailer`, `demo_harmless_firecracker`, and
+    `demo_malicious_firecracker.
+    :return: The paths of the built binaries.
+    """
+    binaries_srcdir = os.path.normpath(
+        os.path.join(os.getcwd(), 'security/demo_advanced_seccomp/')
+    )
+    build_path = os.path.join(
+        test_session_root_path,
+        host.CARGO_RELEASE_REL_PATH
+    )
+    run("cd {} && CARGO_TARGET_DIR={} cargo build --release".format(
+        binaries_srcdir, build_path), shell=True, check=True)
+
+    release_binaries_path = os.path.join(
+        host.CARGO_RELEASE_REL_PATH,
+        host.RELEASE_BINARIES_REL_PATH
+    )
+    release_binaries_path = os.path.join(
+        test_session_root_path,
+        release_binaries_path
+    )
+    demo_advanced_jailer = os.path.normpath(
+        os.path.join(
+            release_binaries_path,
+            'demo_advanced_jailer'
+        )
+    )
+    demo_harmless_firecracker = os.path.normpath(
+        os.path.join(
+            release_binaries_path,
+            'demo_harmless_firecracker'
+        )
+    )
+    demo_malicious_firecracker = os.path.normpath(
+        os.path.join(
+            release_binaries_path,
+            'demo_malicious_firecracker'
+        )
+    )
+
+    yield\
+        demo_advanced_jailer,\
+        demo_harmless_firecracker,\
+        demo_malicious_firecracker
+
+    os.remove(demo_advanced_jailer)
+    os.remove(demo_harmless_firecracker)
+    os.remove(demo_malicious_firecracker)
+
+
+def test_advanced_seccomp_harmless(tmp_advanced_seccomp_binaries):
+    """
+    Test `demo_harmless_firecracker`.
+
+    Test that the built demo jailer allows the built demo harmless firecracker.
+    """
+    demo_advanced_jailer, demo_harmless_firecracker, _ =\
+        tmp_advanced_seccomp_binaries
+
+    assert os.path.exists(demo_advanced_jailer)
+    assert os.path.exists(demo_harmless_firecracker)
+
+    outcome = run([demo_advanced_jailer, demo_harmless_firecracker])
+
+    # The demo harmless firecracker should have terminated gracefully.
+    assert outcome.returncode == 0
+
+
+def test_advanced_seccomp_malicious(tmp_advanced_seccomp_binaries):
+    """
+    Test `demo_malicious_firecracker`.
+
+    Test that the built demo jailer denies the built demo malicious
+    firecracker.
+    """
+    demo_advanced_jailer, _, demo_malicious_firecracker =\
+        tmp_advanced_seccomp_binaries
+
+    assert os.path.exists(demo_advanced_jailer)
+    assert os.path.exists(demo_malicious_firecracker)
+
+    outcome = run([demo_advanced_jailer, demo_malicious_firecracker])
+
+    # The demo malicious firecracker should have received `SIGSYS`.
+    assert outcome.returncode != 0
