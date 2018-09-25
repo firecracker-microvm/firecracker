ecb82590b8fd7457ec3c2dd607316ce7cce2d676
Alexandru Agache <aagch@amazon.com>
2018-09-25 13:57:13 +0300

    fc_util: added fn read_cycles()
    This function returns a timestamp expressed in CPU cycles (pretty much
    what rdtsc() does on x86). It can be used for accurate and cheap
    relative time measurements. There might be some synchronization issues
    if a Firecracker is allowed to jump from one socket to another, and
    there will most likely be synchronization problems if we migrate the
    Firecracker process from one host to another, but these scenarios do
    not seem likely given our current use cases. Moreover, we could rethink
    this function as something that returns an opaque (ideally monotonically
    increasing) value, if/when we start caring about all those things. We
    have to judge on a case by case basis whether this makes sense in the
    parts of the code where it's going to appear. Anyway it's currently
    implemented for x86_64 only, based on rdtsc().
    
    Signed-off-by: Alexandru Agache <aagch@amazon.com>

--- fc_util/src/lib.rs
+++ fc_util/src/lib.rs
@@ -3,3 +3,21 @@ extern crate logger;
 
 pub mod ratelimiter;
 pub mod validators;
+
+#[cfg(target_arch = "x86_64")]
+pub fn timestamp_cycles() -> u64 {
+    // Safe because there's nothing that can go wrong with this call.
+    unsafe { std::arch::x86_64::_rdtsc() as u64 }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_timestamp_cycles() {
+        for _ in 0..1000 {
+            assert!(timestamp_cycles() < timestamp_cycles());
+        }
+    }
+}
