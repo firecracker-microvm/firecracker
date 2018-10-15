9a4102a2c343668ec9c9349d3ebdef7fc7e407c0
Alexandra Iordache <aghecen@amazon.com>
2018-10-15 17:29:21 +0300

    tests: time http requests
    * added a decorator that raises a custom exception if the API call it
      decorates takes too long
    * added a wrapper over request_unixsocket.Session that sets the custom
      callback for is_good_response and overrides the GET, PUT and PATCH
      methods. This way, all the API calls are duration limited
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- tests/framework/defs.py
+++ tests/framework/defs.py
@@ -9,6 +9,8 @@ JAILER_BINARY_NAME = 'jailer'
 """Jailer's binary name."""
 JAILER_DEFAULT_CHROOT = '/srv/jailer'
 """The default location for the chroot."""
+MAX_API_CALL_DURATION_MS = 100
+"""Maximum accepted duration of an API call, in milliseconds."""
 MICROVM_KERNEL_RELPATH = 'kernel/'
 """Relative path to the location of the kernel file."""
 MICROVM_FSFILES_RELPATH = 'fsfiles/'
