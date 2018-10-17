8c0d6af54f4b10387d9c0669eb8649b891ed3a57
Alexandra Iordache <aghecen@amazon.com>
2018-10-17 18:17:26 +0300

    fc_util: add function that gets current CPU time
    This is esentially a wrapper over libc::clock_gettime, because neither
    the time nor the chrono crate offer an implementation of clock_gettime
    for CPU time.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- fc_util/src/lib.rs
+++ fc_util/src/lib.rs
@@ -1,3 +1,5 @@
+extern crate libc;
+
 #[macro_use]
 extern crate logger;
 
@@ -10,6 +12,20 @@ pub fn timestamp_cycles() -> u64 {
     unsafe { std::arch::x86_64::_rdtsc() as u64 }
 }
 
+fn timespec_to_us(time_struct: &libc::timespec) -> u64 {
+    (time_struct.tv_sec as u64) * 1_000_000 + (time_struct.tv_nsec as u64) / 1000
+}
+
+pub fn now_cputime_us() -> u64 {
+    let mut time_struct = libc::timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
+    // Safe because the parameters are valid.
+    unsafe { libc::clock_gettime(libc::CLOCK_PROCESS_CPUTIME_ID, &mut time_struct) };
+    timespec_to_us(&time_struct)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,6 +34,13 @@ mod tests {
     fn test_timestamp_cycles() {
         for _ in 0..1000 {
             assert!(timestamp_cycles() < timestamp_cycles());
+        }
+    }
+
+    #[test]
+    fn test_now_cputime_us() {
+        for _ in 0..1000 {
+            assert!(now_cputime_us() <= now_cputime_us());
         }
     }
 }
