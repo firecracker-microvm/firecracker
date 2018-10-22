c2cf48a6e51a4289731d8122ddb844d3061a28e7
Diana Popa <dpopa@amazon.com>
2018-10-22 09:45:10 -0500

    x86_64: unit tests for interrupts...
    related functionality.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- x86_64/src/interrupts.rs
+++ x86_64/src/interrupts.rs
@@ -17,6 +17,7 @@ pub enum Error {
     GetLapic(sys_util::Error),
     SetLapic(sys_util::Error),
 }
+
 pub type Result<T> = result::Result<T, Error>;
 
 // Defines poached from apicdef.h kernel header.
@@ -32,7 +33,7 @@ fn get_klapic_reg(klapic: &kvm_lapic_state, reg_offset: usize) -> u32 {
         mem::transmute::<&[i8], &[u8]>(&klapic.regs[reg_offset..])
     };
     let mut reader = Cursor::new(sliceu8);
-    // read_u32 can't fail if the offsets defined above are correct.
+    // Following call can't fail if the offsets defined above are correct.
     reader.read_u32::<LittleEndian>().unwrap()
 }
 
@@ -43,7 +44,7 @@ fn set_klapic_reg(klapic: &mut kvm_lapic_state, reg_offset: usize, value: u32) {
         mem::transmute::<&mut [i8], &mut [u8]>(&mut klapic.regs[reg_offset..])
     };
     let mut writer = Cursor::new(sliceu8);
-    // read_u32 can't fail if the offsets defined above are correct.
+    // Following call can't fail if the offsets defined above are correct.
     writer.write_u32::<LittleEndian>(value).unwrap()
 }
 
@@ -72,4 +73,79 @@ pub fn set_lint(vcpu: &kvm::VcpuFd) -> Result<()> {
     );
 
     vcpu.set_lapic(&klapic).map_err(Error::SetLapic)
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+    use self::rand::Rng;
+
+    use super::*;
+    use kvm::{Kvm, VcpuFd, VmFd};
+
+    const KVM_APIC_REG_SIZE: usize = 0x400;
+
+    #[test]
+    fn test_set_and_get_klapic_reg() {
+        let reg_offset = 0x340;
+        let mut klapic = kvm_lapic_state::default();
+        set_klapic_reg(&mut klapic, reg_offset, 3);
+        let value = get_klapic_reg(&klapic, reg_offset);
+        assert_eq!(value, 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_and_get_klapic_out_of_bounds() {
+        let reg_offset = KVM_APIC_REG_SIZE + 10;
+        let mut klapic = kvm_lapic_state::default();
+        set_klapic_reg(&mut klapic, reg_offset, 3);
+    }
+
+    #[test]
+    fn test_apic_delivery_mode() {
+        let mut rng = rand::thread_rng();
+        let mut v: Vec<u32> = (0..20).map(|_| rng.gen::<u32>()).collect();
+
+        v.iter_mut()
+            .for_each(|x| *x = set_apic_delivery_mode(*x, 2));
+        let after: Vec<u32> = v.iter().map(|x| ((*x & !0x700) | ((2) << 8))).collect();
+        assert_eq!(v, after);
+    }
+
+    #[test]
+    fn test_setlint() {
+        let kvm = Kvm::new().unwrap();
+        assert!(kvm.check_extension(kvm::Cap::Irqchip));
+        let vm = VmFd::new(&kvm).unwrap();
+        //the get_lapic ioctl will fail if there is no irqchip created beforehand.
+        assert!(vm.create_irq_chip().is_ok());
+        let vcpu = VcpuFd::new(0, &vm).unwrap();
+        let klapic_before: kvm_lapic_state = vcpu.get_lapic().unwrap();
+
+        // Compute the value that is expected to represent LVT0 and LVT1.
+        let lint0 = get_klapic_reg(&klapic_before, APIC_LVT0);
+        let lint1 = get_klapic_reg(&klapic_before, APIC_LVT1);
+        let lint0_mode_expected = set_apic_delivery_mode(lint0, APIC_MODE_EXTINT);
+        let lint1_mode_expected = set_apic_delivery_mode(lint1, APIC_MODE_NMI);
+
+        set_lint(&vcpu).unwrap();
+
+        // Compute the value that represents LVT0 and LVT1 after set_lint.
+        let klapic_actual: kvm_lapic_state = vcpu.get_lapic().unwrap();
+        let lint0_mode_actual = get_klapic_reg(&klapic_actual, APIC_LVT0);
+        let lint1_mode_actual = get_klapic_reg(&klapic_actual, APIC_LVT1);
+        assert_eq!(lint0_mode_expected, lint0_mode_actual);
+        assert_eq!(lint1_mode_expected, lint1_mode_actual);
+    }
+
+    #[test]
+    fn test_setlint_fails() {
+        let kvm = Kvm::new().unwrap();
+        let vm = VmFd::new(&kvm).unwrap();
+        let vcpu = VcpuFd::new(0, &vm).unwrap();
+        // 'get_lapic' ioctl triggered by the 'set_lint' function will fail if there is no
+        // irqchip created beforehand.
+        assert!(set_lint(&vcpu).is_err());
+    }
 }
