92d4c7d6894872b887da56daae7829f22b3bc5b2
Diana Popa <dpopa@amazon.com>
2018-11-13 11:55:50 -0600

    CI: demo support for spawning multiple microvms
    Two additional integration tests for checking the boottime
    when spawning 10 microvms are added.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/performance/test_boottime.py
+++ tests/integration_tests/performance/test_boottime.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-import pytest
+from framework import decorators
 
 import host_tools.logging as log_tools
 
@@ -16,46 +16,109 @@ MAX_BOOT_TIME_US = 150000
 # Regex for obtaining boot time from some string.
 TIMESTAMP_LOG_REGEX = r'Guest-boot-time\s+\=\s+(\d+)\s+us'
 
+NO_OF_MICROVMS = 10
 
-@pytest.mark.timeout(120)
-def test_microvm_boottime_no_network(test_microvm_with_boottime):
+
+def test_single_microvm_boottime_no_network(test_microvm_with_boottime):
     """Check guest boottime of microvm without network."""
-    boottime_us = _test_microvm_boottime(test_microvm_with_boottime, None)
+    log_fifo, _ = _configure_vm(test_microvm_with_boottime)
+    time.sleep(0.4)
+    boottime_us = _test_microvm_boottime(log_fifo)
     print("Boot time with no network is: " + str(boottime_us) + " us")
 
 
-def test_microvm_boottime_with_network(
+@decorators.test_context('boottime', NO_OF_MICROVMS)
+def test_multiple_microvm_boottime_no_network(test_multiple_microvms):
+    """Check guest boottime without network when spawning multiple microvms."""
+    microvms = test_multiple_microvms
+    assert microvms
+    assert len(microvms) == NO_OF_MICROVMS
+    log_fifos = []
+    for i in range(NO_OF_MICROVMS):
+        log_fifo, _ = _configure_vm(microvms[i])
+        log_fifos.append(log_fifo)
+    time.sleep(0.4)
+    for i in range(NO_OF_MICROVMS):
+        _ = _test_microvm_boottime(log_fifos[i])
+
+
+@decorators.test_context('boottime', NO_OF_MICROVMS)
+def test_multiple_microvm_boottime_with_network(
+        test_multiple_microvms,
+        network_config
+):
+    """Check guest boottime with network when spawning multiple microvms."""
+    microvms = test_multiple_microvms
+    assert microvms
+    assert len(microvms) == NO_OF_MICROVMS
+    log_fifos = []
+    _taps = []
+    for i in range(NO_OF_MICROVMS):
+        log_fifo, _tap = _configure_vm(microvms[i], {
+            "config": network_config, "iface_id": str(i)
+        })
+        log_fifos.append(log_fifo)
+        _taps.append(_tap)
+    time.sleep(0.4)
+    for i in range(NO_OF_MICROVMS):
+        _ = _test_microvm_boottime(log_fifos[i])
+
+
+def test_single_microvm_boottime_with_network(
         test_microvm_with_boottime,
         network_config
 ):
     """Check guest boottime of microvm with network."""
-    boottime_us = _test_microvm_boottime(
-        test_microvm_with_boottime,
-        network_config
-    )
+    log_fifo, _tap = _configure_vm(test_microvm_with_boottime, {
+        "config": network_config, "iface_id": "1"
+    })
+    time.sleep(0.4)
+    boottime_us = _test_microvm_boottime(log_fifo)
     print("Boot time with network configured is: " + str(boottime_us) + " us")
 
 
-def _test_microvm_boottime(
-        microvm,
-        net_config
-):
+def _test_microvm_boottime(log_fifo):
     """Assert that we meet the minimum boot time.
 
     TODO: Should use a microVM with the `boottime` capability.
     """
+    lines = log_fifo.sequential_reader(20)
+
+    boot_time_us = 0
+    for line in lines:
+        timestamps = re.findall(TIMESTAMP_LOG_REGEX, line)
+        if timestamps:
+            boot_time_us = int(timestamps[0])
+
+    assert boot_time_us > 0
+    assert boot_time_us < MAX_BOOT_TIME_US
+    return boot_time_us
+
+
+def _configure_vm(microvm, network_info=None):
+    """Auxiliary function for preparing microvm before measuring boottime."""
     microvm.spawn()
 
+    # Machine configuration specified in the SLA.
     microvm.basic_config(
-        vcpu_count=2,
-        mem_size_mib=1024
+        vcpu_count=1,
+        mem_size_mib=128
     )
-    if net_config:
-        _tap, _, _ = microvm.ssh_network_config(net_config, '1')
+    if network_info:
+        _tap, _, _ = microvm.ssh_network_config(
+            network_info["config"],
+            network_info["iface_id"]
+        )
 
     # Configure logging.
-    log_fifo_path = os.path.join(microvm.path, 'log_fifo')
-    metrics_fifo_path = os.path.join(microvm.path, 'metrics_fifo')
+    log_fifo_path = os.path.join(
+        microvm.path,
+        'log_fifo' + microvm.id.split('-')[0]
+    )
+    metrics_fifo_path = os.path.join(
+        microvm.path,
+        'metrics_fifo' + microvm.id.split('-')[0]
+    )
     log_fifo = log_tools.Fifo(log_fifo_path)
     metrics_fifo = log_tools.Fifo(metrics_fifo_path)
 
@@ -69,15 +132,4 @@ def _test_microvm_boottime(
     assert microvm.api_session.is_good_response(response.status_code)
 
     microvm.start()
-    time.sleep(0.4)
-    lines = log_fifo.sequential_reader(20)
-
-    boot_time_us = 0
-    for line in lines:
-        timestamps = re.findall(TIMESTAMP_LOG_REGEX, line)
-        if timestamps:
-            boot_time_us = int(timestamps[0])
-
-    assert boot_time_us > 0
-    assert boot_time_us < MAX_BOOT_TIME_US
-    return boot_time_us
+    return log_fifo, _tap if network_info else None
