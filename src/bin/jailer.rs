d9bbe6d5d336d20117b6f2c1e9fb9f5f3c9964ef
Alexandra Iordache <aghecen@amazon.com>
2018-11-22 20:17:56 +0200

    jailer: handle internal errors
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/bin/jailer.rs
+++ src/bin/jailer.rs
@@ -7,10 +7,13 @@ extern crate clap;
 extern crate fc_util;
 extern crate jailer;
 
-fn main() -> jailer::Result<()> {
-    jailer::run(
+fn main() {
+    match jailer::run(
         jailer::clap_app().get_matches(),
         (chrono::Utc::now().timestamp_nanos() / 1000) as u64,
         fc_util::now_cputime_us(),
-    )
+    ) {
+        Ok(()) => (),
+        Err(error) => eprintln!("Jailer error: {}", error),
+    }
 }
