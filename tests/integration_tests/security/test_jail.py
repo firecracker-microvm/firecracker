8bdba526726fec2caa501a744ed300b1c897e5df
Andreea Florescu <fandree@amazon.com>
2018-12-27 18:46:12 +0200

    check the size of the jailer id against a min val
    We cannot allow the jailer ID to be empty because all Firecracker
    processes started on the same host will end up overriding
    the chroot path (/srv/jailer/firecracker/root) of the process started
    with the empty ID.
    
    Fixes: https://github.com/firecracker-microvm/firecracker/issues/795
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- tests/integration_tests/security/test_jail.py
+++ tests/integration_tests/security/test_jail.py
@@ -16,3 +16,23 @@ def test_default_chroot(test_microvm_with_ssh):
 
     # Test the expected outcome.
     assert os.path.exists(test_microvm.jailer.api_socket_path())
+
+
+def test_empty_jailer_id(test_microvm_with_ssh):
+    """Test that the jailer ID cannot be empty."""
+    test_microvm = test_microvm_with_ssh
+
+    # Set the jailer ID to None.
+    test_microvm.jailer.jailer_id = ""
+
+    # pylint: disable=W0703
+    try:
+        test_microvm.spawn()
+        # If the exception is not thrown, it means that Firecracker was
+        # started successfully, hence there's a bug in the code due to which
+        # we can set an empty ID.
+        assert False
+    except Exception as err:
+        expected_err = "Jailer error: Invalid instance ID: invalid len (0);" \
+                       "  the length must be between 1 and 64"
+        assert expected_err in str(err)
