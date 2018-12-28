aac8169e940be6936dd893e7c9d0e79a25866113
Andreea Florescu <fandree@amazon.com>
2018-12-29 00:01:42 +0200

    Panic when starting the jailer fails
    We need to panic when we encounter an error when setting up the jailer
    because panics will give us an exit code != 0.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- src/bin/jailer.rs
+++ src/bin/jailer.rs
@@ -13,6 +13,6 @@ fn main() {
         (chrono::Utc::now().timestamp_nanos() / 1000) as u64,
         fc_util::now_cputime_us(),
     ) {
-        eprintln!("Jailer error: {}", error);
+        panic!("Jailer error: {}", error);
     }
 }
