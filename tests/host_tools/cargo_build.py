48e359b4aabd1380807b83f1ef6037627875f477
Andreea Florescu <fandree@amazon.com>
2019-02-05 17:45:08 +0200

    CI: run all tests with vsock-build as well
    All the CI tests that require the firecracker binaries will now
    be run with the vsock build binaries as well.
    
    Also, added support for skipping tests based on the features that
    were used in building.
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- tests/host_tools/cargo_build.py
+++ tests/host_tools/cargo_build.py
@@ -11,10 +11,26 @@ from framework.defs import FC_BINARY_NAME, JAILER_BINARY_NAME
 CARGO_BUILD_REL_PATH = 'firecracker_binaries'
 """Keep a single build path across all build tests."""
 
-CARGO_RELEASE_REL_PATH = os.path.join(CARGO_BUILD_REL_PATH, 'release')
+CARGO_RELEASE_REL_PATH = os.path.join(
+    CARGO_BUILD_REL_PATH, 'release'
+)
 """Keep a single Firecracker release binary path across all test types."""
 
+CARGO_RELEASE_VSOCK_REL_PATH = os.path.join(
+    CARGO_BUILD_REL_PATH, 'release-vsock'
+)
+"""Vsock release binaries relative path."""
+
 RELEASE_BINARIES_REL_PATH = 'x86_64-unknown-linux-musl/release/'
+
+
+class UnknownFeatureException(Exception):
+    """Exception Class for invalid build feature."""
+    def __init__(self):
+        Exception.__init__(
+            self,
+            "Trying to get build binaries for unknown feature!"
+        )
 
 
 def cargo_build(path, extra_args=''):
@@ -26,16 +42,24 @@ def cargo_build(path, extra_args=''):
     run(cmd, shell=True, check=True)
 
 
-def get_firecracker_binaries(root_path):
+def get_firecracker_binaries(root_path, features=''):
     """Build the Firecracker and Jailer binaries if they don't exist.
 
     Returns the location of the firecracker related binaries eventually after
     building them in case they do not exist at the specified root_path.
     """
+
+    if features == '':
+        cargo_binaries_rel_path = CARGO_RELEASE_REL_PATH
+    elif features == 'vsock':
+        cargo_binaries_rel_path = CARGO_RELEASE_VSOCK_REL_PATH
+    else:
+        raise UnknownFeatureException
+
     path_to_binaries = os.path.join(
         root_path,
         os.path.join(
-            CARGO_RELEASE_REL_PATH,
+            cargo_binaries_rel_path,
             RELEASE_BINARIES_REL_PATH
         )
     )
@@ -49,7 +73,7 @@ def get_firecracker_binaries(root_path):
     ):
         build_path = os.path.join(
             root_path,
-            CARGO_RELEASE_REL_PATH
+            cargo_binaries_rel_path
         )
         cargo_build(
             build_path,
