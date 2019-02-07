1bfe5af25749c7f973099a7f00b8526a43d6a698
Deepesh Pathak <deepshpathak@gmail.com>
2019-02-07 06:39:16 +0530

    lint: add clippy allow lint macros in generated modules
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

--- arch_gen/src/lib.rs
+++ arch_gen/src/lib.rs
@@ -1,5 +1,8 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+extern crate memory_model;
+
+#[allow(clippy::all)]
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub mod x86;
