9412e47165375ad896297f4d14a097882a1a2d18
Serban Iorga <seriorga@amazon.com>
2019-02-20 09:45:27 +0200

    add documentation for BitRangeExt
    addresses https://github.com/firecracker-microvm/firecracker/issues/944
    
    Signed-off-by: Serban Iorga <seriorga@amazon.com>

--- rate_limiter/src/lib.rs
+++ rate_limiter/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#![warn(missing_docs)]
+#![deny(missing_docs)]
 //! # Rate Limiter
 //!
 //! Provides a rate limiter written in Rust useful for IO operations that need to
