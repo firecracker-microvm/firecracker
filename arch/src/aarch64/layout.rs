a669ce3a961bc5d51c72ac221497dfbcfcdbdc96
Diana Popa <dpopa@amazon.com>
2019-03-08 17:48:47 +0200

    arm64: added memory layout representation
    The code follows the memory address mapping of a 64-bit guest.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- arch/src/aarch64/layout.rs
+++ arch/src/aarch64/layout.rs
@@ -1,6 +1,59 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+//      ==== Address map in use in ARM development systems today ====
+//
+//              - 32-bit -              - 36-bit -          - 40-bit -
+//1024GB    +                   +                      +-------------------+     <- 40-bit
+//          |                                           | DRAM              |
+//          ~                   ~                       ~                   ~
+//          |                                           |                   |
+//          |                                           |                   |
+//          |                                           |                   |
+//          |                                           |                   |
+//544GB     +                   +                       +-------------------+
+//          |                                           | Hole or DRAM      |
+//          |                                           |                   |
+//512GB     +                   +                       +-------------------+
+//          |                                           |       Mapped      |
+//          |                                           |       I/O         |
+//          ~                   ~                       ~                   ~
+//          |                                           |                   |
+//256GB     +                   +                       +-------------------+
+//          |                                           |       Reserved    |
+//          ~                   ~                       ~                   ~
+//          |                                           |                   |
+//64GB      +                   +-----------------------+-------------------+   <- 36-bit
+//          |                   |                   DRAM                    |
+//          ~                   ~                   ~                       ~
+//          |                   |                                           |
+//          |                   |                                           |
+//34GB      +                   +-----------------------+-------------------+
+//          |                   |                  Hole or DRAM             |
+//32GB      +                   +-----------------------+-------------------+
+//          |                   |                   Mapped I/O              |
+//          ~                   ~                       ~                   ~
+//          |                   |                                           |
+//16GB      +                   +-----------------------+-------------------+
+//          |                   |                   Reserved                |
+//          ~                   ~                       ~                   ~
+//4GB       +-------------------+-----------------------+-------------------+   <- 32-bit
+//          |           2GB of DRAM                                         |
+//          |                                                               |
+//2GB       +-------------------+-----------------------+-------------------+
+//          |                           Mapped I/O                          |
+//1GB       +-------------------+-----------------------+-------------------+
+//          |                          ROM & RAM & I/O                      |
+//0GB       +-------------------+-----------------------+-------------------+   0
+//              - 32-bit -              - 36-bit -              - 40-bit -
+//
+// Taken from (http://infocenter.arm.com/help/topic/com.arm.doc.den0001c/DEN0001C_principles_of_arm_memory_maps.pdf).
+
+/// Start of RAM on 64 bit ARM.
+pub const DRAM_MEM_START: usize = 0x8000_0000; // 2 GB.
+/// The maximum addressable RAM address.
+pub const DRAM_MEM_END: usize = 0x00FF_8000_0000; // 1024 - 2 = 1022 GB.
+
 /// Kernel command line start address.
 pub const CMDLINE_START: usize = 0x0;
 /// Kernel command line start address maximum size.
