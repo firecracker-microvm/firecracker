2e8bfdce3e88413da1c1ebd3af9a3a1fc46f56e2
Diana Popa <dpopa@amazon.com>
2019-03-08 17:48:47 +0200

    arm64: added memory layout representation
    The code follows the memory address mapping of a 64-bit guest.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- arch/src/aarch64/mod.rs
+++ arch/src/aarch64/mod.rs
@@ -3,11 +3,15 @@
 
 pub mod layout;
 
+use std::cmp::min;
+
 use memory_model::{GuestAddress, GuestMemory};
 
-/// Stub function that needs to be implemented when aarch64 functionality is added.
+/// Returns a Vec of the valid memory addresses for aarch64.
+/// See [`layout`](layout) module for a drawing of the specific memory model for this platform.
 pub fn arch_memory_regions(size: usize) -> Vec<(GuestAddress, usize)> {
-    vec![(GuestAddress(0), size)]
+    let dram_size = min(size, layout::DRAM_MEM_END);
+    vec![(GuestAddress(layout::DRAM_MEM_START), dram_size)]
 }
 
 /// Stub function that needs to be implemented when aarch64 functionality is added.
@@ -23,4 +27,25 @@ pub fn configure_system(
 /// Stub function that needs to be implemented when aarch64 functionality is added.
 pub fn get_reserved_mem_addr() -> usize {
     0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn regions_lt_1024gb() {
+        let regions = arch_memory_regions(1usize << 29);
+        assert_eq!(1, regions.len());
+        assert_eq!(GuestAddress(super::layout::DRAM_MEM_START), regions[0].0);
+        assert_eq!(1usize << 29, regions[0].1);
+    }
+
+    #[test]
+    fn regions_gt_1024gb() {
+        let regions = arch_memory_regions(1usize << 41);
+        assert_eq!(1, regions.len());
+        assert_eq!(GuestAddress(super::layout::DRAM_MEM_START), regions[0].0);
+        assert_eq!(super::layout::DRAM_MEM_END, regions[0].1);
+    }
 }
