b15dcf85f70a7eabf98522970c93e2b17d79b2b7
Diana Popa <dpopa@amazon.com>
2019-03-08 19:20:13 +0200

    arm64: add irq assignment for the mmio dev manager
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- arch/src/aarch64/layout.rs
+++ arch/src/aarch64/layout.rs
@@ -5,3 +5,15 @@
 pub const CMDLINE_START: usize = 0x0;
 /// Kernel command line start address maximum size.
 pub const CMDLINE_MAX_SIZE: usize = 0x0;
+
+// As per virt/kvm/arm/vgic/vgic-kvm-device.c we need
+// the number of interrupts our GIC will support to be:
+// * bigger than 32
+// * less than 1023 and
+// * a multiple of 32.
+// We are setting up our interrupt controller to support a maximum of 128 interrupts.
+/// First usable interrupt on aarch64.
+pub const IRQ_BASE: u32 = 32;
+
+/// Last usable interrupt on aarch64.
+pub const IRQ_MAX: u32 = 159;
