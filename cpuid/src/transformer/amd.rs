bdac38916de53b74ca553a68e9cdc5c93feea654
Serban Iorga <seriorga@amazon.com>
2019-04-08 18:41:49 +0300

    AMD: set the total number of threads
    We don't support more then 64 threads right now.
    It's safe to put them all on the same processor.
    
    Signed-off-by: Serban Iorga <seriorga@amazon.com>

--- cpuid/src/transformer/amd.rs
+++ cpuid/src/transformer/amd.rs
@@ -12,6 +12,9 @@ use transformer::common::use_host_cpuid_function;
 
 // Largest extended function. It has to be larger then 0x8000001d (Extended Cache Topology).
 const LARGEST_EXTENDED_FN: u32 = 0x8000_001f;
+// This value allows at most 64 logical threads within a package.
+// See also the documentation for leaf_0x80000008::ecx::THREAD_ID_SIZE_BITRANGE
+const THREAD_ID_MAX_SIZE: u32 = 6;
 
 pub fn update_structured_extended_entry(
     entry: &mut kvm_cpuid_entry2,
@@ -56,6 +59,22 @@ pub fn update_extended_feature_info_entry(
     Ok(())
 }
 
+pub fn update_amd_features_entry(
+    entry: &mut kvm_cpuid_entry2,
+    vm_spec: &VmSpec,
+) -> Result<(), Error> {
+    use cpu_leaf::leaf_0x80000008::*;
+
+    // We don't support more then 64 threads right now.
+    // It's safe to put them all on the same processor.
+    entry
+        .ecx
+        .write_bits_in_range(&ecx::THREAD_ID_SIZE_BITRANGE, THREAD_ID_MAX_SIZE)
+        .write_bits_in_range(&ecx::NUM_THREADS_BITRANGE, u32::from(vm_spec.cpu_count - 1));
+
+    Ok(())
+}
+
 pub fn update_extended_cache_topology_entry(
     entry: &mut kvm_cpuid_entry2,
     vm_spec: &VmSpec,
@@ -78,6 +97,7 @@ impl CpuidTransformer for AmdCpuidTransformer {
             leaf_0x7::LEAF_NUM => Some(amd::update_structured_extended_entry),
             leaf_0x80000000::LEAF_NUM => Some(amd::update_largest_extended_fn_entry),
             leaf_0x80000001::LEAF_NUM => Some(amd::update_extended_feature_info_entry),
+            leaf_0x80000008::LEAF_NUM => Some(amd::update_amd_features_entry),
             leaf_0x8000001d::LEAF_NUM => Some(amd::update_extended_cache_topology_entry),
             0x8000_0002..=0x8000_0004 => Some(common::update_brand_string_entry),
             _ => None,
@@ -169,6 +189,33 @@ mod test {
         assert_eq!(entry.ecx.read_bit(ecx::TOPOEXT_INDEX), true);
     }
 
+    fn check_update_amd_features_entry(cpu_count: u8, ht_enabled: bool) {
+        use cpu_leaf::leaf_0x80000008::*;
+
+        let vm_spec = VmSpec::new(VENDOR_ID_AMD, 0, cpu_count, ht_enabled);
+        let mut entry = &mut kvm_cpuid_entry2 {
+            function: LEAF_NUM,
+            index: 0,
+            flags: 0,
+            eax: 0,
+            ebx: 0,
+            ecx: 0,
+            edx: 0,
+            padding: [0, 0, 0],
+        };
+
+        assert!(update_amd_features_entry(&mut entry, &vm_spec).is_ok());
+
+        assert_eq!(
+            entry.ecx.read_bits_in_range(&ecx::NUM_THREADS_BITRANGE),
+            u32::from(cpu_count - 1)
+        );
+        assert_eq!(
+            entry.ecx.read_bits_in_range(&ecx::THREAD_ID_SIZE_BITRANGE),
+            THREAD_ID_MAX_SIZE
+        );
+    }
+
     #[test]
     fn test_update_extended_cache_topology_entry() {
         let vm_spec = VmSpec::new(VENDOR_ID_AMD, 0, 1, false);
@@ -186,5 +233,25 @@ mod test {
         assert!(update_extended_cache_topology_entry(&mut entry, &vm_spec).is_ok());
 
         assert_eq!(entry.flags & KVM_CPUID_FLAG_SIGNIFCANT_INDEX, 1);
+    }
+
+    #[test]
+    fn test_1vcpu_ht_off() {
+        check_update_amd_features_entry(1, false);
+    }
+
+    #[test]
+    fn test_1vcpu_ht_on() {
+        check_update_amd_features_entry(1, true);
+    }
+
+    #[test]
+    fn test_2vcpu_ht_off() {
+        check_update_amd_features_entry(2, false);
+    }
+
+    #[test]
+    fn test_2vcpu_ht_on() {
+        check_update_amd_features_entry(2, true);
     }
 }
