dfff8bbb578c338f404e73276d3dc8dbd5322bea
Diana Popa <dpopa@amazon.com>
2019-04-16 20:20:18 +0000

    arch: function for obtaining high memory start
    We need it for loading the kernel for each architecture.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -22,16 +22,13 @@ pub enum Error {
 }
 pub type Result<T> = result::Result<T, Error>;
 
-// 1MB.  We don't put anything above here except the kernel itself.
-pub const HIMEM_START: usize = 0x0010_0000;
-
 #[cfg(target_arch = "aarch64")]
 pub mod aarch64;
 
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::{
-    arch_memory_regions, configure_system, get_reserved_mem_addr, layout::CMDLINE_MAX_SIZE,
-    layout::CMDLINE_START, layout::IRQ_BASE, layout::IRQ_MAX,
+    arch_memory_regions, configure_system, get_kernel_start, get_reserved_mem_addr,
+    layout::CMDLINE_MAX_SIZE, layout::CMDLINE_START, layout::IRQ_BASE, layout::IRQ_MAX,
 };
 
 #[cfg(target_arch = "x86_64")]
@@ -40,5 +37,6 @@ pub mod x86_64;
 #[cfg(target_arch = "x86_64")]
 pub use x86_64::{
     arch_memory_regions, configure_system, get_32bit_gap_start as get_reserved_mem_addr,
-    layout::CMDLINE_MAX_SIZE, layout::CMDLINE_START, layout::IRQ_BASE, layout::IRQ_MAX,
+    get_kernel_start, layout::CMDLINE_MAX_SIZE, layout::CMDLINE_START, layout::IRQ_BASE,
+    layout::IRQ_MAX,
 };
