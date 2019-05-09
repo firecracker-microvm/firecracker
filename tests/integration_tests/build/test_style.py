e4e7f538d80642b3e7931193bc67580364bbb5b0
Andreea Florescu <fandree@amazon.com>
2019-05-09 12:37:00 +0300

    added vsock definition in experimental yaml
    Added firecracker-experimental.yaml in api_server/swagger.
    This file is a copy of firecracker.yaml with an additional definiton
    for the vsock API request.
    
    The point of this file is to be used by third-party projects (like
    Kata Containers) to automatically generate an API client that knows
    how to send requests to the Firecracker API. The definition currently
    lies in a different file and not in firecracker.yaml because vsock
    is consider an experimental feature. Once the production ready vsock
    is merged we will get rid of this experimental yaml.
    
    For tracking purposes, this is the issue regarding switching to
    vsock with UDS:
    https://github.com/firecracker-microvm/firecracker/issues/650
    
    In the new proposed vsock implementation, the API request for
    configuring the vsock will also change.
    
    Fixes https://github.com/firecracker-microvm/firecracker/issues/1085
    
    Signed-off-by: Andreea Florescu <fandree@amazon.com>

--- tests/integration_tests/build/test_style.py
+++ tests/integration_tests/build/test_style.py
@@ -86,14 +86,29 @@ def test_rust_clippy():
     )
 
 
-def test_yaml_style():
-    """Fail if our swagger specification is malformed."""
-    yaml_spec = os.path.normpath(
-         os.path.join(os.getcwd(), '../api_server/swagger/firecracker.yaml')
-     )
+def check_swagger_style(yaml_spec):
+    """Check if the swagger definition is correctly formatted."""
     with open(yaml_spec, 'r') as file_stream:
         try:
             yaml.safe_load(file_stream)
         # pylint: disable=broad-except
         except Exception as exception:
             print(str(exception))
+
+
+def test_firecracker_swagger():
+    """Fail if Firecracker swagger specification is malformed."""
+    yaml_spec = os.path.normpath(
+        os.path.join(os.getcwd(), '../api_server/swagger/firecracker.yaml')
+    )
+    check_swagger_style(yaml_spec)
+
+
+def test_experimental_firecracker_swagger():
+    """Fail if experimental Firecracker swagger specification is malformed."""
+    yaml_spec = os.path.normpath(
+        os.path.join(
+            os.getcwd(),
+            '../api_server/swagger/firecracker-experimental.yaml')
+    )
+    check_swagger_style(yaml_spec)
