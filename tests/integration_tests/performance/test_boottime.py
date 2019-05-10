eb22048affd57d31e543dbf0c10b0cb5e957120c
Alexandra Iordache <aghecen@amazon.com>
2019-05-10 09:35:24 +0300

    tests: disable timing on vsock builds
    * don't test minimal boot time because the extra vsock device
      causes longer boot times than the SLA guarantees
    * don't time requests because InstanceStart takes longer on
      vsock builds, also because of the extra device

--- tests/integration_tests/performance/test_boottime.py
+++ tests/integration_tests/performance/test_boottime.py
@@ -6,8 +6,9 @@ import os
 import re
 import time
 
-from framework import decorators
+import pytest
 
+from framework import decorators
 import host_tools.logging as log_tools
 
 # The maximum acceptable boot time in us.
@@ -21,6 +22,10 @@ NO_OF_MICROVMS = 10
 
 def test_single_microvm_boottime_no_network(test_microvm_with_boottime):
     """Check guest boottime of microvm without network."""
+    # Skip on vsock builds.
+    if test_microvm_with_boottime.build_feature == 'vsock':
+        pytest.skip('The vsock feature is enabled.')
+
     log_fifo, _ = _configure_vm(test_microvm_with_boottime)
     time.sleep(0.4)
     boottime_us = _test_microvm_boottime(log_fifo)
@@ -69,6 +74,10 @@ def test_single_microvm_boottime_with_network(
         network_config
 ):
     """Check guest boottime of microvm with network."""
+    # Skip on vsock builds.
+    if test_microvm_with_boottime.build_feature == 'vsock':
+        pytest.skip('The vsock feature is enabled.')
+
     log_fifo, _tap = _configure_vm(test_microvm_with_boottime, {
         "config": network_config, "iface_id": "1"
     })
