5be5644038c1386e73ee5dd242687ddfb1da55d8
Damien Stanton <damien.stanton@gmail.com>
2019-05-30 11:04:07 -0400

    fix missing std feature for backtrace in main. firecracker main now passes cargo check

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@
 
 use vmm;
 #[cfg(target_arch = "x86_64")]
-use backtrace::Backtrace;
-use clap::{App, Arg};
+use clap::{App, Arg, crate_version, crate_authors};
 
 use std::io::ErrorKind;
 use std::panic;
@@ -13,6 +12,10 @@ use std::path::PathBuf;
 use std::process;
 use std::sync::mpsc::channel;
 use std::sync::{Arc, RwLock};
+use backtrace::Backtrace;
+
+#[macro_use]
+extern crate logger;
 
 use api_server::{ApiServer, Error};
 use fc_util::validators::validate_instance_id;
