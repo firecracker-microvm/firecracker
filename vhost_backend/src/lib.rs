02efe5db26f48ee0f49a2c0ada3d2517ccd88ab1
Damien Stanton <damien.stanton@gmail.com>
2019-08-15 16:42:22 -0400

    vhost_backend pass 2

--- vhost_backend/src/lib.rs
+++ vhost_backend/src/lib.rs
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the THIRD-PARTY file.
 
-extern crate libc;
 
-extern crate memory_model;
-extern crate sys_util;
-extern crate vhost_gen;
+
+
+
+
 
 mod vsock;
 pub use crate::vsock::Vsock;
