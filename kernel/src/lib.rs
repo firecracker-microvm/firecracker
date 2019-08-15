141b0ce6faf4544eae672f0dc72c6032a7cf9d61
Damien Stanton <damien.stanton@gmail.com>
2019-08-15 16:02:00 -0400

    kernel pass 2

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -8,5 +8,5 @@
 pub mod cmdline;
 pub mod loader;
 
-extern crate memory_model;
-extern crate sys_util;
+
+
--- kernel/src/loader/mod.rs
+++ kernel/src/loader/mod.rs
@@ -38,7 +38,7 @@ pub enum Error {
 }
 
 impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{}",
