94fbc3f394efcb70ca4c474ff1a05191ea2cac40
Andrei Sandu <sandreim@amazon.com>
2019-08-28 16:29:41 +0300

    Changed earlycon settings to use mmio 8 bit access.
    - Remove data_len from Serial device implementation
    - Rename LegacyDeviceManager to PortIODeviceManager
    
    Signed-off-by: Andrei Sandu <sandreim@amazon.com>

--- vmm/src/device_manager/legacy.rs
+++ vmm/src/device_manager/legacy.rs
@@ -4,25 +4,20 @@
 // Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the THIRD-PARTY file.
-
 use std::fmt;
 use std::io::{self, stdout};
 use std::sync::{Arc, Mutex};
 
 use devices;
 use sys_util::EventFd;
-#[cfg(target_arch = "x86_64")]
-use sys_util::Terminal;
 
-/// Errors corresponding to the `LegacyDeviceManager`.
+/// Errors corresponding to the `PortIODeviceManager`.
 #[derive(Debug)]
 pub enum Error {
     /// Cannot add legacy device to Bus.
     BusError(devices::BusError),
     /// Cannot create EventFd.
     EventFd(io::Error),
-    /// Cannot set mode for terminal.
-    StdinHandle(io::Error),
 }
 
 impl fmt::Display for Error {
@@ -32,17 +27,16 @@ impl fmt::Display for Error {
         match *self {
             BusError(ref err) => write!(f, "Failed to add legacy device to Bus: {}", err),
             EventFd(ref err) => write!(f, "Failed to create EventFd: {}", err),
-            StdinHandle(ref err) => write!(f, "Failed to set mode for terminal: {}", err),
         }
     }
 }
 
 type Result<T> = ::std::result::Result<T, Error>;
 
-/// The `LegacyDeviceManager` is a wrapper that is used for registering legacy devices
+/// The `PortIODeviceManager` is a wrapper that is used for registering legacy devices
 /// on an I/O Bus. It currently manages the uart and i8042 devices.
 /// The `LegacyDeviceManger` should be initialized only by using the constructor.
-pub struct LegacyDeviceManager {
+pub struct PortIODeviceManager {
     pub io_bus: devices::Bus,
     pub stdio_serial: Arc<Mutex<devices::legacy::Serial>>,
     pub i8042: Arc<Mutex<devices::legacy::I8042Device>>,
@@ -50,10 +44,9 @@ pub struct LegacyDeviceManager {
     pub com_evt_1_3: EventFd,
     pub com_evt_2_4: EventFd,
     pub kbd_evt: EventFd,
-    pub stdin_handle: io::Stdin,
 }
 
-impl LegacyDeviceManager {
+impl PortIODeviceManager {
     /// Create a new DeviceManager handling legacy devices (uart, i8042).
     pub fn new() -> Result<Self> {
         let io_bus = devices::Bus::new();
@@ -63,7 +56,6 @@ impl LegacyDeviceManager {
         let stdio_serial = Arc::new(Mutex::new(devices::legacy::Serial::new_out(
             com_evt_1_3.try_clone().map_err(Error::EventFd)?,
             Box::new(stdout()),
-            None,
         )));
 
         // Create exit event for i8042
@@ -73,14 +65,13 @@ impl LegacyDeviceManager {
             kbd_evt.try_clone().unwrap(),
         )));
 
-        Ok(LegacyDeviceManager {
+        Ok(PortIODeviceManager {
             io_bus,
             stdio_serial,
             i8042,
             com_evt_1_3,
             com_evt_2_4,
             kbd_evt,
-            stdin_handle: io::stdin(),
         })
     }
 
@@ -94,7 +85,6 @@ impl LegacyDeviceManager {
             .insert(
                 Arc::new(Mutex::new(devices::legacy::Serial::new_sink(
                     self.com_evt_2_4.try_clone().map_err(Error::EventFd)?,
-                    None,
                 ))),
                 0x2f8,
                 0x8,
@@ -104,7 +94,6 @@ impl LegacyDeviceManager {
             .insert(
                 Arc::new(Mutex::new(devices::legacy::Serial::new_sink(
                     self.com_evt_1_3.try_clone().map_err(Error::EventFd)?,
-                    None,
                 ))),
                 0x3e8,
                 0x8,
@@ -114,16 +103,11 @@ impl LegacyDeviceManager {
             .insert(
                 Arc::new(Mutex::new(devices::legacy::Serial::new_sink(
                     self.com_evt_2_4.try_clone().map_err(Error::EventFd)?,
-                    None,
                 ))),
                 0x2e8,
                 0x8,
             )
             .map_err(Error::BusError)?;
-        self.stdin_handle
-            .lock()
-            .set_raw_mode()
-            .map_err(Error::StdinHandle)?;
         self.io_bus
             .insert(self.i8042.clone(), 0x060, 0x5)
             .map_err(Error::BusError)?;
@@ -138,12 +122,9 @@ mod tests {
     #[test]
     #[cfg(target_arch = "x86_64")]
     fn test_register_legacy_devices() {
-        let ldm = LegacyDeviceManager::new();
+        let ldm = PortIODeviceManager::new();
         assert!(ldm.is_ok());
         assert!(&ldm.unwrap().register_devices().is_ok());
-        // we need to reset the terminal otherwise stdin will remain in raw mode
-        let stdin_handle = io::stdin();
-        stdin_handle.lock().set_canon_mode().unwrap();
     }
 
     #[test]
@@ -159,13 +140,6 @@ mod tests {
             format!("{}", Error::EventFd(io::Error::from_raw_os_error(1))),
             format!(
                 "Failed to create EventFd: {}",
-                io::Error::from_raw_os_error(1)
-            )
-        );
-        assert_eq!(
-            format!("{}", Error::StdinHandle(io::Error::from_raw_os_error(1))),
-            format!(
-                "Failed to set mode for terminal: {}",
                 io::Error::from_raw_os_error(1)
             )
         );
