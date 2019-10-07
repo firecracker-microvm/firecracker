c9ac268f31ad4caa33943b5a156140f0d0dc3442
George Pisaltu <gpl@amazon.com>
2019-10-07 18:35:14 +0300

    add support for boot source in new api server
    Signed-off-by: George Pisaltu <gpl@amazon.com>

--- api_server/src/parsed_request.rs
+++ api_server/src/parsed_request.rs
@@ -4,6 +4,7 @@
 use serde_json::Value;
 
 use micro_http::{Body, Method, Request, Response, StatusCode, Version};
+use request::boot_source::parse_put_boot_source;
 use request::drive::{parse_patch_drive, parse_put_drive};
 use request::instance_info::parse_get_instance_info;
 use request::logger::parse_put_logger;
@@ -38,6 +39,7 @@ impl ParsedRequest {
             (Method::Get, "", None) => parse_get_instance_info(),
             (Method::Get, "machine-config", None) => parse_get_machine_config(),
             (Method::Get, "mmds", None) => parse_get_mmds(),
+            (Method::Put, "boot-source", Some(body)) => parse_put_boot_source(body),
             (Method::Put, "drives", maybe_body) => parse_put_drive(maybe_body, path_tokens.get(1)),
             (Method::Put, "logger", Some(body)) => parse_put_logger(body),
             (Method::Put, "machine-config", maybe_body) => parse_put_machine_config(maybe_body),
--- api_server/src/request/boot_source.rs
+++ api_server/src/request/boot_source.rs
@@ -1,49 +1,17 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::result;
-
-use futures::sync::oneshot;
-use hyper::Method;
-
-use super::{VmmAction, VmmRequest};
-use request::{IntoParsedRequest, ParsedRequest};
+use super::super::VmmAction;
+use logger::{Metric, METRICS};
+use request::{Body, Error, ParsedRequest};
 use vmm::vmm_config::boot_source::BootSourceConfig;
 
-impl IntoParsedRequest for BootSourceConfig {
-    fn into_parsed_request(
-        self,
-        _: Option<String>,
-        _: Method,
-    ) -> result::Result<ParsedRequest, String> {
-        let (sender, receiver) = oneshot::channel();
-        Ok(ParsedRequest::Sync(
-            VmmRequest::new(VmmAction::ConfigureBootSource(self), sender),
-            receiver,
-        ))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_into_parsed_request() {
-        let body = BootSourceConfig {
-            kernel_image_path: String::from("/foo/bar"),
-            boot_args: Some(String::from("foobar")),
-        };
-        let same_body = BootSourceConfig {
-            kernel_image_path: String::from("/foo/bar"),
-            boot_args: Some(String::from("foobar")),
-        };
-        let (sender, receiver) = oneshot::channel();
-        assert!(body
-            .into_parsed_request(None, Method::Put)
-            .eq(&Ok(ParsedRequest::Sync(
-                VmmRequest::new(VmmAction::ConfigureBootSource(same_body), sender),
-                receiver
-            ))))
-    }
+pub fn parse_put_boot_source(body: &Body) -> Result<ParsedRequest, Error> {
+    METRICS.put_api_requests.boot_source_count.inc();
+    Ok(ParsedRequest::Sync(VmmAction::ConfigureBootSource(
+        serde_json::from_slice::<BootSourceConfig>(body.raw()).map_err(|e| {
+            METRICS.put_api_requests.boot_source_fails.inc();
+            Error::SerdeJson(e)
+        })?,
+    )))
 }
