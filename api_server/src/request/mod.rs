7237eef419caa03669b67632c0bb1b90a9c1a461
George Pisaltu <gpl@amazon.com>
2019-10-07 18:31:36 +0300

    add support for drives in new api server
    Signed-off-by: George Pisaltu <gpl@amazon.com>

--- api_server/src/request/mod.rs
+++ api_server/src/request/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod drive;
 pub mod instance_info;
 pub mod logger;
 pub mod machine_configuration;
