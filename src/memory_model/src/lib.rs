9af11711fc4acf4a1569cb165a2d1467524a17fa
Serban Iorga <seriorga@amazon.com>
2019-12-16 12:36:08 +0200

    [memory_model] add Bytes trait
    Signed-off-by: Serban Iorga <seriorga@amazon.com>

--- src/memory_model/src/lib.rs
+++ src/memory_model/src/lib.rs
@@ -16,7 +16,7 @@ mod guest_address;
 mod guest_memory;
 mod mmap;
 
-pub use bytes::ByteValued;
+pub use bytes::{ByteValued, Bytes};
 pub use guest_address::Address;
 pub use guest_address::GuestAddress;
 pub use guest_memory::Error as GuestMemoryError;
