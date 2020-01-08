4eabba68ede45a8c475e58084b3772675eccf715
Iulian Barbu <iul@amazon.com>
2020-01-08 16:47:04 +0200

    Renamed `region_end` to `last_addr`
    * Renamed `region_end` to `last_addr`.
    * Modified the behaviour of the function.
    BEFORE: The function returned the last address of
    a region exclusive.
    NOW: The function returns the last address of a region
    inclusive.
    * Changed the function from being orphan to being bound
    to the MemoryRegion struct.
    
    Signed-off-by: Iulian Barbu <iul@amazon.com>

--- src/arch/src/aarch64/fdt.rs
+++ src/arch/src/aarch64/fdt.rs
@@ -326,7 +326,7 @@ fn create_cpu_nodes(fdt: &mut Vec<u8>, vcpu_mpidr: &Vec<u64>) -> Result<()> {
 }
 
 fn create_memory_node(fdt: &mut Vec<u8>, guest_mem: &GuestMemoryMmap) -> Result<()> {
-    let mem_size = guest_mem.end_addr().raw_value() - super::layout::DRAM_MEM_START;
+    let mem_size = guest_mem.last_addr().raw_value() - super::layout::DRAM_MEM_START + 1;
     // See https://github.com/torvalds/linux/blob/master/Documentation/devicetree/booting-without-of.txt#L960
     // for an explanation of this.
     let mem_reg_prop = generate_prop64(&[super::layout::DRAM_MEM_START as u64, mem_size as u64]);
