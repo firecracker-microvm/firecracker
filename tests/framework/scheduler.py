firecracker-microvm/firecracker

commit 803c8fae14b1194ba103cf7f0ce817376bf9dfc4

tests: fixed test_rescan_dev
Fixed the block device rescan test, which used to get skipped due to
incorrect handling of loop devices inside the container:
- bind-mounted `/dev` inside the container, in order to make devices
  that are created post container start visible to the container;
- added a custom init process (tini) as the entry point for the dev
  container, since `docker --init` conflicts with bind-mounting `/dev`
  (docker places the init process under `/dev/init`).

Signed-off-by: Dan Horobeanu <dhr@amazon.com>

tools/devctr/Dockerfile.aarch64
@@ -5,6 +5,7 @@ FROM ubuntu:18.04
 # dependencies layer intact for much longer.
 
 ARG RUST_TOOLCHAIN="1.39.0"
+ARG TINI_VERSION_TAG="v0.18.0"
 ARG TMP_BUILD_DIR=/tmp/build
 ARG FIRECRACKER_SRC_DIR="/firecracker"
 ARG FIRECRACKER_BUILD_DIR="$FIRECRACKER_SRC_DIR/build"
@@ -71,5 +72,10 @@ RUN curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "$RUST_TOOL
         && ln -s "$CARGO_REGISTRY_DIR" "$CARGO_HOME/registry" \
         && rm -rf "$CARGO_HOME/git" \
         && ln -s "$CARGO_GIT_REGISTRY_DIR" "$CARGO_HOME/git"
+#
+# Add the tini init binary.
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION_TAG}/tini-static-arm64 /sbin/tini
+RUN chmod +x /sbin/tini
 
 WORKDIR "$FIRECRACKER_SRC_DIR"
+ENTRYPOINT ["/sbin/tini", "--"]

tools/devctr/Dockerfile.x86_64
@@ -5,6 +5,7 @@ FROM ubuntu:18.04
 # dependencies layer intact for much longer.
 
 ARG RUST_TOOLCHAIN="1.39.0"
+ARG TINI_VERSION_TAG="v0.18.0"
 ARG TMP_BUILD_DIR=/tmp/build
 ARG FIRECRACKER_SRC_DIR="/firecracker"
 ARG FIRECRACKER_BUILD_DIR="$FIRECRACKER_SRC_DIR/build"
@@ -81,4 +82,9 @@ RUN mkdir "$TMP_BUILD_DIR" \
     && cd / \
     && rm -rf "$TMP_BUILD_DIR"
 
+# Add the tini init binary.
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION_TAG}/tini-static-amd64 /sbin/tini
+RUN chmod +x /sbin/tini
+
 WORKDIR "$FIRECRACKER_SRC_DIR"
+ENTRYPOINT ["/sbin/tini", "--"]

tools/devtool
@@ -73,7 +73,7 @@
 # Development container image (name:tag)
 # This should be updated whenever we upgrade the development container.
 # (Yet another step on our way to reproducible builds.)
-DEVCTR_IMAGE="fcuvm/dev:v13"
+DEVCTR_IMAGE="fcuvm/dev:v14"
 
 # Naming things is hard
 MY_NAME="Firecracker $(basename "$0")"
@@ -351,8 +351,8 @@ run_devctr() {
     # Use 'z' on the --volume parameter for docker to automatically relabel the
     # content and allow sharing between containers.
     docker run "${docker_args[@]}" \
-        --init \
         --rm \
+        --volume /dev:/dev \
         --volume "$FC_ROOT_DIR:$CTR_FC_ROOT_DIR:z" \
         --env OPT_LOCAL_IMAGES_PATH="$(dirname "$CTR_MICROVM_IMAGES_DIR")" \
         --env PYTHONDONTWRITEBYTECODE=1 \

commit e31e97f9d6e29ec8ec41473f3a96c4f5c495713a

tests: fixed exit code regression
Fixed a regression introduced by #1478, whereby the pytest session would
return an incorrect exit code.

Signed-off-by: Dan Horobeanu <dhr@amazon.com>

tests/framework/scheduler.py
@@ -28,6 +28,8 @@ import sys
 from random import random
 from select import select
 from time import sleep
+import pytest
+from _pytest.main import ExitCode
 
 from . import mpsing  # pylint: disable=relative-beyond-top-level
 
@@ -171,6 +173,23 @@ class PytestScheduler(mpsing.MultiprocessSingleton):
             self._run_batch(batch)
 
         return "stahp"
+
+    @pytest.mark.tryfirst
+    # pylint: disable=unused-argument
+    # pylint: disable=no-self-use
+    def pytest_sessionfinish(self, session, exitstatus):
+        """Pytest hook. Wrap up the whole testing session.
+
+        Since the scheduler is more or less mangling the test session in order
+        to distribute test items to worker processes, the main pytest process
+        can become unaware of test failures and errors. Using this session
+        wrap-up hook to set the correct exit code.
+        """
+        trep = session.config.pluginmanager.getplugin("terminalreporter")
+        if "error" in trep.stats:
+            session.exitstatus = ExitCode.INTERNAL_ERROR
+        if "failed" in trep.stats:
+            session.exitstatus = ExitCode.TESTS_FAILED
 
     def _run_batch(self, batch):
         """Run the tests in this batch, spread across multiple workers.