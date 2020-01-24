81fb9ad9a85ad5b41ada1c523a33ed86a15a7acd
Adrian Catangiu <acatan@amazon.com>
2020-01-24 20:06:35 +0200

    move the vmm-building inside the preboot controller
    The RPC PrebootApiController now also has a method to drive itself
    all the way to the Vmm being built.
    
    That logic was previously in the firecracker crate, but through some
    refactoring, it's now generic enough to live in the rpc_interface.rs.
    
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- src/vmm/src/rpc_interface.rs
+++ src/vmm/src/rpc_interface.rs
@@ -139,6 +139,8 @@ pub struct PrebootApiController<'a> {
     vm_resources: &'a mut VmResources,
     epoll_context: &'a mut EpollContext,
     event_manager: &'a mut EventManager,
+
+    built_vmm: Option<Arc<Mutex<Vmm>>>,
 }
 
 impl<'a> PrebootApiController<'a> {
@@ -156,7 +158,47 @@ impl<'a> PrebootApiController<'a> {
             vm_resources,
             epoll_context,
             event_manager,
+
+            built_vmm: None,
         }
+    }
+
+    /// Default implementation for the function that builds and starts a microVM.
+    /// It takes two closures `recv_req` and `respond` as params which abstract away
+    /// the message transport.
+    ///
+    /// Returns a populated `VmResources` object and a running `Vmm` object.
+    pub fn build_microvm_from_requests<F, G>(
+        seccomp_level: u32,
+        epoll_context: &mut EpollContext,
+        event_manager: &mut EventManager,
+        firecracker_version: String,
+        recv_req: F,
+        respond: G,
+    ) -> (VmResources, Arc<Mutex<Vmm>>)
+    where
+        F: Fn() -> VmmAction,
+        G: Fn(std::result::Result<VmmData, VmmActionError>),
+    {
+        let mut vm_resources = VmResources::default();
+        let mut preboot_controller = PrebootApiController::new(
+            seccomp_level,
+            firecracker_version,
+            &mut vm_resources,
+            epoll_context,
+            event_manager,
+        );
+        // Configure and start microVM through successive API calls.
+        // Iterate through API calls to configure microVm.
+        // The loop breaks when a microVM is successfully started, and a running Vmm is built.
+        while preboot_controller.built_vmm.is_none() {
+            // Get request, process it, send back the response.
+            respond(preboot_controller.handle_preboot_request(recv_req()));
+        }
+
+        // Safe to unwrap because previous loop cannot end on None.
+        let vmm = preboot_controller.built_vmm.unwrap();
+        (vm_resources, vmm)
     }
 
     /// Handles the incoming preboot request and provides a response for it.
@@ -164,14 +206,10 @@ impl<'a> PrebootApiController<'a> {
     pub fn handle_preboot_request(
         &mut self,
         request: VmmAction,
-    ) -> (
-        std::result::Result<VmmData, VmmActionError>,
-        Option<Arc<Mutex<Vmm>>>,
-    ) {
+    ) -> std::result::Result<VmmData, VmmActionError> {
         use self::VmmAction::*;
 
-        let mut maybe_vmm = None;
-        let response = match request {
+        match request {
             // Supported operations allowed pre-boot.
             ConfigureBootSource(boot_source_body) => self
                 .vm_resources
@@ -222,7 +260,7 @@ impl<'a> PrebootApiController<'a> {
                 self.seccomp_level,
             )
             .map(|vmm| {
-                maybe_vmm = Some(vmm);
+                self.built_vmm = Some(vmm);
                 VmmData::Empty
             })
             .map_err(VmmActionError::StartMicrovm),
@@ -233,9 +271,7 @@ impl<'a> PrebootApiController<'a> {
             }
             #[cfg(target_arch = "x86_64")]
             SendCtrlAltDel => Err(VmmActionError::OperationNotSupportedPreBoot),
-        };
-
-        (response, maybe_vmm)
+        }
     }
 }
 
