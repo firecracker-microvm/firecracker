3fe6d146856b4c44c6c7adeaa3f2d6b76f09feff
Alexandra Iordache <aghecen@amazon.com>
2020-01-28 17:43:08 +0200

    vmm: add functionality to fetch dirty bitmap
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/vmm/src/rpc_interface.rs
+++ src/vmm/src/rpc_interface.rs
@@ -1,6 +1,7 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::fs::OpenOptions;
 use std::io::{Seek, SeekFrom};
