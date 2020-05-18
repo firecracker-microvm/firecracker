c7e56e0f025bea85fa8b6dc0bfba0cf86560fa07
Adrian Catangiu <acatan@amazon.com>
2020-05-18 11:58:41 +0300

    tests: increase binary size limit and update coverage
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- tests/integration_tests/build/test_binary_size.py
+++ tests/integration_tests/build/test_binary_size.py
@@ -11,10 +11,10 @@ import host_tools.cargo_build as host
 MACHINE = platform.machine()
 """ Platform definition used to select the correct size target"""
 
-FC_BINARY_SIZE_TARGET = 3013464 if MACHINE == "x86_64" else 3251464
+FC_BINARY_SIZE_TARGET = 3200000 if MACHINE == "x86_64" else 3251464
 """Firecracker target binary size in bytes"""
 
-FC_BINARY_SIZE_LIMIT = 3200000 if MACHINE == "x86_64" else 3400000
+FC_BINARY_SIZE_LIMIT = 3500000 if MACHINE == "x86_64" else 3400000
 """Firecracker maximum binary size in bytes"""
 
 JAILER_BINARY_SIZE_TARGET = 2568384 if MACHINE == "x86_64" else 2792688
