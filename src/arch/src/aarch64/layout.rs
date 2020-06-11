a6d6645b8e3ea6c56ad173cc05a7559aa2b29056
Wei Yang <richard.weiyang@linux.alibaba.com>
2020-06-11 17:54:11 +0800

    aarch64: fix DRAM_MEM_END in layout.rs
    According to the definition, DRAM_MEM_MAX_SIZE should be 1024G. Let's
    remove the DRAM_MEM_END and just leave DRAM_MEM_MAX_SIZE.
    
    Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>

--- src/arch/src/aarch64/layout.rs
+++ src/arch/src/aarch64/layout.rs
@@ -51,10 +51,8 @@
 
 /// Start of RAM on 64 bit ARM.
 pub const DRAM_MEM_START: u64 = 0x8000_0000; // 2 GB.
-/// The maximum addressable RAM address.
-pub const DRAM_MEM_END: u64 = 0x00FF_8000_0000; // 1024 - 2 = 1022 GB.
 /// The maximum RAM size.
-pub const DRAM_MEM_MAX_SIZE: u64 = DRAM_MEM_END - DRAM_MEM_START;
+pub const DRAM_MEM_MAX_SIZE: u64 = 0x00FF_8000_0000; // 1024 - 2 = 1022G.
 
 /// Kernel command line maximum size.
 /// As per `arch/arm64/include/uapi/asm/setup.h`.
