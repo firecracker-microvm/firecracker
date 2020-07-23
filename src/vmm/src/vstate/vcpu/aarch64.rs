b7533146d55107a74bef2cad968bc97d8380ea10
Diana Popa <dpopa@amazon.com>
2020-07-23 17:43:53 +0300

    aarch64: support for save/restore of VcpuState
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- src/vmm/src/vstate/vcpu/aarch64.rs
+++ src/vmm/src/vstate/vcpu/aarch64.rs
@@ -19,15 +19,19 @@ use vm_memory::{Address, GuestAddress, GuestMemoryMmap};
 #[derive(Debug)]
 pub enum Error {
     /// Error configuring the general purpose aarch64 registers.
-    REGSConfiguration(arch::aarch64::regs::Error),
+    ConfigureRegisters(arch::aarch64::regs::Error),
+    /// Cannot open the kvm related file descriptor.
+    CreateFd(kvm_ioctls::Error),
+    /// Error getting the Vcpu preferred target on Arm.
+    GetPreferredTarget(kvm_ioctls::Error),
+    /// Error doing Vcpu Init on Arm.
+    Init(kvm_ioctls::Error),
+    /// Failed to set value for some arm specific register.
+    RestoreState(arch::aarch64::regs::Error),
+    /// Failed to fetch value for some arm specific register.
+    SaveState(arch::aarch64::regs::Error),
     /// Operation not supported.
     UnsupportedAction(&'static str),
-    /// Cannot open the VCPU file descriptor.
-    VcpuFd(kvm_ioctls::Error),
-    /// Error doing Vcpu Init on Arm.
-    VcpuInit(kvm_ioctls::Error),
-    /// Error getting the Vcpu preferred target on Arm.
-    VcpuPreferredTarget(kvm_ioctls::Error),
 }
 
 impl Display for Error {
@@ -35,18 +39,18 @@ impl Display for Error {
         use self::Error::*;
 
         match self {
-            REGSConfiguration(e) => write!(
+            ConfigureRegisters(e) => write!(
                 f,
                 "Error configuring the general purpose registers: {:?}",
                 e
             ),
+            CreateFd(e) => write!(f, "Error in opening the VCPU file descriptor: {}", e),
+            GetPreferredTarget(e) => write!(f, "Error retrieving the vcpu preferred target: {}", e),
+            Init(e) => write!(f, "Error initializing the vcpu: {}", e),
+            RestoreState(e) => write!(f, "Failed to restore the state of the vcpu: {}", e),
+            SaveState(e) => write!(f, "Failed to save the state of the vcpu: {}", e),
             UnsupportedAction(msg) => {
                 write!(f, "{} is not yet supported on this architecture", msg)
-            }
-            VcpuFd(e) => write!(f, "Error in opening the VCPU file descriptor: {}", e),
-            VcpuInit(e) => write!(f, "Error initializing the vcpu: {}", e),
-            VcpuPreferredTarget(e) => {
-                write!(f, "Error retrieving the vcpu preferred target: {}", e)
             }
         }
     }
@@ -72,7 +76,7 @@ impl KvmVcpu {
     /// * `id` - Represents the CPU number between [0, max vcpus).
     /// * `vm` - The vm to which this vcpu will get attached.
     pub fn new(index: u8, vm: &Vm) -> Result<Self> {
-        let kvm_vcpu = vm.fd().create_vcpu(index).map_err(Error::VcpuFd)?;
+        let kvm_vcpu = vm.fd().create_vcpu(index).map_err(Error::CreateFd)?;
 
         Ok(KvmVcpu {
             index,
@@ -105,7 +109,7 @@ impl KvmVcpu {
         // This reads back the kernel's preferred target type.
         vm_fd
             .get_preferred_target(&mut kvi)
-            .map_err(Error::VcpuPreferredTarget)?;
+            .map_err(Error::GetPreferredTarget)?;
         // We already checked that the capability is supported.
         kvi.features[0] |= 1 << kvm_bindings::KVM_ARM_VCPU_PSCI_0_2;
         // Non-boot cpus are powered off initially.
@@ -113,28 +117,44 @@ impl KvmVcpu {
             kvi.features[0] |= 1 << kvm_bindings::KVM_ARM_VCPU_POWER_OFF;
         }
 
-        self.fd.vcpu_init(&kvi).map_err(Error::VcpuInit)?;
+        self.fd.vcpu_init(&kvi).map_err(Error::Init)?;
         arch::aarch64::regs::setup_boot_regs(
             &self.fd,
             self.index,
             kernel_load_addr.raw_value(),
             guest_mem,
         )
-        .map_err(Error::REGSConfiguration)?;
+        .map_err(Error::ConfigureRegisters)?;
 
-        self.mpidr = arch::aarch64::regs::read_mpidr(&self.fd).map_err(Error::REGSConfiguration)?;
+        self.mpidr =
+            arch::aarch64::regs::read_mpidr(&self.fd).map_err(Error::ConfigureRegisters)?;
 
         Ok(())
     }
 
     /// Save the KVM internal state.
     pub fn save_state(&self) -> Result<VcpuState> {
-        Err(Error::UnsupportedAction("Saving the state"))
+        let mut state = VcpuState::default();
+
+        // Get this vCPUs multiprocessing state.
+        state.mp_state = arch::regs::get_mpstate(&self.fd).map_err(Error::SaveState)?;
+
+        arch::regs::save_core_registers(&self.fd, &mut state.regs).map_err(Error::SaveState)?;
+
+        arch::regs::save_system_registers(&self.fd, &mut state.regs).map_err(Error::SaveState)?;
+
+        state.mpidr = arch::aarch64::regs::read_mpidr(&self.fd).map_err(Error::SaveState)?;
+
+        Ok(state)
     }
 
     /// Use provided state to populate KVM internal state.
-    pub fn restore_state(&self, _state: &VcpuState) -> Result<()> {
-        Err(Error::UnsupportedAction("Restoring the state"))
+    pub fn restore_state(&self, state: &VcpuState) -> Result<()> {
+        arch::regs::restore_registers(&self.fd, &state.regs).map_err(Error::RestoreState)?;
+
+        arch::regs::set_mpstate(&self.fd, state.mp_state).map_err(Error::RestoreState)?;
+
+        Ok(())
     }
 
     /// Runs the vCPU in KVM context and handles the kvm exit reason.
@@ -151,7 +171,14 @@ impl KvmVcpu {
 
 /// Structure holding VCPU kvm state.
 #[derive(Clone, Default)]
-pub struct VcpuState {}
+pub struct VcpuState {
+    pub mp_state: kvm_bindings::kvm_mp_state,
+    pub regs: Vec<kvm_bindings::kvm_one_reg>,
+    // We will be using the mpidr for passing it to the VmState.
+    // The VmState will give this away for saving restoring the icc and redistributor
+    // registers.
+    pub mpidr: u64,
+}
 
 #[cfg(test)]
 mod tests {
@@ -167,6 +194,12 @@ mod tests {
         vm.setup_irqchip(1).unwrap();
 
         (vm, vcpu, vm_mem)
+    }
+
+    fn init_vcpu(vcpu: &VcpuFd, vm: &VmFd) {
+        let mut kvi: kvm_bindings::kvm_vcpu_init = kvm_bindings::kvm_vcpu_init::default();
+        vm.get_preferred_target(&mut kvi).unwrap();
+        vcpu.vcpu_init(&kvi).unwrap();
     }
 
     #[test]
@@ -214,20 +247,28 @@ mod tests {
 
     #[test]
     fn test_vcpu_save_restore_state() {
-        let (_vm, vcpu, _mem) = setup_vcpu(0x1000);
+        let (vm, vcpu, _mem) = setup_vcpu(0x1000);
 
+        // Calling KVM_GET_REGLIST before KVM_VCPU_INIT will result in error.
         let res = vcpu.save_state();
         assert!(res.is_err());
         assert_eq!(
             res.err().unwrap().to_string(),
-            "Saving the state is not yet supported on this architecture".to_string()
+            "Failed to save the state of the vcpu: Failed to get X0 register: Exec format error (os error 8)".to_string()
         );
 
-        let res = vcpu.restore_state(&VcpuState::default());
-        assert!(res.is_err());
-        assert_eq!(
-            res.err().unwrap().to_string(),
-            "Restoring the state is not yet supported on this architecture".to_string()
-        );
+        init_vcpu(&vcpu.fd, &vm.fd());
+        let state = vcpu.save_state().expect("Cannot save state of vcpu");
+        assert!(!state.regs.is_empty());
+        vcpu.restore_state(&state)
+            .expect("Cannot restore state of vcpu");
+        let addr = vcpu
+            .fd
+            .get_one_reg(0x6030_0000_0010_003E)
+            .expect("Cannot get sp core register");
+        assert!(state.regs.contains(&kvm_bindings::kvm_one_reg {
+            id: 0x6030_0000_0010_003E,
+            addr
+        }));
     }
 }
