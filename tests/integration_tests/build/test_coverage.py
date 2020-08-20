6091e8e01a87e83dc2ba09a60bb1c06a28c4f483
Adrian Catangiu <acatan@amazon.com>
2020-08-20 15:07:14 +0300

    snapshot: add devices restore sanity checks
    Most device properties are validated when the device is built,
    whether it's built from specs or from snapshot. The virtio queues
    and configuration however are simply taken from the snapshot as
    they should theoretically have values configured during guest boot.
    
    We cannot fully validate snapshot contents without replaying the
    boot/device-configuration process, but we can do some basic sanity
    checks against some virtio properties.
    This commit adds said sanity checks.
    
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -23,7 +23,7 @@ import host_tools.proc as proc
 # this contains the frequency while on AMD it does not.
 # Checkout the cpuid crate. In the future other
 # differences may appear.
-COVERAGE_DICT = {"Intel": 84.50, "AMD": 84.45}
+COVERAGE_DICT = {"Intel": 84.56, "AMD": 84.55}
 PROC_MODEL = proc.proc_type()
 
 COVERAGE_MAX_DELTA = 0.05
