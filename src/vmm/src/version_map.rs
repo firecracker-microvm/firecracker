d3602debdd71809a00958fbcb1466f896063b09a
Diana Popa <dpopa@amazon.com>
2020-08-25 00:04:26 +0300

    aarch64: enable snapshot functionality
    This basically translates into removing x86_64 labels.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- src/vmm/src/version_map.rs
+++ src/vmm/src/version_map.rs
@@ -5,28 +5,19 @@
 
 use std::collections::HashMap;
 
-// Currently only supports x86_64.
-#[cfg(target_arch = "x86_64")]
 use crate::device_manager::persist::DeviceStates;
 
 use lazy_static::lazy_static;
 use versionize::VersionMap;
-#[cfg(target_arch = "x86_64")]
 use versionize::Versionize;
 
 lazy_static! {
     // Note: until we have a better design, this needs to be updated when the version changes.
     /// Static instance used for handling microVM state versions.
     pub static ref VERSION_MAP: VersionMap = {
-        #[cfg(target_arch = "x86_64")]
-        {
-            let mut version_map = VersionMap::new();
-            version_map.new_version().set_type_version(DeviceStates::type_id(), 2);
-            version_map
-        }
-
-        #[cfg(not(target_arch = "x86_64"))]
-        VersionMap::new()
+        let mut version_map = VersionMap::new();
+        version_map.new_version().set_type_version(DeviceStates::type_id(), 2);
+        version_map
     };
 
     /// Static instance used for creating a 1:1 mapping between Firecracker release version
