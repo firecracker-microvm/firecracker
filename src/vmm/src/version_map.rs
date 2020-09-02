4f41d3ee69dafd1fa8b590a2f0064a4c0e23a18b
George Pisaltu <gpl@amazon.com>
2020-09-02 18:50:17 +0300

    Set FC version to 0.24.0 for balloon version map
    Signed-off-by: George Pisaltu <gpl@amazon.com>

--- src/vmm/src/version_map.rs
+++ src/vmm/src/version_map.rs
@@ -5,13 +5,27 @@
 
 use std::collections::HashMap;
 
+// Currently only supports x86_64.
+#[cfg(target_arch = "x86_64")]
+use crate::device_manager::persist::DeviceStates;
+
 use lazy_static::lazy_static;
 use versionize::VersionMap;
+#[cfg(target_arch = "x86_64")]
+use versionize::Versionize;
 
 lazy_static! {
     // Note: until we have a better design, this needs to be updated when the version changes.
     /// Static instance used for handling microVM state versions.
     pub static ref VERSION_MAP: VersionMap = {
+        #[cfg(target_arch = "x86_64")]
+        {
+            let mut version_map = VersionMap::new();
+            version_map.new_version().set_type_version(DeviceStates::type_id(), 2);
+            version_map
+        }
+
+        #[cfg(not(target_arch = "x86_64"))]
         VersionMap::new()
     };
 
@@ -20,6 +34,7 @@ lazy_static! {
     pub static ref FC_VERSION_TO_SNAP_VERSION: HashMap<String, u16> = {
         let mut mapping = HashMap::new();
         mapping.insert(String::from("0.23.0"), 1);
+        mapping.insert(String::from("0.24.0"), 2);
 
         mapping
     };
