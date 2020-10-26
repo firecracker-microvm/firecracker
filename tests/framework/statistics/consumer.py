4600be0f2d6abab2cfc6860c149468afd861ed8a
Iulian Barbu <iul@amazon.com>
2020-10-26 20:11:50 +0200

    stats: propagate consumer processing result
    When pass criteria fail we need to map the failure to a specific
    Producer/Consumer pipe. These pipes reside in the statistics core.
    The core is the sole owner of the information regarding where
    a failure came from.
    
    Signed-off-by: Iulian Barbu <iul@amazon.com>

--- tests/framework/statistics/consumer.py
+++ tests/framework/statistics/consumer.py
@@ -4,9 +4,10 @@
 """Module for multiple statistics consumers."""
 
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import Any
 from collections import defaultdict
 from .types import MeasurementDef, StatisticDef
+from .criteria import Failed
 
 
 class Consumer(ABC):
@@ -69,33 +70,44 @@ class Consumer(ABC):
         """Set measurement definition."""
         self._measurements_defs[value.name] = value
 
-    def process(self) -> Dict[str, dict]:
-        """Generate statistics as a dictionary."""
-        # Verify that the statistics/measurements correspondence
-        # is backed by corresponding measurements definitions.
+    def _validate(self):
+        """Verify that the statistics/measurements correspondence...
+
+        is backed by corresponding measurements definitions.
+        """
         for ms_name in self._statistics_defs:
-            assert ms_name in self._measurements_defs
+            if ms_name not in self._measurements_defs:
+                return False
 
         if self._consume_stats:
             # Verify if the gathered results for a measurement are
             # backed by measurements definitions.
             for ms_name in self._results:
-                assert ms_name in self._measurements_defs
+                if ms_name not in self._measurements_defs:
+                    return False
                 # Verify if the gathered statistics are backed by
                 # statistics definitions.
                 for st_name in self._results[ms_name]:
-                    assert st_name in self._statistics_defs[ms_name]
+                    if st_name not in self._statistics_defs[ms_name]:
+                        return False
         else:
             # Verify if the gathered measurements are backed by
             # measurements definitions.
             for ms_name in self._results:
-                assert ms_name in self._measurements_defs
+                if ms_name not in self._measurements_defs:
+                    return False
 
             # Verify if the defined statistics have corresponding
             # gathered measurements.
             for ms_name in self._statistics_defs:
-                assert ms_name in self._results
+                if ms_name not in self._results:
+                    return False
 
+        return True
+
+    def process(self) -> (dict, dict):
+        """Generate statistics as a dictionary."""
+        assert self._validate()
         # Generate consumer stats.
         for ms_name in self._statistics_defs:
             self._statistics.setdefault(ms_name, {})[self.UNIT_KEY] \
@@ -114,7 +126,10 @@ class Consumer(ABC):
                 # Check pass criteria.
                 if stat.criteria:
                     res = self._statistics[ms_name][st_name]
-                    stat.criteria.check(res)
+                    try:
+                        stat.criteria.check(res)
+                    except Failed as err:
+                        raise Failed(msg=f"'{ms_name}/{st_name}': {err.msg}")
 
         return self._statistics, self._custom
 
