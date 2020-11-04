2a4019ac47426edbbc97ac5a4bc54b9ef5bc3cca
Diana Popa <dpopa@amazon.com>
2020-11-04 12:43:29 +0200

    CI: move build independent tests to a folder
    Some of our integration tests can very well be run on a
    single platform.
    In order to do that all the build independent tests were
    moved to a separate folder and conftest was adjusted to ignore
    this folder on other platforms different than AMD.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -40,43 +40,6 @@ KCOV_TOTAL_LINES_REGEX = r'"total_lines" : "(\d+)"'
 """Regex for extracting number of total executable lines found by kcov."""
 
 
-@pytest.mark.timeout(120)
-@pytest.mark.skipif(
-    platform.machine() != "x86_64",
-    reason="no need to test it on multiple platforms"
-)
-def test_ensure_mod_tests():
-    """Check that files containing unit tests have a 'tests' module defined."""
-    # List all source files containing rust #[test] attribute,
-    # (excluding generated files and integration test directories).
-    # Take the list and check each file contains 'mod tests {', output file
-    # name if it doesn't.
-    cmd = (
-        '/bin/bash '
-        '-c '
-        '"grep '
-        '--files-without-match '
-        '\'mod tests {\' '
-        '\\$(grep '
-        '--files-with-matches '
-        '--recursive '
-        '--exclude-dir=src/*_gen/* '
-        '\'\\#\\[test\\]\' ../src/*/src)" '
-    )
-
-    # The outer grep returns 0 even if it finds files without the match, so we
-    # ignore the return code.
-    result = utils.run_cmd(cmd, no_shell=False, ignore_return_code=True)
-
-    error_msg = (
-        'Tests found in files without a "tests" module:\n {}'
-        'To ensure code coverage is reported correctly, please check that '
-        'your tests are in a module named "tests".'.format(result.stdout)
-    )
-
-    assert not result.stdout, error_msg
-
-
 @pytest.mark.timeout(400)
 @pytest.mark.skipif(
     platform.machine() != "x86_64",
