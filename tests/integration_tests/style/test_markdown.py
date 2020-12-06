43953bbbac4daf63bbb4c35cfa89d3be9ec47a9f
Amani Duncker <amaniduncker@utexas.edu>
2020-12-05 18:04:56 -0600

    first commit

--- tests/integration_tests/style/test_markdown.py
+++ tests/integration_tests/style/test_markdown.py
@@ -1,29 +1,43 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Tests ensuring codebase style compliance for Markdown files."""
 import os
 import framework.utils as utils
 
 
 def _validate_markdown(filename):
-    if filename.endswith(('.md')):
-        with open(filename) as file:
-            for line in file:
-                #check line length
-                if len(line) >= 80:
-                    print("length of the line too long ", line) 
-                    return False
-                #check trailing whitespace or tab
-                if line.endswith(' ') or line.endswith('\t'):
-                    print("trailing white space ", line)
-                    return False
-                #check hard tab
-                if '\t' in line:
-                    print("hard tab ", line)
-                    return False
-    return True
+    errors = []
+    linenum = 0;
+    with open(filename, 'r+', encoding='utf-8') as file:
+        for line in file:
+            #check line length
+            if len(line) > 80:
+                errors.append("length of the line too long: line number " + str(linenum))
+            #check trailing whitespace or tab
+            if line.endswith(' ') or line.endswith('\t'):
+                errors.append("trailing white space: line number "+ str(linenum))
+            #check hard tab
+            if '\t' in line:
+                errors.append("hard tab: line number "+ str(linenum))
+            linenum += 1
+    return '\n'.join(errors)
 
 def test_markdown_style():
     """Fail if a file violates markdown style."""
-    for subdir, _, files in os.walk(os.getcwd()):
-        for file in files:
-            filepath = os.path.join(subdir, file)
-            assert _validate_markdown(filepath) is True, "%s has invalid markdown" % filepath
+    errors = {}
+    # Get all *.py files from the project
+    markdown_files = utils.get_files_from(
+        find_path="..",
+        pattern="*.md",
+        exclude_names=["build"])
+
+    for filepath in markdown_files:
+        print(filepath)
+        res = _validate_markdown(filepath)
+        if len(res) > 0:
+            errors.update({filepath:res})
+            #print(filepath + " has invalid markdown\n" + res)
+    for file in errors:
+        print(file + "\n " + errors[file])
+    print(str(len(errors)) + " files have markdown errors")
+    assert len(errors) == 0 
