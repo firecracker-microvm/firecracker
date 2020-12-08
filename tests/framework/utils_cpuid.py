1fe9514b0c1b2012091af34c8979dcc4e6fd45c6
Diana Popa <dpopa@amazon.com>
2020-12-08 21:07:28 +0200

    CI: move function to utils_cpuid
    Also renamed the functions to something more
    suggestive.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/framework/utils_cpuid.py
+++ tests/framework/utils_cpuid.py
@@ -6,6 +6,7 @@ import subprocess
 from enum import Enum, auto
 
 from framework.utils import run_cmd
+import host_tools.network as net_tools
 
 
 class CpuVendor(Enum):
@@ -29,3 +30,45 @@ def get_cpu_model_name():
     info = stdout.strip().split(sep=":")
     assert len(info) == 2
     return info[1].strip()
+
+
+def check_guest_cpuid_output(vm, guest_cmd, expected_header,
+                             expected_separator,
+                             expected_key_value_store):
+    """Parse cpuid output inside guest and match with expected one."""
+    ssh_connection = net_tools.SSHConnection(vm.ssh_config)
+    _, stdout, stderr = ssh_connection.execute_command(guest_cmd)
+
+    assert stderr.read() == ''
+    while True:
+        line = stdout.readline()
+        if line != '':
+            # All the keys have been matched. Stop.
+            if not expected_key_value_store:
+                break
+
+            # Try to match the header if needed.
+            if expected_header not in (None, ''):
+                if line.strip() == expected_header:
+                    expected_header = None
+                continue
+
+            # See if any key matches.
+            # We Use a try-catch block here since line.split() may fail.
+            try:
+                [key, value] = list(
+                    map(lambda x: x.strip(), line.split(expected_separator)))
+            except ValueError:
+                continue
+
+            if key in expected_key_value_store.keys():
+                assert value == expected_key_value_store[key], \
+                    "%s does not have the expected value" % key
+                del expected_key_value_store[key]
+
+        else:
+            break
+
+    assert not expected_key_value_store, \
+        "some keys in dictionary have not been found in the output: %s" \
+        % expected_key_value_store
