7bfd66a9b0494c0a1d2e4677551674e429ddcbee
Diana Popa <dpopa@amazon.com>
2020-12-09 15:58:46 +0200

    CI: add mprotect to the whitelist syscalls
    Upon exec, a mprotect call is triggered. Since
    this syscall is not whitelisted the CI would fail
    with "Bad System Call". This is needed only in the CI.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/security/demo_seccomp/src/bin/demo_malicious.rs
+++ tests/integration_tests/security/demo_seccomp/src/bin/demo_malicious.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 fn main() {
     unsafe {
-        // In this example, the malicious component is outputing to standard input.
+        // In this example, the malicious component is outputting to standard input.
         libc::syscall(libc::SYS_write, libc::STDIN_FILENO, "Hello, world!\n", 14);
     }
 }
