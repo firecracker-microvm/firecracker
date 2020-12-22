3393737167099a3b7ba8083eb614088592545eb4
Diana Popa <dpopa@amazon.com>
2020-12-22 10:44:16 +0200

    CI: add git commit message linter
    Fixes #1231
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

---
+++ tests/framework/gitlint_rules.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""The user defined rules for gitlint."""
+
+from gitlint.rules import CommitRule, RuleViolation
+
+
+class SignedOffBy(CommitRule):
+    """Make sure that each commit contains a "Signed-off-by" line."""
+
+    # The name of the rule.
+    name = "body-requires-signed-off-by"
+
+    # The unique id of the rule.
+    id = "UC2"
+
+    def validate(self, commit):
+        """Validate user defined gitlint rules."""
+        for line in commit.message.body:
+            if line.startswith("Signed-off-by"):
+                return []
+
+        msg = "Body does not contain a 'Signed-off-by' line"
+        return [RuleViolation(self.id, msg, line_nr=1)]
