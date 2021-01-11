9e5861a10972d2f06e9e229c895a4a84de5207e5
Iulian Barbu <iul@amazon.com>
2021-01-11 16:32:42 +0200

    net: refactored throughput test
    * Used statistics defs compacted interface.
    
    Signed-off-by: Iulian Barbu <iul@amazon.com>

--- tests/integration_tests/performance/configs/network_tcp_throughput_test_config.py
+++ tests/integration_tests/performance/configs/network_tcp_throughput_test_config.py
@@ -3,6 +3,8 @@
 """Configuration file for the network TCP throughput test."""
 
 # pylint: disable=C0302
+from framework.statistics.types import DefaultMeasurement
+
 DEBUG = False
 
 IPERF3 = "iperf3"
@@ -12,11 +14,11 @@ DURATION = "duration"
 DURATION_TOTAL = "total"
 RETRANSMITS = "retransmits"
 RETRANSMITS_TOTAL = "total"
-CPU_UTILIZATION_HOST = "cpu_utilization_host"
-CPU_UTILIZATION_GUEST = "cpu_utilization_guest"
 BASE_PORT = 5000
-CPU_UTILIZATION_VMM_TAG = "vmm"
-CPU_UTILIZATION_VCPUS_TOTAL_TAG = "vcpus_total"
+CPU_UTILIZATION_VMM = \
+    f"{DefaultMeasurement.CPU_UTILIZATION_VMM.name.lower()}"
+CPU_UTILIZATION_VCPUS_TOTAL = \
+    f"{DefaultMeasurement.CPU_UTILIZATION_VCPUS_TOTAL.name.lower()}"
 IPERF3_CPU_UTILIZATION_PERCENT_OUT_TAG = "cpu_utilization_percent"
 IPERF3_END_RESULTS_TAG = "end"
 DEBUG_CPU_UTILIZATION_VMM_SAMPLES_TAG = "cpu_utilization_vmm_samples"
