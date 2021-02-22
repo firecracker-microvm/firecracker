35eed741187750c564783ce716ac84d071bec625
alindima <alindima@amazon.com>
2021-02-22 13:09:43 +0200

    seccomp: refactor package structure
    Refactor the general seccomp package structure, in order
    to restrict public access to the seccompiler internals.
    
    Previously, the library crate was solely in the lib.rs file,
    and it was impossible to restrict the public visibility of the
    unneeded structures, since the binary crate from the same folder
    needed access to those data structures.
    
    Now, the items that need to be exported to the public library are
    still in the lib.rs file. The other internal functionality and
    data types were moved to backend.rs.
    
    Data types that are common between the lib and bin crates are now
    in the common.rs file.
    
    This achieves a minimal lib.rs file, exposing the desired
    helper functions and nothing more.
    
    To summarise the folder structure:
    
    - lib.rs -> public lib interface (with helper functions used by FC)
    - common.rs -> data types used by both the bin and lib crates.
    - seccompiler.rs -> main() binary, for compiling JSON into BPF.
    - backend.rs -> used by the binary for bpf generation from IR.
    - compiler.rs -> used by the binary for driving the enire compilation
    - syscall_table module -> used by the binary for mapping syscall
            names to the arch-specific number.
    
    Also restricted the pub to pub(crate) or private, where applicable.
    
    Also updated the respective cargo-docs.
    
    Signed-off-by: alindima <alindima@amazon.com>

---
+++ src/seccomp/src/common.rs
@@ -0,0 +1,29 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Module that defines common data structures used by both the library crate
+//! and the seccompiler binary.
+
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+/// The maximum seccomp-BPF program length allowed by the linux kernel.
+pub(crate) const BPF_MAX_LEN: usize = 4096;
+
+/// BPF instruction structure definition.
+/// See /usr/include/linux/filter.h .
+#[repr(C)]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+#[doc(hidden)]
+pub struct sock_filter {
+    pub code: ::std::os::raw::c_ushort,
+    pub jt: ::std::os::raw::c_uchar,
+    pub jf: ::std::os::raw::c_uchar,
+    pub k: ::std::os::raw::c_uint,
+}
+
+/// Program made up of a sequence of BPF instructions.
+pub type BpfProgram = Vec<sock_filter>;
+
+/// Type that associates a thread category to a BPF program.
+pub type BpfThreadMap = HashMap<String, BpfProgram>;
