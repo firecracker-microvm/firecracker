d3bf8af93ceb20260e7117570bb7386a754b6d3a
alindima <alindima@amazon.com>
2021-02-24 10:42:01 +0200

    remove outdated comments about seccomp-level
    Signed-off-by: alindima <alindima@amazon.com>

--- src/vmm/src/vstate/vcpu/mod.rs
+++ src/vmm/src/vstate/vcpu/mod.rs
@@ -264,7 +264,7 @@ impl Vcpu {
     /// anything useful.
     pub fn run(&mut self, seccomp_filter: BpfProgramRef) {
         // Load seccomp filters for this vCPU thread.
-        // Execution panics if filters cannot be loaded, use --seccomp-level=0 if skipping filters
+        // Execution panics if filters cannot be loaded, use --no-seccomp if skipping filters
         // altogether is the desired behaviour.
         if let Err(e) = seccomp::apply_filter(seccomp_filter) {
             panic!(
