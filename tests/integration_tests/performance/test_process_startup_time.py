86d8bf3c2b6936e6155359a542568b148bd6a27e
alindima <alindima@amazon.com>
2021-02-24 10:55:04 +0200

    add perf startup time test for custom seccomp
    Signed-off-by: alindima <alindima@amazon.com>

--- tests/integration_tests/performance/test_process_startup_time.py
+++ tests/integration_tests/performance/test_process_startup_time.py
@@ -5,7 +5,7 @@
 import json
 import os
 import platform
-import time
+import framework.utils as utils
 
 import host_tools.logging as log_tools
 
@@ -14,13 +14,7 @@ MAX_STARTUP_TIME_CPU_US = {'x86_64': 5500, 'aarch64': 2600}
 # TODO: Keep a `current` startup time in S3 and validate we don't regress
 
 
-def test_startup_time(test_microvm_with_api):
-    """Check the startup time for jailer and Firecracker up to socket bind."""
-    microvm = test_microvm_with_api
-    microvm.spawn()
-
-    microvm.basic_config(vcpu_count=2, mem_size_mib=1024)
-
+def _startup_time_util(microvm):
     # Configure metrics.
     metrics_fifo_path = os.path.join(microvm.path, 'metrics_fifo')
     metrics_fifo = log_tools.Fifo(metrics_fifo_path)
@@ -31,7 +25,6 @@ def test_startup_time(test_microvm_with_api):
     assert microvm.api_session.is_status_no_content(response.status_code)
 
     microvm.start()
-    time.sleep(0.4)
 
     # The metrics fifo should be at index 1.
     # Since metrics are flushed at InstanceStart, the first line will suffice.
@@ -40,7 +33,57 @@ def test_startup_time(test_microvm_with_api):
     startup_time_us = metrics['api_server']['process_startup_time_us']
     cpu_startup_time_us = metrics['api_server']['process_startup_time_cpu_us']
 
+    return startup_time_us, cpu_startup_time_us
+
+
+def _custom_filter_setup(test_microvm):
+    bpf_path = os.path.join(test_microvm.path, 'bpf.out')
+
+    cargo_target = '{}-unknown-linux-musl'.format(platform.machine())
+    json_path = '../resources/seccomp/{}.json'.format(cargo_target)
+
+    cmd = 'cargo run -p seccomp --target {} -- --input-file {} --target-arch\
+        {} --output-file {}'.format(cargo_target, json_path,
+                                    platform.machine(), bpf_path)
+
+    utils.run_cmd(cmd)
+
+    test_microvm.create_jailed_resource(bpf_path)
+    test_microvm.jailer.extra_args.update({"seccomp-filter": 'bpf.out'})
+
+
+def test_startup_time(test_microvm_with_api):
+    """Check the startup time for jailer and Firecracker up to socket bind."""
+    microvm = test_microvm_with_api
+    microvm.spawn()
+
+    microvm.basic_config(vcpu_count=2, mem_size_mib=1024)
+
+    startup_time_us, cpu_startup_time_us = _startup_time_util(microvm)
+
     print('Process startup time is: {} us ({} CPU us)'
+          .format(startup_time_us, cpu_startup_time_us))
+
+    assert cpu_startup_time_us > 0
+    assert cpu_startup_time_us <= MAX_STARTUP_TIME_CPU_US[platform.machine()]
+
+
+def test_startup_time_custom_seccomp(test_microvm_with_api):
+    """Check the startup time for jailer and Firecracker up to socket bind, ...
+
+    when using custom seccomp filters via the `--seccomp-filter` param.
+    """
+    microvm = test_microvm_with_api
+
+    _custom_filter_setup(microvm)
+
+    microvm.spawn()
+
+    microvm.basic_config(vcpu_count=2, mem_size_mib=1024)
+
+    startup_time_us, cpu_startup_time_us = _startup_time_util(microvm)
+
+    print('Process startup time with custom seccomp is: {} us ({} CPU us)'
           .format(startup_time_us, cpu_startup_time_us))
 
     assert cpu_startup_time_us > 0
