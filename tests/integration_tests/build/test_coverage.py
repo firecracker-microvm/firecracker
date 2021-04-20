6292f3289a8a5acdbd15d7e042a89ece95c31fb4
YUAN LYU <lyuyuan92@gmail.com>
2021-04-20 09:08:06 -0400

    Use micro-http from firecracker-microvm/micro-http
    Signed-off-by: YUAN LYU <lyuyuan92@gmail.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -23,7 +23,7 @@ import host_tools.proc as proc
 # this contains the frequency while on AMD it does not.
 # Checkout the cpuid crate. In the future other
 # differences may appear.
-COVERAGE_DICT = {"Intel": 85.24, "AMD": 84.54, "ARM": 83.35}
+COVERAGE_DICT = {"Intel": 84.73, "AMD": 83.98, "ARM": 82.91}
 
 PROC_MODEL = proc.proc_type()
 
