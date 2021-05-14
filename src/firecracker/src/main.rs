d8b6819cb4afa9942783fa6aba0fc99d6b78da87
alindima <alindima@amazon.com>
2021-05-14 16:42:34 +0300

    build_microvm() no longer deletes seccomp filters
    Previously, the InstanceStart request, was using a mutable
    reference of the seccomp filter collection, removing the
    entries for each thread, after they were launched.
    
    If a recoverable error occured, that did not bring down
    the process, because the BPF map was mutated, a subsequent
    InstanceStart would fail because the filters would have
    already been freed.
    
    This commit fixes this, by changing the mutable reference
    on the PrebootAPIController to be a regular reference,
    kept valid for the entire lifetime of the preboot controller
    (until the instance is started).
    
    Signed-off-by: alindima <alindima@amazon.com>

--- src/firecracker/src/main.rs
+++ src/firecracker/src/main.rs
@@ -290,12 +290,12 @@ fn main() {
             boot_timer_enabled,
         );
     } else {
-        let mut seccomp_filters: BpfThreadMap = seccomp_filters
+        let seccomp_filters: BpfThreadMap = seccomp_filters
             .into_iter()
             .filter(|(k, _)| k != "api")
             .collect();
         run_without_api(
-            &mut seccomp_filters,
+            &seccomp_filters,
             vmm_config_json,
             &instance_info,
             boot_timer_enabled,
@@ -367,7 +367,7 @@ fn print_snapshot_data_format(snapshot_path: &str) {
 
 // Configure and start a microVM as described by the command-line JSON.
 fn build_microvm_from_json(
-    seccomp_filters: &mut BpfThreadMap,
+    seccomp_filters: &BpfThreadMap,
     event_manager: &mut EventManager,
     config_json: String,
     instance_info: &InstanceInfo,
@@ -396,7 +396,7 @@ fn build_microvm_from_json(
 }
 
 fn run_without_api(
-    seccomp_filters: &mut BpfThreadMap,
+    seccomp_filters: &BpfThreadMap,
     config_json: Option<String>,
     instance_info: &InstanceInfo,
     bool_timer_enabled: bool,
