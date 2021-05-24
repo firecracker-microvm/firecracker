91b01f90da187bce4f56e26520f592ca8fc867f3
George Pisaltu <gpl@amazon.com>
2021-05-24 12:44:56 +0300

    added integration test for ssbd mitigation
    Signed-off-by: George Pisaltu <gpl@amazon.com>
    Signed-off-by: alindima <alindima@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -24,7 +24,7 @@ import host_tools.proc as proc
 # this contains the frequency while on AMD it does not.
 # Checkout the cpuid crate. In the future other
 # differences may appear.
-COVERAGE_DICT = {"Intel": 85.36, "AMD": 84.79, "ARM": 83.62}
+COVERAGE_DICT = {"Intel": 85.36, "AMD": 84.79, "ARM": 83.52}
 
 PROC_MODEL = proc.proc_type()
 
