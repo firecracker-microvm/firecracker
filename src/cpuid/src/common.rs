551ac00f4441b17ffc1beb0d3d2373af63fa91e3
George Pisaltu <gpl@amazon.com>
2021-06-04 12:23:37 +0300

    cpuid: add function for identifying same cpu model
    Signed-off-by: George Pisaltu <gpl@amazon.com>

--- src/cpuid/src/common.rs
+++ src/cpuid/src/common.rs
@@ -1,6 +1,7 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::bit_helper::BitHelper;
 use crate::cpu_leaf::*;
 #[cfg(target_arch = "x86_64")]
 use kvm_bindings::CpuId;
@@ -91,6 +92,70 @@ pub fn get_vendor_id_from_cpuid(cpuid: &CpuId) -> Result<[u8; 12], Error> {
     Err(Error::NotSupported)
 }
 
+/// Validates that the provided CPUID belongs to a CPU of the same
+/// model as the host's.
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub fn is_same_model(cpuid: &CpuId) -> bool {
+    // Try to get the vendor IDs from the host and the CPUID struct.
+    if let (Ok(host_vendor_id), Ok(cpuid_vendor_id)) =
+        (get_vendor_id_from_host(), get_vendor_id_from_cpuid(cpuid))
+    {
+        // If the vendor IDs aren't the same, the CPUs are not identical.
+        if host_vendor_id != cpuid_vendor_id {
+            return false;
+        }
+    } else {
+        // This only fails when CPUID is not supported, in which case
+        // we can't tell if the CPUs are identical.
+        return false;
+    }
+
+    // Try to get the feature information leaf from the host CPUID.
+    let host_feature_info_leaf = get_cpuid(leaf_0x1::LEAF_NUM, 0);
+
+    // The relevant information for this comparison is in the EAX register.
+    let host_feature_info_leaf_eax = match host_feature_info_leaf {
+        Ok(leaf) => leaf.eax,
+        Err(_) => {
+            // If this fails, we can't tell if the CPUs are identical.
+            return false;
+        }
+    };
+
+    // Search for the entry for leaf0x1.
+    let feature_info_leaf = cpuid
+        .as_slice()
+        .iter()
+        .find(|entry| entry.function == leaf_0x1::LEAF_NUM);
+
+    // The relevant information is in EAX.
+    let feature_info_leaf_eax = match feature_info_leaf {
+        Some(leaf) => leaf.eax,
+        None => {
+            // Fail fast if we can't retrieve the relevant
+            // information from CPUID.
+            return false;
+        }
+    };
+
+    // Validate that all of these properties are the same.
+    for elem in &[
+        leaf_0x1::eax::EXTENDED_FAMILY_ID_BITRANGE,
+        leaf_0x1::eax::EXTENDED_PROCESSOR_MODEL_BITRANGE,
+        leaf_0x1::eax::PROCESSOR_FAMILY_BITRANGE,
+        leaf_0x1::eax::PROCESSOR_MODEL_BITRANGE,
+        leaf_0x1::eax::STEPPING_BITRANGE,
+    ] {
+        if feature_info_leaf_eax.read_bits_in_range(elem)
+            != host_feature_info_leaf_eax.read_bits_in_range(elem)
+        {
+            return false;
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 pub mod tests {
     use crate::common::*;
@@ -146,5 +211,43 @@ pub mod tests {
         let vendor_id = get_vendor_id_from_host();
         assert!(vendor_id.is_ok());
         matches!(&vendor_id.ok().unwrap(), VENDOR_ID_INTEL | VENDOR_ID_AMD);
+    }
+
+    #[test]
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    fn test_is_same_model() {
+        let mut curr_cpuid = CpuId::new(2).unwrap();
+
+        // Add the vendor ID leaf.
+        let vendor = get_cpuid(0x0, 0).unwrap();
+        curr_cpuid.as_mut_slice()[0].function = 0x0;
+        curr_cpuid.as_mut_slice()[0].index = 0;
+        curr_cpuid.as_mut_slice()[0].ebx = vendor.ebx;
+        curr_cpuid.as_mut_slice()[0].ecx = vendor.ecx;
+        curr_cpuid.as_mut_slice()[0].edx = vendor.edx;
+
+        // Add the feature info leaf.
+        let feature_info = get_cpuid(0x1, 0).unwrap();
+        curr_cpuid.as_mut_slice()[1].function = 0x1;
+        curr_cpuid.as_mut_slice()[1].index = 0;
+        curr_cpuid.as_mut_slice()[1].eax = feature_info.eax;
+
+        assert!(is_same_model(&curr_cpuid));
+
+        let mut diff_vendor_cpuid = curr_cpuid.clone();
+        for mut entry in diff_vendor_cpuid.as_mut_slice() {
+            if entry.function == 0x0 && entry.index == 0 {
+                entry.ebx = 0xFFFF_FFFF;
+            }
+        }
+        assert!(!is_same_model(&diff_vendor_cpuid));
+
+        let mut diff_feature_cpuid = curr_cpuid;
+        for mut entry in diff_feature_cpuid.as_mut_slice() {
+            if entry.function == 0x1 && entry.index == 0 {
+                entry.eax ^= 0x1;
+            }
+        }
+        assert!(!is_same_model(&diff_feature_cpuid));
     }
 }
