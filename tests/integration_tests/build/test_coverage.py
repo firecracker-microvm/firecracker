2a5e23ed48886da6f256179f16ab6b066b3e4fdc
AlexandruCihodaru <cihodar@amazon.com>
2021-09-18 12:34:05 +0300

    [CI]: Updated coverage
    Signed-off-by: AlexandruCihodaru <cihodar@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -24,7 +24,7 @@ import host_tools.proc as proc
 # this contains the frequency while on AMD it does not.
 # Checkout the cpuid crate. In the future other
 # differences may appear.
-COVERAGE_DICT = {"Intel": 84.79, "AMD": 84.19, "ARM": 82.96}
+COVERAGE_DICT = {"Intel": 84.73, "AMD": 84.13, "ARM": 82.83}
 
 PROC_MODEL = proc.proc_type()
 
