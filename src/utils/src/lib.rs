511759a1094dc74276d0cbeb8dda390b3966453f
alindima <alindima@amazon.com>
2021-10-12 13:08:18 +0000

    io_uring: verify supported features and ops
    Signed-off-by: alindima <alindima@amazon.com>

--- src/utils/src/lib.rs
+++ src/utils/src/lib.rs
@@ -6,7 +6,8 @@
 // More specifically, we are re-exporting modules from `vmm_sys_util` as part
 // of the `utils` crate.
 pub use vmm_sys_util::{
-    epoll, errno, eventfd, fam, ioctl, rand, syscall, tempdir, tempfile, terminal,
+    epoll, errno, eventfd, fam, generate_fam_struct_impl, ioctl, rand, syscall, tempdir, tempfile,
+    terminal,
 };
 pub use vmm_sys_util::{ioctl_expr, ioctl_ioc_nr, ioctl_iow_nr};
 
