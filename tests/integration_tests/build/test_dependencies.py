63dd6ad5a78acf1aafbf8866fa401044dd5c5ee3
Diana Popa <dpopa@amazon.com>
2021-10-20 18:57:28 +0300

    CI: enforce dependency check
    We have created a file at tests/framework/dependencies.txt
    where we hold the current build dependencies. The test
    fails if the number of dependencies is bigger and displays
    the difference.
    
    The test also fails if the expected dependencies are different
    than the current ones. This mechanism ensures that the list of
    dependencies stays up to date and that whenever a dependency
    is removed we reset our dependency number.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/build/test_dependencies.py
+++ tests/integration_tests/build/test_dependencies.py
@@ -3,6 +3,8 @@
 """Enforces controls over dependencies."""
 
 import os
+import ast
+import pytest
 import framework.utils as utils
 
 
@@ -21,3 +23,46 @@ def test_licenses():
     )
     utils.run_cmd('cargo deny --manifest-path {} check licenses'.
                   format(toml_file))
+
+
+@pytest.mark.parametrize(
+    "dep_file",
+    ["framework/dependencies.txt"]
+)
+def test_num_dependencies(dep_file):
+    """Enforce minimal dependency check.
+
+    @type: build
+    """
+    _, stdout, _ = utils.run_cmd('cargo tree --prefix none -e no-dev '
+                                 '--workspace')
+    deps = stdout.splitlines()
+
+    current_deps = set()
+    # cargo tree displays a tree of dependencies which means
+    # some of them will repeat. Below is a mechanism for filtering
+    # unique dependencies.
+    # cargo tree tries to display a (*) at the end of each dependency that
+    # was already encountered but it does not do very well (libc appears
+    # multiple times).
+    for line in deps:
+        if line and "(*)" not in line:
+            current_deps.add(line)
+
+    # Use the code below to update the expected dependencies.
+    # with open(dep_file, "w") as prev_deps:
+    #     prev_deps.write(str(current_deps))
+
+    with open(dep_file) as prev_deps:
+        prev_deps = ast.literal_eval(prev_deps.read())
+    if len(current_deps) > len(prev_deps):
+        difference = current_deps - set(prev_deps)
+        msg = "The number of build dependencies has increased." \
+              " Is this expected? New dependencies {}".\
+            format(list(difference))
+        assert False, msg
+    elif len(current_deps) != len(prev_deps):
+        msg = "The build dependencies have changed." \
+              " Use the code above to modify the {} file.". \
+            format(dep_file)
+        assert False, msg
