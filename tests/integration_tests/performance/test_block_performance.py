7d0cf4aa7768cbf36cc3f0f639f378a7486425da
Diana Popa <dpopa@amazon.com>
2022-01-26 19:55:23 +0200

    [perf][block5.10] add io_uring baselines
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/performance/test_block_performance.py
+++ tests/integration_tests/performance/test_block_performance.py
@@ -274,6 +274,8 @@ def test_block_performance_async(bin_cloner_path, results_file_dumper):
     kernel_artifacts = ArtifactSet(artifacts.kernels())
     disk_artifacts = ArtifactSet(artifacts.disks(keyword="ubuntu"))
 
+    logger.info("Testing on processor %s", get_cpu_model_name())
+
     # Create a test context and add builder, logger, network.
     test_context = TestContext()
     test_context.custom = {
