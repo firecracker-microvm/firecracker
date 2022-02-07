98c4ddae7c877a3bb5984cb9a66ff77d6bfadb58
Serban Iorga <seriorga@amazon.com>
2022-02-07 14:52:24 +0200

    use rebase_snap in CI
    Signed-off-by: Serban Iorga <seriorga@amazon.com>

--- tests/host_tools/snapshot_helper.py
+++ tests/host_tools/snapshot_helper.py
@@ -1,12 +1,10 @@
 # Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Utility functions for snapshot testing."""
-from framework.utils import run_cmd
+from host_tools.cargo_build import run_rebase_snap_bin
 
 
 # Merges layer on top of base.
-def merge_memory_bitmaps(base, layer, block_size=4096):
+def merge_memory_bitmaps(base, layer):
     """Merge a sparse layer on top of base."""
-    dd_command = 'dd bs={} if={} of={} conv=sparse,notrunc'
-    dd_command = dd_command.format(block_size, layer, base)
-    _ = run_cmd(dd_command)
+    run_rebase_snap_bin(base, layer)
