3d1767100cc9fbec63bea19b392a1a8e2716f3c8
alindima <alindima@amazon.com>
2022-03-07 10:21:19 +0000

    increase coverage target
    Signed-off-by: alindima <alindima@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -29,9 +29,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 85.08, "AMD": 84.57, "ARM": 84.37}
+    COVERAGE_DICT = {"Intel": 85.18, "AMD": 84.66, "ARM": 84.36}
 else:
-    COVERAGE_DICT = {"Intel": 82.10, "AMD": 81.58, "ARM": 81.33}
+    COVERAGE_DICT = {"Intel": 82.2, "AMD": 81.68, "ARM": 81.31}
 
 PROC_MODEL = proc.proc_type()
 
