e089d22da8e4d852220f2a053f3681638a325971
Luminita Voicu <lumivo@amazon.com>
2022-04-12 15:45:55 +0300

    tests: update coverage
    Signed-off-by: Luminita Voicu <lumivo@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -29,9 +29,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 85.18, "AMD": 84.66, "ARM": 84.42}
+    COVERAGE_DICT = {"Intel": 84.97, "AMD": 84.46, "ARM": 84.1}
 else:
-    COVERAGE_DICT = {"Intel": 82.2, "AMD": 81.68, "ARM": 81.44}
+    COVERAGE_DICT = {"Intel": 82.01, "AMD": 81.51, "ARM": 81.1}
 
 PROC_MODEL = proc.proc_type()
 
