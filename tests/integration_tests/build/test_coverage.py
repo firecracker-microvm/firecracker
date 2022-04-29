37372914e55b38624408a547142d161cba463679
Babis Chalios <bchalios@amazon.es>
2022-04-29 13:29:58 +0000

    CI: update code coverage
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -29,9 +29,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 84.82, "AMD": 84.32, "ARM": 83.91}
+    COVERAGE_DICT = {"Intel": 84.89, "AMD": 84.38, "ARM": 84.06}
 else:
-    COVERAGE_DICT = {"Intel": 81.86, "AMD": 81.36, "ARM": 80.90}
+    COVERAGE_DICT = {"Intel": 81.89, "AMD": 81.43, "ARM": 81.05}
 
 PROC_MODEL = proc.proc_type()
 
