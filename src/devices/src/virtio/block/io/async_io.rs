4b716392a1a20712bdead0950b48da9a145c82eb
Matthew Schlebusch <schlebus@amazon.com>
2022-05-04 09:14:40 +0100

    Streamline dev-preview logging
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

--- src/devices/src/virtio/block/io/async_io.rs
+++ src/devices/src/virtio/block/io/async_io.rs
@@ -11,7 +11,7 @@ use io_uring::{
     Error as IoUringError, IoUring,
 };
 
-use logger::{warn, DEV_PREVIEW_LOG_PREFIX};
+use logger::log_dev_preview_warning;
 use utils::eventfd::EventFd;
 use vm_memory::{mark_dirty_mem, GuestAddress, GuestMemory, GuestMemoryMmap};
 
@@ -66,10 +66,7 @@ impl<T> WrappedUserData<T> {
 
 impl<T> AsyncFileEngine<T> {
     pub fn from_file(file: File) -> Result<AsyncFileEngine<T>, Error> {
-        warn!(
-            "{} {}",
-            DEV_PREVIEW_LOG_PREFIX, "Async file IO is in development preview."
-        );
+        log_dev_preview_warning("Async file IO", Option::None);
 
         let completion_evt = EventFd::new(libc::EFD_NONBLOCK).map_err(Error::EventFd)?;
         let ring = IoUring::new(
