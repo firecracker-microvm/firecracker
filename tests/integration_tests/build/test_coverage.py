a31b826b8d1e4a1ae5011f65ba76ffd5539537be
Matthew Schlebusch <schlebus@amazon.com>
2022-06-10 05:50:36 +0100

    Adjust coverage thresholds for ARM tests
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -29,9 +29,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 85.09, "AMD": 84.51, "ARM": 84.07}
+    COVERAGE_DICT = {"Intel": 85.09, "AMD": 84.51, "ARM": 84.00}
 else:
-    COVERAGE_DICT = {"Intel": 82.03, "AMD": 81.5, "ARM": 81.0}
+    COVERAGE_DICT = {"Intel": 82.03, "AMD": 81.5, "ARM": 80.94}
 
 PROC_MODEL = proc.proc_type()
 
