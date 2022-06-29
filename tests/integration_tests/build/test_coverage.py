0f3abfc8b1124d84a297f61a1d3ef48d46f91773
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2022-06-29 09:42:20 +0300

    Updated test coverage
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -29,9 +29,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 84.83, "AMD": 84.32, "ARM": 84.00}
+    COVERAGE_DICT = {"Intel": 84.77, "AMD": 84.26, "ARM": 83.86}
 else:
-    COVERAGE_DICT = {"Intel": 81.89, "AMD": 81.37, "ARM": 81.00}
+    COVERAGE_DICT = {"Intel": 81.89, "AMD": 81.37, "ARM": 80.95}
 
 PROC_MODEL = proc.proc_type()
 
