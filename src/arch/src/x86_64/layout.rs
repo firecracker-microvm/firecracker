9b165839f7c3593e165ce35ae13b3c48f7bb661e
Traistaru Andrei Cristian <atc@amazon.com>
2022-09-19 10:22:39 +0000

    Cmdline max size changed on x86
    The maximum size of the kernel command line is defined in the
    COMMAND_LINE_SIZE constant for the Linux kernel according to
    the spec:
    
    www.kernel.org/doc/html/latest/admin-guide/kernel-parameters.html
    
    For x86 platform, it should be 2048.
    
    Signed-off-by: Traistaru Andrei Cristian <atc@amazon.com>

--- src/arch/src/x86_64/layout.rs
+++ src/arch/src/x86_64/layout.rs
@@ -12,8 +12,8 @@ pub const BOOT_STACK_POINTER: u64 = 0x8ff0;
 
 /// Kernel command line start address.
 pub const CMDLINE_START: u64 = 0x20000;
-/// Kernel command line start address maximum size.
-pub const CMDLINE_MAX_SIZE: usize = 0x10000;
+/// Kernel command line maximum size.
+pub const CMDLINE_MAX_SIZE: usize = 2048;
 
 /// Start of the high memory.
 pub const HIMEM_START: u64 = 0x0010_0000; // 1 MB.
