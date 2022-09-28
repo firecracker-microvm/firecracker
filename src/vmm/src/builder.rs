55d922937b2fb8893e83d2f1473bd7c32dd24858
Babis Chalios <bchalios@amazon.es>
2022-09-28 09:28:00 +0000

    feat(entropy): add support for virtio-rng
    Add initial implementation for the virtio-rng device. This commit adds
    the basic functionality of the device. The device is always enabled at
    boot time.
    
    Subsequent commits will extend this to:
    1. Allow configuration through the Firecracker API.
    2. Enable snapshotting of the device.
    3. Add support for rate limiting.
    4. Add integration testing.
    5. Add metrics for the device.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- src/vmm/src/builder.rs
+++ src/vmm/src/builder.rs
@@ -127,6 +127,9 @@ pub enum StartMicrovmError {
     /// Unable to set VmResources.
     #[error("Cannot set vm resources: {0}")]
     SetVmResources(VmConfigError),
+    /// Failed to create an Entropy device
+    #[error("Cannot create the entropy device: {0}")]
+    CreateEntropyDevice(devices::virtio::rng::Error),
 }
 
 /// It's convenient to automatically convert `linux_loader::cmdline::Error`s
@@ -312,6 +315,8 @@ pub fn build_microvm_for_boot(
     if vm_resources.boot_timer {
         attach_boot_timer_device(&mut vmm, request_ts)?;
     }
+
+    attach_entropy_device(&mut vmm, event_manager, &mut boot_cmdline)?;
 
     if let Some(balloon) = vm_resources.balloon.get() {
         attach_balloon_device(&mut vmm, &mut boot_cmdline, balloon, event_manager)?;
@@ -921,6 +926,24 @@ pub(crate) fn attach_boot_timer_device(
         .register_mmio_boot_timer(boot_timer)
         .map_err(RegisterMmioDevice)?;
 
+    Ok(())
+}
+
+fn attach_entropy_device(
+    vmm: &mut Vmm,
+    event_manager: &mut EventManager,
+    cmdline: &mut LoaderKernelCmdline,
+) -> std::result::Result<(), StartMicrovmError> {
+    let entropy_device = Arc::new(Mutex::new(
+        devices::virtio::rng::Entropy::new().map_err(StartMicrovmError::CreateEntropyDevice)?,
+    ));
+    let id = entropy_device
+        .lock()
+        .expect("Poisoned lock")
+        .id()
+        .to_string();
+
+    attach_virtio_device(event_manager, vmm, id, entropy_device, cmdline)?;
     Ok(())
 }
 
