572b75e57e2a893a9efdcf70524416c3169b57df
Alexandru-Cezar Sardan <alsardan@amazon.com>
2022-10-04 13:20:39 +0000

    snap/restore: save MSRs based on CPUID
    Some additional MSRs need to be saved if certain features are enabled in
    CPUID.
    Add the capability to infer which MSRs to save based on the feature bits
    set in CPUID. The dependencies between CPUID features and MSRs are
    defined for each CPU vendor.
    Through this commit, the list of MSRs we need to save becomes
    architectural_msrs + msrs_based_on_cpuid + msrs_defined_in_template.
    
    The dependency list now contains only one entry for the MPX feature bit
    so it is by no means exhaustive. We'll add more dependencies in
    following commits. Until then the right coverage should be supplied
    through templates and the ALLOWED_MSR_RANGES list.
    
    Signed-off-by: Alexandru-Cezar Sardan <alsardan@amazon.com>

--- src/cpuid/src/template/intel/mod.rs
+++ src/cpuid/src/template/intel/mod.rs
@@ -9,7 +9,13 @@ pub mod t2;
 /// that are speciffic to an Intel Skylake CPU.
 pub mod t2s;
 
+use std::collections::HashSet;
+
+use arch_gen::x86::msr_index::*;
+use kvm_bindings::CpuId;
+
 use crate::common::{get_vendor_id_from_host, VENDOR_ID_INTEL};
+use crate::cpuid_is_feature_set;
 use crate::transformer::Error;
 
 pub fn validate_vendor_id() -> Result<(), Error> {
@@ -19,4 +25,30 @@ pub fn validate_vendor_id() -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+/// Returns MSRs to be saved based on the Intel CPUID features that are enabled.
+pub(crate) fn msrs_to_save_by_cpuid(cpuid: &CpuId) -> HashSet<u32> {
+    use crate::cpu_leaf::*;
+
+    let mut msrs = HashSet::new();
+
+    // Macro used for easy definition of CPUID-MSR dependencies.
+    macro_rules! cpuid_msr_dep {
+        ($leaf:expr, $index:expr, $reg:tt, $feature_bit:expr, [$($msr:expr),+]) => {
+            if cpuid_is_feature_set!(cpuid, $leaf, $index, $reg, $feature_bit) {
+                msrs.extend(&[$($msr),*]);
+            }
+        };
+    }
+
+    // TODO: Add more dependencies.
+    cpuid_msr_dep!(
+        0x7,
+        0,
+        ebx,
+        leaf_0x7::index0::ebx::MPX_BITINDEX,
+        [MSR_IA32_BNDCFGS]
+    );
+    msrs
 }
