b3d8959590afa84ac9cdef845afe3ecb9adf92a7
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2022-10-21 15:32:16 +0100

    Update code coverage to grcov
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_unittests.py
+++ tests/integration_tests/build/test_unittests.py
@@ -7,9 +7,10 @@ import platform
 import host_tools.cargo_build as host  # pylint:disable=import-error
 
 MACHINE = platform.machine()
-# No need to run unittests for musl since
-# we run coverage with musl for all platforms.
-TARGET = "{}-unknown-linux-gnu".format(MACHINE)
+# Currently profiling with `aarch64-unknown-linux-musl` is unsupported (see
+# https://github.com/rust-lang/rustup/issues/3095#issuecomment-1280705619) therefore we profile and
+# run coverage with the `gnu` toolchains and run unit tests with the `musl` toolchains.
+TARGET = "{}-unknown-linux-musl".format(MACHINE)
 
 
 def test_unittests(test_fc_session_root_path):
@@ -20,7 +21,4 @@ def test_unittests(test_fc_session_root_path):
     """
     extra_args = "--release --target {} ".format(TARGET)
 
-    host.cargo_test(
-        test_fc_session_root_path,
-        extra_args=extra_args
-    )
+    host.cargo_test(test_fc_session_root_path, extra_args=extra_args)
