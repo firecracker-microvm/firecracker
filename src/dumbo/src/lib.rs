7d5e288139c3b4463b3b4a33535c8ec934580094
StemCll <lydjotj6f@mozmail.com>
2022-10-27 22:14:12 +0200

    Add clippy::cast_precision_loss warning
    Related issue - #3199
    
    Signed-off-by: StemCll lydjotj6f@mozmail.com

--- src/dumbo/src/lib.rs
+++ src/dumbo/src/lib.rs
@@ -3,6 +3,7 @@
 
 #![deny(missing_docs)]
 #![warn(clippy::ptr_as_ptr)]
+#![warn(clippy::cast_precision_loss)]
 //! Provides helper logic for parsing and writing protocol data units, and minimalist
 //! implementations of a TCP listener, a TCP connection, and an HTTP/1.1 server.
 pub mod pdu;
