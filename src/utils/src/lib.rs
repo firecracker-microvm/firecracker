a354e87e27d581c774f32e39bde2001c830d67cd
Patrick Roy <roypat@amazon.co.uk>
2022-11-08 18:26:25 +0000

    Update vmm-sys-util to 0.10.0
    Due to unlucky dependency management coinciding with a breaking
    change, cargo update caused an unbuildable workspace, and
    due to external factors, #3248 does not fix this.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/utils/src/lib.rs
+++ src/utils/src/lib.rs
@@ -8,9 +8,10 @@
 // dependency easier (i.e. update only in one place `vmm_sys_util` version).
 // More specifically, we are re-exporting modules from `vmm_sys_util` as part
 // of the `utils` crate.
+pub use vmm_sys_util::ioctl::ioctl_expr;
 pub use vmm_sys_util::{
-    epoll, errno, eventfd, fam, generate_fam_struct_impl, ioctl, ioctl_expr, ioctl_ioc_nr,
-    ioctl_iow_nr, rand, seek_hole, sock_ctrl_msg, syscall, tempdir, tempfile, terminal,
+    epoll, errno, eventfd, fam, generate_fam_struct_impl, ioctl, ioctl_ioc_nr, ioctl_iow_nr, rand,
+    seek_hole, sock_ctrl_msg, syscall, tempdir, tempfile, terminal,
 };
 
 pub mod arg_parser;
