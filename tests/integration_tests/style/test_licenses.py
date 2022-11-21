6527e5569cd7122a82db619f7837b7675fe4e634
Traistaru Andrei Cristian <atc@amazon.com>
2022-11-21 08:21:51 +0000

    Excluding .buildkite from style tests
    Signed-off-by: Traistaru Andrei Cristian <atc@amazon.com>

--- tests/integration_tests/style/test_licenses.py
+++ tests/integration_tests/style/test_licenses.py
@@ -31,7 +31,7 @@ ALIBABA_LICENSE = (
     "SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause"
 )
 
-EXCLUDE = ["build", ".kernel", ".git"]
+EXCLUDE = ["build", ".kernel", ".git", ".buildkite"]
 
 
 def _has_amazon_copyright(string):
