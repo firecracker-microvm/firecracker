7f55b42cf72a7e5de922fc34f677d8f2df79fe0e
Diana Popa <dpopa@amazon.com>
2022-12-12 17:13:09 +0200

    chore: check that repo is not dirty
    Added an integration test that checks all files
    impacted by changes are checked in.
    
    Signed-off-by: Diana Popa <dpopa@amazon.com>

--- tests/integration_tests/style/test_repo.py
+++ tests/integration_tests/style/test_repo.py
@@ -5,6 +5,8 @@
 
 import subprocess
 
+from framework.utils import run_cmd
+
 
 def test_repo_no_spaces_in_paths():
     """
@@ -21,3 +23,13 @@ def test_repo_no_spaces_in_paths():
     )
     # If grep doesn't find any, it will exit with status 1. Otherwise 0
     assert res.returncode == 1, "Some files have spaces:\n" + res.stdout.decode()
+
+
+def test_repo_not_dirty():
+    """
+    Ensure there are no dirty files in repo.
+
+    @type: style
+    """
+    _, stdout, _ = run_cmd("git diff --stat")
+    assert stdout == "", "Repository is dirty: \n{}".format(stdout)
