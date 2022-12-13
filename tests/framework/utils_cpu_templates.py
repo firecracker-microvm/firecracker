d911ef398da7e28d65a4756c19e9ee68d66eb4e0
Nikita Kalyazin <kalyazin@amazon.com>
2022-12-13 16:11:01 +0000

    test: verify that MPX feature flag is disabled
    Intel MPX is a deprecated technology that has not been included
    in modern Intel CPUs. AMD does not have this feature.
    Support for MPX is being removed also from mainstream compilers.
    
    The new test verifies that the MPX feature flag is disabled
    in all supported CPU templates.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

---
+++ tests/framework/utils_cpu_templates.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Utilities for CPU template related functionality."""
+
+import framework.utils_cpuid as cpuid_utils
+
+# All existing CPU templates available on Intel
+INTEL_TEMPLATES = ["C3", "T2", "T2S"]
+# All existing CPU templates available on AMD
+AMD_TEMPLATES = []
+# All existing CPU templates
+ALL_TEMPLATES = INTEL_TEMPLATES + AMD_TEMPLATES
+
+
+def get_supported_cpu_templates():
+    """
+    Return the list of CPU templates supported by the platform.
+    """
+    vendor = cpuid_utils.get_cpu_vendor()
+    if vendor == cpuid_utils.CpuVendor.INTEL:
+        return INTEL_TEMPLATES
+    if vendor == cpuid_utils.CpuVendor.AMD:
+        return AMD_TEMPLATES
+    return []
+
+
+SUPPORTED_CPU_TEMPLATES = get_supported_cpu_templates()
--- tests/framework/utils_cpuid.py
+++ tests/framework/utils_cpuid.py
@@ -4,6 +4,7 @@
 
 import platform
 import subprocess
+import re
 from enum import Enum, auto
 
 from framework.utils import run_cmd
@@ -101,3 +102,35 @@ def check_guest_cpuid_output(
         "some keys in dictionary have not been found in the output: %s"
         % expected_key_value_store
     )
+
+
+def build_cpuid_dict(raw_cpuid_output):
+    """Build CPUID dict based on raw cpuid output"""
+    cpuid_dict = {}
+    ptrn = re.compile("^ *(.*) (.*): eax=(.*) ebx=(.*) ecx=(.*) edx=(.*)$")
+    for line in raw_cpuid_output:
+        match = re.match(ptrn, line)
+        assert match, f"`{line}` does not match the regex pattern."
+        leaf, subleaf, eax, ebx, ecx, edx = [int(x, 16) for x in match.groups()]
+        cpuid_dict[(leaf, subleaf, "eax")] = eax
+        cpuid_dict[(leaf, subleaf, "ebx")] = ebx
+        cpuid_dict[(leaf, subleaf, "ecx")] = ecx
+        cpuid_dict[(leaf, subleaf, "edx")] = edx
+    return cpuid_dict
+
+
+def get_guest_cpuid(vm):
+    """
+    Return the guest CPUID in the form of a dictionary where the key is a tuple:
+     - leaf (integer)
+     - subleaf (integer)
+     - register ("eax", "ebx", "ecx" or "edx")
+    and the value is the register value (integer).
+    """
+    ssh_conn = net_tools.SSHConnection(vm.ssh_config)
+
+    read_cpuid_cmd = "cpuid -1 --raw | grep -v CPU"
+    _, stdout, stderr = ssh_conn.execute_command(read_cpuid_cmd)
+    assert stderr.read() == ""
+
+    return build_cpuid_dict(stdout)
