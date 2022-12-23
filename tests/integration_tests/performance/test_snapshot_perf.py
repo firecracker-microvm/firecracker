cfc56697b4d7a829ed9399e1fd365f73e7ab9db2
Matthew Schlebusch <schlebus@amazon.com>
2022-12-23 11:50:05 +0000

    Increase snapshot perf target on AMD for 512mb
    Backport 063ed186e75b52935399368c47402c9b794ede0c.
    
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- tests/integration_tests/performance/test_snapshot_perf.py
+++ tests/integration_tests/performance/test_snapshot_perf.py
@@ -90,7 +90,7 @@ LOAD_LATENCY_BASELINES = {
             "4.14": {
                 "sync": {
                     "2vcpu_256mb.json": {"target": 15},
-                    "2vcpu_512mb.json": {"target": 15},
+                    "2vcpu_512mb.json": {"target": 19},
                 }
             },
             "5.10": {
