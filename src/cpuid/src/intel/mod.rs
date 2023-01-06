b1223b69d7c13c49c63032a72c6c6143979af2ae
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-01-06 18:05:11 +0000

    refactor: `IntelCpuid::default_brand_string`
    Refactors `cpuid::intel::IntelCpuid::default_brand_string` to add tests.
    
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- src/cpuid/src/intel/mod.rs
+++ src/cpuid/src/intel/mod.rs
@@ -7,9 +7,6 @@
     clippy::unsafe_derive_deserialize
 )]
 
-#[cfg(cpuid)]
-use std::convert::TryInto;
-
 /// Leaf structs.
 mod leaves;
 
@@ -44,103 +41,6 @@ impl CpuidTrait for IntelCpuid {
     #[inline]
     fn get_mut(&mut self, key: &CpuidKey) -> Option<&mut CpuidEntry> {
         self.0.get_mut(key)
-    }
-}
-
-/// Error type for [`IntelCpuid::default_brand_string`].
-#[cfg(cpuid)]
-#[derive(Debug, Eq, PartialEq, thiserror::Error)]
-pub enum DefaultBrandStringError {
-    /// Missing frequency.
-    #[error("Missing frequency: {0:?}.")]
-    Missingfrequency([u8; crate::BRAND_STRING_LENGTH]),
-    /// Missing space.
-    #[error("Missing space: {0:?}.")]
-    MissingSpace([u8; crate::BRAND_STRING_LENGTH]),
-    /// Insufficient space in brand string.
-    #[error("Insufficient space in brand string.")]
-    Overflow,
-}
-
-impl IntelCpuid {
-    /// Gets the brand string always used for Intel.
-    ///
-    /// # Errors
-    ///
-    /// When unable to parse the host brand string.
-    /// `brand_string.try_into().unwrap()` cannot panic as we know
-    /// `brand_string.len() == BRAND_STRING_LENGTH`
-    ///
-    /// # Panics
-    ///
-    /// Never.
-    // As we pass through host frequency, we require CPUID and thus `cfg(cpuid)`.
-    // TODO: Use `split_array_ref`
-    // (https://github.com/firecracker-microvm/firecracker/issues/3347)
-    #[allow(
-        clippy::indexing_slicing,
-        clippy::integer_arithmetic,
-        clippy::arithmetic_side_effects
-    )]
-    #[cfg(cpuid)]
-    #[inline]
-    pub fn default_brand_string(
-    ) -> Result<[u8; super::BRAND_STRING_LENGTH], DefaultBrandStringError> {
-        /// We always use this brand string.
-        const DEFAULT_BRAND_STRING_BASE: &[u8] = b"Intel(R) Xeon(R) Processor @";
-
-        // Get host brand string.
-        // This will look like b"Intel(4) Xeon(R) Processor @ 3.00GHz".
-        let host_brand_string: [u8; super::BRAND_STRING_LENGTH] = super::host_brand_string();
-
-        // The slice of the host string before the frequency suffix
-        // e.g. b"Intel(4) Xeon(R) Processor @ 3.00" and "GHz"
-        let (before, after) = 'outer: {
-            for i in 0..host_brand_string.len() {
-                // Find position of b"THz" or b"GHz" or b"MHz"
-                if let [b'T' | b'G' | b'M', b'H', b'z', ..] = host_brand_string[i..] {
-                    break 'outer Ok(host_brand_string.split_at(i));
-                }
-            }
-            Err(DefaultBrandStringError::Missingfrequency(host_brand_string))
-        }?;
-
-        // We iterate from the end until hitting a space, getting the frequency number
-        // e.g. b"Intel(4) Xeon(R) Processor @ " and "3.00"
-        let (_, frequency) = 'outer: {
-            for i in (0..before.len()).rev() {
-                if before[i] == b' ' {
-                    break 'outer Ok(before.split_at(i));
-                }
-            }
-            Err(DefaultBrandStringError::MissingSpace(host_brand_string))
-        }?;
-
-        // As `DEFAULT_BRAND_STRING_BASE.len() + frequency.len() + after.len()` is guaranteed
-        // to be less than or equal to  `2*BRAND_STRING_LENGTH` and we know
-        // `2*BRAND_STRING_LENGTH <= usize::MAX` since `BRAND_STRING_LENGTH==48`, this is always
-        // safe.
-        let len = DEFAULT_BRAND_STRING_BASE.len() + frequency.len() + after.len();
-
-        let brand_string = DEFAULT_BRAND_STRING_BASE
-            .iter()
-            .copied()
-            // Include frequency e.g. "3.00"
-            .chain(frequency.iter().copied())
-            // Include frequency suffix e.g. "GHz"
-            .chain(after.iter().copied())
-            // Pad with 0s to `BRAND_STRING_LENGTH`
-            .chain(
-                std::iter::repeat(b'\0').take(
-                    super::BRAND_STRING_LENGTH
-                        .checked_sub(len)
-                        .ok_or(DefaultBrandStringError::Overflow)?,
-                ),
-            )
-            .collect::<Vec<_>>();
-
-        // SAFETY: Padding ensures `brand_string.len() == BRAND_STRING_LENGTH`.
-        Ok(unsafe { brand_string.try_into().unwrap_unchecked() })
     }
 }
 
