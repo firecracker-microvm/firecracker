c06557623ee160ce69c310f3c83d898e1203f902
Felipe R. Monteiro <felisous@amazon.com>
2023-01-17 15:01:34 +0000

    test: Add kani harnesses for Dumbo ethernet
    Harnesses for verifying that our processing of ethernet frames is
    correct.
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>
    Co-authored-by: Patrick Roy <roypat@amazon.co.uk>

--- src/dumbo/src/pdu/ethernet.rs
+++ src/dumbo/src/pdu/ethernet.rs
@@ -236,3 +236,306 @@ mod tests {
         }
     }
 }
+
+#[cfg(kani)]
+mod kani_proofs {
+    use utils::net::mac::MAC_ADDR_LEN;
+
+    use super::*;
+
+    // See the Virtual I/O Device (VIRTIO) specification, Sec. 5.1.6.2.
+    // https://docs.oasis-open.org/virtio/virtio/v1.2/csd01/virtio-v1.2-csd01.pdf
+    pub const MAX_FRAME_SIZE: usize = 1514;
+
+    impl<'a, T: NetworkBytesMut + Debug> EthernetFrame<'a, T> {
+        fn is_valid(&self) -> bool {
+            self.len() >= PAYLOAD_OFFSET
+        }
+    }
+
+    // We consider the MMDS Network Stack spec for all postconditions in the harnesses.
+    // See https://github.com/firecracker-microvm/firecracker/blob/main/docs/mmds/mmds-design.md#mmds-network-stack
+
+    #[kani::proof]
+    fn verify_from_bytes_unchecked() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+        let slice_length = bytes.len();
+
+        // Verify from_bytes_unchecked
+        let ethernet = EthernetFrame::from_bytes_unchecked(bytes.as_mut());
+
+        // Check for post-conditions
+        assert_eq!(ethernet.len(), slice_length);
+        assert!(
+            !(ethernet.is_valid()) || (ethernet.payload().len() == slice_length - PAYLOAD_OFFSET)
+        );
+    }
+
+    #[kani::proof]
+    fn verify_from_bytes() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+        let slice_length = bytes.len();
+
+        // Verify from_bytes
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+
+        // Check for post-conditions
+        if slice_length >= PAYLOAD_OFFSET {
+            let ethernet = ethernet.unwrap();
+            assert!(ethernet.is_valid());
+            assert_eq!(ethernet.len(), slice_length);
+            assert_eq!(ethernet.payload().len(), slice_length - PAYLOAD_OFFSET);
+        } else {
+            assert!(ethernet.is_err());
+        }
+    }
+
+    #[kani::proof]
+    fn verify_dst_mac() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic ethernet
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+        kani::assume(ethernet.is_ok());
+        let mut ethernet = ethernet.unwrap();
+
+        // Verify set_dst_mac
+        let mac_bytes: [u8; MAC_ADDR_LEN] = kani::any();
+        let dst_mac = MacAddr::from(mac_bytes);
+        ethernet.set_dst_mac(dst_mac);
+
+        // Verify dst_mac
+        let dst_addr = EthernetFrame::dst_mac(&ethernet);
+
+        // Check for post-conditions
+
+        // MAC addresses should always have 48 bits
+        assert_eq!(dst_addr.get_bytes().len(), MAC_ADDR_LEN);
+
+        // Check duality between set_dst_mac and dst_mac operations
+        let i: usize = kani::any();
+        kani::assume(i < mac_bytes.len());
+        assert_eq!(mac_bytes[i], dst_addr.get_bytes()[i]);
+    }
+
+    #[kani::proof]
+    fn verify_src_mac() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic ethernet
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+        kani::assume(ethernet.is_ok());
+        let mut ethernet = ethernet.unwrap();
+
+        // Verify set_src_mac
+        let mac_bytes: [u8; MAC_ADDR_LEN] = kani::any();
+        let src_mac = MacAddr::from(mac_bytes);
+        ethernet.set_src_mac(src_mac);
+
+        // Verify src_mac
+        let src_addr = EthernetFrame::src_mac(&ethernet);
+
+        // Check for post-conditions
+
+        // MAC addresses should always have 48 bits
+        assert_eq!(src_addr.get_bytes().len(), MAC_ADDR_LEN);
+
+        // Check duality between set_src_mac and src_mac operations
+        let i: usize = kani::any();
+        kani::assume(i < mac_bytes.len());
+        assert_eq!(mac_bytes[i], src_addr.get_bytes()[i]);
+    }
+
+    #[kani::proof]
+    fn verify_src_mac_isolation() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic ethernet
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+        kani::assume(ethernet.is_ok());
+        let mut ethernet = ethernet.unwrap();
+
+        // Verify set_src_mac
+        let mac_bytes: [u8; MAC_ADDR_LEN] = kani::any();
+        let src_mac = MacAddr::from(mac_bytes);
+        ethernet.set_src_mac(src_mac);
+
+        let payload_offset = ethernet.payload_offset();
+
+        if kani::any() {
+            let dst_mac_bytes: [u8; MAC_ADDR_LEN] = kani::any();
+            let dst_mac = MacAddr::from(dst_mac_bytes);
+            ethernet.set_dst_mac(dst_mac);
+        }
+        if kani::any() {
+            let ethertype_in: u16 = kani::any();
+            ethernet.set_ethertype(ethertype_in);
+        }
+
+        // Payload info doesn't change
+        assert_eq!(ethernet.payload_offset(), payload_offset);
+
+        // Verify src_mac
+        let src_addr = EthernetFrame::src_mac(&ethernet);
+
+        // Check for post-conditions
+
+        // MAC addresses should always have 48 bits
+        assert_eq!(src_addr.get_bytes().len(), MAC_ADDR_LEN);
+
+        // Check duality between set_src_mac and src_mac operations
+        let i: usize = kani::any();
+        kani::assume(i < mac_bytes.len());
+        assert_eq!(mac_bytes[i], src_addr.get_bytes()[i]);
+    }
+
+    #[kani::proof]
+    fn verify_ethertype() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic ethernet
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+        kani::assume(ethernet.is_ok());
+        let mut ethernet = ethernet.unwrap();
+
+        // Verify set_ethertype
+        let ethertype_in: u16 = kani::any();
+        ethernet.set_ethertype(ethertype_in);
+
+        // Verify ethertype
+        let ethertype_out = ethernet.ethertype();
+
+        // Check for post-conditions
+
+        // Check duality between set_ethertype and ethertype operations
+        assert_eq!(ethertype_in, ethertype_out);
+    }
+
+    #[kani::proof]
+    #[kani::unwind(1515)]
+    fn verify_payload() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic ethernet
+        let ethernet = EthernetFrame::from_bytes(bytes.as_mut());
+        kani::assume(ethernet.is_ok());
+        let ethernet = ethernet.unwrap();
+
+        // Verify payload_offset
+        let payload_offset = ethernet.payload_offset();
+
+        // Verify payload()
+        let payload = ethernet.payload();
+
+        // Verify payload_mut
+        let payload_mut = ethernet.payload();
+
+        // Check for post-conditions
+
+        // Check payload_offset value
+        assert_eq!(payload_offset, PAYLOAD_OFFSET);
+
+        // Check equivalence
+        assert_eq!(payload, payload_mut);
+    }
+
+    #[kani::proof]
+    fn verify_new_with_header() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+        let bytes_length = bytes.len();
+
+        // Create valid non-deterministic dst_mac
+        let dst_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let dst_mac = MacAddr::from(dst_mac_bytes);
+
+        // Create valid non-deterministic src_mac
+        let src_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let src_mac = MacAddr::from(src_mac_bytes);
+
+        // Create valid non-deterministic ethertype
+        let ethertype: u16 = kani::any();
+
+        // Verify new_with_header
+        let frame =
+            EthernetFrame::new_with_header(bytes.as_mut(), dst_mac, src_mac, ethertype).unwrap();
+
+        // Check for post-conditions
+        assert_eq!(frame.dst_mac(), dst_mac);
+        assert_eq!(frame.src_mac(), src_mac);
+        assert_eq!(frame.ethertype(), ethertype);
+        assert_eq!(frame.len(), bytes_length);
+        assert!(frame.is_valid() && (frame.payload().len() == bytes_length - PAYLOAD_OFFSET));
+    }
+
+    #[kani::proof]
+    fn verify_write_incomplete() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic dst_mac
+        let dst_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let dst_mac = MacAddr::from(dst_mac_bytes);
+
+        // Create valid non-deterministic src_mac
+        let src_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let src_mac = MacAddr::from(src_mac_bytes);
+
+        // Create valid non-deterministic ethertype
+        let ethertype: u16 = kani::any();
+
+        // Verify write_incomplete
+        let incomplete_frame =
+            EthernetFrame::write_incomplete(bytes.as_mut(), dst_mac, src_mac, ethertype).unwrap();
+
+        // Check for post-conditions
+        assert_eq!(incomplete_frame.inner.dst_mac(), dst_mac);
+        assert_eq!(incomplete_frame.inner.src_mac(), src_mac);
+        assert_eq!(incomplete_frame.inner.ethertype(), ethertype);
+    }
+
+    #[kani::proof]
+    #[kani::unwind(1515)]
+    #[kani::solver(cadical)]
+    fn verify_with_payload_len_unchecked() {
+        // Create non-deterministic stream of bytes up to MAX_FRAME_SIZE
+        let mut bytes: [u8; MAX_FRAME_SIZE] = kani::Arbitrary::any_array::<MAX_FRAME_SIZE>();
+
+        // Create valid non-deterministic dst_mac
+        let dst_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let dst_mac = MacAddr::from(dst_mac_bytes);
+
+        // Create valid non-deterministic src_mac
+        let src_mac_bytes: [u8; MAC_ADDR_LEN] = kani::Arbitrary::any_array::<MAC_ADDR_LEN>();
+        let src_mac = MacAddr::from(src_mac_bytes);
+
+        // Create valid non-deterministic ethertype
+        let ethertype: u16 = kani::any();
+
+        // Create a non-deterministic incomplete frame
+        let incomplete_frame =
+            EthernetFrame::write_incomplete(bytes.as_mut(), dst_mac, src_mac, ethertype).unwrap();
+        let incomplete_frame_payload_offset = incomplete_frame.inner.payload_offset();
+        let incomplete_frame_len = incomplete_frame.inner.len();
+
+        // Create a non-deterministic payload_len
+        let payload_len: usize = kani::any();
+        kani::assume(payload_len <= incomplete_frame_len - incomplete_frame_payload_offset);
+
+        // Verify with_payload_len_unchecked
+        let unchecked_frame = incomplete_frame.with_payload_len_unchecked(payload_len);
+
+        // Check for post-conditions
+        assert!(unchecked_frame.is_valid());
+        assert_eq!(unchecked_frame.dst_mac(), dst_mac);
+        assert_eq!(unchecked_frame.src_mac(), src_mac);
+        assert_eq!(unchecked_frame.ethertype(), ethertype);
+    }
+}
