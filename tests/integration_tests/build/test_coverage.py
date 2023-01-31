9a83c540841b49370bc19da206b5cb7118f3bb2e
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-01-31 15:06:58 +0000

    test: Update code coverage
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -18,10 +18,9 @@ from host_tools.cargo_build import cargo
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 77.81, "AMD": 78.54, "ARM": 72.26}
+    COVERAGE_DICT = {"Intel": 78.08, "AMD": 78.90, "ARM": 72.91}
 else:
-    COVERAGE_DICT = {"Intel": 80.32, "AMD": 79.5, "ARM": 79.44}
-
+    COVERAGE_DICT = {"Intel": 75.81, "AMD": 76.62, "ARM": 70.22}
 
 PROC_MODEL = proc.proc_type()
 
