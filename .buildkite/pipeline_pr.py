730b084c62c4adbfba09d4e8c8ed1631d771809f
Pablo Barbáchano <pablob@amazon.com>
2023-02-07 18:36:12 +0100

    chore(ci): migrate pipelines to new CI infrastructure
    Migrate the PR pipeline to the new infra
    
    Also raise the priority of the performance tests so ag=1 agents pick up
    those jobs before ag=n jobs.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- .buildkite/pipeline_pr.py
+++ .buildkite/pipeline_pr.py
@@ -8,14 +8,13 @@ import json
 import subprocess
 from pathlib import Path
 
-INSTANCES = [
-    "m5d.metal",
-    "m6i.metal",
-    "m6a.metal",
-    "m6gd.metal",
-]
+from common import group, DEFAULT_INSTANCES, DEFAULT_KERNELS
 
-KERNELS = ["4.14", "5.10"]
+
+DEFAULT_QUEUE = "public-prod-us-east-1"
+# Buildkite default job priority is 0. Setting this to 1 prioritizes PRs over
+# scheduled jobs and other batch jobs.
+DEFAULT_PRIORITY = 1
 
 
 def get_changed_files(branch):
@@ -26,73 +25,56 @@ def get_changed_files(branch):
     return [Path(line) for line in stdout.decode().splitlines()]
 
 
-def group(group_name, command, agent_tags=None, priority=0, timeout=30):
-    """
-    Generate a group step with specified parameters, for each instance+kernel
-    combination
-
-    https://buildkite.com/docs/pipelines/group-step
-    """
-    if agent_tags is None:
-        agent_tags = []
-    # Use the 1st character of the group name (should be an emoji)
-    label1 = group_name[0]
-    group_steps = []
-    for instance in INSTANCES:
-        for kv in KERNELS:
-            agents = [
-                f"type={instance}",
-                f"kv={kv}",
-            ]
-            agents.extend(agent_tags)
-            step = {
-                "command": command,
-                "label": f"{label1} {instance} kv={kv}",
-                "priority": priority,
-                "timeout": timeout,
-                "agents": agents,
-            }
-            group_steps.append(step)
-
-    return {"group": group_name, "steps": group_steps}
-
-
 step_style = {
     "command": "./tools/devtool -y test -- ../tests/integration_tests/style/",
     "label": "🪶 Style",
-    # we only install the required dependencies in x86_64
-    "agents": ["platform=x86_64.metal"],
+    "priority": DEFAULT_PRIORITY,
+}
+
+defaults = {
+    "instances": DEFAULT_INSTANCES,
+    "kernels": DEFAULT_KERNELS,
+    # buildkite step parameters
+    "priority": DEFAULT_PRIORITY,
+    "timeout_in_minutes": 30,
 }
 
 build_grp = group(
     "📦 Build",
     "./tools/devtool -y test -- ../tests/integration_tests/build/",
-    priority=1,
+    **defaults
 )
 
 functional_1_grp = group(
     "⚙ Functional [a-n]",
     "./tools/devtool -y test -- `cd tests; ls integration_tests/functional/test_[a-n]*.py`",
-    priority=1,
+    **defaults
 )
 
 functional_2_grp = group(
     "⚙ Functional [o-z]",
     "./tools/devtool -y test -- `cd tests; ls integration_tests/functional/test_[o-z]*.py`",
-    priority=1,
+    **defaults
 )
 
 security_grp = group(
     "🔒 Security",
     "./tools/devtool -y test -- ../tests/integration_tests/security/",
-    priority=1,
+    **defaults
+)
+
+defaults_for_performance = defaults.copy()
+defaults_for_performance.update(
+    # We specify higher priority so the ag=1 jobs get picked up before the ag=n
+    # jobs in ag=1 agents
+    priority=DEFAULT_PRIORITY + 1,
+    agent_tags=["ag=1"],
 )
 
 performance_grp = group(
     "⏱ Performance",
     "./tools/devtool -y test -- ../tests/integration_tests/performance/",
-    priority=1,
-    agent_tags=["ag=1"],
+    **defaults_for_performance,
 )
 
 steps = [step_style]
@@ -107,7 +89,8 @@ if any(x.suffix != ".md" for x in changed_files):
     ]
 
 pipeline = {
-    "agents": {"queue": "default"},
+    "env": {},
+    "agents": {"queue": DEFAULT_QUEUE},
     "steps": steps,
 }
 
