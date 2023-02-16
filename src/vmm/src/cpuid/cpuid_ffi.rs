d2cfa816fcead2c314c2af93aa2e0c0cc5b6c87e
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-02-16 10:38:08 +0000

    fix: Reduce std `unchecked` calls
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- src/vmm/src/cpuid/cpuid_ffi.rs
+++ src/vmm/src/cpuid/cpuid_ffi.rs
@@ -59,7 +59,7 @@ pub struct RawCpuidResizeError(std::alloc::LayoutError);
 
 impl super::CpuidTrait for RawCpuid {
     /// Gets a given sub-leaf.
-    #[allow(clippy::transmute_ptr_to_ptr)]
+    #[allow(clippy::transmute_ptr_to_ptr, clippy::unwrap_used)]
     #[inline]
     fn get(&self, CpuidKey { leaf, subleaf }: &CpuidKey) -> Option<&CpuidEntry> {
         let entry_opt = self
@@ -71,15 +71,14 @@ impl super::CpuidTrait for RawCpuid {
             // SAFETY: The `RawKvmCpuidEntry` and `CpuidEntry` are `repr(C)` with known sizes.
             unsafe {
                 let arr: &[u8; size_of::<RawKvmCpuidEntry>()] = transmute(entry);
-                let arr2: &[u8; size_of::<CpuidEntry>()] =
-                    arr.get_unchecked(8..28).try_into().unwrap_unchecked();
+                let arr2: &[u8; size_of::<CpuidEntry>()] = arr[8..28].try_into().unwrap();
                 transmute::<_, &CpuidEntry>(arr2)
             }
         })
     }
 
     /// Gets a given sub-leaf.
-    #[allow(clippy::transmute_ptr_to_ptr)]
+    #[allow(clippy::transmute_ptr_to_ptr, clippy::unwrap_used)]
     #[inline]
     fn get_mut(&mut self, CpuidKey { leaf, subleaf }: &CpuidKey) -> Option<&mut CpuidEntry> {
         let entry_opt = self
@@ -91,7 +90,7 @@ impl super::CpuidTrait for RawCpuid {
             unsafe {
                 let arr: &mut [u8; size_of::<RawKvmCpuidEntry>()] = transmute(entry);
                 let arr2: &mut [u8; size_of::<CpuidEntry>()] =
-                    arr.get_unchecked_mut(8..28).try_into().unwrap_unchecked();
+                    (&mut arr[8..28]).try_into().unwrap();
                 transmute::<_, &mut CpuidEntry>(arr2)
             }
         })
