5bb86053eedbb1ec62d4fe38ed3b325355b55c16
Pablo Barbáchano <pablob@amazon.com>
2023-02-20 22:51:30 +0100

    tests: rewrite pylint test
    The old test created hundreds of processes/threads (up to 1021 measured
    by pgrep) to check each file one by one, and took 40s.
    
    Worse, if it fails it only shows a single file worth of errors. There
    could be more errors, but one would only find out after fixing the first
    ones.
    
    Make the directories proper modules so we can make a single pylint call,
    that creates up to 15 processes/threads and takes 20s to run.
    
    As a side bonus, we also get nice ANSI output.
    
    Also, rename an option that is only available in pylint 2.16. We will
    have to re-rename it in 2.16+.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

---
+++ tests/host_tools/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Are you happy pylint?"""
--- tests/integration_tests/style/test_pylint.py
+++ tests/integration_tests/style/test_pylint.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 """Tests ensuring codebase style compliance for Python."""
 
-from framework import utils
+import sys
+
+from subprocess import run
 
 
 def test_python_pylint():
@@ -14,23 +16,21 @@ def test_python_pylint():
     # List of linter commands that should be executed for each file
     linter_cmd = (
         # Pylint
-        "python3 -m pylint --jobs=0 --persistent=no --score=no "
+        "pylint --jobs=0 --persistent=no --score=no "
         '--output-format=colorized --attr-rgx="[a-z_][a-z0-9_]{1,30}$" '
         '--argument-rgx="[a-z_][a-z0-9_]{1,35}$" '
         '--variable-rgx="[a-z_][a-z0-9_]{1,30}$" --disable='
         "fixme,too-many-instance-attributes,import-error,"
         "too-many-locals,too-many-arguments,consider-using-f-string,"
         "consider-using-with,implicit-str-concat,line-too-long,"
-        "broad-exception-raised"
+        "broad-except,duplicate-code tests tools .buildkite/*.py"
     )
-
-    # Get all *.py files from the project
-    python_files = utils.get_files_from(
-        find_path="..", pattern="*.py", exclude_names=["build", ".kernel", ".git"]
+    run(
+        linter_cmd,
+        # we let pytest capture stdout/stderr for us
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        shell=True,
+        cwd="..",
+        check=True,
     )
-
-    # Assert if somehow no python files were found
-    assert len(python_files) != 0
-
-    # Run commands
-    utils.run_cmd_list_async([f"{linter_cmd} {fname}" for fname in python_files])
