45dfc699aecce062fc591a82bfafd6402af9c65e
Luminita Voicu <lumivo@amazon.com>
2023-02-20 14:31:14 +0200

    test: uffd: add unit tests for handler
    Signed-off-by: Luminita Voicu <lumivo@amazon.com>

--- src/uffd_handler/src/common.rs
+++ src/uffd_handler/src/common.rs
@@ -30,3 +30,71 @@ pub fn parse_unix_stream(stream: &UnixStream) -> Result<(File, String), StreamEr
 
     Ok((file, body))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+    use std::os::unix::io::AsRawFd;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_unix_stream_successful() {
+        let (sender, receiver) = UnixStream::pair().unwrap();
+        let expected_body = "test body".to_string();
+        let fd = sender.socket_fd();
+
+        let sent = sender.send_with_fd(expected_body.as_bytes(), fd).unwrap();
+        assert_eq!(sent, expected_body.len());
+
+        // Successfully receive and parse a message body and file descriptor from sender.
+        let (actual_fd, actual_body) = parse_unix_stream(&receiver).unwrap();
+        assert_eq!(actual_body, expected_body);
+        assert!(actual_fd.as_raw_fd() >= 0);
+        assert_ne!(actual_fd.as_raw_fd(), receiver.as_raw_fd());
+        // `send_with_fd()` only sends the reference to the file description, not the actual
+        // descriptor number. We expect the fd number to change between processes, but the
+        // kernel structure it represents will not.
+        assert_ne!(actual_fd.as_raw_fd(), sender.as_raw_fd());
+    }
+
+    #[test]
+    fn test_parse_unix_stream_no_msg() {
+        let (_sender, receiver) = UnixStream::pair().unwrap();
+        // Force receiver to not wait for message.
+        receiver.set_nonblocking(true).unwrap();
+
+        // Fail stream parsing when no message was received.
+        let res = parse_unix_stream(&receiver);
+        assert!(res.is_err());
+        assert!(matches!(res.err().unwrap(), StreamError::RecvWithFd(_)));
+    }
+
+    #[test]
+    fn test_parse_unix_stream_no_fd() {
+        let (mut sender, receiver) = UnixStream::pair().unwrap();
+        let expected_body = "test body".to_string();
+
+        // Fail stream parsing when the file descriptor is missing.
+        sender.write_all(expected_body.as_bytes()).unwrap();
+        let res = parse_unix_stream(&receiver);
+        assert!(res.is_err());
+        assert_eq!(
+            StreamError::NoFd.to_string(),
+            res.err().unwrap().to_string()
+        );
+    }
+
+    #[test]
+    fn test_parse_unix_stream_not_utf8() {
+        let (sender, receiver) = UnixStream::pair().unwrap();
+        let fd = sender.socket_fd();
+        let invalid_utf8 = [0xfe, 0xfe, 0xff, 0xff];
+
+        // Fail stream parsing when the message is not UTF-8.
+        sender.send_with_fd(invalid_utf8.as_slice(), fd).unwrap();
+        let res = parse_unix_stream(&receiver);
+        assert!(res.is_err());
+        assert!(matches!(res.err().unwrap(), StreamError::InvalidData(_)));
+    }
+}
