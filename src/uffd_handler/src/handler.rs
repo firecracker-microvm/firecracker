cc8cee1f55553854d66ba8ec51e9e83201e5110c
Luminita Voicu <lumivo@amazon.com>
2023-02-20 17:59:44 +0200

    fix: uffd: transform panics into errors
    Signed-off-by: Luminita Voicu <lumivo@amazon.com>

--- src/uffd_handler/src/handler.rs
+++ src/uffd_handler/src/handler.rs
@@ -8,23 +8,53 @@ use utils::get_page_size;
 
 use crate::memory_region::{MemPageState, MemRegion};
 
+#[derive(Debug, thiserror::Error)]
+pub enum HandlerError {
+    #[error("Page fault address does not belong to any of the guest memory regions.")]
+    AddressNotInRegion,
+    #[error("Userfaultfd event is not ready.")]
+    EventNotReady,
+    #[error("Failed to fetch system's page size: {0}")]
+    PageSize(utils::errno::Error),
+    #[error("Failed to read userfaultfd event: {0}.")]
+    ReadEvent(userfaultfd::Error),
+    #[error("Userfaultfd copy failed: {0}")]
+    UffdCopy(userfaultfd::Error),
+    #[error("Userfaultfd {0} operation returned 0 bytes.")]
+    UffdNoOperation(String),
+    #[error("Userfaultfd failed to zero out pages: {0}")]
+    UffdZero(userfaultfd::Error),
+}
+
+type Result<T> = std::result::Result<T, HandlerError>;
+
 pub trait UffdManager: AsRawFd {
-    fn poll_fd(&self) -> Event;
+    fn poll_fd(&self) -> Result<Event>;
 
-    fn populate_from_file(&self, buff: *const libc::c_void, region: &MemRegion) -> (usize, usize);
+    fn populate_from_file(
+        &self,
+        buff: *const libc::c_void,
+        region: &MemRegion,
+    ) -> Result<(usize, usize)>;
 
-    fn zero_out(&self, addr: usize) -> (usize, usize);
+    fn zero_out(&self, addr: usize) -> Result<(usize, usize)>;
 }
 
 impl UffdManager for Uffd {
-    fn poll_fd(&self) -> Event {
+    fn poll_fd(&self) -> Result<Event> {
         // Read an event from the userfaultfd.
-        self.read_event()
-            .expect("Failed to read uffd_msg")
-            .expect("uffd_msg not ready")
+        let event_op = self.read_event().map_err(HandlerError::ReadEvent)?;
+        match event_op {
+            Some(event) => Ok(event),
+            None => Err(HandlerError::EventNotReady),
+        }
     }
 
-    fn populate_from_file(&self, buff: *const libc::c_void, region: &MemRegion) -> (usize, usize) {
+    fn populate_from_file(
+        &self,
+        buff: *const libc::c_void,
+        region: &MemRegion,
+    ) -> Result<(usize, usize)> {
         let src = buff as u64 + region.mapping.offset;
         let start_addr = region.mapping.base_host_virt_addr;
         let len = region.mapping.size;
@@ -34,27 +64,31 @@ impl UffdManager for Uffd {
         // SAFETY: this is safe because the parameters are valid.
         let ret = unsafe {
             self.copy(src as *const _, start_addr as *mut _, len, true)
-                .expect("Uffd copy failed")
+                .map_err(HandlerError::UffdCopy)?
         };
 
         // Make sure the UFFD copied some bytes.
-        assert!(ret > 0);
+        if ret == 0 {
+            return Err(HandlerError::UffdNoOperation(String::from("copy")));
+        }
 
-        (start_addr, start_addr + ret)
+        Ok((start_addr, start_addr + ret))
     }
 
-    fn zero_out(&self, addr: usize) -> (usize, usize) {
-        let page_size = get_page_size().unwrap();
+    fn zero_out(&self, addr: usize) -> Result<(usize, usize)> {
+        let page_size = get_page_size().map_err(HandlerError::PageSize)?;
 
         // SAFETY: this is safe because the parameters are valid.
         let ret = unsafe {
             self.zeropage(addr as *mut _, page_size, true)
-                .expect("Uffd zeropage failed")
+                .map_err(HandlerError::UffdZero)?
         };
         // Make sure the UFFD zeroed out some bytes.
-        assert!(ret > 0);
+        if ret == 0 {
+            return Err(HandlerError::UffdNoOperation(String::from("zero")));
+        }
 
-        (addr, addr + ret)
+        Ok((addr, addr + ret))
     }
 }
 
@@ -90,8 +124,8 @@ where
         }
     }
 
-    pub fn serve_pf(&mut self, addr: usize) {
-        let page_size = get_page_size().unwrap();
+    pub fn serve_pf(&mut self, addr: usize) -> Result<()> {
+        let page_size = get_page_size().map_err(HandlerError::PageSize)?;
 
         // Find the start of the page that the current faulting address belongs to.
         let fault_page_addr = addr & !(page_size - 1);
@@ -108,22 +142,19 @@ where
                 //    event was received. This can be a consequence of guest reclaiming back its
                 //    memory from the host (through balloon device)
                 Some(MemPageState::Uninitialized) | Some(MemPageState::FromFile) => {
-                    let (start, end) = self.uffd.populate_from_file(self.backing_buffer, region);
+                    let (start, end) = self.uffd.populate_from_file(self.backing_buffer, region)?;
                     self.update_mem_state_mappings(start, end, MemPageState::FromFile);
-                    return;
+                    return Ok(());
                 }
                 Some(MemPageState::Removed) | Some(MemPageState::Anonymous) => {
-                    let (start, end) = self.uffd.zero_out(fault_page_addr);
+                    let (start, end) = self.uffd.zero_out(fault_page_addr)?;
                     self.update_mem_state_mappings(start, end, MemPageState::Anonymous);
-                    return;
+                    return Ok(());
                 }
                 None => {}
             }
         }
 
-        panic!(
-            "Could not find addr: {:?} within guest region mappings.",
-            addr
-        );
+        Err(HandlerError::AddressNotInRegion)
     }
 }
