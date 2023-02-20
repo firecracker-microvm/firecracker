b99ea94e399db185d1c9c03de9327f9af5f5d38f
Luminita Voicu <lumivo@amazon.com>
2023-02-20 17:59:44 +0200

    fix: uffd: transform panics into errors
    Signed-off-by: Luminita Voicu <lumivo@amazon.com>

--- src/uffd_handler/src/common.rs
+++ src/uffd_handler/src/common.rs
@@ -6,17 +6,27 @@ use std::os::unix::net::UnixStream;
 
 use utils::sock_ctrl_msg::ScmSocket;
 
+#[derive(Debug, thiserror::Error)]
+pub enum StreamError {
+    #[error("Invalid data received over uffd socket: {0}")]
+    InvalidData(std::string::FromUtf8Error),
+    #[error("No userfaultfd was received through the uffd socket.")]
+    NoFd,
+    #[error("Receiving data and file descriptor from uffd socket failed: {0}")]
+    RecvWithFd(utils::errno::Error),
+}
+
 /// Parse the unix stream received from the Firecracker process to obtain
 /// the userfaultfd used to poll for events and the message containing memory mappings.
-pub fn parse_unix_stream(stream: &UnixStream) -> (File, String) {
+pub fn parse_unix_stream(stream: &UnixStream) -> Result<(File, String), StreamError> {
     let mut message_buf = vec![0u8; 1024];
     let (bytes_read, file) = stream
         .recv_with_fd(&mut message_buf[..])
-        .expect("Cannot recv_with_fd");
+        .map_err(StreamError::RecvWithFd)?;
     message_buf.resize(bytes_read, 0);
 
-    let body = String::from_utf8(message_buf).unwrap();
-    let file = file.expect("Uffd not passed through UDS!");
+    let body = String::from_utf8(message_buf).map_err(StreamError::InvalidData)?;
+    let file = file.ok_or(StreamError::NoFd)?;
 
-    (file, body)
+    Ok((file, body))
 }
--- src/uffd_handler/src/main.rs
+++ src/uffd_handler/src/main.rs
@@ -12,27 +12,68 @@ mod memory_region;
 use std::fs::File;
 use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd};
 use std::os::unix::net::{UnixListener, UnixStream};
-use std::{mem, process, ptr};
+use std::{io, mem, process, ptr};
 
 use userfaultfd::Uffd;
 
-use crate::common::parse_unix_stream;
-use crate::handler::{PageFaultHandler, UffdManager};
+use crate::common::{parse_unix_stream, StreamError};
+use crate::handler::{HandlerError, PageFaultHandler, UffdManager};
 use crate::memory_region::{create_mem_regions, deserialize_mappings, MemPageState};
 
 const EXIT_CODE_ERROR: i32 = 1;
+/// Timeout for poll()ing on the userfaultfd for events.
+/// A negative value translates to an infinite timeout. Page faults are not meant to
+/// appear at a constant frequency, so depending on the guest workload, there can be
+/// situations when we need to wait longer for events.
+const POLL_TIMEOUT: i32 = -1;
 
-fn create_handler<T>() -> PageFaultHandler<Uffd>
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Failed to accept connection on userfaultfd socket: {0}")]
+    AcceptConnection(io::Error),
+    #[error("Failed to parse arguments: {0}")]
+    ArgumentParsing(String),
+    #[error("Failed to bind userfaultfd socket path: {0}")]
+    BindSocket(io::Error),
+    #[error(
+        "Size of snapshot memory file differs from the size of memory mappings.  Mappings \
+         received through uffd socket might be corrupted."
+    )]
+    CorruptedMemoryMappings,
+    #[error("Deserializing guest memory mappings failed: {0}")]
+    DeserializeMemoryMappings(serde_json::Error),
+    #[error("Failed to get metadata of snapshot memory file: {0}")]
+    Metadata(io::Error),
+    #[error("Mmap failed: {0}")]
+    Mmap(io::Error),
+    #[error("Failed to open snapshot memory file: {0}")]
+    Open(io::Error),
+    #[error("Parsing data stream failed: {0}")]
+    ParseStream(StreamError),
+    #[error("Failed to obtain Firecracker's credentials.")]
+    PeerCredentials,
+    #[error("Failed to create userfaultfd handler: {0}")]
+    UffdHandler(HandlerError),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+fn create_handler<U>() -> Result<PageFaultHandler<Uffd>>
 where
-    T: UffdManager,
+    U: UffdManager,
 {
-    let uffd_sock_path = std::env::args().nth(1).expect("No socket path given");
-    let mem_file_path = std::env::args().nth(2).expect("No memory file given");
+    let uffd_sock_path = std::env::args()
+        .nth(1)
+        .ok_or_else(|| Error::ArgumentParsing(String::from("No socket path provided")))?;
+    let mem_file_path = std::env::args()
+        .nth(2)
+        .ok_or_else(|| Error::ArgumentParsing(String::from("No memory file provided")))?;
 
-    let file = File::open(mem_file_path).expect("Cannot open memfile");
-    let size = file.metadata().unwrap().len() as usize;
+    let file = File::open(mem_file_path).map_err(Error::Open)?;
+    let metadata = file.metadata().map_err(Error::Metadata)?;
+    let size = metadata.len() as usize;
 
-    // mmap a memory area used to bring in the faulting regions.
+    // mmap() a memory area used to bring in the faulting regions.
     // SAFETY: Safe because the parameters are valid.
     let memfile_buffer = unsafe {
         libc::mmap(
@@ -45,29 +86,33 @@ where
         )
     };
     if memfile_buffer == libc::MAP_FAILED {
-        panic!("mmap failed");
+        return Err(Error::Mmap(io::Error::last_os_error()));
     }
 
     // Get Uffd from UDS. We'll use the uffd to handle PFs for Firecracker.
-    let listener = UnixListener::bind(uffd_sock_path).expect("Cannot bind to socket path");
+    let listener = UnixListener::bind(uffd_sock_path).map_err(Error::BindSocket)?;
+    let (stream, _) = listener.accept().map_err(Error::AcceptConnection)?;
 
-    let (stream, _) = listener.accept().expect("Cannot listen on UDS socket");
-
-    let (file, msg_body) = parse_unix_stream(&stream);
+    let (file, msg_body) = parse_unix_stream(&stream).map_err(Error::ParseStream)?;
     // SAFETY: Safe because it wraps the Uffd object around the valid raw file descriptor.
     let uffd = unsafe { Uffd::from_raw_fd(file.into_raw_fd()) };
 
     // Create guest memory regions from mappings received from Firecracker process.
-    let mappings = deserialize_mappings(&msg_body, size);
+    let mappings = deserialize_mappings(&msg_body, size)?;
     let mem_regions = create_mem_regions(mappings);
 
     // Get credentials of Firecracker process sent through the stream.
-    let creds: libc::ucred = get_peer_process_credentials(stream);
+    let creds: libc::ucred = get_peer_process_credentials(stream)?;
 
-    PageFaultHandler::new(mem_regions, memfile_buffer, uffd, creds.pid as u32)
+    Ok(PageFaultHandler::new(
+        mem_regions,
+        memfile_buffer,
+        uffd,
+        creds.pid as u32,
+    ))
 }
 
-fn get_peer_process_credentials(stream: UnixStream) -> libc::ucred {
+fn get_peer_process_credentials(stream: UnixStream) -> Result<libc::ucred> {
     let mut creds: libc::ucred = libc::ucred {
         pid: 0,
         gid: 0,
@@ -89,14 +134,17 @@ fn get_peer_process_credentials(stream: UnixStream) -> libc::ucred {
         )
     };
     if ret != 0 {
-        panic!("Failed to get peer process credentials");
+        return Err(Error::PeerCredentials);
     }
 
-    creds
+    Ok(creds)
 }
 
 fn main() {
-    let mut uffd_handler = create_handler::<Uffd>();
+    let mut uffd_handler = create_handler::<Uffd>().unwrap_or_else(|err| {
+        eprintln!("Creating userfaulfd handler failed: {:?}", err);
+        process::exit(EXIT_CODE_ERROR);
+    });
 
     let mut pollfd = libc::pollfd {
         fd: uffd_handler.uffd.as_raw_fd(),
@@ -107,26 +155,35 @@ fn main() {
     // Loop, handling incoming events on the userfaultfd file descriptor.
     loop {
         // SAFETY: Safe because fd, nfds and timeout are valid parameters.
-        let nready = unsafe { libc::poll(&mut pollfd, 1, -1) };
+        let nready = unsafe { libc::poll(&mut pollfd, 1, POLL_TIMEOUT) };
+        // Poll has an infinite timeout, therefore in theory, this case should never happen.
         if nready == -1 {
-            panic!("Could not poll for events!")
+            unreachable!();
         }
 
         // Read an event from the userfaultfd.
-        let event = uffd_handler.uffd.poll_fd();
+        let event = uffd_handler.uffd.poll_fd().unwrap_or_else(|err| {
+            eprintln!("Reading event from userfaultfd failed: {:?}", err);
+            process::exit(EXIT_CODE_ERROR);
+        });
 
         // We expect to receive either a Page Fault or Removed
         // event (if the balloon device is enabled).
         match event {
-            userfaultfd::Event::Pagefault { addr, .. } => uffd_handler.serve_pf(addr as usize),
+            userfaultfd::Event::Pagefault { addr, .. } => {
+                uffd_handler.serve_pf(addr as usize).unwrap_or_else(|err| {
+                    eprintln!("Processing page fault failed: {:?}", err);
+                    process::exit(EXIT_CODE_ERROR);
+                })
+            }
             userfaultfd::Event::Remove { start, end } => uffd_handler.update_mem_state_mappings(
                 start as usize,
                 end as usize,
                 MemPageState::Removed,
             ),
             _ => {
-                println!("Unexpected event received on userfaultfd.");
-                process::exit(EXIT_CODE_ERROR)
+                eprintln!("Unexpected event received on userfaultfd.");
+                process::exit(EXIT_CODE_ERROR);
             }
         }
     }
