43b381f6ef5278232f9d2723dbea6a1f73a564fb
Luminita Voicu <lumivo@amazon.com>
2023-02-24 17:00:20 +0200

    refactor: uffd: move GuestRegionUffdMapp to utils
    When userspace is in charge of handling page faults, Firecracker
    sends its guest memory mapping to the userfaulfd handler process.
    The mappings are represented through `GuestRegionUffdMapping`
    structure, which is defined in both vmm and uffd handler logic.
    This commit moves `GuestRegionUffdMapping` to the utils crate,
    in order to remove duplicate code and be able to use the same
    structure in both crates. This also has the advantage that any
    change to GuestRegionUffdMapping will be reflected in both
    Firecracker and uffd handler.
    
    Signed-off-by: Luminita Voicu <lumivo@amazon.com>

--- src/uffd_handler/src/common.rs
+++ src/uffd_handler/src/common.rs
@@ -1,11 +1,10 @@
 // Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-use serde::Deserialize;
 use std::collections::HashMap;
 use std::fs::File;
 use std::os::unix::net::UnixStream;
-use utils::get_page_size;
+use utils::{get_page_size, GuestRegionUffdMapping};
 
 use utils::sock_ctrl_msg::ScmSocket;
 
@@ -22,24 +21,6 @@ pub fn parse_unix_stream(stream: &UnixStream) -> (File, String) {
     let file = file.expect("Uffd not passed through UDS!");
 
     (file, body)
-}
-
-// This is the same with the one used in src/vmm.
-/// This describes the mapping between Firecracker base virtual address and offset in the
-/// buffer or file backend for a guest memory region. It is used to tell an external
-/// process/thread where to populate the guest memory data for this range.
-///
-/// E.g. Guest memory contents for a region of `size` bytes can be found in the backend
-/// at `offset` bytes from the beginning, and should be copied/populated into `base_host_address`.
-#[derive(Clone, Debug, Deserialize)]
-pub struct GuestRegionUffdMapping {
-    /// Base host virtual address where the guest memory contents for this region
-    /// should be copied/populated.
-    pub base_host_virt_addr: usize,
-    /// Region size.
-    pub size: usize,
-    /// Offset in the backend file/buffer where the region contents are.
-    pub offset: u64,
 }
 
 pub struct MemRegion {
