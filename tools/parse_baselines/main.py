firecracker-microvm/firecracker

commit 8269b419f353bc651f9e436a58c0e84e3a32bee9

chore(parse_baselines): include instance type in filenames
Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/parse_baselines/main.py
@@ -1,9 +1,10 @@
+#!/bin/env python3
 # Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Script used to calculate baselines from raw performance test output."""
 
 # We need to call sys.path.append(os.path.join(os.getcwd(), 'tests'))
-# before importing SUPPORTED_KERNELS. But this results in a pyling error.
+# before importing SUPPORTED_KERNELS. But this results in a pylint error.
 # pylint: disable=wrong-import-position
 
 import argparse
@@ -56,7 +57,7 @@ def read_data_files(args):
     assert os.path.isdir(args.data_folder)
 
     res_files = [
-        f"{filename}_results_{args.kernel}.json"
+        f"{filename}_results_{args.instance}_{args.kernel}.json"
         for filename in OUTPUT_FILENAMES[args.test]
     ]
     # Get all files in the dir tree that have the right name.

tests/integration_tests/performance/conftest.py
@@ -10,9 +10,9 @@ import json
 
 import pytest
 
+from framework import defs, utils
+from framework.properties import global_props
 from framework.stats import core
-from framework import defs
-from framework import utils
 
 
 # pylint: disable=too-few-public-methods
@@ -41,7 +41,10 @@ class JsonFileDumper(ResultsDumperInterface):
         # Create the root directory, if it doesn't exist.
         self._root_path.mkdir(exist_ok=True)
         kv = utils.get_kernel_version(level=1)
-        self._results_file = self._root_path / f"{test_name}_results_{kv}.json"
+        instance = global_props.instance
+        self._results_file = (
+            self._root_path / f"{test_name}_results_{instance}_{kv}.ndjson"
+        )
 
     def dump(self, result):
         """Dump the results in JSON format."""

commit 83023a56bc1c29fe10bec65c5ed4d4ad96bac9ca

fix(parse_baselines): fix extension of test results
Fixes: 8269b419f353bc651f9e436a58c0e84e3a32bee9
Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/parse_baselines/main.py
@@ -57,7 +57,7 @@ def read_data_files(args):
     assert os.path.isdir(args.data_folder)
 
     res_files = [
-        f"{filename}_results_{args.instance}_{args.kernel}.json"
+        f"{filename}_results_{args.instance}_{args.kernel}.ndjson"
         for filename in OUTPUT_FILENAMES[args.test]
     ]
     # Get all files in the dir tree that have the right name.