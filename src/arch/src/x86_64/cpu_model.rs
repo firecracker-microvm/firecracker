48cfce5ad68ed0a99e81d11efcfa28a8891e5a7d
Nikita Kalyazin <kalyazin@amazon.com>
2023-02-28 14:53:00 +0000

    chore(arch): remove redundant clippy exception
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/arch/src/x86_64/cpu_model.rs
+++ src/arch/src/x86_64/cpu_model.rs
@@ -24,7 +24,6 @@ impl CpuModel {
     pub fn get_cpu_model() -> Self {
         // SAFETY: This operation is safe as long as the processor implements this CPUID function.
         // 0x1 is the defined code for getting the processor version information.
-        #[allow(clippy::undocumented_unsafe_blocks)]
         let eax = unsafe { host_cpuid(0x1) }.eax;
         CpuModel::from(&eax)
     }
