797d59c0ab3ba070224d29c4fc1bae27227e84fa
Matthew Schlebusch <schlebus@amazon.com>
2023-03-02 07:01:54 +0000

    feat: Add wrappers for CPU config
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

--- src/vmm/src/guest_config/mod.rs
+++ src/vmm/src/guest_config/mod.rs
@@ -1,6 +1,16 @@
 // Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(target_arch = "x86_64")]
 pub mod cpuid;
+
 /// cpuid static template functions.
 pub(crate) mod static_templates;
+
+/// Module containing type implementations needed for aarch64 (ARM) CPU configuration
+#[cfg(target_arch = "aarch64")]
+pub mod aarch64;
+
+/// Module containing type implementations needed for x86 CPU configuration
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64;
