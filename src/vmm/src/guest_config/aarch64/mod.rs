5f511f6769572e219769502aefc92258fcbf9728
Matthew Schlebusch <schlebus@amazon.com>
2023-03-02 07:01:54 +0000

    feat: Add wrappers for CPU config
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

---
+++ src/vmm/src/guest_config/aarch64/mod.rs
@@ -0,0 +1,12 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::collections::HashMap;
+
+/// CPU configuration for aarch64 CPUs
+pub struct Aarch64CpuConfiguration {
+    /// Register values as a key pair
+    /// Key: Register pointer
+    /// Value: Register value
+    pub regs: HashMap<u64, u128>,
+}
