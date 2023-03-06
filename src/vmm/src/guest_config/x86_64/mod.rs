ff547b501ee389f826639fc2415b1010ed863ca6
Matthew Schlebusch <schlebus@amazon.com>
2023-03-06 09:09:50 +0000

    feat: Add MSR deserializer for CPU templates
    Signed-off-by: Matthew Schlebusch <schlebus@amazon.com>

--- src/vmm/src/guest_config/x86_64/mod.rs
+++ src/vmm/src/guest_config/x86_64/mod.rs
@@ -3,14 +3,13 @@
 
 use std::collections::HashMap;
 
-use crate::guest_config::cpuid::Cpuid;
-
 /// CPU configuration for x86_64 CPUs
+#[derive(Clone, Debug)]
 pub struct X86_64CpuConfiguration {
     /// CPUID configuration
-    pub cpuid: Cpuid,
+    // pub cpuid: Cpuid,
     /// Register values as a key pair for model specific registers
     /// Key: MSR address
     /// Value: MSR value
-    pub msrs: HashMap<u64, u64>,
+    pub msrs: HashMap<u32, u64>,
 }
