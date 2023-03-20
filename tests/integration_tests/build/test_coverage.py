7a8ce5bfc2874feca54492a88e66ccda7e5f4661
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-03-20 16:27:36 +0000

    fix: Remove `bit-fields` and `bit-fields-macros`
    Removes `bit-fields` and `bit-fields-macros` crates as their are not
    frequently enough to warrant the additional complexity.
    
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -17,9 +17,9 @@ from host_tools import proc
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 78.28, "AMD": 79.12, "ARM": 82.10}
+    COVERAGE_DICT = {"Intel": 81.99, "AMD": 84.68, "ARM": 82.44}
 else:
-    COVERAGE_DICT = {"Intel": 76.02, "AMD": 76.86, "ARM": 79.06}
+    COVERAGE_DICT = {"Intel": 79.51, "AMD": 82.19, "ARM": 79.37}
 
 PROC_MODEL = proc.proc_type()
 
