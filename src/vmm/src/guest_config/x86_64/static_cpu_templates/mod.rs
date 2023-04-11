5902ea0d9abe9cae5baae79b9ca3ea5b859c6cbc
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-04-11 15:23:41 +0100

    refactor: moved x86_64 static cpu templates
    Moved from `guest_config/static_templates`
    into `guest_config/x86_64/static_templates`
    
    This is done to be consistent with placement of
    static templates for aarch64.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- src/vmm/src/guest_config/x86_64/static_cpu_templates/mod.rs
+++ src/vmm/src/guest_config/x86_64/static_cpu_templates/mod.rs
@@ -5,6 +5,17 @@ use serde::{Deserialize, Serialize};
 use versionize::{VersionMap, Versionize, VersionizeError, VersionizeResult};
 use versionize_derive::Versionize;
 
+/// Module with C3 CPU template for x86_64
+pub mod c3;
+/// Module with T2 CPU template for x86_64
+pub mod t2;
+/// Module with T2A CPU template for x86_64
+pub mod t2a;
+/// Module with T2CL CPU template for x86_64
+pub mod t2cl;
+/// Module with T2S CPU template for x86_64
+pub mod t2s;
+
 /// Template types available for configuring the x86 CPU features that map
 /// to EC2 instances.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Versionize)]
