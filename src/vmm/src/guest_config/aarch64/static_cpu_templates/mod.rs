6ead782ae093694ae7ae3d9a12871dafe4c923f1
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-04-11 15:24:23 +0100

    feat: added v1n1 aarch64 cpu template
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- src/vmm/src/guest_config/aarch64/static_cpu_templates/mod.rs
+++ src/vmm/src/guest_config/aarch64/static_cpu_templates/mod.rs
@@ -5,6 +5,9 @@ use serde::{Deserialize, Serialize};
 use versionize::{VersionMap, Versionize, VersionizeError, VersionizeResult};
 use versionize_derive::Versionize;
 
+/// Module with V1N1 CPU template for aarch64
+pub mod v1n1;
+
 /// Templates available for configuring the supported ARM CPU types.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Versionize)]
 pub enum StaticCpuTemplate {
