d467ffa74b93f805a5681bd928b96272c8ba3b95
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-04-11 15:14:13 +0100

    refactor: updated VmConfig and VmConfigBuilder
    Renamed:
    - VmConfig -> MachineConfig
    - VmUpdateConfig -> MachineConfigUpdate
    
    This is done to better separate structs that are used in
    `/machine-config` API endpoint from `VmConfig` that is used internally.
    
    Reworked VmConfigBuilder:
    - Renamed back to VmConfig
    - Replaced internal fields with fields from MachineConfig with
      the only difference is `cpu_template`, which is now
      `Option<CpuTemplateType>`.
    
    This is done to make `cpu_template` have only 3 states in 1 field:
    - None -> no static or custom template specified
    - Some(Static) -> static template is specified
    - Some(Custom) -> custom template is specified
    All of these states are valid.
    
    Previously there were 4 states with 2 fields:
    - (Static::None, None) -> no static or custom template is specified
    - (Static::Some, None) -> static template is specified
    - (Static::None, Some(Custom)) -> custom template is specified
    - (Static::Some, Some(Custom)) -> both types of templates are
    specified.
    
    This implementation had 1 invalid state (Static::Some, Some(Custom))
    which should have been explicitly checked and disallowed.
    
    3 state approach is less error prone, because it eliminates invalid
    states
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- src/vmm/src/utilities/mock_resources/mod.rs
+++ src/vmm/src/utilities/mock_resources/mod.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 use crate::guest_config::templates::CpuTemplate;
 use crate::resources::VmResources;
 use crate::vmm_config::boot_source::BootSourceConfig;
-use crate::vmm_config::machine_config::{VmConfig, VmUpdateConfig};
+use crate::vmm_config::machine_config::{MachineConfig, MachineConfigUpdate};
 
 pub const DEFAULT_BOOT_ARGS: &str = "reboot=k panic=1 pci=off";
 #[cfg(target_arch = "x86_64")]
@@ -78,19 +78,19 @@ impl MockVmResources {
         self
     }
 
-    pub fn with_vm_config(mut self, vm_config: VmConfig) -> Self {
-        let machine_config = VmUpdateConfig::from(vm_config);
+    pub fn with_vm_config(mut self, vm_config: MachineConfig) -> Self {
+        let machine_config = MachineConfigUpdate::from(vm_config);
         self.0.update_vm_config(&machine_config).unwrap();
         self
     }
 
     pub fn set_cpu_template(&mut self, cpu_template: CpuTemplate) {
-        self.0.vm_config.custom_cpu_template = Some(cpu_template);
+        self.0.vm_config.set_custom_cpu_template(cpu_template);
     }
 }
 
 #[derive(Default)]
-pub struct MockVmConfig(VmConfig);
+pub struct MockVmConfig(MachineConfig);
 
 impl MockVmConfig {
     pub fn new() -> MockVmConfig {
@@ -105,4 +105,4 @@ impl MockVmConfig {
 
 generate_from!(MockBootSourceConfig, BootSourceConfig);
 generate_from!(MockVmResources, VmResources);
-generate_from!(MockVmConfig, VmConfig);
+generate_from!(MockVmConfig, MachineConfig);
