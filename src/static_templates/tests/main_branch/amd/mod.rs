e0d541529c17827d9f2c17aa06fc0ad9e4a1da8f
Takahiro Itazuri <itazur@amazon.com>
2023-04-14 20:26:37 +0000

    refactor: move static CPU templates
    Just move static CPU templates defined with internal data structures
    used in custom CPU template into a temporal directory inside vmm crate
    with no functional changes, in order to wire up them in successive
    commits.
    
    The moved static CPU templates were added in PR #3598 in
    static_templates crate temporarily to check that they are compatible
    with static CPU templates in the main branch.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/static_templates/tests/main_branch/amd/mod.rs
+++
@@ -1,6 +0,0 @@
-// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-/// Follows the T2 template for setting up the CPUID.
-/// Also disables AMD-specific features.
-pub mod t2a;
