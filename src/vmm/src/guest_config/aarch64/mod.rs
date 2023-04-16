06a83a09e4826838ace0252044eec14034c8b067
Takahiro Itazuri <itazur@amazon.com>
2023-04-16 15:58:18 +0000

    refactor(vmm): 2-step CpuConfiguration build for aarch64
    As done in x86_64, build a custom `CpuConfiguration` with 2 steps:
    1. Construct a base `CpuConfiguration`.
    2. Apply CPU template to the default `CpuConfiguration`.
    
    By providing the same interface between x86_64 and aarch64, it becomes
    possible to unify some operations.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>
    Co-authored-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- src/vmm/src/guest_config/aarch64/mod.rs
+++ src/vmm/src/guest_config/aarch64/mod.rs
@@ -4,16 +4,15 @@
 /// Module with CPU templates for aarch64
 pub mod static_cpu_templates;
 
-use kvm_ioctls::VcpuFd;
 pub use static_cpu_templates::*;
 
-use super::templates::{CpuTemplateType, CustomCpuTemplate};
+use super::templates::CustomCpuTemplate;
 use crate::arch::regs::{Aarch64Register, Error as ArchError};
 
 /// Errors thrown while configuring templates.
 #[derive(Debug, PartialEq, Eq, thiserror::Error)]
 #[error("Failed to create a guest cpu configuration: {0}")]
-pub struct Error(#[from] ArchError);
+pub struct Error(#[from] pub ArchError);
 
 /// CPU configuration for aarch64
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
@@ -24,56 +23,16 @@ pub struct CpuConfiguration {
 
 impl CpuConfiguration {
     /// Creates new guest CPU config based on the provided template
-    pub fn new(vcpu: &VcpuFd, template: &Option<CpuTemplateType>) -> Result<Self, Error> {
-        match template {
-            Some(ref cpu_template) => Self::with_template(vcpu, cpu_template),
-            None => Ok(Self::default()),
+    pub fn apply_template(mut self, template: &CustomCpuTemplate) -> Result<Self, Error> {
+        for (modifier, reg) in template.reg_modifiers.iter().zip(self.regs.iter_mut()) {
+            reg.value = modifier.bitmap.apply(reg.value);
         }
-    }
-
-    /// Creates new guest CPU config based on the provided template
-    fn with_template(vcpu: &VcpuFd, template: &CpuTemplateType) -> Result<Self, Error> {
-        match template {
-            CpuTemplateType::Custom(template) => Self::with_applied_template(vcpu, template),
-            CpuTemplateType::Static(StaticCpuTemplate::V1N1) => {
-                let template = static_cpu_templates::v1n1::v1n1();
-                Self::with_applied_template(vcpu, &template)
-            }
-            _ => unreachable!("Options other than V1N1 are invalid"),
-        }
-    }
-
-    /// Creates new guest CPU config based on the provided template
-    fn with_applied_template(vcpu: &VcpuFd, template: &CustomCpuTemplate) -> Result<Self, Error> {
-        let regs = template
-            .reg_modifiers
-            .iter()
-            .map(|modifier| {
-                vcpu.get_one_reg(modifier.addr)
-                    .map(|value| Aarch64Register {
-                        id: modifier.addr,
-                        value: modifier.bitmap.apply(value),
-                    })
-                    .map_err(|e| Error(ArchError::GetSysRegister(modifier.addr, e)))
-            })
-            .collect::<Result<Vec<_>, Error>>()?;
-
-        Ok(Self { regs })
+        Ok(self)
     }
 
     /// Returns ids of registers that are changed
     /// by this template
     pub fn register_ids(&self) -> Vec<u64> {
         self.regs.iter().map(|reg| reg.id).collect()
-    }
-
-    /// Applies cpu template to vcpu
-    /// Used inside Vcpu to configure it
-    pub fn apply(&self, vcpu: &VcpuFd) -> Result<(), ArchError> {
-        for Aarch64Register { id, value } in self.regs.iter() {
-            vcpu.set_one_reg(*id, *value)
-                .map_err(|error| ArchError::SetSysRegister(*id, error))?;
-        }
-        Ok(())
     }
 }
