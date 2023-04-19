83caa5b6d1f78dc7e553ed39458ebf2dc30cdfab
Takahiro Itazuri <itazur@amazon.com>
2023-04-19 17:24:37 +0000

    feat(tool): add CPU config dump feature
    Add a feature to dump CPU configuration exposed to guests in the custom
    CPU template JSON format.
    
    To create a custom CPU template, customers need to check and comprehend
    what CPU configuration are exposed to guests by default. Even after
    custom CPU template creation is done, it is required to monitor if the
    default CPU configuration has not been changed since its creation. By
    storing the dumped CPU configuration at the time of CPU template
    creation as a metadata and comparing it with the current one, customers
    can make sure that it has not been changed.
    
    The CPU configuration dump feature takes a config file same as
    firecracker, builds a microVM based on it by using
    `build_microvm_for_boot()` function from vmm crate, and sends
    `VcpuEvent::DumpCpuConfig` events to vCPUs to obtain CPU configuration.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/cpu-template-helper/src/main.rs
+++ src/cpu-template-helper/src/main.rs
@@ -1,12 +1,22 @@
 // Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::fs::{read_to_string, write};
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
+
+mod dump;
 
 const EXIT_CODE_ERROR: i32 = 1;
 
 #[derive(Debug, thiserror::Error)]
-enum Error {}
+enum Error {
+    #[error("Failed to operate file: {0}")]
+    FileIo(#[from] std::io::Error),
+    #[error("Failed to dump CPU configuration: {0}")]
+    DumpCpuConfig(#[from] dump::Error),
+}
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -18,18 +28,30 @@ struct Cli {
 }
 
 #[derive(Subcommand)]
-enum Command {}
+enum Command {
+    /// Dump CPU configuration in custom CPU template format.
+    Dump {
+        /// Path of firecracker config file.
+        #[arg(short, long, value_name = "PATH")]
+        config: PathBuf,
+        /// Path of output file.
+        #[arg(short, long, value_name = "PATH", default_value = "cpu_config.json")]
+        output: PathBuf,
+    },
+}
 
-// TODO: remove the following allowance of clippy warnings.
-#[allow(unreachable_code)]
 fn run(cli: Cli) -> Result<()> {
-    match cli.command {};
+    match cli.command {
+        Command::Dump { config, output } => {
+            let config = read_to_string(config)?;
+            let dump_result = dump::dump(config)?;
+            write(output, dump_result)?;
+        }
+    };
 
     Ok(())
 }
 
-// TODO: remove the following allowance of clippy warnings.
-#[allow(unused_variables, unreachable_code)]
 fn main() {
     let cli = Cli::parse();
 
