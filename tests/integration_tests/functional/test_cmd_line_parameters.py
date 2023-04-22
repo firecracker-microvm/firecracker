3f6ad64bb01486dee916b0bfa3731c5cada6cfaf
Pablo Barbáchano <pablob@amazon.com>
2023-04-22 12:32:52 +0200

    test: revamp snapshot support to Microvm
    Add some abstractions and functions around the snapshotting process.
    This is something that otherwise gets repeated over and over in
    integration tests, and makes debugging tests hard.
    
    Cleaning up existing tests is left over to the next changes in the
    series.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/integration_tests/functional/test_cmd_line_parameters.py
+++ tests/integration_tests/functional/test_cmd_line_parameters.py
@@ -2,63 +2,43 @@
 # SPDX-License-Identifier: Apache-2.0
 """Tests that ensure the correctness of the command line parameters."""
 
-import logging
 import platform
 from pathlib import Path
 
 import host_tools.logging as log_tools
-from framework.builder import MicrovmBuilder, SnapshotBuilder, SnapshotType
 from framework.utils import run_cmd
 from host_tools.cargo_build import get_firecracker_binaries
 
 
-def test_describe_snapshot_all_versions(bin_cloner_path, firecracker_release):
+def test_describe_snapshot_all_versions(
+    microvm_factory, guest_kernel, rootfs, firecracker_release
+):
     """
     Test `--describe-snapshot` correctness for all snapshot versions.
 
     For each release create a snapshot and verify the data version of the
     snapshot state file.
     """
-    logger = logging.getLogger("describe_snapshot")
-    builder = MicrovmBuilder(bin_cloner_path)
     jailer = firecracker_release.jailer()
     target_version = firecracker_release.snapshot_version
-
-    logger.info(
-        "Creating snapshot with Firecracker: %s", firecracker_release.local_path()
+    vm = microvm_factory.build(
+        guest_kernel,
+        rootfs,
+        fc_binary_path=firecracker_release.local_path(),
+        jailer_binary_path=jailer.local_path(),
     )
-    logger.info("Using Jailer: %s", jailer.local_path())
-    logger.info("Using target version: %s", target_version)
-
-    vm_instance = builder.build_vm_nano(
-        fc_binary=firecracker_release.local_path(),
-        jailer_binary=jailer.local_path(),
-        diff_snapshots=True,
-    )
-    vm = vm_instance.vm
+    vm.spawn()
+    vm.basic_config(track_dirty_pages=True)
     vm.start()
-
-    # Create a snapshot builder from a microvm.
-    snapshot_builder = SnapshotBuilder(vm)
-    disks = [vm_instance.disks[0].local_path()]
-
-    # Version 0.24 and greater have Diff support.
-    snap_type = SnapshotType.DIFF
-
-    snapshot = snapshot_builder.create(
-        disks,
-        vm_instance.ssh_key,
-        snapshot_type=snap_type,
-    )
-    logger.debug("========== Firecracker create snapshot log ==========")
-    logger.debug(vm.log_data)
+    snapshot = vm.snapshot_diff()
+    print("========== Firecracker create snapshot log ==========")
+    print(vm.log_data)
     vm.kill()
 
     # Fetch Firecracker binary for the latest version
     fc_binary, _ = get_firecracker_binaries()
     # Verify the output of `--describe-snapshot` command line parameter
     cmd = [fc_binary] + ["--describe-snapshot", snapshot.vmstate]
-
     code, stdout, stderr = run_cmd(cmd)
     assert code == 0, stderr
     assert stderr == ""
@@ -145,23 +125,12 @@ def test_cli_metrics_if_resume_no_metrics(test_microvm_with_api, microvm_factory
     uvm1.spawn(metrics_path=metrics_path)
     uvm1.basic_config()
     uvm1.start()
-
-    mem_path = Path(uvm1.jailer.chroot_path()) / "test.mem"
-    snapshot_path = Path(uvm1.jailer.chroot_path()) / "test.snap"
-    uvm1.pause_to_snapshot(
-        mem_file_path=mem_path.name,
-        snapshot_path=snapshot_path.name,
-    )
-    assert mem_path.exists()
+    snapshot = uvm1.snapshot_full()
 
     # When: restoring from the snapshot
     uvm2 = microvm_factory.build()
     uvm2.spawn()
-    uvm2.restore_from_snapshot(
-        snapshot_vmstate=snapshot_path,
-        snapshot_mem=mem_path,
-        snapshot_disks=[uvm1.rootfs_file],
-    )
+    uvm2.restore_from_snapshot(snapshot)
 
     # Then: the old metrics configuration does not exist
     metrics2 = Path(uvm2.jailer.chroot_path()) / metrics_path.name
