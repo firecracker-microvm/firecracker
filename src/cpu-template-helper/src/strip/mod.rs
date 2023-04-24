5598fdc286a5a6a9d93d0f014a3a5721eaedda68
Takahiro Itazuri <itazur@amazon.com>
2023-04-24 17:17:27 +0000

    feat(tool): Add remove_common() for strip feature
    Add `remove_common()` to retrieve common items shared by all the given
    list of `HashSet` and remove the shared items from them.
    
    Implementation for converting a list of CPU templates
    `Vec<CustomCpuTemplate>` to a list of sets of modifiers
    `Vec<HashSet<Modifier>>` is required for each architecture (x86_64 and
    aarch64), because they have different structures for
    `CustomCpuTemplate`. But the core part to remove shared modifiers can be
    the same logic and this commit adds it.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/cpu-template-helper/src/strip/mod.rs
+++ src/cpu-template-helper/src/strip/mod.rs
@@ -1,6 +1,9 @@
 // Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::HashSet;
+use std::hash::Hash;
+
 use vmm::guest_config::templates::CustomCpuTemplate;
 
 #[derive(Debug, thiserror::Error)]
@@ -25,4 +28,42 @@ pub fn strip(input: Vec<String>) -> Result<Vec<String>, Error> {
         .map(serde_json::to_string_pretty)
         .collect::<Result<Vec<_>, serde_json::Error>>()?;
     Ok(result)
+}
+
+pub fn remove_common<T>(sets: &mut [HashSet<T>])
+where
+    T: Clone + Hash + Eq + PartialEq,
+{
+    // Get common items shared by all the sets.
+    let mut common = sets[0].clone();
+    common.retain(|item| sets[1..].iter().all(|set| set.contains(item)));
+
+    // Remove the common items from all the sets.
+    for item in common {
+        for set in sets.iter_mut() {
+            set.remove(&item);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_common() {
+        let mut input = vec![
+            HashSet::from([0, 1, 2, 3]),
+            HashSet::from([0, 2, 4]),
+            HashSet::from([0, 1, 2, 5, 6]),
+        ];
+        let expected = vec![
+            HashSet::from([1, 3]),
+            HashSet::from([4]),
+            HashSet::from([1, 5, 6]),
+        ];
+
+        remove_common(&mut input);
+        assert_eq!(input, expected);
+    }
 }
