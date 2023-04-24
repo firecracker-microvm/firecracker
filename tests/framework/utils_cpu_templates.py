efa416279792d7d24d65630f7651a0f94f1e02a1
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-04-24 17:17:29 +0100

    feat: added V1N1 template for aarch64 to tests
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- tests/framework/utils_cpu_templates.py
+++ tests/framework/utils_cpu_templates.py
@@ -12,23 +12,29 @@ import framework.utils_cpuid as cpuid_utils
 INTEL_TEMPLATES = ["C3", "T2", "T2CL", "T2S"]
 # All existing CPU templates available on AMD
 AMD_TEMPLATES = ["T2A"]
-# All existing CPU templates
-ALL_TEMPLATES = INTEL_TEMPLATES + AMD_TEMPLATES
+# All existing CPU templates available on ARM
+ARM_TEMPLATES = ["V1N1"]
 
 
 def get_supported_cpu_templates():
     """
     Return the list of CPU templates supported by the platform.
     """
-    vendor = cpuid_utils.get_cpu_vendor()
-    if vendor == cpuid_utils.CpuVendor.INTEL:
-        # T2CL template is only supported on Cascade Lake and newer CPUs.
-        skylake_model = "Intel(R) Xeon(R) Platinum 8175M CPU @ 2.50GHz"
-        if cpuid_utils.get_cpu_model_name() == skylake_model:
-            return set(INTEL_TEMPLATES) - set(["T2CL"])
-        return INTEL_TEMPLATES
-    if vendor == cpuid_utils.CpuVendor.AMD:
-        return AMD_TEMPLATES
+    match cpuid_utils.get_cpu_vendor():
+        case cpuid_utils.CpuVendor.INTEL:
+            # T2CL template is only supported on Cascade Lake and newer CPUs.
+            skylake_model = "Intel(R) Xeon(R) Platinum 8175M CPU @ 2.50GHz"
+            if cpuid_utils.get_cpu_model_name() == skylake_model:
+                return set(INTEL_TEMPLATES) - set(["T2CL"])
+            return INTEL_TEMPLATES
+        case cpuid_utils.CpuVendor.AMD:
+            return AMD_TEMPLATES
+        case cpuid_utils.CpuVendor.ARM:
+            match cpuid_utils.get_instance_type():
+                case "m6g.metal":
+                    return []
+                case "c7g.metal":
+                    return ARM_TEMPLATES
     return []
 
 
