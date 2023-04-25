8a0bff418393537f88c16b8e2d3cc9deca307e34
Takahiro Itazuri <itazur@amazon.com>
2023-04-25 20:19:50 +0000

    feat(tool): Apply register-level strip
    Strip shared CPUID modifiers with a granularity of register.
    
    Previously the strip feature for CPUID modifiers work in leaf-wise,
    because the template format for CPUID has a nested structure where leaf
    modifiers have register modifiers inside and it is not straightforward.
    This commit introduces a new data structure `CpuidModifier` that has a
    flatten modifier and implements `From` trait for type conversion.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/cpu-template-helper/src/strip/x86_64.rs
+++ src/cpu-template-helper/src/strip/x86_64.rs
@@ -3,9 +3,73 @@
 
 use std::collections::HashSet;
 
+use vmm::guest_config::cpuid::KvmCpuidFlags;
+use vmm::guest_config::templates::x86_64::{
+    CpuidLeafModifier, CpuidRegister, CpuidRegisterModifier, RegisterValueFilter,
+};
 use vmm::guest_config::templates::CustomCpuTemplate;
 
 use crate::strip::remove_common;
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct CpuidModifier {
+    leaf: u32,
+    subleaf: u32,
+    flags: KvmCpuidFlags,
+    register: CpuidRegister,
+    bitmap: RegisterValueFilter,
+}
+
+struct CpuidModifierSet(HashSet<CpuidModifier>);
+
+impl From<Vec<CpuidLeafModifier>> for CpuidModifierSet {
+    fn from(leaf_modifiers: Vec<CpuidLeafModifier>) -> Self {
+        let mut set = HashSet::new();
+        for leaf_modifier in leaf_modifiers {
+            for reg_modifier in leaf_modifier.modifiers {
+                set.insert(CpuidModifier {
+                    leaf: leaf_modifier.leaf,
+                    subleaf: leaf_modifier.subleaf,
+                    flags: leaf_modifier.flags,
+                    register: reg_modifier.register,
+                    bitmap: reg_modifier.bitmap,
+                });
+            }
+        }
+        CpuidModifierSet(set)
+    }
+}
+
+impl From<CpuidModifierSet> for Vec<CpuidLeafModifier> {
+    fn from(cpuid_modifiers: CpuidModifierSet) -> Self {
+        let mut leaf_modifiers = Vec::<CpuidLeafModifier>::new();
+        for modifier in cpuid_modifiers.0 {
+            let leaf_modifier = leaf_modifiers.iter_mut().find(|leaf_modifier| {
+                leaf_modifier.leaf == modifier.leaf
+                    && leaf_modifier.subleaf == modifier.subleaf
+                    && leaf_modifier.flags == modifier.flags
+            });
+
+            if let Some(leaf_modifier) = leaf_modifier {
+                leaf_modifier.modifiers.push(CpuidRegisterModifier {
+                    register: modifier.register,
+                    bitmap: modifier.bitmap,
+                });
+            } else {
+                leaf_modifiers.push(CpuidLeafModifier {
+                    leaf: modifier.leaf,
+                    subleaf: modifier.subleaf,
+                    flags: modifier.flags,
+                    modifiers: vec![CpuidRegisterModifier {
+                        register: modifier.register,
+                        bitmap: modifier.bitmap,
+                    }],
+                });
+            }
+        }
+        leaf_modifiers
+    }
+}
 
 #[allow(dead_code)]
 pub fn strip(templates: Vec<CustomCpuTemplate>) -> Vec<CustomCpuTemplate> {
@@ -14,7 +78,7 @@ pub fn strip(templates: Vec<CustomCpuTemplate>) -> Vec<CustomCpuTemplate> {
         .into_iter()
         .map(|template| {
             (
-                template.cpuid_modifiers.into_iter().collect::<HashSet<_>>(),
+                CpuidModifierSet::from(template.cpuid_modifiers).0,
                 template.msr_modifiers.into_iter().collect::<HashSet<_>>(),
             )
         })
@@ -29,10 +93,16 @@ pub fn strip(templates: Vec<CustomCpuTemplate>) -> Vec<CustomCpuTemplate> {
         .into_iter()
         .zip(msr_modifiers_sets.into_iter())
         .map(|(cpuid_modifiers_set, msr_modifiers_set)| {
-            let mut cpuid_modifiers = cpuid_modifiers_set.into_iter().collect::<Vec<_>>();
+            let mut cpuid_modifiers: Vec<CpuidLeafModifier> =
+                CpuidModifierSet(cpuid_modifiers_set).into();
             let mut msr_modifiers = msr_modifiers_set.into_iter().collect::<Vec<_>>();
 
             cpuid_modifiers.sort_by_key(|modifier| (modifier.leaf, modifier.subleaf));
+            cpuid_modifiers.iter_mut().for_each(|leaf_modifier| {
+                leaf_modifier
+                    .modifiers
+                    .sort_by_key(|modifier| modifier.register.clone())
+            });
             msr_modifiers.sort_by_key(|modifier| modifier.addr);
 
             CustomCpuTemplate {
@@ -93,8 +163,9 @@ mod tests {
     //   different, it should be removed.
     // * As CPUID leaf 0x1 / subleaf 0x0 modifier only exists in the second template, it should be
     //   preserved.
-    // * As CPUID leaf 0x2 / subleaf 0x1 modifier exists in all the templates but EBX values are
-    //   different, the leaf modifier (not only the register modifier EBX) should be preserved.
+    // * As CPUID leaf 0x2 / subleaf 0x1 modifier exists in all the templates, EAX values are same
+    //   but EBX values are different, the EAX register modifier should be removed and the EBX
+    //   register modifier should be preserved.
     #[rustfmt::skip]
     fn build_input_cpuid_templates() -> Vec<CustomCpuTemplate> {
         vec![
@@ -146,7 +217,6 @@ mod tests {
             CustomCpuTemplate {
                 cpuid_modifiers: vec![
                     cpuid_leaf_modifier!(0x2, 0x1, KvmCpuidFlags::SIGNIFICANT_INDEX, vec![
-                        cpuid_reg_modifier!(Eax, 0x0),
                         cpuid_reg_modifier!(Ebx, 0x0),
                     ]),
                 ],
@@ -158,7 +228,6 @@ mod tests {
                         cpuid_reg_modifier!(Eax, 0x0),
                     ]),
                     cpuid_leaf_modifier!(0x2, 0x1, KvmCpuidFlags::SIGNIFICANT_INDEX, vec![
-                        cpuid_reg_modifier!(Eax, 0x0),
                         cpuid_reg_modifier!(Ebx, 0x1),
                     ]),
                 ],
@@ -167,7 +236,6 @@ mod tests {
             CustomCpuTemplate {
                 cpuid_modifiers: vec![
                     cpuid_leaf_modifier!(0x2, 0x1, KvmCpuidFlags::SIGNIFICANT_INDEX, vec![
-                        cpuid_reg_modifier!(Eax, 0x0),
                         cpuid_reg_modifier!(Ebx, 0x2),
                     ]),
                 ],
