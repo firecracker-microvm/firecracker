90baeadadbe01895ecdb09e9f857e08e53022087
Nikita Kalyazin <kalyazin@amazon.com>
2023-04-28 09:25:59 +0000

    fix(vmm): call msrs_to_save_by_cpuid() on amd
    msrs_to_save_by_cpuid() was made Intel-specific in
    
    commit cbb2c0566cbeed1ebe4d374e4678120ca0c9c4b7
    Author: Jonathan Woollett-Light <jcawl@amazon.co.uk>
    Date:   Mon Jan 23 17:46:45 2023 +0000
    
        feat: Updated CPUID
    
    however it should not be.
    
    This change makes it common for Intel and AMD again.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/vmm/src/guest_config/cpuid/common.rs
+++ src/vmm/src/guest_config/cpuid/common.rs
@@ -2,9 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 #![allow(clippy::restriction)]
 
-use super::CpuidTrait;
-use crate::guest_config::cpuid::intel::intel_msrs_to_save_by_cpuid;
-
 /// Error type for [`get_cpuid`].
 #[derive(Debug, thiserror::Error, PartialEq, Eq)]
 pub enum GetCpuidError {
@@ -63,19 +60,88 @@ pub fn get_vendor_id_from_host() -> Result<[u8; 12], GetCpuidError> {
 pub struct Leaf0NotFoundInCpuid;
 
 /// Returns MSRs to be saved based on CPUID features that are enabled.
-///
-/// # Errors
-///
-/// When CPUID leaf 0 is not supported.
-pub fn msrs_to_save_by_cpuid(
-    cpuid: &kvm_bindings::CpuId,
-) -> Result<std::collections::HashSet<u32>, Leaf0NotFoundInCpuid> {
-    let vendor_id = cpuid.vendor_id().ok_or(Leaf0NotFoundInCpuid)?;
-    match &vendor_id {
-        super::VENDOR_ID_INTEL => Ok(intel_msrs_to_save_by_cpuid(cpuid)),
-        // We don't have MSR-CPUID dependencies set for other vendors yet.
-        _ => Ok(std::collections::HashSet::new()),
+pub(crate) fn msrs_to_save_by_cpuid(cpuid: &kvm_bindings::CpuId) -> std::collections::HashSet<u32> {
+    /// Memory Protection Extensions
+    const MPX_BITINDEX: u32 = 14;
+
+    /// Memory Type Range Registers
+    const MTRR_BITINDEX: u32 = 12;
+
+    /// Memory Check Exception
+    const MCE_BITINDEX: u32 = 7;
+
+    /// Scans through the CPUID and determines if a feature bit is set.
+    // TODO: This currently involves a linear search which would be improved
+    //       when we'll refactor the cpuid crate.
+    macro_rules! cpuid_is_feature_set {
+        ($cpuid:ident, $leaf:expr, $index:expr, $reg:tt, $feature_bit:expr) => {{
+            let mut res = false;
+            for entry in $cpuid.as_slice().iter() {
+                if entry.function == $leaf && entry.index == $index {
+                    if entry.$reg & (1 << $feature_bit) != 0 {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            res
+        }};
     }
+
+    let mut msrs = std::collections::HashSet::new();
+
+    // Macro used for easy definition of CPUID-MSR dependencies.
+    macro_rules! cpuid_msr_dep {
+        ($leaf:expr, $index:expr, $reg:tt, $feature_bit:expr, $msr:expr) => {
+            if cpuid_is_feature_set!(cpuid, $leaf, $index, $reg, $feature_bit) {
+                msrs.extend($msr)
+            }
+        };
+    }
+
+    // TODO: Add more dependencies.
+    cpuid_msr_dep!(
+        0x7,
+        0,
+        ebx,
+        MPX_BITINDEX,
+        [crate::arch_gen::x86::msr_index::MSR_IA32_BNDCFGS]
+    );
+
+    // IA32_MTRR_PHYSBASEn, IA32_MTRR_PHYSMASKn
+    cpuid_msr_dep!(0x1, 0, edx, MTRR_BITINDEX, 0x200..0x210);
+
+    // Other MTRR MSRs
+    cpuid_msr_dep!(
+        0x1,
+        0,
+        edx,
+        MTRR_BITINDEX,
+        [
+            0x250, // IA32_MTRR_FIX64K_00000
+            0x258, // IA32_MTRR_FIX16K_80000
+            0x259, // IA32_MTRR_FIX16K_A0000
+            0x268, // IA32_MTRR_FIX4K_C0000
+            0x269, // IA32_MTRR_FIX4K_C8000
+            0x26a, // IA32_MTRR_FIX4K_D0000
+            0x26b, // IA32_MTRR_FIX4K_D8000
+            0x26c, // IA32_MTRR_FIX4K_E0000
+            0x26d, // IA32_MTRR_FIX4K_E8000
+            0x26e, // IA32_MTRR_FIX4K_F0000
+            0x26f, // IA32_MTRR_FIX4K_F8000
+            0x277, // IA32_PAT
+            0x2ff  // IA32_MTRR_DEF_TYPE
+        ]
+    );
+
+    // MCE MSRs
+    // We are saving 32 MCE banks here as this is the maximum number supported by KVM
+    // and configured by default.
+    // The physical number of the MCE banks depends on the CPU.
+    // The number of emulated MCE banks can be configured via KVM_X86_SETUP_MCE.
+    cpuid_msr_dep!(0x1, 0, edx, MCE_BITINDEX, 0x400..0x480);
+
+    msrs
 }
 
 #[cfg(test)]
