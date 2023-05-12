686f377d5b91c6b2284167e21b5b385352e00323
Takahiro Itazuri <itazur@amazon.com>
2023-05-12 18:20:52 +0000

    feat(tool): Add fingerprint comparison feature
    Add a feature to compare two fingerprint files with an ability of
    selecting which fields to be compared.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/cpu-template-helper/src/fingerprint/mod.rs
+++ src/cpu-template-helper/src/fingerprint/mod.rs
@@ -4,14 +4,54 @@
 use serde::{Deserialize, Serialize};
 use vmm::cpu_config::templates::CustomCpuTemplate;
 
+pub mod compare;
 pub mod dump;
 
-#[derive(Serialize, Deserialize)]
-pub struct Fingerprint {
-    pub firecracker_version: String,
-    pub kernel_version: String,
-    pub microcode_version: String,
-    pub bios_version: String,
-    pub bios_revision: String,
-    pub guest_cpu_config: CustomCpuTemplate,
+macro_rules! declare_fingerprint_struct_and_enum {
+    ($($field_name:ident : $field_type:ty),+) => {
+        #[derive(Serialize, Deserialize)]
+        pub struct Fingerprint {
+            $(pub $field_name: $field_type),+
+        }
+
+        #[allow(non_camel_case_types)]
+        #[derive(clap::ValueEnum, Clone, Debug)]
+        #[value(rename_all = "snake_case")]
+        pub enum FingerprintField {
+            $($field_name),+
+        }
+    };
 }
+
+// This macro is expanded as follows:
+// ```rs
+// #[derive(Serialize, Deserialize)]
+// pub struct Fingerprint {
+//     pub firecracker_version: String,
+//     pub kernel_version: String,
+//     pub microcode_version: String,
+//     pub bios_version: String,
+//     pub bios_revision: String,
+//     pub guest_cpu_config: CustomCpuTemplate,
+// }
+//
+// #[allow(non_camel_case_types)]
+// #[derive(clap::ValueEnum, Clone, Debug)]
+// #[value(rename_all = "snake_case")]
+// pub enum FingerprintField {
+//     FirecrackerVersion,
+//     KernelVersion,
+//     MicrocodeVersion,
+//     BiosVersion,
+//     BiosRevision,
+//     GuestCpuConfig,
+// }
+// ```
+declare_fingerprint_struct_and_enum!(
+    firecracker_version: String,
+    kernel_version: String,
+    microcode_version: String,
+    bios_version: String,
+    bios_revision: String,
+    guest_cpu_config: CustomCpuTemplate
+);
