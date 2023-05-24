95e00f6cbf97da339a4c1463dfbc40f20cd44f06
Pablo Barbáchano <pablob@amazon.com>
2023-05-24 20:10:14 +0200

    refactor(devtool): delete create_snapshot_artifacts command
    We can just call it through `devtool sh`.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tools/create_snapshot_artifact/main.py
+++ tools/create_snapshot_artifact/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Script used to generate snapshots of microVMs."""
