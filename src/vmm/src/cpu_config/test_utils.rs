db1de5e72b00c782fef3c2e0f63cdc2e7cc32d64
Takahiro Itazuri <itazur@amazon.com>
2023-06-07 08:49:36 +0000

    test(vmm): Add test for consistency of static CPU templates
    Adds a test that verify consistency of static CPU templates between
    hardcoded ones and JSON ones in order not to forget to update one or the
    other.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

---
+++ src/vmm/src/cpu_config/test_utils.rs
@@ -0,0 +1,19 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::path::PathBuf;
+
+use crate::cpu_config::templates::CustomCpuTemplate;
+
+// Get a static CPU template stored as a JSON file.
+pub fn get_json_template(filename: &str) -> CustomCpuTemplate {
+    let json_path = [
+        env!("CARGO_MANIFEST_DIR"),
+        "../../resources/tests/static_cpu_templates",
+        filename,
+    ]
+    .iter()
+    .collect::<PathBuf>();
+
+    serde_json::from_str(&std::fs::read_to_string(json_path).unwrap()).unwrap()
+}
