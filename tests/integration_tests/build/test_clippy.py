e51cccd875ae2009ac833680954e000ac86ed2e7
Jonathan Woollett-Light <jcawl@amazon.co.uk>
2023-07-05 12:25:20 +0100

    tests: clippy-tracing
    Adds test to check functions are instrumented.
    
    Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

--- tests/integration_tests/build/test_clippy.py
+++ tests/integration_tests/build/test_clippy.py
@@ -7,6 +7,7 @@ import platform
 
 import pytest
 
+from framework import utils
 from host_tools.cargo_build import cargo
 
 SUCCESS_CODE = 0
@@ -23,3 +24,14 @@ def test_rust_clippy(target):
     Test that clippy does not generate any errors/warnings.
     """
     cargo("clippy", f"--target {target} --all --profile test", "-D warnings")
+
+def test_clippy_tracing():
+    """
+    Tests clippy-tracing
+    """
+
+    # This is only temporary for testing before creating a new docker container.
+    # TODO Remove this and instead add the tool to the docker container.
+    utils.run_cmd("cargo install --git https://github.com/JonathanWoollett-Light/clippy-tracing --rev da3403e4feb6ff3ec6d74d735e374e21e03488f4")
+
+    utils.run_cmd("clippy-tracing --action check path --path ../src --exclude virtio_gen,bindings.rs,net_gen,benches")
