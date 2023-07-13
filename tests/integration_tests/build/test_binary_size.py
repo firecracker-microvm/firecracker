c9229135a414988133d7e460166f1f265d458ae4
Pablo Barbáchano <pablob@amazon.com>
2023-07-13 21:09:53 +0200

    test: add a test to measure Firecracker memory overhead
    Our current binary size test measures space in disk, as a proxy for used
    memory. This is not accurate, as parts of the binary are not loaded into
    memory.
    
    This commits adds a new test that measures memory overhead directly and
    published metrics for different guest configurations.
    
    Also remove thresholds from test_binary_size.py test, as we measure
    memory overhead directly now.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/integration_tests/build/test_binary_size.py
+++ tests/integration_tests/build/test_binary_size.py
@@ -1,8 +1,12 @@
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
-"""Tests that check if the release binary sizes fall within expected size."""
+"""Tests that check if the release binary sizes fall within expected size.
 
-import os
+This is not representative of the actual memory overhead of Firecracker.
+
+A more representative test is file:../performance/test_memory_overhead.py
+"""
+
 import platform
 
 import pytest
@@ -10,27 +14,6 @@ import pytest
 import host_tools.cargo_build as host
 
 MACHINE = platform.machine()
-""" Platform definition used to select the correct size target"""
-
-SIZES_DICT = {
-    "x86_64": {
-        "FC_BINARY_SIZE_TARGET": 3063120,
-        "JAILER_BINARY_SIZE_TARGET": 1080088,
-    },
-    "aarch64": {
-        "FC_BINARY_SIZE_TARGET": 2441008,
-        "JAILER_BINARY_SIZE_TARGET": 1040928,
-    },
-}
-
-FC_BINARY_SIZE_TARGET = SIZES_DICT[MACHINE]["FC_BINARY_SIZE_TARGET"]
-"""Firecracker target binary size in bytes"""
-
-JAILER_BINARY_SIZE_TARGET = SIZES_DICT[MACHINE]["JAILER_BINARY_SIZE_TARGET"]
-"""Jailer target binary size in bytes"""
-
-BINARY_SIZE_TOLERANCE = 0.05
-"""Tolerance of 5% allowed for binary size"""
 
 
 @pytest.mark.timeout(500)
@@ -38,19 +21,9 @@ def test_firecracker_binary_size(record_property, metrics):
     """
     Test if the size of the firecracker binary is within expected ranges.
     """
-    fc_binary, _ = host.get_firecracker_binaries()
-
-    result = check_binary_size(
-        "firecracker",
-        fc_binary,
-        FC_BINARY_SIZE_TARGET,
-        BINARY_SIZE_TOLERANCE,
-    )
-
-    record_property(
-        "firecracker_binary_size",
-        f"{result}B ({FC_BINARY_SIZE_TARGET}B ±{BINARY_SIZE_TOLERANCE:.0%})",
-    )
+    fc_binary = host.get_binary("firecracker")
+    result = fc_binary.stat().st_size
+    record_property("firecracker_binary_size", f"{result}B")
     metrics.set_dimensions({"cpu_arch": MACHINE})
     metrics.put_metric("firecracker_binary_size", result, unit="Bytes")
 
@@ -60,51 +33,8 @@ def test_jailer_binary_size(record_property, metrics):
     """
     Test if the size of the jailer binary is within expected ranges.
     """
-    _, jailer_binary = host.get_firecracker_binaries()
-
-    result = check_binary_size(
-        "jailer",
-        jailer_binary,
-        JAILER_BINARY_SIZE_TARGET,
-        BINARY_SIZE_TOLERANCE,
-    )
-
-    record_property(
-        "jailer_binary_size",
-        f"{result}B ({JAILER_BINARY_SIZE_TARGET}B ±{BINARY_SIZE_TOLERANCE:.0%})",
-    )
+    jailer_binary = host.get_binary("jailer")
+    result = jailer_binary.stat().st_size
+    record_property("jailer_binary_size", f"{result}B")
     metrics.set_dimensions({"cpu_arch": MACHINE})
     metrics.put_metric("jailer_binary_size", result, unit="Bytes")
-
-
-def check_binary_size(name, binary_path, size_target, tolerance):
-    """Check if the specified binary falls within the expected range."""
-    # Get the size of the release binary.
-    binary_size = os.path.getsize(binary_path)
-
-    # Get the name of the variable that needs updating.
-    namespace = globals()
-    size_target_name = [name for name in namespace if namespace[name] is size_target][0]
-
-    # Compute concrete binary size difference.
-    delta_size = size_target - binary_size
-
-    binary_low_msg = (
-        "Current {} binary size of {} bytes is below the target"
-        " of {} bytes with {} bytes.\n"
-        "Update the {} threshold".format(
-            name, binary_size, size_target, delta_size, size_target_name
-        )
-    )
-
-    assert binary_size > size_target * (1 - tolerance), binary_low_msg
-
-    binary_high_msg = (
-        "Current {} binary size of {} bytes is above the target"
-        " of {} bytes with {} bytes.\n".format(
-            name, binary_size, size_target, -delta_size
-        )
-    )
-
-    assert binary_size < size_target * (1 + tolerance), binary_high_msg
-    return binary_size
