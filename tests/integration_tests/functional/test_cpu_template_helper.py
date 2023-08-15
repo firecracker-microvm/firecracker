27d4e403066076d0b978edb973182056eacfd6d1
Takahiro Itazuri <itazur@amazon.com>
2023-08-15 08:25:01 +0000

    test: Ignore CPUID.20000000h in test_cpu_config_dump_vs_actual
    CPUID.20000000h is not documented in Intel SDM and AMD APM. KVM doesn't
    report it, but the userspace cpuid command in ubuntu 22 does.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- tests/integration_tests/functional/test_cpu_template_helper.py
+++ tests/integration_tests/functional/test_cpu_template_helper.py
@@ -158,6 +158,9 @@ UNAVAILABLE_CPUID_ON_DUMP_LIST = [
     # support it, the userspace cpuid command in ubuntu 22 reports not only
     # the subleaf 0 but also the subleaf 1.
     (0x1B, 0x1),
+    # CPUID.20000000h is not documented in Intel SDM and AMD APM. KVM doesn't
+    # report it, but the userspace cpuid command in ubuntu 22 does.
+    (0x20000000, 0x0),
     # CPUID.8000001Bh or later are not supported on kernel 4.14 with an
     # exception CPUID.8000001Dh and CPUID.8000001Eh normalized by firecracker.
     # https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/arch/x86/kvm/cpuid.c?h=v4.14.313#n637
