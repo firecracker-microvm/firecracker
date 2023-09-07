2990b7be8e6f9bbed0e1324ac927c738c962e6df
Pablo Barbáchano <pablob@amazon.com>
2023-09-07 18:05:03 +0200

    test: test the current binary as a release
    With our current firecracker_release fixture, some tests are only run
    against released firecracker versions that we keep in an S3 bucket. This
    means the binary in the working copy does not execute those tests, and
    we may only realize until we make a release, at which point is too late
    and we have to make a patch release.
    
    Fix it by extending the firecracker_release fixture to also test the
    current binary as a regular release, so it goes through the same tests.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/framework/artifacts.py
+++ tests/framework/artifacts.py
@@ -14,6 +14,7 @@ import host_tools.network as net_tools
 from framework.defs import ARTIFACT_DIR
 from framework.properties import global_props
 from framework.utils import get_firecracker_version_from_toml
+from host_tools.cargo_build import get_binary
 
 
 def select_supported_kernels():
@@ -103,6 +104,20 @@ class FirecrackerArtifact:
         return ".".join(str(x) for x in self.snapshot_version_tuple)
 
 
+def current_release(version):
+    """Massage this working copy Firecracker binary to look like a normal
+    release, so it can run the same tests.
+    """
+    binaries = []
+    for binary in ["firecracker", "jailer"]:
+        bin_path1 = get_binary(binary)
+        bin_path2 = bin_path1.with_name(f"{binary}-v{version}")
+        bin_path2.unlink(missing_ok=True)
+        bin_path2.hardlink_to(bin_path1)
+        binaries.append(bin_path2)
+    return binaries
+
+
 def firecracker_artifacts():
     """Return all supported firecracker binaries."""
     cargo_version = get_firecracker_version_from_toml()
@@ -121,6 +136,9 @@ def firecracker_artifacts():
         if version >= max_version:
             continue
         yield pytest.param(fc, id=fc.name)
+
+    fc = FirecrackerArtifact(current_release(cargo_version.base_version)[0])
+    yield pytest.param(fc, id=fc.name)
 
 
 @dataclass(frozen=True, repr=True)
