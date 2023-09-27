a035ac4ebcfe944138eb782e839893d72a362bef
Patrick Roy <roypat@amazon.co.uk>
2023-09-27 16:58:53 +0100

    test: Add network throughput A/B-test
    Converts the existing network tcp throughput test into an A/B-compatible
    TCP throughput test. This test is added to test_network_ab.py, since
    the converted test is short enough that we can collect all the network
    related tests in a single file.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/performance/test_network_ab.py
+++ tests/integration_tests/performance/test_network_ab.py
@@ -8,6 +8,7 @@ import statistics
 import pytest
 
 from framework.utils import CpuMap
+from framework.utils_iperf import IPerf3Test, emit_iperf3_metrics
 
 # each iteration is 30 * 0.2s = 6s
 # Thus 30 iterations are 6s * 30 = 5min
@@ -89,3 +90,88 @@ def test_network_latency(microvm_factory, guest_kernel, rootfs, metrics):
 
     for sample in samples:
         metrics.put_metric("latency", sample, "Milliseconds")
+
+
+class TCPIPerf3Test(IPerf3Test):
+    """IPerf3 runner for the TCP throughput performance test"""
+
+    BASE_PORT = 5000
+
+    # How many clients/servers should be spawned per vcpu
+    LOAD_FACTOR = 1
+
+    # Time (in seconds) for which iperf "warms up"
+    WARMUP_SEC = 5
+
+    # Time (in seconds) for which iperf runs after warmup is done
+    RUNTIME_SEC = 20
+
+    def __init__(self, microvm, mode, host_ip, payload_length):
+        self._host_ip = host_ip
+
+        super().__init__(
+            microvm,
+            self.BASE_PORT,
+            self.RUNTIME_SEC,
+            self.WARMUP_SEC,
+            mode,
+            self.LOAD_FACTOR * microvm.vcpus_count,
+            host_ip,
+            payload_length=payload_length,
+        )
+
+
+@pytest.mark.nonci
+@pytest.mark.timeout(3600)
+@pytest.mark.parametrize("vcpus", [1, 2])
+@pytest.mark.parametrize("payload_length", ["128K", "1024K"], ids=["p128K", "p1024K"])
+@pytest.mark.parametrize("mode", ["g2h", "h2g", "bd"])
+def test_network_tcp_throughput(
+    microvm_factory,
+    guest_kernel,
+    rootfs,
+    vcpus,
+    payload_length,
+    mode,
+    metrics,
+):
+    """
+    Iperf between guest and host in both directions for TCP workload.
+    """
+
+    # We run bi-directional tests only on uVM with more than 2 vCPus
+    # because we need to pin one iperf3/direction per vCPU, and since we
+    # have two directions, we need at least two vCPUs.
+    if mode == "bd" and vcpus < 2:
+        pytest.skip("bidrectional test only done with at least 2 vcpus")
+
+    vm = microvm_factory.build(guest_kernel, rootfs, monitor_memory=False)
+    vm.spawn(log_level="Info")
+    vm.basic_config(vcpu_count=vcpus, mem_size_mib=GUEST_MEM_MIB)
+    iface = vm.add_net_iface()
+    vm.start()
+
+    # Check if the needed CPU cores are available. We have the API thread, VMM
+    # thread and then one thread for each configured vCPU. Lastly, we need one for
+    # the iperf server on the host.
+    assert CpuMap.len() > 2 + vm.vcpus_count
+
+    # Pin uVM threads to physical cores.
+    assert vm.pin_vmm(0), "Failed to pin firecracker thread."
+    assert vm.pin_api(1), "Failed to pin fc_api thread."
+    for i in range(vm.vcpus_count):
+        assert vm.pin_vcpu(i, i + 2), f"Failed to pin fc_vcpu {i} thread."
+
+    test = TCPIPerf3Test(vm, mode, iface.host_ip, payload_length)
+    data = test.run_test(vm.vcpus_count + 2)
+
+    metrics.set_dimensions(
+        {
+            "performance_test": "test_network_tcp_throughput",
+            "payload_length": payload_length,
+            "mode": mode,
+            **vm.dimensions,
+        }
+    )
+
+    emit_iperf3_metrics(metrics, data)
