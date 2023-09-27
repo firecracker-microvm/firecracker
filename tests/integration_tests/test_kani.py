4c0669c7252f80438bb21f3ec3f078080d8a1a17
Patrick Roy <roypat@amazon.co.uk>
2023-09-27 12:01:47 +0100

    test: Skip kani if not CI
    Trying to run the kani integration test outside of the CI will almost
    certainly result in out of memory conditions in the best case, and in
    system hangs in the worst case. Therefore, do not attempt to run kani if
    running tests locally.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/test_kani.py
+++ tests/integration_tests/test_kani.py
@@ -4,6 +4,7 @@
 Proofs ensuring memory safety properites, user-defined assertions,
 absence of panics and some types of unexpected behavior (e.g., arithmetic overflows).
 """
+import os
 import platform
 
 import pytest
@@ -15,6 +16,10 @@ PLATFORM = platform.machine()
 
 @pytest.mark.timeout(1800)
 @pytest.mark.skipif(PLATFORM != "x86_64", reason="Kani proofs run only on x86_64.")
+@pytest.mark.skipif(
+    os.environ.get("BUILDKITE") != "true",
+    reason="Kani's memory requirements likely cannot be satisfied locally",
+)
 def test_kani(results_dir):
     """
     Test all Kani proof harnesses.
@@ -24,10 +29,8 @@ def test_kani(results_dir):
     # -j enables kani harnesses to be verified in parallel (required to keep CI time low)
     # --output-format terse is required by -j
     # --enable-unstable is needed for each of the above
-    rc, stdout, stderr = utils.run_cmd(
+    _, stdout, _ = utils.run_cmd(
         "cargo kani --enable-unstable --enable-stubbing --restrict-vtable -j --output-format terse"
     )
-
-    assert rc == 0, stderr
 
     (results_dir / "kani_log").write_text(stdout, encoding="utf-8")
