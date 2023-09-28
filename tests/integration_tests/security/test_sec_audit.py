8297c8f3a5cddc3b709f3ee26db913cf66fbcbb6
Patrick Roy <roypat@amazon.co.uk>
2023-09-28 16:33:49 +0100

    test: A/B-ify test_cargo_audit
    The test_cargo_audit test can fail due to new advisories being published
    on crates that firecracker already consumes. We already get notified of
    this because we do a nightly run that alerts us whenever any advisory
    for one of our dependencies exists. There is no need to do the same on
    PRs, as that means all development activities are blocked until the
    advisory is resolved (which might be non-trivial). Thus use A/B-testing
    in CI to only fail a PR if a newly introduced dependency has a
    pre-existing advisory (in which case we do not want to introduce the
    dependency). This is done by computing the set of
    warnings/vulnerabilities that currently affect the repository, and
    ensuring that there are no additions to this set (while subtractions,
    for example due to a PR removing a vulnerable dependency, are allowed).
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/security/test_sec_audit.py
+++ tests/integration_tests/security/test_sec_audit.py
@@ -1,26 +1,40 @@
 # Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Tests ensuring security vulnerabilities are not present in dependencies."""
-
+import json
 
 import pytest
 
-from framework import defs
+from framework.ab_test import (
+    git_ab_test_host_command_if_pr,
+    set_did_not_grow_comparator,
+)
+from framework.utils import CommandReturn
 from framework.utils_cpuid import CpuVendor, get_cpu_vendor
-from host_tools.cargo_build import cargo
 
 
 @pytest.mark.skipif(
     get_cpu_vendor() != CpuVendor.INTEL,
-    reason="The audit is based on cargo.lock which " "is identical on all platforms",
+    reason="The audit is based on cargo.lock which is identical on all platforms",
 )
 def test_cargo_audit():
     """
     Run cargo audit to check for crates with security vulnerabilities.
     """
-    # Run command and raise exception if non-zero return code
-    cargo(
-        "audit",
-        "--deny warnings -q",
-        cwd=defs.FC_WORKSPACE_DIR,
+
+    def set_of_vulnerabilities(output: CommandReturn):
+        output = json.loads(output.stdout)
+
+        return set(
+            frozenset(vulnerability)
+            for vulnerability in output["vulnerabilities"]["list"]
+        ).union(
+            frozenset(warning)
+            for warning_kind, warnings in output["warnings"].items()
+            for warning in warnings
+        )
+
+    git_ab_test_host_command_if_pr(
+        "cargo audit --deny warnings -q --json",
+        comparator=set_did_not_grow_comparator(set_of_vulnerabilities),
     )
