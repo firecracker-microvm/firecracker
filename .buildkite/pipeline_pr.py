b690683c31c2b59573aaf4c90e36ec4df3f11836
Patrick Roy <roypat@amazon.co.uk>
2023-10-06 10:10:37 +0100

    test: Run kani if dependencies are updated
    Currently, we only run kani if a .rs file is modified. We should also
    run it if dependencies are changed.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- .buildkite/pipeline_pr.py
+++ .buildkite/pipeline_pr.py
@@ -109,7 +109,9 @@ if any(x.name == "Dockerfile" for x in changed_files):
 if any(x.parent.name == "tools" and "release" in x.name for x in changed_files):
     steps.append(release_grp)
 
-if not changed_files or any(x.suffix == ".rs" for x in changed_files):
+if not changed_files or any(
+    x.suffix in [".rs", ".toml", ".lock"] for x in changed_files
+):
     steps.append(kani_grp)
 
 if run_all_tests(changed_files):
