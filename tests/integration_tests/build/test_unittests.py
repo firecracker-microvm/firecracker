e71046f3b6e878c875f17045cec2d84d5a9dabc2
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-10-16 18:45:14 +0100

    feat: added GNU for unit tests targets
    Now unit tests are run for musl and gnu targets.
    This is done, because we have some tests that
    only work with gnu.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- tests/integration_tests/build/test_unittests.py
+++ tests/integration_tests/build/test_unittests.py
@@ -12,7 +12,10 @@ MACHINE = platform.machine()
 # Currently profiling with `aarch64-unknown-linux-musl` is unsupported (see
 # https://github.com/rust-lang/rustup/issues/3095#issuecomment-1280705619) therefore we profile and
 # run coverage with the `gnu` toolchains and run unit tests with the `musl` toolchains.
-TARGET = "{}-unknown-linux-musl".format(MACHINE)
+TARGETS = [
+    "{}-unknown-linux-musl".format(MACHINE),
+    "{}-unknown-linux-gnu".format(MACHINE),
+]
 
 
 @pytest.mark.timeout(600)
@@ -20,11 +23,13 @@ def test_unittests(test_fc_session_root_path):
     """
     Run unit and doc tests for all supported targets.
     """
-    extra_args = "--target {} ".format(TARGET)
 
-    host.cargo_test(test_fc_session_root_path, extra_args=extra_args)
+    for target in TARGETS:
+        extra_args = "--target {} ".format(target)
+        host.cargo_test(test_fc_session_root_path, extra_args=extra_args)
 
 
 def test_benchmarks_compile():
     """Checks that all benchmarks compile"""
-    host.cargo("bench", f"--all --no-run --target {TARGET}")
+
+    host.cargo("bench", f"--all --no-run --target {TARGETS[0]}")
