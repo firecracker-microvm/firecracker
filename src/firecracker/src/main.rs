firecracker-microvm/firecracker

commit 332f2184ae29265fbae1648469e073fc7f987378

feat: Update the logger
- Refactors the logger implementation, simplifying the implementation
  and reducing the binary size by ~20kb.
- Adds support for runtime reconfiguration.
- Updates the check for the `Running Firecracker` log to match.
- Updates the default log level to `Info`.

Signed-off-by: Jonathan Woollett-Light <jcawl@amazon.co.uk>

src/firecracker/src/main.rs
@@ -8,6 +8,7 @@ mod seccomp;
 use std::fs::{self, File};
 use std::path::PathBuf;
 use std::process::ExitCode;
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use std::{io, panic};
 
@@ -20,12 +21,13 @@ use utils::arg_parser::{ArgParser, Argument};
 use utils::terminal::Terminal;
 use utils::validators::validate_instance_id;
 use vmm::builder::StartMicrovmError;
-use vmm::logger::{error, info, ProcessTimeReporter, StoreMetric, LOGGER, METRICS};
+use vmm::logger::{
+    debug, error, info, LoggerConfig, ProcessTimeReporter, StoreMetric, LOGGER, METRICS,
+};
 use vmm::resources::VmResources;
 use vmm::signal_handler::register_signal_handlers;
 use vmm::version_map::{FC_VERSION_TO_SNAP_VERSION, VERSION_MAP};
 use vmm::vmm_config::instance_info::{InstanceInfo, VmState};
-use vmm::vmm_config::logger_config::{init_logger, LoggerConfig, LoggerConfigError, LoggerLevel};
 use vmm::vmm_config::metrics::{init_metrics, MetricsConfig, MetricsConfigError};
 use vmm::{EventManager, FcExitCode, HTTP_MAX_PAYLOAD_SIZE};
 
@@ -35,12 +37,13 @@ use crate::seccomp::SeccompConfig;
 // runtime file.
 // see https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch03s15.html for more information.
 const DEFAULT_API_SOCK_PATH: &str = "/run/firecracker.socket";
-const DEFAULT_INSTANCE_ID: &str = "anonymous-instance";
 const FIRECRACKER_VERSION: &str = env!("CARGO_PKG_VERSION");
 const MMDS_CONTENT_ARG: &str = "metadata";
 
 #[derive(Debug, thiserror::Error, displaydoc::Display)]
 enum MainError {
+    /// Failed to set the logger: {0}
+    SetLogger(log::SetLoggerError),
     /// Failed to register signal handlers: {0}
     RegisterSignalHandlers(#[source] utils::errno::Error),
     /// Arguments parsing error: {0} \n\nFor more information try --help.
@@ -48,9 +51,9 @@ enum MainError {
     /// When printing Snapshot Data format: {0}
     PrintSnapshotDataFormat(#[from] SnapshotVersionError),
     /// Invalid value for logger level: {0}.Possible values: [Error, Warning, Info, Debug]
-    InvalidLogLevel(LoggerConfigError),
+    InvalidLogLevel(vmm::logger::LevelFilterFromStrError),
     /// Could not initialize logger: {0}
-    LoggerInitialization(LoggerConfigError),
+    LoggerInitialization(vmm::logger::LoggerUpdateError),
     /// Could not initialize metrics: {0:?}
     MetricsInitialization(MetricsConfigError),
     /// Seccomp error: {0}
@@ -99,9 +102,10 @@ fn main() -> ExitCode {
 }
 
 fn main_exec() -> Result<(), MainError> {
-    LOGGER
-        .configure(Some(DEFAULT_INSTANCE_ID.to_string()))
-        .expect("Failed to register logger");
+    // Initialize the logger.
+    log::set_logger(&LOGGER).map_err(MainError::SetLogger)?;
+    log::set_max_level(vmm::logger::DEFAULT_LEVEL);
+    info!("Running Firecracker v{FIRECRACKER_VERSION}");
 
     register_signal_handlers().map_err(MainError::RegisterSignalHandlers)?;
 
@@ -113,7 +117,7 @@ fn main_exec() -> Result<(), MainError> {
             // These errors are non-critical: In the worst case we have worse snapshot restore
             // performance.
             ResizeFdTableError::GetRlimit | ResizeFdTableError::Dup2(_) => {
-                vmm::logger::debug!("Failed to resize fdtable: {err}")
+                debug!("Failed to resize fdtable: {err}")
             }
             // This error means that we now have a random file descriptor lying around, abort to be
             // cautious.
@@ -149,124 +153,114 @@ fn main_exec() -> Result<(), MainError> {
 
     let http_max_payload_size_str = HTTP_MAX_PAYLOAD_SIZE.to_string();
 
-    let mut arg_parser = ArgParser::new()
-        .arg(
-            Argument::new("api-sock")
-                .takes_value(true)
-                .default_value(DEFAULT_API_SOCK_PATH)
-                .help("Path to unix domain socket used by the API."),
-        )
-        .arg(
-            Argument::new("id")
-                .takes_value(true)
-                .default_value(DEFAULT_INSTANCE_ID)
-                .help("MicroVM unique identifier."),
-        )
-        .arg(
-            Argument::new("seccomp-filter")
-                .takes_value(true)
-                .forbids(vec!["no-seccomp"])
-                .help(
-                    "Optional parameter which allows specifying the path to a custom seccomp \
-                     filter. For advanced users.",
+    let mut arg_parser =
+        ArgParser::new()
+            .arg(
+                Argument::new("api-sock")
+                    .takes_value(true)
+                    .default_value(DEFAULT_API_SOCK_PATH)
+                    .help("Path to unix domain socket used by the API."),
+            )
+            .arg(
+                Argument::new("id")
+                    .takes_value(true)
+                    .help("MicroVM unique identifier."),
+            )
+            .arg(
+                Argument::new("seccomp-filter")
+                    .takes_value(true)
+                    .forbids(vec!["no-seccomp"])
+                    .help(
+                        "Optional parameter which allows specifying the path to a custom seccomp \
+                         filter. For advanced users.",
+                    ),
+            )
+            .arg(
+                Argument::new("no-seccomp")
+                    .takes_value(false)
+                    .forbids(vec!["seccomp-filter"])
+                    .help(
+                        "Optional parameter which allows starting and using a microVM without \
+                         seccomp filtering. Not recommended.",
+                    ),
+            )
+            .arg(
+                Argument::new("start-time-us").takes_value(true).help(
+                    "Process start time (wall clock, microseconds). This parameter is optional.",
                 ),
-        )
-        .arg(
-            Argument::new("no-seccomp")
-                .takes_value(false)
-                .forbids(vec!["seccomp-filter"])
-                .help(
-                    "Optional parameter which allows starting and using a microVM without seccomp \
-                     filtering. Not recommended.",
-                ),
-        )
-        .arg(
-            Argument::new("start-time-us")
-                .takes_value(true)
-                .help("Process start time (wall clock, microseconds). This parameter is optional."),
-        )
-        .arg(
-            Argument::new("start-time-cpu-us").takes_value(true).help(
+            )
+            .arg(Argument::new("start-time-cpu-us").takes_value(true).help(
                 "Process start CPU time (wall clock, microseconds). This parameter is optional.",
-            ),
-        )
-        .arg(Argument::new("parent-cpu-time-us").takes_value(true).help(
-            "Parent process CPU time (wall clock, microseconds). This parameter is optional.",
-        ))
-        .arg(
-            Argument::new("config-file")
-                .takes_value(true)
-                .help("Path to a file that contains the microVM configuration in JSON format."),
-        )
-        .arg(
-            Argument::new(MMDS_CONTENT_ARG)
-                .takes_value(true)
-                .help("Path to a file that contains metadata in JSON format to add to the mmds."),
-        )
-        .arg(
-            Argument::new("no-api")
-                .takes_value(false)
-                .requires("config-file")
-                .help(
-                    "Optional parameter which allows starting and using a microVM without an \
-                     active API socket.",
+            ))
+            .arg(Argument::new("parent-cpu-time-us").takes_value(true).help(
+                "Parent process CPU time (wall clock, microseconds). This parameter is optional.",
+            ))
+            .arg(
+                Argument::new("config-file")
+                    .takes_value(true)
+                    .help("Path to a file that contains the microVM configuration in JSON format."),
+            )
+            .arg(
+                Argument::new(MMDS_CONTENT_ARG).takes_value(true).help(
+                    "Path to a file that contains metadata in JSON format to add to the mmds.",
                 ),
-        )
-        .arg(
-            Argument::new("log-path")
-                .takes_value(true)
-                .help("Path to a fifo or a file used for configuring the logger on startup."),
-        )
-        .arg(
-            Argument::new("level")
-                .takes_value(true)
-                .requires("log-path")
-                .default_value("Warning")
-                .help("Set the logger level."),
-        )
-        .arg(
-            Argument::new("show-level")
-                .takes_value(false)
-                .requires("log-path")
-                .help("Whether or not to output the level in the logs."),
-        )
-        .arg(
-            Argument::new("show-log-origin")
-                .takes_value(false)
-                .requires("log-path")
-                .help(
-                    "Whether or not to include the file path and line number of the log's origin.",
-                ),
-        )
-        .arg(
-            Argument::new("metrics-path")
-                .takes_value(true)
-                .help("Path to a fifo or a file used for configuring the metrics on startup."),
-        )
-        .arg(Argument::new("boot-timer").takes_value(false).help(
-            "Whether or not to load boot timer device for logging elapsed time since \
-             InstanceStart command.",
-        ))
-        .arg(Argument::new("version").takes_value(false).help(
-            "Print the binary version number and a list of supported snapshot data format \
-             versions.",
-        ))
-        .arg(
-            Argument::new("describe-snapshot")
-                .takes_value(true)
-                .help("Print the data format version of the provided snapshot state file."),
-        )
-        .arg(
-            Argument::new("http-api-max-payload-size")
-                .takes_value(true)
-                .default_value(&http_max_payload_size_str)
-                .help("Http API request payload max size, in bytes."),
-        )
-        .arg(
-            Argument::new("mmds-size-limit")
-                .takes_value(true)
-                .help("Mmds data store limit, in bytes."),
-        );
+            )
+            .arg(
+                Argument::new("no-api")
+                    .takes_value(false)
+                    .requires("config-file")
+                    .help(
+                        "Optional parameter which allows starting and using a microVM without an \
+                         active API socket.",
+                    ),
+            )
+            .arg(
+                Argument::new("log-path")
+                    .takes_value(true)
+                    .help("Path to a fifo or a file used for configuring the logger on startup."),
+            )
+            .arg(
+                Argument::new("level")
+                    .takes_value(true)
+                    .help("Set the logger level."),
+            )
+            .arg(
+                Argument::new("show-level")
+                    .takes_value(false)
+                    .help("Whether or not to output the level in the logs."),
+            )
+            .arg(Argument::new("show-log-origin").takes_value(false).help(
+                "Whether or not to include the file path and line number of the log's origin.",
+            ))
+            .arg(
+                Argument::new("metrics-path")
+                    .takes_value(true)
+                    .help("Path to a fifo or a file used for configuring the metrics on startup."),
+            )
+            .arg(Argument::new("boot-timer").takes_value(false).help(
+                "Whether or not to load boot timer device for logging elapsed time since \
+                 InstanceStart command.",
+            ))
+            .arg(Argument::new("version").takes_value(false).help(
+                "Print the binary version number and a list of supported snapshot data format \
+                 versions.",
+            ))
+            .arg(
+                Argument::new("describe-snapshot")
+                    .takes_value(true)
+                    .help("Print the data format version of the provided snapshot state file."),
+            )
+            .arg(
+                Argument::new("http-api-max-payload-size")
+                    .takes_value(true)
+                    .default_value(&http_max_payload_size_str)
+                    .help("Http API request payload max size, in bytes."),
+            )
+            .arg(
+                Argument::new("mmds-size-limit")
+                    .takes_value(true)
+                    .help("Mmds data store limit, in bytes."),
+            );
 
     arg_parser.parse_from_cmdline()?;
     let arguments = arg_parser.arguments();
@@ -304,23 +298,29 @@ fn main_exec() -> Result<(), MainError> {
         app_name: "Firecracker".to_string(),
     };
 
-    LOGGER.set_instance_id(instance_id.to_owned());
+    let id = arguments
+        .single_value("id")
+        .map(|s| s.as_str())
+        .unwrap_or(vmm::logger::DEFAULT_INSTANCE_ID);
+    vmm::logger::INSTANCE_ID.set(String::from(id)).unwrap();
 
-    if let Some(log) = arguments.single_value("log-path") {
-        // It's safe to unwrap here because the field's been provided with a default value.
-        let level = arguments.single_value("level").unwrap().to_owned();
-        let logger_level = LoggerLevel::from_string(level).map_err(MainError::InvalidLogLevel)?;
-        let show_level = arguments.flag_present("show-level");
-        let show_log_origin = arguments.flag_present("show-log-origin");
-
-        let logger_config = LoggerConfig {
-            log_path: PathBuf::from(log),
-            level: logger_level,
+    // Apply the logger configuration.
+    let log_path = arguments.single_value("log-path").map(PathBuf::from);
+    let level = arguments
+        .single_value("level")
+        .map(|s| vmm::logger::LevelFilter::from_str(s))
+        .transpose()
+        .map_err(MainError::InvalidLogLevel)?;
+    let show_level = arguments.flag_present("show-level").then_some(true);
+    let show_log_origin = arguments.flag_present("show-level").then_some(true);
+    LOGGER
+        .update(LoggerConfig {
+            log_path,
+            level,
             show_level,
             show_log_origin,
-        };
-        init_logger(logger_config, &instance_info).map_err(MainError::LoggerInitialization)?;
-    }
+        })
+        .map_err(MainError::LoggerInitialization)?;
 
     if let Some(metrics_path) = arguments.single_value("metrics-path") {
         let metrics_config = MetricsConfig {

src/api_server/src/request/logger.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use vmm::logger::{IncMetric, METRICS};
-use vmm::vmm_config::logger_config::LoggerConfig;
 
 use super::super::VmmAction;
 use crate::parsed_request::{Error, ParsedRequest};
@@ -10,7 +9,7 @@ use crate::request::Body;
 
 pub(crate) fn parse_put_logger(body: &Body) -> Result<ParsedRequest, Error> {
     METRICS.put_api_requests.logger_count.inc();
-    let res = serde_json::from_slice::<LoggerConfig>(body.raw());
+    let res = serde_json::from_slice::<vmm::logger::LoggerConfig>(body.raw());
     let config = res.map_err(|err| {
         METRICS.put_api_requests.logger_fails.inc();
         err
@@ -22,7 +21,7 @@ pub(crate) fn parse_put_logger(body: &Body) -> Result<ParsedRequest, Error> {
 mod tests {
     use std::path::PathBuf;
 
-    use vmm::vmm_config::logger_config::LoggerLevel;
+    use vmm::logger::{LevelFilter, LoggerConfig};
 
     use super::*;
     use crate::parsed_request::tests::vmm_action_from_request;
@@ -30,16 +29,17 @@ mod tests {
     #[test]
     fn test_parse_put_logger_request() {
         let body = r#"{
-            "log_path": "log",
-            "level": "Warning",
-            "show_level": false,
-            "show_log_origin": false
-        }"#;
+                "log_path": "log",
+                "level": "Warning",
+                "show_level": false,
+                "show_log_origin": false
+              }"#;
+
         let expected_config = LoggerConfig {
-            log_path: PathBuf::from("log"),
-            level: LoggerLevel::Warning,
-            show_level: false,
-            show_log_origin: false,
+            log_path: Some(PathBuf::from("log")),
+            level: Some(LevelFilter::Warn),
+            show_level: Some(false),
+            show_log_origin: Some(false),
         };
         assert_eq!(
             vmm_action_from_request(parse_put_logger(&Body::new(body)).unwrap()),
@@ -47,16 +47,17 @@ mod tests {
         );
 
         let body = r#"{
-            "log_path": "log",
-            "level": "DEBUG",
-            "show_level": false,
-            "show_log_origin": false
-        }"#;
+                "log_path": "log",
+                "level": "DEBUG",
+                "show_level": false,
+                "show_log_origin": false
+              }"#;
+
         let expected_config = LoggerConfig {
-            log_path: PathBuf::from("log"),
-            level: LoggerLevel::Debug,
-            show_level: false,
-            show_log_origin: false,
+            log_path: Some(PathBuf::from("log")),
+            level: Some(LevelFilter::Debug),
+            show_level: Some(false),
+            show_log_origin: Some(false),
         };
         assert_eq!(
             vmm_action_from_request(parse_put_logger(&Body::new(body)).unwrap()),

src/api_server/swagger/firecracker.yaml
@@ -983,8 +983,8 @@ definitions:
       level:
         type: string
         description: Set the level. The possible values are case-insensitive.
-        enum: [Error, Warning, Info, Debug]
-        default: Warning
+        enum: [Error, Warning, Info, Debug, Trace, Off]
+        default: Info
       log_path:
         type: string
         description: Path to the named pipe or file for the human readable log output.

src/firecracker/Cargo.toml
@@ -16,6 +16,7 @@ bench = false
 displaydoc = "0.2.4"
 event-manager = "0.3.0"
 libc = "0.2.148"
+log = "0.4.20"
 serde_json = "1.0.107"
 thiserror = "1.0.49"
 timerfd = "1.5.0"

src/vmm/src/logger/init.rs
@@ -1,117 +1,0 @@
-// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-use std::sync::atomic::{AtomicUsize, Ordering};
-
-/// Errors associated with the `Init` struct.
-#[derive(Debug, thiserror::Error, displaydoc::Display)]
-pub enum Error {
-    /// The component is already initialized.
-    AlreadyInitialized,
-    /// The component is initializing. Can't perform the requested action right now.
-    InitializationInProgress,
-}
-
-/// A helper structure which can be used to run a one-time initialization.
-#[derive(Debug)]
-pub struct Init {
-    state: AtomicUsize,
-}
-
-impl Init {
-    const UNINITIALIZED: usize = 0;
-    const INITIALIZING: usize = 1;
-    const INITIALIZED: usize = 2;
-
-    /// Creates a new instance of `Init`.
-    pub const fn new() -> Self {
-        Self {
-            state: AtomicUsize::new(Self::UNINITIALIZED),
-        }
-    }
-
-    /// Performs an initialization routine.
-    ///
-    /// The given closure will be executed if the current state is `UNINITIALIZED`.
-    /// Otherwise an Error will be returned.
-    ///
-    /// If the closure returns `true`, the state will be changed to `INITIALIZED`.
-    /// If the closure returns `false`, the state will remain `UNINITIALIZED`.
-    pub fn call_init<F>(&self, f: F) -> Result<(), Error>
-    where
-        F: FnOnce() -> bool,
-    {
-        match self.state.compare_exchange(
-            Self::UNINITIALIZED,
-            Self::INITIALIZING,
-            Ordering::SeqCst,
-            Ordering::SeqCst,
-        ) {
-            Err(Self::INITIALIZING) => {
-                return Err(Error::InitializationInProgress);
-            }
-            Err(Self::INITIALIZED) => {
-                return Err(Error::AlreadyInitialized);
-            }
-            _ => {}
-        }
-
-        let state = if f() {
-            Self::INITIALIZED
-        } else {
-            Self::UNINITIALIZED
-        };
-
-        self.state.store(state, Ordering::SeqCst);
-
-        Ok(())
-    }
-
-    /// Checks if the current state is `INITIALIZED`.
-    #[inline]
-    pub fn is_initialized(&self) -> bool {
-        self.state.load(Ordering::Relaxed) == Self::INITIALIZED
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_call_init_with_true() {
-        let init = Init::new();
-        assert!(init.call_init(|| { true }).is_ok());
-        assert!(init.state.load(Ordering::Relaxed) == Init::INITIALIZED);
-    }
-
-    #[test]
-    fn test_call_init_with_false() {
-        let init = Init::new();
-        assert!(init.call_init(|| { false }).is_ok());
-        assert!(init.state.load(Ordering::Relaxed) == Init::UNINITIALIZED);
-    }
-
-    #[test]
-    fn test_call_init_errors() {
-        let init = Init::new();
-
-        init.state.store(Init::INITIALIZED, Ordering::SeqCst);
-        assert!(init.call_init(|| { true }).is_err());
-
-        init.state.store(Init::INITIALIZING, Ordering::SeqCst);
-        assert!(init.call_init(|| { true }).is_err());
-    }
-
-    #[test]
-    fn test_error_messages() {
-        assert_eq!(
-            format!("{}", Error::InitializationInProgress),
-            "The component is initializing. Can't perform the requested action right now."
-        );
-        assert_eq!(
-            format!("{}", Error::AlreadyInitialized),
-            "The component is already initialized."
-        );
-    }
-}

src/vmm/src/logger/mod.rs
@@ -6,18 +6,17 @@
 //! Crate that implements Firecracker specific functionality as far as logging and metrics
 //! collecting.
 
-mod init;
 mod logging;
 mod metrics;
 
-use std::sync::LockResult;
-
-pub use log::{debug, error, info, log_enabled, trace, warn, Level, LevelFilter};
-
-pub use crate::logger::logging::{LoggerError, LOGGER};
+pub use log::{debug, error, info, log_enabled, trace, warn, Level};
+pub use logging::{
+    LevelFilter, LevelFilterFromStrError, LoggerConfig, LoggerUpdateError, DEFAULT_INSTANCE_ID,
+    DEFAULT_LEVEL, INSTANCE_ID, LOGGER,
+};
 #[cfg(target_arch = "aarch64")]
-pub use crate::logger::metrics::RTCDeviceMetrics;
-pub use crate::logger::metrics::{
+pub use metrics::RTCDeviceMetrics;
+pub use metrics::{
     IncMetric, MetricsError, ProcessTimeReporter, SerialDeviceMetrics, SharedIncMetric,
     SharedStoreMetric, StoreMetric, METRICS,
 };
@@ -37,15 +36,6 @@ pub fn log_dev_preview_warning(feature_name: &str, msg_opt: Option<String>) {
         Some(msg) => {
             warn!("{DEV_PREVIEW_LOG_PREFIX} {feature_name} is in development preview - {msg}")
         }
-    }
-}
-
-fn extract_guard<G>(lock_result: LockResult<G>) -> G {
-    match lock_result {
-        Ok(guard) => guard,
-        // If a thread panics while holding this lock, the writer within should still be usable.
-        // (we might get an incomplete log line or something like that).
-        Err(poisoned) => poisoned.into_inner(),
     }
 }
 

src/vmm/src/resources.rs
@@ -21,7 +21,6 @@ use crate::vmm_config::boot_source::{
 use crate::vmm_config::drive::*;
 use crate::vmm_config::entropy::*;
 use crate::vmm_config::instance_info::InstanceInfo;
-use crate::vmm_config::logger_config::{init_logger, LoggerConfig, LoggerConfigError};
 use crate::vmm_config::machine_config::{
     MachineConfig, MachineConfigUpdate, VmConfig, VmConfigError,
 };
@@ -44,7 +43,7 @@ pub enum ResourcesError {
     /// Invalid JSON: {0}
     InvalidJson(serde_json::Error),
     /// Logger error: {0}
-    Logger(LoggerConfigError),
+    Logger(crate::logger::LoggerUpdateError),
     /// Metrics error: {0}
     Metrics(MetricsConfigError),
     /// MMDS error: {0}
@@ -73,7 +72,7 @@ pub struct VmmConfig {
     #[serde(rename = "cpu-config")]
     cpu_config: Option<PathBuf>,
     #[serde(rename = "logger")]
-    logger: Option<LoggerConfig>,
+    logger: Option<crate::logger::LoggerConfig>,
     #[serde(rename = "machine-config")]
     machine_config: Option<MachineConfig>,
     #[serde(rename = "metrics")]
@@ -126,8 +125,8 @@ impl VmResources {
     ) -> Result<Self, ResourcesError> {
         let vmm_config = serde_json::from_str::<VmmConfig>(config_json)?;
 
-        if let Some(logger) = vmm_config.logger {
-            init_logger(logger, instance_info)?;
+        if let Some(logger_config) = vmm_config.logger {
+            crate::logger::LOGGER.update(logger_config)?;
         }
 
         if let Some(metrics) = vmm_config.metrics {
@@ -478,7 +477,6 @@ mod tests {
     use super::*;
     use crate::cpu_config::templates::{CpuTemplateType, StaticCpuTemplate};
     use crate::devices::virtio::vsock::VSOCK_DEV_ID;
-    use crate::logger::{LevelFilter, LOGGER};
     use crate::resources::VmResources;
     use crate::vmm_config::boot_source::{
         BootConfig, BootSource, BootSourceConfig, DEFAULT_KERNEL_CMDLINE,
@@ -851,12 +849,9 @@ mod tests {
             HTTP_MAX_PAYLOAD_SIZE,
             None,
         ) {
-            Err(ResourcesError::Logger(LoggerConfigError::InitializationFailure { .. })) => (),
+            Err(ResourcesError::Logger(crate::logger::LoggerUpdateError(_))) => (),
             _ => unreachable!(),
         }
-
-        // The previous call enables the logger. We need to disable it.
-        LOGGER.set_max_level(LevelFilter::Off);
 
         // Invalid path for metrics pipe.
         json = format!(

commit efbd227b6c55a700a4da0cada152b1b7976468ed

fix: option --show-log-origin has no effect
show-log-origin was turned on by the --show-level option.

Use the correct flag to enable it.

Fixes: 332f2184

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

src/firecracker/src/main.rs
@@ -278,7 +278,7 @@ fn main_exec() -> Result<(), MainError> {
         .transpose()
         .map_err(MainError::InvalidLogLevel)?;
     let show_level = arguments.flag_present("show-level").then_some(true);
-    let show_log_origin = arguments.flag_present("show-level").then_some(true);
+    let show_log_origin = arguments.flag_present("show-log-origin").then_some(true);
     LOGGER
         .update(LoggerConfig {
             log_path,