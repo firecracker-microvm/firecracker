d1b3f00c8abade9a57fa51184161bfb929c6d593
Egor Lazarchuk <yegorlz@amazon.co.uk>
2023-11-09 16:07:26 +0000

    refactor(block): moved fixture to the utils
    Moved fixture that creates a file and gets its partuuid
    to the `utils_drive.py` file. This fixture will also
    be used for vhost-user-block tests.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- tests/framework/utils_drive.py
+++ tests/framework/utils_drive.py
@@ -6,8 +6,36 @@
 import os
 import subprocess
 import time
+from subprocess import check_output
 
 from framework import utils
+
+MB = 1024 * 1024
+
+
+def partuuid_and_disk_path(rootfs_ubuntu_22, disk_path):
+    """
+    We create a new file with specified path, get its partuuid and use it as a rootfs.
+    """
+    initial_size = rootfs_ubuntu_22.stat().st_size + 50 * MB
+    disk_path.touch()
+    os.truncate(disk_path, initial_size)
+    check_output(f"echo type=83 | sfdisk {str(disk_path)}", shell=True)
+    stdout = check_output(
+        f"losetup --find --partscan --show {str(disk_path)}", shell=True
+    )
+    loop_dev = stdout.decode("ascii").strip()
+    check_output(f"dd if={str(rootfs_ubuntu_22)} of={loop_dev}p1", shell=True)
+
+    # UUID=$(sudo blkid -s UUID -o value "${loop_dev}p1")
+    stdout = check_output(f"blkid -s PARTUUID -o value {loop_dev}p1", shell=True)
+    partuuid = stdout.decode("ascii").strip()
+
+    # cleanup: release loop device
+    check_output(f"losetup -d {loop_dev}", shell=True)
+
+    return (partuuid, disk_path)
+
 
 VHOST_USER_SOCKET = "/vub.socket"
 
