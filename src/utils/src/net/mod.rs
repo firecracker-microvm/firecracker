a8b4f7d98f683ecd21bf35719ff761d6645d6f17
Patrick Roy <roypat@amazon.co.uk>
2023-11-16 12:49:11 +0000

    fix: Downgrade #![deny(...)] to #![warn(...)]
    In our CI, we explicitly check that no warnings are present (by meanings
    of -Dwarnings during build tests). This means that there is no need to
    have #![deny(...)] instead of #![warn(...)] directives in the source
    code. On the other hand, having deny directives makes prototyping more
    annoying, as scrappy code will not compile until all compiler warnings
    are fixed (which might not neccessarily make sense when prototyping).
    
    See also https://github.com/firecracker-microvm/firecracker/pull/4106
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/utils/src/net/mod.rs
+++ src/utils/src/net/mod.rs
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the THIRD-PARTY file.
 
-#![deny(missing_docs)]
+#![warn(missing_docs)]
 //! # Network-related utilities
 //!
 //! Provides tools for representing and handling network related concepts like MAC addresses and
