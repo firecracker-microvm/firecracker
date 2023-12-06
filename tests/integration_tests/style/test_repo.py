199ffc89e7a9f547b088ac5eb359c3e8808a5b18
Pablo Barbáchano <pablob@amazon.com>
2023-12-06 22:05:33 +0100

    test(repo): ensure CHANGELOG.md is valid
    It is easy to forget to add the brackets when adding a new "Unreleased"
    version. Add a simple test to validate this.
    
    Also fix a small issue the test caught.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/integration_tests/style/test_repo.py
+++ tests/integration_tests/style/test_repo.py
@@ -3,6 +3,7 @@
 
 """Tests enforcing git repository structure"""
 
+import re
 import subprocess
 from pathlib import Path
 
@@ -34,3 +35,25 @@ def test_repo_validate_yaml():
         if str(path).startswith("../build/"):
             continue
         yaml.safe_load(path.open(encoding="utf-8"))
+
+
+def test_repo_validate_changelog():
+    """Make sure the CHANGELOG.md file follows the Keep a Changelog format"""
+
+    changelog_path = Path("../CHANGELOG.md")
+    changelog = changelog_path.read_text(encoding="utf-8").splitlines()
+    errors = []
+    for lineno, line in enumerate(changelog, start=1):
+        if line.startswith("## "):
+            if not re.match(r"^## \[.+\]$", line):
+                msg = "Level 2 headings (versions) should be wrapped in []"
+                errors.append((lineno, msg, line))
+        if line.startswith("### "):
+            if not re.match(r"^### (Added|Changed|Deprecated|Removed|Fixed)$", line):
+                msg = "Unknown Level 3 heading"
+                errors.append((lineno, msg, line))
+
+    for lineno, msg, line in errors:
+        print(msg)
+        print(f"\t{lineno}:{line}")
+    assert len(errors) == 0
