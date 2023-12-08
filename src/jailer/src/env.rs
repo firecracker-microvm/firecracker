firecracker-microvm/firecracker

commit da92bf6d4afa678565dcf9df2e4e00d2fa273d96

fix: use double fork mechanism for daemonize
Use the double fork method to daemonize jailer
as suggested in https://0xjet.github.io/3OHA/2022/04/11/post.html
1st fork to make sure setsid doesn't fail and 2nd fork is to
make sure the daemon code cannot reacquire a controlling terminal.

Signed-off-by: Sudan Landge <sudanl@amazon.com>

src/jailer/src/env.rs
@@ -8,7 +8,7 @@ use std::os::unix::fs::PermissionsExt;
 use std::os::unix::io::AsRawFd;
 use std::os::unix::process::CommandExt;
 use std::path::{Component, Path, PathBuf};
-use std::process::{Command, Stdio};
+use std::process::{exit, Command, Stdio};
 use std::{fmt, io};
 
 use utils::arg_parser::Error::MissingValue;
@@ -674,12 +674,45 @@ impl Env {
 
         // Daemonize before exec, if so required (when the dev_null variable != None).
         if let Some(dev_null) = dev_null {
-            // Call setsid().
+            // We follow the double fork method to daemonize the jailer referring to
+            // https://0xjet.github.io/3OHA/2022/04/11/post.html
+            // setsid() will fail if the calling process is a process group leader.
+            // By calling fork(), we guarantee that the newly created process inherits
+            // the PGID from its parent and, therefore, is not a process group leader.
+            // SAFETY: Safe because it's a library function.
+            let child_pid = unsafe { libc::fork() };
+            if child_pid < 0 {
+                return Err(JailerError::Daemonize(io::Error::last_os_error()));
+            }
+
+            if child_pid != 0 {
+                // parent exiting
+                exit(0);
+            }
+
+            // Call setsid() in child
             // SAFETY: Safe because it's a library function.
             SyscallReturnCode(unsafe { libc::setsid() })
                 .into_empty_result()
                 .map_err(JailerError::SetSid)?;
 
+            // Daemons should not have controlling terminals.
+            // If a daemon has a controlling terminal, it can receive signals
+            // from it that might cause it to halt or exit unexpectedly.
+            // The second fork() ensures that grandchild is not a session,
+            // leader and thus cannot reacquire a controlling terminal.
+            // SAFETY: Safe because it's a library function.
+            let grandchild_pid = unsafe { libc::fork() };
+            if grandchild_pid < 0 {
+                return Err(JailerError::Daemonize(io::Error::last_os_error()));
+            }
+
+            if grandchild_pid != 0 {
+                // child exiting
+                exit(0);
+            }
+
+            // grandchild is the daemon
             // Replace the stdio file descriptors with the /dev/null fd.
             dup2(dev_null.as_raw_fd(), STDIN_FILENO)?;
             dup2(dev_null.as_raw_fd(), STDOUT_FILENO)?;

src/jailer/src/main.rs
@@ -64,6 +64,8 @@ pub enum JailerError {
     CreateDir(PathBuf, io::Error),
     #[error("Encountered interior \\0 while parsing a string")]
     CStringParsing(NulError),
+    #[error("Failed to daemonize: {0}")]
+    Daemonize(io::Error),
     #[error("Failed to open directory {0}: {1}")]
     DirOpen(String, String),
     #[error("Failed to duplicate fd: {0}")]

commit dda3f751f3f9cc8a94424e97a48295b504686ba3

fix(jailer): account time spent in the jailer more accurately
After da92bf6d, the CPU time we got from get_time_us was much shorter,
and that caused an underflow when calculating the jailer_cpu_time_us.
That resulted in Firecracker getting called with a huge value for
`--parent-cpu-time`.

Before this fix:

    /firecracker [...] --parent-cpu-time-us 18446744073709551368

After this fix:

    /firecracker [...] --parent-cpu-time-us 4375

The new time is higher than before da92bf6d, because a) that commit
introduced a fork, which carries some overhead, and b) we now account
for CPU time during forks.

Fixes: da92bf6d4afa678565dcf9df2e4e00d2fa273d96

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

src/jailer/src/env.rs
@@ -343,21 +343,12 @@ impl Env {
     }
 
     fn exec_into_new_pid_ns(&mut self, chroot_exec_file: PathBuf) -> Result<(), JailerError> {
-        // Compute jailer's total CPU time up to the current time.
-        self.jailer_cpu_time_us =
-            utils::time::get_time_us(utils::time::ClockType::ProcessCpu) - self.start_time_cpu_us;
-
         // Duplicate the current process. The child process will belong to the previously created
         // PID namespace. The current process will not be moved into the newly created namespace,
         // but its first child will assume the role of init(1) in the new namespace.
         let pid = clone(std::ptr::null_mut(), libc::CLONE_NEWPID)?;
         match pid {
-            0 => {
-                // Reset process start time.
-                self.start_time_cpu_us = 0;
-
-                Err(JailerError::Exec(self.exec_command(chroot_exec_file)))
-            }
+            0 => Err(JailerError::Exec(self.exec_command(chroot_exec_file))),
             child_pid => {
                 // Save the PID of the process running the exec file provided
                 // inside <chroot_exec_file>.pid file.
@@ -674,6 +665,9 @@ impl Env {
 
         // Daemonize before exec, if so required (when the dev_null variable != None).
         if let Some(dev_null) = dev_null {
+            // Meter CPU usage before fork()
+            self.jailer_cpu_time_us = utils::time::get_time_us(utils::time::ClockType::ProcessCpu);
+
             // We follow the double fork method to daemonize the jailer referring to
             // https://0xjet.github.io/3OHA/2022/04/11/post.html
             // setsid() will fail if the calling process is a process group leader.
@@ -695,6 +689,9 @@ impl Env {
             SyscallReturnCode(unsafe { libc::setsid() })
                 .into_empty_result()
                 .map_err(JailerError::SetSid)?;
+
+            // Meter CPU usage before fork()
+            self.jailer_cpu_time_us += utils::time::get_time_us(utils::time::ClockType::ProcessCpu);
 
             // Daemons should not have controlling terminals.
             // If a daemon has a controlling terminal, it can receive signals
@@ -718,6 +715,12 @@ impl Env {
             dup2(dev_null.as_raw_fd(), STDOUT_FILENO)?;
             dup2(dev_null.as_raw_fd(), STDERR_FILENO)?;
         }
+
+        // Compute jailer's total CPU time up to the current time.
+        self.jailer_cpu_time_us +=
+            utils::time::get_time_us(utils::time::ClockType::ProcessCpu) - self.start_time_cpu_us;
+        // Reset process start time.
+        self.start_time_cpu_us = 0;
 
         // If specified, exec the provided binary into a new PID namespace.
         if self.new_pid_ns {