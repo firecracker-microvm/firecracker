ba18601d564ed7ff07890c6415de6ffc6b66a795
Patrick Roy <roypat@amazon.co.uk>
2023-12-08 11:12:05 +0000

    test: run vsock tests with all supported guest kernels
    The purpose of this test is to check that the vsock device works
    correctly with all supported guest kernels (e.g. their drivers). Since
    driver behavior can be different in different kernel versions (e.g. 6.1
    uses 1 virtio descriptor per vsock package, while 5.10 and 4.14 use two,
    it makes sense to parameterize the test by guest kernel, instead of only
    using 5.10.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/functional/test_vsock.py
+++ tests/integration_tests/functional/test_vsock.py
@@ -34,14 +34,14 @@ NEGATIVE_TEST_CONNECTION_COUNT = 100
 TEST_WORKER_COUNT = 10
 
 
-def test_vsock(test_microvm_with_api, bin_vsock_path, test_fc_session_root_path):
+def test_vsock(uvm_plain_any, bin_vsock_path, test_fc_session_root_path):
     """
     Test guest and host vsock initiated connections.
 
     Check the module docstring for details on the setup.
     """
 
-    vm = test_microvm_with_api
+    vm = uvm_plain_any
     vm.spawn()
 
     vm.basic_config()
