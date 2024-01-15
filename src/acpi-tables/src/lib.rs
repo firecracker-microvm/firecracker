3641e9252e3e8a024a1400cdbab17026c54b543c
Babis Chalios <bchalios@amazon.es>
2024-01-15 18:40:05 +0000

    acpi: create acpi-tables crate
    Add a lib crate for handling ACPI tables. The crate exports types
    representing the various tables we handle and methods for creating
    them, modifying them and writing them in guest memory.
    
    The crate is based on the cloudhypervisor implementation:
    https://github.com/rust-vmm/acpi_tables. We reuse the logic for creating
    AML bytecode but we diverge from the way they create and manipulate the
    tables. They rely mainly on the SdtHeader type and work with byte
    offsets, whereas we define concrete types for each table and use those.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

---
+++ src/acpi-tables/src/lib.rs
@@ -0,0 +1,147 @@
+// Copyright © 2019 Intel Corporation
+// Copyright 2023 Rivos, Inc.
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use vm_memory::{GuestAddress, GuestMemory, GuestMemoryError};
+
+pub mod aml;
+pub mod dsdt;
+pub mod fadt;
+pub mod madt;
+pub mod rsdp;
+pub mod xsdt;
+
+pub use aml::Aml;
+pub use dsdt::Dsdt;
+pub use fadt::Fadt;
+pub use madt::Madt;
+pub use rsdp::Rsdp;
+pub use xsdt::Xsdt;
+use zerocopy::little_endian::{U32, U64};
+use zerocopy::AsBytes;
+
+// This is the creator ID that we will embed in ACPI tables that are created using this crate.
+const FC_ACPI_CREATOR_ID: [u8; 4] = *b"FCAT";
+// This is the created ID revision that we will embed in ACPI tables that are created using this
+// crate.
+const FC_ACPI_CREATOR_REVISION: u32 = 0x20240119;
+
+fn checksum(buf: &[&[u8]]) -> u8 {
+    (255 - buf
+        .iter()
+        .flat_map(|b| b.iter())
+        .fold(0u8, |acc, x| acc.wrapping_add(*x)))
+    .wrapping_add(1)
+}
+
+#[derive(Debug, thiserror::Error, displaydoc::Display)]
+pub enum AcpiError {
+    /// Guest memory error: {0}
+    GuestMemory(#[from] GuestMemoryError),
+    /// Invalid guest address
+    InvalidGuestAddress,
+    /// Invalid register size
+    InvalidRegisterSize,
+}
+
+pub type Result<T> = std::result::Result<T, AcpiError>;
+
+/// ACPI type representing memory addresses
+#[repr(packed)]
+#[derive(AsBytes, Clone, Copy, Debug, Default)]
+pub struct GenericAddressStructure {
+    pub address_space_id: u8,
+    pub register_bit_width: u8,
+    pub register_bit_offset: u8,
+    pub access_size: u8,
+    pub address: U64,
+}
+
+impl GenericAddressStructure {
+    pub fn new(
+        address_space_id: u8,
+        register_bit_width: u8,
+        register_bit_offset: u8,
+        access_size: u8,
+        address: u64,
+    ) -> Self {
+        Self {
+            address_space_id,
+            register_bit_width,
+            register_bit_offset,
+            access_size,
+            address: U64::new(address),
+        }
+    }
+}
+
+/// Header included in all System Descriptor Tables
+#[repr(packed)]
+#[derive(Clone, Debug, Copy, Default, AsBytes)]
+pub struct SdtHeader {
+    pub signature: [u8; 4],
+    pub length: U32,
+    pub revision: u8,
+    pub checksum: u8,
+    pub oem_id: [u8; 6],
+    pub oem_table_id: [u8; 8],
+    pub oem_revision: U32,
+    pub creator_id: [u8; 4],
+    pub creator_revison: U32,
+}
+
+impl SdtHeader {
+    pub(crate) fn new(
+        signature: [u8; 4],
+        length: u32,
+        table_revision: u8,
+        oem_id: [u8; 6],
+        oem_table_id: [u8; 8],
+        oem_revision: u32,
+    ) -> Self {
+        SdtHeader {
+            signature,
+            length: U32::new(length),
+            revision: table_revision,
+            checksum: 0,
+            oem_id,
+            oem_table_id,
+            oem_revision: U32::new(oem_revision),
+            creator_id: FC_ACPI_CREATOR_ID,
+            creator_revison: U32::new(FC_ACPI_CREATOR_REVISION),
+        }
+    }
+}
+
+/// A trait for functionality around System Descriptor Tables.
+pub trait Sdt {
+    /// Get the length of the table
+    fn len(&self) -> usize;
+
+    /// Return true if Sdt is empty
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Write the table in guest memory
+    fn write_to_guest<M: GuestMemory>(&mut self, mem: &M, address: GuestAddress) -> Result<()>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::checksum;
+
+    #[test]
+    fn test_checksum() {
+        assert_eq!(checksum(&[&[]]), 0u8);
+        assert_eq!(checksum(&[]), 0u8);
+        assert_eq!(checksum(&[&[1, 2, 3]]), 250u8);
+        assert_eq!(checksum(&[&[1, 2, 3], &[]]), 250u8);
+        assert_eq!(checksum(&[&[1, 2], &[3]]), 250u8);
+        assert_eq!(checksum(&[&[1, 2], &[3], &[250]]), 0u8);
+        assert_eq!(checksum(&[&[255]]), 1u8);
+        assert_eq!(checksum(&[&[1, 2], &[3], &[250], &[255]]), 1u8);
+    }
+}
---
+++ src/acpi-tables/src/madt.rs
@@ -0,0 +1,137 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2023 Rivos, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use std::mem::size_of;
+
+use vm_memory::{Address, Bytes, GuestAddress, GuestMemory};
+use zerocopy::little_endian::U32;
+use zerocopy::AsBytes;
+
+use crate::{checksum, AcpiError, Result, Sdt, SdtHeader};
+
+const MADT_CPU_ENABLE_FLAG: u32 = 0;
+
+// clippy doesn't understand that we actually "use" the fields of this struct when we serialize
+// them as bytes in guest memory, so here we just ignore dead code to avoid having to name
+// everything with an underscore prefix
+#[allow(dead_code)]
+#[repr(packed)]
+#[derive(Copy, Clone, Debug, Default, AsBytes)]
+pub struct LocalAPIC {
+    r#type: u8,
+    length: u8,
+    processor_uid: u8,
+    apic_id: u8,
+    flags: U32,
+}
+
+impl LocalAPIC {
+    pub fn new(cpu_id: u8) -> Self {
+        Self {
+            r#type: 0,
+            length: 8,
+            processor_uid: cpu_id,
+            apic_id: cpu_id,
+            flags: U32::new(1u32 << MADT_CPU_ENABLE_FLAG),
+        }
+    }
+}
+
+// clippy doesn't understand that we actually "use" the fields of this struct when we serialize
+// them as bytes in guest memory, so here we just ignore dead code to avoid having to name
+// everything with an underscore prefix
+#[allow(dead_code)]
+#[repr(packed)]
+#[derive(Copy, Clone, Debug, Default, AsBytes)]
+pub struct IoAPIC {
+    r#type: u8,
+    length: u8,
+    ioapic_id: u8,
+    reserved: u8,
+    apic_address: U32,
+    gsi_base: U32,
+}
+
+impl IoAPIC {
+    pub fn new(ioapic_id: u8, apic_address: u32) -> Self {
+        IoAPIC {
+            r#type: 1,
+            length: 12,
+            ioapic_id,
+            reserved: 0,
+            apic_address: U32::new(apic_address),
+            gsi_base: U32::ZERO,
+        }
+    }
+}
+
+// clippy doesn't understand that we actually "use" the fields of this struct when we serialize
+// them as bytes in guest memory, so here we just ignore dead code to avoid having to name
+// everything with an underscore prefix
+#[allow(dead_code)]
+#[repr(packed)]
+#[derive(Debug, AsBytes)]
+struct MadtHeader {
+    sdt: SdtHeader,
+    base_address: U32,
+    flags: U32,
+}
+
+#[derive(Debug)]
+pub struct Madt {
+    header: MadtHeader,
+    interrupt_controllers: Vec<u8>,
+}
+
+impl Madt {
+    pub fn new(
+        oem_id: [u8; 6],
+        oem_table_id: [u8; 8],
+        oem_revision: u32,
+        base_address: u32,
+        interrupt_controllers: Vec<u8>,
+    ) -> Self {
+        let length = size_of::<MadtHeader>() + interrupt_controllers.len();
+        let sdt_header = SdtHeader::new(
+            *b"APIC",
+            // It is ok to unwrap the conversion of `length` to u32. `SdtHeader` is 36 bytes long,
+            // so `length` here has a value of 44.
+            length.try_into().unwrap(),
+            6,
+            oem_id,
+            oem_table_id,
+            oem_revision,
+        );
+
+        let mut header = MadtHeader {
+            sdt: sdt_header,
+            base_address: U32::new(base_address),
+            flags: U32::ZERO,
+        };
+
+        header.sdt.checksum = checksum(&[header.as_bytes(), interrupt_controllers.as_bytes()]);
+
+        Madt {
+            header,
+            interrupt_controllers,
+        }
+    }
+}
+
+impl Sdt for Madt {
+    fn len(&self) -> usize {
+        self.header.sdt.length.get().try_into().unwrap()
+    }
+
+    fn write_to_guest<M: GuestMemory>(&mut self, mem: &M, address: GuestAddress) -> Result<()> {
+        mem.write_slice(self.header.as_bytes(), address)?;
+        let address = address
+            .checked_add(size_of::<MadtHeader>() as u64)
+            .ok_or(AcpiError::InvalidGuestAddress)?;
+        mem.write_slice(self.interrupt_controllers.as_bytes(), address)?;
+
+        Ok(())
+    }
+}
