24f39800576ac969d5cebe081b43ae3896935183
Muskaan Singla <ec2-user@ip-172-31-1-28.us-west-2.compute.internal>
2024-01-19 13:21:30 +0000

    fix: reset dirty log on full snapshot path
    When a diff snapshot it taken, it always contains all pages that were
    dirtied since either instance start, or since the last diff snapshot was
    taken, whichever is more recent. This is not what we document it to do:
    
    > the diff consists of the memory pages which have been dirtied since
    > the last snapshot creation or since the creation of the microVM,
    > whichever of these events was the most recent.
    
    Here "last snapshot creation" includes full snapshots, not just diff
    snapshots (and that makes sense, if I take a diff snapshot after a full
    snapshot, I expect the diff snapshot to be a diff compared to the full
    snapshot).
    
    Signed-off-by: Roman Kovtyukh <HelloDearGrandma@gmail.com>
    Co-authored-by: Muskaan Singla <msinglaa@amazon.com>

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -249,7 +249,10 @@ fn snapshot_memory_to_file(
                 .dump_dirty(&mut file, &dirty_bitmap)
                 .map_err(Memory)
         }
-        SnapshotType::Full => vmm.guest_memory().dump(&mut file).map_err(Memory),
+        SnapshotType::Full => {
+            let _ = vmm.get_dirty_bitmap().map_err(DirtyBitmap)?;
+            vmm.guest_memory().dump(&mut file).map_err(Memory)
+        }
     }?;
     file.flush()
         .map_err(|err| MemoryBackingFile("flush", err))?;
