de1eb0de2c2c86648077fb7823cd27faff95d686
Babis Chalios <bchalios@amazon.es>
2024-02-29 10:04:49 +0100

    test: allow using 6.1 kernel in selected tests
    At the moment, we filter out 6.1 guest kernel in all tests apart from
    PTP on Graviton. Add a new filter that allows select any kernel that
    exists in our CI artifacts folder and use it to create a pytest fixture
    for guest kernel 6.1, which works independently of the platform we run
    on.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/framework/artifacts.py
+++ tests/framework/artifacts.py
@@ -58,14 +58,27 @@ def kernels(glob) -> Iterator:
                 break
 
 
+def kernels_unfiltered(glob) -> Iterator:
+    """Return kernels from the CI artifacts. This one does not filter for
+    supported kernels. It will return any kernel in the CI artifacts folder
+    that matches the 'glob'
+    """
+    all_kernels = [r"vmlinux-\d.\d+.\d+", r"vmlinux-5.10-no-sve-bin"]
+    for kernel in sorted(ARTIFACT_DIR.rglob(glob)):
+        for kernel_regex in all_kernels:
+            if re.fullmatch(kernel_regex, kernel.name):
+                yield kernel
+                break
+
+
 def disks(glob) -> Iterator:
     """Return supported rootfs"""
     yield from sorted(ARTIFACT_DIR.glob(glob))
 
 
-def kernel_params(glob="vmlinux-*") -> Iterator:
+def kernel_params(glob="vmlinux-*", select=kernels) -> Iterator:
     """Return supported kernels"""
-    for kernel in kernels(glob):
+    for kernel in select(glob):
         yield pytest.param(kernel, id=kernel.name)
 
 
