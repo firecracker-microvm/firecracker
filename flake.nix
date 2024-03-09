214d7cc921a7daf1d223e8510c3e0d80e38d78b7
joaoleal <devjoaoleal1006@gmail.com>
2024-03-09 17:18:12 -0300

    Add: flake.nix to root of the project
    Reason: Some developers can benefit from the Nix and NixOs package management feature called flakes...
    This feature automatically enables all the declared packages and all the dependencies configuration
    in flake.nix file.
    
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    I understand and agree that this project and the contribution
    are public and that a record of the contribution (including
    all personal information I submit with it, including my
    sign-off) is maintained indefinitely and may be redistributed
    consistent with this project or the open source license(s)
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I
        have the right to submit it under the open source license
        indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best
        of my knowledge, is covered under an appropriate open source
        license and I have the right under that license to submit that
        work with modifications, whether created in whole or in part
        by me, under the same open source license (unless I am
        permitted to submit under a different license), as indicated
        in the file; or
    
    (c) The contribution was provided directly to me by some other
        person who certified (a), (b) or (c) and I have not modified
        it.
    
    (d) I understand and agree that this project and the contribution
        are public and that a record of the contribution (including all
        personal information I submit with it, including my sign-off) is
        maintained indefinitely and may be redistributed consistent with
        this project or the open source license(s) involved.
        involved.
    
    Signed-off-by: joaoleal <devjoaoleal1006@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Firecracker dependecies for developer environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (import rust-overlay)
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell.override { stdenv = pkgs.clangStdenv; } {
+          # Point bindgen to where the clang library would be
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          # Make clang aware of a few headers (stdbool.h, wchar.h)
+          BINDGEN_EXTRA_CLANG_ARGS = with pkgs; ''
+            -isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion clang}/include
+            -isystem ${llvmPackages.libclang.out}/lib/clang/${lib.getVersion clang}/include
+            -isystem ${glibc.dev}/include
+          '';
+
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
