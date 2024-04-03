firecracker-microvm/firecracker

commit 21a3a6dec64b2111d9167a487f0178ac0dc7793c

fix: Update static CPU templates with `RFDS_NO` bit
Passthrough in T2CL template, set to 1 in T2S template.

Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

src/vmm/src/cpu_config/x86_64/static_cpu_templates/t2cl.rs
@@ -277,12 +277,13 @@ pub fn t2cl() -> CustomCpuTemplate {
             // - Bit 19: RRSBA
             // - Bit 24: PBRSB_NO
             // - Bit 26: GDS_NO
+            // - Bit 27: RFDS_NO
             //
             // Note that this MSR is specific to Intel processors.
             RegisterModifier {
                 addr: 0x10a,
                 bitmap: RegisterValueFilter {
-                    filter: 0b1111_1111_1111_1111_1111_1111_1111_1111_1111_1010_1111_0101_0001_1110_0000_0000,
+                    filter: 0b1111_1111_1111_1111_1111_1111_1111_1111_1111_0010_1111_0101_0001_1110_0000_0000,
                     value: 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
                 },
             },

src/vmm/src/cpu_config/x86_64/static_cpu_templates/t2s.rs
@@ -255,12 +255,13 @@ pub fn t2s() -> CustomCpuTemplate {
             // - Bit 23: OVERCLOCKING_STATUS
             // - Bit 24: PBRSB_NO
             // - Bit 26: GDS_NO
+            // - BIT 27: RFDS_NO
             // - Bits 63-25: Reserved
             RegisterModifier {
             addr: 0x10a,
             bitmap: RegisterValueFilter {
                 filter: 0b1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111_1111,
-                value: 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0100_0000_1000_0000_1100_0100_1100,
+                value: 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_1100_0000_1000_0000_1100_0100_1100,
             },
         }],
         ..Default::default()

commit e8144058af842087e4bed6a32e365bcc77243e5d

fix: Update T2CL to pass through IA32_ARCH_CAPABILITIES.RFDS_CLEAR
KVM considers not only `RFDS_NO` but also `RFDS_CLEAR` can be passed
through. Processors enumerate `RFDS_CLEAR` if they are affected by RFDS
and let software know that the VERW instruction will overwrite buffers
affected by RFDS. T2CL doesn't aim to provide security feature parity
unlike T2S, so the bit should also be passed through.

Fixes: 21a3a6dec64b ("fix: Update static CPU templates with RFDS_NO
bit")
Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

src/vmm/src/cpu_config/x86_64/static_cpu_templates/t2cl.rs
@@ -260,7 +260,7 @@ pub fn t2cl() -> CustomCpuTemplate {
             // by making the most use of available mitigations on the processor. Thus, T2CL template
             // passes through security mitigation bits that KVM thinks are able to be passed
             // through. The list of such bits are found in the following link.
-            // https://elixir.bootlin.com/linux/v6.1.46/source/arch/x86/kvm/x86.c#L1600
+            // https://elixir.bootlin.com/linux/v6.8.2/source/arch/x86/kvm/x86.c#L1621
             // - Bit 00: RDCL_NO
             // - Bit 01: IBRS_ALL
             // - Bit 02: RSBA
@@ -278,12 +278,13 @@ pub fn t2cl() -> CustomCpuTemplate {
             // - Bit 24: PBRSB_NO
             // - Bit 26: GDS_NO
             // - Bit 27: RFDS_NO
+            // - Bit 28: RFDS_CLEAR
             //
             // Note that this MSR is specific to Intel processors.
             RegisterModifier {
                 addr: 0x10a,
                 bitmap: RegisterValueFilter {
-                    filter: 0b1111_1111_1111_1111_1111_1111_1111_1111_1111_0010_1111_0101_0001_1110_0000_0000,
+                    filter: 0b1111_1111_1111_1111_1111_1111_1111_1111_1110_0010_1111_0101_0001_1110_0000_0000,
                     value: 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000,
                 },
             },