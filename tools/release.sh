firecracker-microvm/firecracker

commit c9cc8d1245c378ba788efd1aa9bad98d2f709da2

fix(tests): strip debug information before a testrun
Before df112e39c the tests compiled Firecracked and stripped the
debuginfo. When we moved to running release.sh the behavior changed
since we only strip when making a release.

Solve it by always stripping symbols when building Firecracker in
--release profile.

Some care was taken so it is safe to re-run, so we don't lose the debug
symbols if the original binary has not changed.

Fixes: df112e39ccd558c51494b0d0e4ac8b288f6d140d

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/release.sh
@@ -45,10 +45,13 @@ function check_bin_artifact {
 
 function strip-and-split-debuginfo {
     local bin=$1
+    if [ $bin -ot $bin.debug ]; then
+        return
+    fi
     echo "STRIP $bin"
     objcopy --only-keep-debug $bin $bin.debug
     chmod a-x $bin.debug
-    objcopy --strip-debug --add-gnu-debuglink=$bin.debug $bin
+    objcopy --preserve-dates --strip-debug --add-gnu-debuglink=$bin.debug $bin
 }
 
 function get-firecracker-version {
@@ -129,6 +132,10 @@ say "Building version=$VERSION, profile=$PROFILE, target=$CARGO_TARGET, Rust too
 # shellcheck disable=SC2086
 cargo build --target "$CARGO_TARGET" $CARGO_OPTS --workspace --bins --examples
 
+for file in "${ARTIFACTS[@]}"; do
+    strip-and-split-debuginfo "$CARGO_TARGET_DIR/$file"
+done
+
 say "Binaries placed under $CARGO_TARGET_DIR"
 
 # Check static linking:
@@ -156,7 +163,7 @@ mkdir "$RELEASE_DIR"
 for file in "${ARTIFACTS[@]}"; do
     check_bin_artifact "$CARGO_TARGET_DIR/$file" "$VERSION"
     cp -v "$CARGO_TARGET_DIR/$file" "$RELEASE_DIR/$file-$SUFFIX"
-    strip-and-split-debuginfo "$RELEASE_DIR/$file-$SUFFIX"
+    cp -v "$CARGO_TARGET_DIR/$file.debug" "$RELEASE_DIR/$file-$SUFFIX.debug"
 done
 cp -v "resources/seccomp/$CARGO_TARGET.json" "$RELEASE_DIR/seccomp-filter-$SUFFIX.json"
 # Copy over arch independent assets

commit e6de76ebfced3a3393fc71bd0c238aa8d3b6b174

fix(build): only strip binaries in release builds
Fixes: c9cc8d1245c378ba788efd1aa9bad98d2f709da2

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

tools/release.sh
@@ -132,9 +132,12 @@ say "Building version=$VERSION, profile=$PROFILE, target=$CARGO_TARGET, Rust too
 # shellcheck disable=SC2086
 cargo build --target "$CARGO_TARGET" $CARGO_OPTS --workspace --bins --examples
 
-for file in "${ARTIFACTS[@]}"; do
-    strip-and-split-debuginfo "$CARGO_TARGET_DIR/$file"
-done
+# Only strip in release mode
+if [ "$PROFILE" = "release" ]; then
+    for file in "${ARTIFACTS[@]}"; do
+        strip-and-split-debuginfo "$CARGO_TARGET_DIR/$file"
+    done
+fi
 
 say "Binaries placed under $CARGO_TARGET_DIR"
 