895c1a43f702085c72e8bb96f02c54ccd5566a3d
Takahiro Itazuri <itazur@amazon.com>
2024-04-18 09:26:06 +0000

    test: Print debug information of find and lsblk command
    We run into multiple intermittent issues when commands exit with status
    code 255 without any useful info in stderr on microVMs restored from
    snapshots. Currently, we are not sure whether the commands themselves
    failed or something before entering the commands failed. To distinguish
    them, enable debug print for some commands that we have observed failed
    so far.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- tests/integration_tests/security/test_vulnerabilities.py
+++ tests/integration_tests/security/test_vulnerabilities.py
@@ -439,7 +439,7 @@ def check_vulnerabilities_files_on_guest(microvm):
     """
     # Retrieve a list of vulnerabilities files available inside guests.
     vuln_dir = "/sys/devices/system/cpu/vulnerabilities"
-    ecode, stdout, stderr = microvm.ssh.run(f"find {vuln_dir} -type f")
+    ecode, stdout, stderr = microvm.ssh.run(f"find -D all {vuln_dir} -type f")
     assert ecode == 0, f"stdout:\n{stdout}\nstderr:\n{stderr}\n"
     vuln_files = stdout.split("\n")
 
