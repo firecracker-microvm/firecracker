firecracker-microvm/firecracker

commit 8ca2ea46e7c662e3b1c5c2760ce83dfec525c532

test: run test_kani.py on m6a.metal instead of m6i.metal
m6a.metal has significantly more memory, and is also slightly faster.

Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

.buildkite/pipeline_pr.py
@@ -85,7 +85,7 @@ defaults_for_kani = overlay_dict(
     defaults_for_performance,
     {
         # Kani runs fastest on m6i.metal
-        "instances": ["m6i.metal"],
+        "instances": ["m6a.metal"],
         "platforms": [("al2", "linux_5.10")],
         "timeout_in_minutes": 300,
     },

commit 6e5a7acb943492dfaef786dfd2ce6da67c02c623

ci: match comment to code
When we changed the instance we forgot to update the comment.

Fixes: 8ca2ea46e7c662e3b1c5c2760ce83dfec525c532

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/pipeline_pr.py
@@ -50,7 +50,7 @@ if not changed_files or any(
         "🔍 Kani",
         "./tools/devtool -y test -- ../tests/integration_tests/test_kani.py -n auto",
         # Kani step default
-        # Kani runs fastest on m6i.metal
+        # Kani runs fastest on m6a.metal
         instances=["m6a.metal"],
         platforms=[("al2", "linux_5.10")],
         timeout_in_minutes=300,