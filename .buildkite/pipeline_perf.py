firecracker-microvm/firecracker

commit f5906eeb47f71f1ba412b24786d6762fc06dcbf4

ci: move automatic retry to BKPipeline class
The automatic retry when a BK agent disconnects we want in all
pipelines.

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/pipeline_perf.py
@@ -72,21 +72,14 @@ BKPipeline.parser.add_argument(
     action="append",
 )
 
-retry = {
-    "automatic": [
-        # Agent was lost, retry one time
-        # this can happen if we terminate the instance or the agent gets
-        # disconnected for whatever reason
-        {"exit_status": -1, "limit": 1},
-    ]
-}
+retry = {}
 if REVISION_A:
     # Enable automatic retry and disable manual retries to suppress spurious issues.
     retry["automatic"].append({"exit_status": 1, "limit": 1})
     retry["manual"] = False
 
 pipeline = BKPipeline(
-    # Boost priority from 1 to 2 so these jobs are preferred by ag-1 agents
+    # Boost priority from 1 to 2 so these jobs are preferred by ag=1 agents
     priority=2,
     # use ag=1 instances to make sure no two performance tests are scheduled on the same instance
     agents={"ag": 1},

.buildkite/common.py
@@ -224,11 +224,18 @@ class BKPipeline:
     def __init__(self, initial_steps=None, **kwargs):
         self.steps = initial_steps or []
         self.args = args = self.parser.parse_args()
+        # Retry one time if agent was lost. This can happen if we terminate the
+        # instance or the agent gets disconnected for whatever reason
+        retry = {
+            "automatic": [{"exit_status": -1, "limit": 1}],
+        }
+        retry = overlay_dict(retry, kwargs.pop("retry", {}))
         # Calculate step defaults with parameters and kwargs
         per_instance = {
             "instances": args.instances,
             "platforms": args.platforms,
             "artifact_paths": ["./test_results/**/*"],
+            "retry": retry,
             **kwargs,
         }
         self.per_instance = overlay_dict(per_instance, args.step_param)

.buildkite/pipeline_cross.py
@@ -15,6 +15,9 @@ from common import DEFAULT_PLATFORMS, BKPipeline
 
 if __name__ == "__main__":
     pipeline = BKPipeline()
+    per_instance = pipeline.per_instance.copy()
+    per_instance.pop("instances")
+    per_instance.pop("platforms")
     instances_x86_64 = ["c5n.metal", "m5n.metal", "m6i.metal", "m6a.metal"]
     instances_aarch64 = ["m7g.metal"]
     commands = [
@@ -86,6 +89,7 @@ if __name__ == "__main__":
             "label": f"🎬 {src_instance} {src_kv} ➡️ {dst_instance} {dst_kv}",
             "timeout": 30,
             "agents": {"instance": dst_instance, "kv": dst_kv, "os": dst_os},
+            **per_instance,
         }
         steps.append(step)
     pipeline.add_step(

commit 301cdd468cce5b5a4ea6490b2a47da60e435cbc8

fix(ci): set retry list
In commit f5906eeb47f71f1ba412b24786d6762fc06dcbf4 we moved the
initialization of the list but forgot to change it here.

Fixes: f5906eeb47f71f1ba412b24786d6762fc06dcbf4

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/pipeline_perf.py
@@ -75,7 +75,10 @@ BKPipeline.parser.add_argument(
 retry = {}
 if REVISION_A:
     # Enable automatic retry and disable manual retries to suppress spurious issues.
-    retry["automatic"].append({"exit_status": 1, "limit": 1})
+    retry["automatic"] = [
+        {"exit_status": -1, "limit": 1},
+        {"exit_status": 1, "limit": 1},
+    ]
     retry["manual"] = False
 
 pipeline = BKPipeline(