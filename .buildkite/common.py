firecracker-microvm/firecracker

commit 0c55e106fd90b3827ed4e355a44bbdcf369e03d7

ci: reuse Firecracker build over several jobs
Build Firecracker as the build first step, and then re-use it over all
of that build's jobs.

Avoids downloading and compiling and should shave ~2 minutes from each
build job.

For example for the PR pipeline: 6 instances * 3 OS * 3 groups * 2
revisions: 108

| pipeline      | before | after |
|---------------|-------:|------:|
| pr            |    108 |     4 |
| pr_no_block   |     36 |     4 |
| perf          |    252 |     4 |
| cross         |     82 |     2 |
| CPU templates |     72 |     2 |

Total: 550 -> 16 (-97%)

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/common.py
@@ -172,6 +172,39 @@ COMMON_PARSER.add_argument(
 )
 
 
+def revision_a():
+    """Determine if there is a base revision"""
+    if os.environ.get("BUILDKITE_PULL_REQUEST", "false") != "false":
+        return os.environ.get("BUILDKITE_PULL_REQUEST_BASE_BRANCH", "main")
+    return os.environ.get("REVISION_A")
+
+
+def shared_build():
+    """Helper function to make it simple to share a compilation artifacts for a
+    whole Buildkite build
+    """
+    build_cmds = ["./tools/devtool -y build --release"]
+
+    # If we are running in a PR context, then also build the base branch in the
+    # expected location, for the A/B tests machinery.
+    rev_a = revision_a()
+    if rev_a is not None:
+        build_cmds += [
+            f"git clone -b {rev_a} . build/{rev_a}",
+            f"cd build/{rev_a} && ./tools/devtool -y build --release && cd -",
+        ]
+    revision_b = os.environ.get("REVISION_B")
+    if revision_b is not None:
+        build_cmds.append(f"ln -svfT . build/{revision_b}")
+    binary_dir = "build_$(uname -m).tar.gz"
+    build_cmds += [
+        "du -sh build/*",
+        f"tar czf {binary_dir} build",
+        f"buildkite-agent artifact upload {binary_dir}",
+    ]
+    return build_cmds, binary_dir
+
+
 class BKPipeline:
     """
     Buildkite Pipeline class abstraction
@@ -195,10 +228,11 @@ class BKPipeline:
         self.per_arch = self.per_instance.copy()
         self.per_arch["instances"] = ["m6i.metal", "m7g.metal"]
         self.per_arch["platforms"] = [("al2", "linux_5.10")]
-        self.binary_dir = None
-        if args.binary_dir is not None:
-            self.binary_dir = args.binary_dir
-            self.shared_build = f"{self.binary_dir}/$(uname -m)/*"
+        self.binary_dir = args.binary_dir
+        # Build sharing
+        build_cmds, self.shared_build = shared_build()
+        step_build = group("🏗️ Build", build_cmds, **self.per_arch)
+        self.steps += [step_build, "wait"]
 
     def add_step(self, step, decorate=True):
         """
@@ -216,12 +250,17 @@ class BKPipeline:
 
     def _adapt_group(self, group):
         """"""
-        prepend = []
+        prepend = [
+            f'buildkite-agent artifact download "{self.shared_build}" .',
+            f"tar xzf {self.shared_build}",
+        ]
         if self.binary_dir is not None:
-            prepend = [
-                f'buildkite-agent artifact download "{self.shared_build}" .',
-                f"chmod -v a+x {self.binary_dir}/**/*",
-            ]
+            prepend.extend(
+                [
+                    f'buildkite-agent artifact download "{self.binary_dir}/$(uname -m)/*" .',
+                    f"chmod -v a+x {self.binary_dir}/**/*",
+                ]
+            )
 
         for step in group["steps"]:
             step["command"] = prepend + step["command"]
@@ -254,7 +293,7 @@ class BKPipeline:
     def devtool_test(self, devtool_opts=None, pytest_opts=None):
         """Generate a `devtool test` command"""
         cmds = []
-        parts = ["./tools/devtool -y test"]
+        parts = ["./tools/devtool -y test", "--no-build"]
         if devtool_opts:
             parts.append(devtool_opts)
         parts.append("--")

.buildkite/pipeline_cpu_template.py
@@ -23,7 +23,7 @@ class BkStep(str, Enum):
 cpu_template_test = {
     "rdmsr": {
         BkStep.COMMAND: [
-            "tools/devtool -y test -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_rdmsr' "
+            "tools/devtool -y test --no-build -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_rdmsr' "
         ],
         BkStep.LABEL: "📖 rdmsr",
         "instances": ["c5n.metal", "m5n.metal", "m6a.metal", "m6i.metal"],
@@ -31,7 +31,7 @@ cpu_template_test = {
     },
     "fingerprint": {
         BkStep.COMMAND: [
-            "tools/devtool -y test -- -m no_block_pr integration_tests/functional/test_cpu_template_helper.py -k test_guest_cpu_config_change",
+            "tools/devtool -y test --no-build -- -m no_block_pr integration_tests/functional/test_cpu_template_helper.py -k test_guest_cpu_config_change",
         ],
         BkStep.LABEL: "🖐️ fingerprint",
         "instances": DEFAULT_INSTANCES,
@@ -40,7 +40,7 @@ cpu_template_test = {
     "cpuid_wrmsr": {
         "snapshot": {
             BkStep.COMMAND: [
-                "tools/devtool -y test -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_wrmsr_snapshot or test_cpu_cpuid_snapshot'",
+                "tools/devtool -y test --no-build -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_wrmsr_snapshot or test_cpu_cpuid_snapshot'",
                 "mkdir -pv tests/snapshot_artifacts_upload/{instance}_{os}_{kv}",
                 "sudo mv tests/snapshot_artifacts/* tests/snapshot_artifacts_upload/{instance}_{os}_{kv}",
             ],
@@ -52,7 +52,7 @@ cpu_template_test = {
             BkStep.COMMAND: [
                 "buildkite-agent artifact download tests/snapshot_artifacts_upload/{instance}_{os}_{kv}/**/* .",
                 "mv tests/snapshot_artifacts_upload/{instance}_{os}_{kv} tests/snapshot_artifacts",
-                "tools/devtool -y test -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_wrmsr_restore or test_cpu_cpuid_restore'",
+                "tools/devtool -y test --no-build -- -s -ra -m nonci -n4 --log-cli-level=INFO integration_tests/functional/test_cpu_features.py -k 'test_cpu_wrmsr_restore or test_cpu_cpuid_restore'",
             ],
             BkStep.LABEL: "📸 load snapshot artifacts created on {instance} {snapshot_os} {snapshot_kv} to {restore_instance} {restore_os} {restore_kv}",
             BkStep.TIMEOUT: 30,

.buildkite/pipeline_cross.py
@@ -13,28 +13,6 @@ import itertools
 
 from common import DEFAULT_PLATFORMS, BKPipeline
 
-
-def restore_step(label, src_instance, src_kv, dst_instance, dst_os, dst_kv):
-    """Generate a restore step"""
-    pytest_keyword_for_instance = {
-        "c5n.metal": "-k 'not None'",
-        "m5n.metal": "-k 'not None'",
-        "m6i.metal": "-k 'not None'",
-        "m6a.metal": "",
-    }
-    k_val = pytest_keyword_for_instance.get(dst_instance, "")
-    return {
-        "command": [
-            f"buildkite-agent artifact download snapshots/{src_instance}_{src_kv}/* .",
-            f"mv -v snapshots/{src_instance}_{src_kv} snapshot_artifacts",
-            f"./tools/devtool -y test -- -m nonci {k_val} integration_tests/functional/test_snapshot_restore_cross_kernel.py",
-        ],
-        "label": label,
-        "timeout": 30,
-        "agents": {"instance": dst_instance, "kv": dst_kv, "os": dst_os},
-    }
-
-
 if __name__ == "__main__":
     pipeline = BKPipeline()
     instances_x86_64 = ["c5n.metal", "m5n.metal", "m6i.metal", "m6a.metal"]
@@ -90,14 +68,25 @@ if __name__ == "__main__":
         ):
             continue
 
-        step = restore_step(
-            f"🎬 {src_instance} {src_kv} ➡️ {dst_instance} {dst_kv}",
-            src_instance,
-            src_kv,
-            dst_instance,
-            dst_os,
-            dst_kv,
-        )
+        pytest_keyword_for_instance = {
+            "c5n.metal": "-k 'not None'",
+            "m5n.metal": "-k 'not None'",
+            "m6i.metal": "-k 'not None'",
+            "m6a.metal": "",
+        }
+        k_val = pytest_keyword_for_instance.get(dst_instance, "")
+        step = {
+            "command": [
+                f"buildkite-agent artifact download snapshots/{src_instance}_{src_kv}/* .",
+                f"mv -v snapshots/{src_instance}_{src_kv} snapshot_artifacts",
+                *pipeline.devtool_test(
+                    pytest_opts=f"-m nonci {k_val} integration_tests/functional/test_snapshot_restore_cross_kernel.py",
+                ),
+            ],
+            "label": f"🎬 {src_instance} {src_kv} ➡️ {dst_instance} {dst_kv}",
+            "timeout": 30,
+            "agents": {"instance": dst_instance, "kv": dst_kv, "os": dst_os},
+        }
         steps.append(step)
     pipeline.add_step(
         {"group": "🎬 restore across instances and kernels", "steps": steps}

commit f2d622e152d501e81d508a84236f82d9f981bb70

fix(ab): fix shared build location
In 0c55e106fd90b3827ed4e355a44bbdcf369e03d7 we introduced shared builds
but that change was not effective until
467c90d5ab69e191262781a46987c961a0b80cc1 at which point it broke AB
tests.

Fix commands so that AB tests work again.

Fixes: 0c55e106fd90b3827ed4e355a44bbdcf369e03d7

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/common.py
@@ -202,7 +202,7 @@ def shared_build():
         ]
     revision_b = os.environ.get("REVISION_B")
     if revision_b is not None:
-        build_cmds.append(f"ln -svfT . build/{revision_b}")
+        build_cmds.append(f"ln -svfT .. build/{revision_b}")
     binary_dir = f"build_$(uname -m)_{random_str(k=8)}.tar.gz"
     build_cmds += [
         "du -sh build/*",

tools/ab_test.py
@@ -158,7 +158,8 @@ def load_data_series(report_path: Path, revision: str = None, *, reemit: bool = 
 
 def collect_data(binary_dir: Path, tests: list[str]):
     """Executes the specified test using the provided firecracker binaries"""
-    revision = binary_dir.name
+    # Example binary_dir: ../build/main/build/cargo_target/x86_64-unknown-linux-musl/release
+    revision = binary_dir.parents[3].name
 
     print(f"Collecting samples with {binary_dir}")
     subprocess.run(
@@ -323,7 +324,7 @@ def ab_performance_test(
     print(commit_list.strip())
 
     def test_runner(workspace, _is_ab: bool):
-        bin_dir = ".." / get_binary("firecracker", workspace_dir=workspace).parent
+        bin_dir = get_binary("firecracker", workspace_dir=workspace).parent
         return collect_data(bin_dir, tests)
 
     return git_ab_test(