bb8394a127e6f6578dd14d203ef71e4a1a1cc14b
Egor Lazarchuk <yegorlz@amazon.co.uk>
2024-08-09 12:10:33 +0100

    refactor(queue): move used ring update into a separate method
    Move used ring update into a separate method. This
    will help with preprocessing of descriptor chains in later
    commits where we will advance used ring once after
    processing multiple descriptors.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- src/vmm/src/devices/virtio/queue.rs
+++ src/vmm/src/devices/virtio/queue.rs
@@ -454,15 +454,19 @@ impl Queue {
             len,
         };
         self.write_used_ring(mem, next_used, used_element)?;
+        self.advance_used_ring(mem, 1);
+        Ok(())
+    }
 
-        self.num_added += Wrapping(1);
-        self.next_used += Wrapping(1);
+    /// Advance number of used descriptor heads by `n`.
+    pub fn advance_used_ring<M: GuestMemory>(&mut self, mem: &M, n: u16) {
+        self.num_added += Wrapping(n);
+        self.next_used += Wrapping(n);
 
         // This fence ensures all descriptor writes are visible before the index update is.
         fence(Ordering::Release);
 
         self.set_used_ring_idx(self.next_used.0, mem);
-        Ok(())
     }
 
     fn write_used_ring<M: GuestMemory>(
