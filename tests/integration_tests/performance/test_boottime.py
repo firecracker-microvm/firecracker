b41c69b0958b1c7813fb806680c0e9fa41c1e79f
Pablo Barbáchano <pablob@amazon.com>
2024-09-03 10:53:12 +0200

    test: remove old boottime test
    Removing these tests as we don't emit metrics from them and they are
    still using kernel 4.14.
    
    There is no point on preserving these tests as we have now track boot
    time in test_boottime.py
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/integration_tests/performance/test_boottime.py
+++ tests/integration_tests/performance/test_boottime.py
@@ -10,11 +10,6 @@ import pytest
 
 from framework.properties import global_props
 
-# The maximum acceptable boot time in us.
-# It is a bit bigger than the default 150_000 because
-# we have ftrace enabled in the guest kernels.
-MAX_BOOT_TIME_US = 170000
-
 # Regex for obtaining boot time from some string.
 TIMESTAMP_LOG_REGEX = r"Guest-boot-time\s+\=\s+(\d+)\s+us"
 
@@ -30,80 +25,6 @@ DIMENSIONS = {
     "host_os": global_props.host_os,
     "host_kernel": "linux-" + global_props.host_linux_version,
 }
-
-
-@pytest.fixture
-def fast_microvm(microvm_factory, guest_kernel_linux_4_14, rootfs_rw):
-    """The microvm defined for the boottime SLA
-
-    Guest kernel 4.14
-    Rootfs: Ubuntu 22.04 ext4
-
-    Using ext4 seems to result in a faster boot than with squashfs. Probably
-    because we have to spend CPU time decompressing and extracting into memory.
-    """
-    return microvm_factory.build(kernel=guest_kernel_linux_4_14, rootfs=rootfs_rw)
-
-
-def test_no_boottime(uvm_plain):
-    """
-    Check that boot timer device is not present by default.
-    """
-    vm = uvm_plain
-    _configure_and_run_vm(vm)
-    # microvm.start() ensures that the vm is in Running mode,
-    # so there is no need to sleep and wait for log message.
-    timestamps = re.findall(TIMESTAMP_LOG_REGEX, uvm_plain.log_data)
-    assert not timestamps
-
-
-def test_boottime_no_network(fast_microvm, record_property, metrics):
-    """
-    Check boot time of microVM without a network device.
-    """
-
-    vm = fast_microvm
-    vm.jailer.extra_args.update({"boot-timer": None})
-    _configure_and_run_vm(vm)
-    boottime_us = _get_microvm_boottime(vm)
-    print(f"Boot time with no network is: {boottime_us} us")
-    record_property("boottime_no_network", f"{boottime_us} us < {MAX_BOOT_TIME_US} us")
-    metrics.set_dimensions(DIMENSIONS)
-    metrics.put_metric("boot_time", boottime_us, unit="Microseconds")
-    assert (
-        boottime_us < MAX_BOOT_TIME_US
-    ), f"boot time {boottime_us} cannot be greater than: {MAX_BOOT_TIME_US} us"
-
-
-def test_boottime_with_network(fast_microvm, record_property, metrics):
-    """Check boot time of microVM with a network device."""
-    vm = fast_microvm
-    vm.jailer.extra_args.update({"boot-timer": None})
-    _configure_and_run_vm(vm, network=True)
-    boottime_us = _get_microvm_boottime(vm)
-    print(f"Boot time with network configured is: {boottime_us} us")
-    record_property(
-        "boottime_with_network", f"{boottime_us} us < {MAX_BOOT_TIME_US} us"
-    )
-    metrics.set_dimensions(DIMENSIONS)
-    metrics.put_metric("boot_time_with_net", boottime_us, unit="Microseconds")
-    assert (
-        boottime_us < MAX_BOOT_TIME_US
-    ), f"boot time {boottime_us} cannot be greater than: {MAX_BOOT_TIME_US} us"
-
-
-def test_initrd_boottime(uvm_with_initrd, record_property, metrics):
-    """
-    Check boot time of microVM when using an initrd.
-    """
-    vm = uvm_with_initrd
-    vm.jailer.extra_args.update({"boot-timer": None})
-    _configure_and_run_vm(vm, initrd=True)
-    boottime_us = _get_microvm_boottime(vm)
-    print(f"Boot time with initrd is: {boottime_us} us")
-    record_property("boottime_initrd", f"{boottime_us} us")
-    metrics.set_dimensions(DIMENSIONS)
-    metrics.put_metric("boot_time_with_initrd", boottime_us, unit="Microseconds")
 
 
 def _get_microvm_boottime(vm):
@@ -127,28 +48,6 @@ def _get_microvm_boottime(vm):
         f"Thread backtraces:\n{vm.thread_backtraces}"
     )
     return boot_time_us
-
-
-def _configure_and_run_vm(microvm, network=False, initrd=False):
-    """Auxiliary function for preparing microvm before measuring boottime."""
-    microvm.spawn()
-
-    # Machine configuration specified in the SLA.
-    config = {
-        "vcpu_count": 1,
-        "mem_size_mib": 128,
-        "boot_args": DEFAULT_BOOT_ARGS + " init=/usr/local/bin/init",
-        "enable_entropy_device": True,
-    }
-    if initrd:
-        config["add_root_device"] = False
-        config["use_initrd"] = True
-
-    microvm.basic_config(**config)
-    if network:
-        microvm.add_net_iface()
-    microvm.start()
-    microvm.pin_threads(0)
 
 
 def find_events(log_data):
