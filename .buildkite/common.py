3deed47d57df26360352485aaf9bc89cb2b22598
Babis Chalios <bchalios@amazon.es>
2024-09-10 12:41:59 +0200

    chore: pin all 6.1 kernels for all pipelines
    Latest 6.1 introduced a regression in the parsing of VirtIO headers for
    the network device. This causes failures in the TX path, where
    Firecracker fails to write network frames in the TAP device.
    
    Pin the buildkite agents with 6.1 kernel to the latest known good
    version.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- .buildkite/common.py
+++ .buildkite/common.py
@@ -24,7 +24,8 @@ DEFAULT_INSTANCES = [
 
 DEFAULT_PLATFORMS = [
     ("al2", "linux_5.10"),
-    ("al2023", "linux_6.1"),
+    # TODO: unpin 6.1 AMI once the bug is fixed
+    ("al2023", "linux_6.1-pinned"),
 ]
 
 
