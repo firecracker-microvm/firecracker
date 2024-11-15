46ba7487ec8808e771fd4f0947f398fe4fc2c79c
Patrick Roy <roypat@amazon.co.uk>
2024-11-15 10:33:18 +0000

    refactor: Remove some getters and setters for boot_source
    The initialization code for the `boot_source` field in
    `build_boot_source` had me do a double take when I first saw, with all
    the setters and getters obscuring what is really happening. Clean this
    up by simply using a struct initializer, and also remove the getters and
    setters altogether, since all the fields are pub anyway.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -64,7 +64,7 @@ impl From<&VmResources> for VmInfo {
             mem_size_mib: value.vm_config.mem_size_mib as u64,
             smt: value.vm_config.smt,
             cpu_template: StaticCpuTemplate::from(&value.vm_config.cpu_template),
-            boot_source: value.boot_source_config().clone(),
+            boot_source: value.boot_source.config.clone(),
             huge_pages: value.vm_config.huge_pages,
         }
     }
