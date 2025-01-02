firecracker-microvm/firecracker

commit c6add4fccf27cd0522a7e49a2c5dc67ab7067980

ci: allow passing a dict/list as a step parameter
In some cases we may want to pass a complex value to a step. For
example:

  .buildkite/pipeline_pr.py \
    --step-param 'retry/automatic=[{"exit_status": "*", "limit": 2}]'

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/common.py
@@ -6,6 +6,7 @@ Common helpers to create Buildkite pipelines
 """
 
 import argparse
+import ast
 import json
 import os
 import random
@@ -138,7 +139,7 @@ class DictAction(argparse.Action):
         res = getattr(namespace, self.dest, {})
         key_str, val = value.split("=", maxsplit=1)
         keys = key_str.split("/")
-        update = {keys[-1]: val}
+        update = {keys[-1]: ast.literal_eval(val)}
         for key in list(reversed(keys))[1:]:
             update = {key: update}
         res = overlay_dict(res, update)

commit df20a9453773f52af94de55db4b590c27d91a666

ci: fix passing simple values to step parameters
In c6add4fccf27cd0522a7e49a2c5dc67ab7067980 while adding support for
more complex parameters we broke passing simple strings.

While it can be worked around by quoting the string with '', that makes
the simple case more complex than it was before, and breaks previous
pipeline definitions.

Fix it so if the first character does not look like a python expression,
take the value verbatim.

Fixes: c6add4fccf27cd0522a7e49a2c5dc67ab7067980

Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

.buildkite/common.py
@@ -139,7 +139,8 @@ class DictAction(argparse.Action):
         res = getattr(namespace, self.dest, {})
         key_str, val = value.split("=", maxsplit=1)
         keys = key_str.split("/")
-        update = {keys[-1]: ast.literal_eval(val)}
+        # Interpret it as a literal iff it starts like one
+        update = {keys[-1]: ast.literal_eval(val) if val[0] in "[{'" else val}
         for key in list(reversed(keys))[1:]:
             update = {key: update}
         res = overlay_dict(res, update)