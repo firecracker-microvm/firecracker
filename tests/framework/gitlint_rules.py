8c02c93e4c230b64ac5fe89915706947ca675e27
Pablo Barbáchano <pablob@amazon.com>
2025-01-20 19:59:39 +0100

    tests: rework gitlint_rules.py
    Make doctests and code match. Now running `--doctest-modules` passes the
    doctests.
    
    Simplified the code a bit to make it easier to understand.
    
    Signed-off-by: Pablo Barbáchano <pablob@amazon.com>

--- tests/framework/gitlint_rules.py
+++ tests/framework/gitlint_rules.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 """The user defined rules for gitlint."""
 
+import re
+
 from gitlint.rules import CommitRule, RuleViolation
 
 
@@ -22,16 +24,20 @@ class EndsSigned(CommitRule):
     def validate(self, commit):
         r"""Validates Signed-off-by and Co-authored-by tags as Linux's scripts/checkpatch.pl
 
-        >>> from gitlint.tests.base import BaseTestCase
+        >>> from gitlint.git import GitContext
         >>> from gitlint.rules import RuleViolation
         ...
         >>> ends_signed = EndsSigned()
+        >>> miss_sob_follows_coab = "Missing 'Signed-off-by' following 'Co-authored-by'"
+        >>> miss_sob = "'Signed-off-by' not found in commit message body"
+        >>> non_sign = "Non 'Co-authored-by' or 'Signed-off-by' string found following 1st 'Signed-off-by'"
+        >>> email_no_match = "'Co-authored-by' and 'Signed-off-by' name/email do not match"
         ...
         >>> msg1 = (
         ... f"Title\n\nMessage.\n\n"
         ... f"Signed-off-by: name <email@domain>"
         ... )
-        >>> commit1 = BaseTestCase.gitcommit(msg1)
+        >>> commit1 = GitContext.from_commit_msg(msg1).commits[0]
         >>> ends_signed.validate(commit1)
         []
         >>> msg2 = (
@@ -39,52 +45,41 @@ class EndsSigned(CommitRule):
         ... f"Co-authored-by: name <email>\n\n"
         ... f"Signed-off-by: name <email>"
         ... )
-        >>> commit2 = BaseTestCase.gitcommit(msg2)
+        >>> commit2 = GitContext.from_commit_msg(msg2).commits[0]
         >>> ends_signed.validate(commit2)
         []
-        >>> msg3 = (
-        ... f"Title\n\nMessage.\n\n"
-        ... )
-        >>> commit3 = BaseTestCase.gitcommit(msg3)
+        >>> msg3 = f"Title\n\nMessage.\n\n"
+        >>> commit3 = GitContext.from_commit_msg(msg3).commits[0]
         >>> vio3 = ends_signed.validate(commit3)
-        >>> vio_msg3 = (
-        ... f"'Signed-off-by:' not found in commit message body"
-        ... )
-        >>> vio3 == [RuleViolation("UC2", vio_msg3)]
+        >>> vio3 == [RuleViolation("UC2", miss_sob)]
         True
         >>> msg4 = (
         ... f"Title\n\nMessage.\n\n"
         ... f"Signed-off-by: name <email@domain>\n\na sentence"
         ... )
-        >>> commit4 = BaseTestCase.gitcommit(msg4)
+        >>> commit4 = GitContext.from_commit_msg(msg4).commits[0]
         >>> vio4 = ends_signed.validate(commit4)
-        >>> vio_msg4 = (
-        ... f"Non 'Co-authored-by:' or 'Signed-off-by:' string found following 1st 'Signed-off-by:'"
-        ... )
-        >>> vio4 == [RuleViolation("UC2", vio_msg4, None, 5)]
+        >>> vio4 == [RuleViolation("UC2", non_sign, None, 6)]
         True
         >>> msg5 = (
         ... f"Title\n\nMessage.\n\n"
         ... f"Co-authored-by: name <email@domain>"
         ... )
-        >>> commit5 = BaseTestCase.gitcommit(msg5)
+        >>> commit5 = GitContext.from_commit_msg(msg5).commits[0]
         >>> vio5 = ends_signed.validate(commit5)
-        >>> vio_msg5 = (
-        ... f"Missing 'Signed-off-by:' following 'Co-authored-by:'"
-        ... )
-        >>> vio5 == [RuleViolation("UC2", vio_msg5, None, 2)]
+        >>> vio5 == [
+        ...   RuleViolation("UC2", miss_sob, None, None),
+        ...   RuleViolation("UC2", miss_sob_follows_coab, None, 5)
+        ... ]
         True
         >>> msg6 = (
         ... f"Title\n\nMessage.\n\n"
         ... f"Co-authored-by: name <email@domain>\n\n"
         ... f"Signed-off-by: different name <email@domain>"
         ... )
-        >>> commit6 = BaseTestCase.gitcommit(msg6)
+        >>> commit6 = GitContext.from_commit_msg(msg6).commits[0]
         >>> vio6 = ends_signed.validate(commit6)
-        >>> vio_msg6 = (
-        ... f"'Co-authored-by:' and 'Signed-off-by:' name/email do not match"
-        ... )
-        >>> vio6 == [RuleViolation("UC2", vio_msg6, None, 6)]
+        >>> vio6 == [RuleViolation("UC2", email_no_match, None, 6)]
         True
         """
 
@@ -92,59 +87,50 @@ class EndsSigned(CommitRule):
 
         # Utilities
         def vln(stmt, i):
-            return RuleViolation(self.id, stmt, None, i)
+            violations.append(RuleViolation(self.id, stmt, None, i))
 
-        co_auth = "Co-authored-by:"
-        sig = "Signed-off-by:"
+        coab = "Co-authored-by"
+        sob = "Signed-off-by"
 
-        message_iter = enumerate(commit.message.original.split("\n"))
-
-        # Skip ahead to the first signoff or co-author tag
+        # find trailers
+        trailers = []
+        for i, line in enumerate(commit.message.original.splitlines()):
+            # ignore empty lines
+            if not line:
+                continue
+            match = re.match(r"([\w-]+):\s+(.*)", line)
+            if match:
+                key, val = match.groups()
+                trailers.append((i, key, val))
+            else:
+                trailers.append((i, "line", line))
+        # artificial line so we can check any "previous line" rules
+        trailers.append((trailers[-1][0] + 1, None, None))
 
         # Checks commit message contains a `Signed-off-by` string
-        for i, line in message_iter:
-            if line.startswith(sig) or line.startswith(co_auth):
-                break
-        else:
-            # No signature was found in the message (before `message_iter` ended)
-            # This check here can have false-negatives (e.g. if the body ends with only
-            # a 'Co-authored-by' tag), but then below will realize that the co-authored-by
-            # tag isnt followed by a Signed-off-by tag and fail (and also the DCO check will
-            # complain).
-            violations.append(vln(f"'{sig}' not found in commit message body", None))
+        if not [x for x in trailers if x[1] == sob]:
+            vln(f"'{sob}' not found in commit message body", None)
 
-        # Check that from here on out we only have signatures and co-authors, and that
-        # every co-author is immediately followed by a signature with the same name/email.
-        for i, line in message_iter:
-            if line.startswith(co_auth):
-                try:
-                    _, next_line = next(message_iter)
-                except StopIteration:
-                    violations.append(
-                        vln(f"Missing '{sig}' tag following '{co_auth}'", i)
-                    )
-                else:
-                    if not next_line.startswith(sig):
-                        violations.append(
-                            vln(f"Missing '{sig}' tag following '{co_auth}'", i + 1)
-                        )
-                        continue
-
-                    if next_line.split(":")[1].strip() != line.split(":")[1].strip():
-                        violations.append(
-                            vln(f"{co_auth} and {sig} name/email do not match", i + 1)
-                        )
-                continue
-
-            if line.startswith(sig) or not line.strip():
-                continue
-
-            violations.append(
+        prev_trailer, prev_value = None, None
+        sig_trailers = False
+        for i, trailer, value in trailers:
+            if trailer in {sob, coab}:
+                sig_trailers = True
+            elif trailer not in {sob, coab, None} and sig_trailers:
                 vln(
-                    f"Non '{co_auth}' or '{sig}' string found following 1st '{sig}'",
+                    f"Non '{coab}' or '{sob}' string found following 1st '{sob}'",
                     i,
                 )
-            )
+            # Every co-author is immediately followed by a signature
+            if prev_trailer == coab:
+                if trailer != sob:
+                    vln(f"Missing '{sob}' following '{coab}'", i)
+                else:
+                    # with the same name/email.
+                    if value != prev_value:
+                        vln(f"'{coab}' and '{sob}' name/email do not match", i)
+
+            prev_trailer, prev_value = trailer, value
 
         # Return errors
         return violations
