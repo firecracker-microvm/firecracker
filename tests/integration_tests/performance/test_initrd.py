cf773a3bd625494cfbf7e76e445ebf34fc86b0e0
Patrick Roy <roypat@amazon.co.uk>
2025-01-20 12:55:44 +0000

    test: generalize initrd test
    Run it with all supported guest kernels, and handle the huge page
    configuration via pytest parametrization, so that we do not have the
    same test twice.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/integration_tests/performance/test_initrd.py
+++ tests/integration_tests/performance/test_initrd.py
@@ -1,13 +1,27 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Tests for initrd."""
+import pytest
 
-from framework.microvm import Serial
+from framework.microvm import HugePagesConfig, Serial
 
 INITRD_FILESYSTEM = "rootfs"
 
 
-def test_microvm_initrd_with_serial(uvm_with_initrd):
+@pytest.fixture
+def uvm_with_initrd(microvm_factory, guest_kernel, record_property, artifact_dir):
+    """
+    See file:../docs/initrd.md
+    """
+    fs = artifact_dir / "initramfs.cpio"
+    record_property("rootfs", fs.name)
+    uvm = microvm_factory.build(guest_kernel)
+    uvm.initrd_file = fs
+    yield uvm
+
+
+@pytest.mark.parametrize("huge_pages", HugePagesConfig)
+def test_microvm_initrd_with_serial(uvm_with_initrd, huge_pages):
     """
     Test that a boot using initrd successfully loads the root filesystem.
     """
@@ -21,6 +35,7 @@ def test_microvm_initrd_with_serial(uvm_with_initrd):
         vcpu_count=1,
         boot_args="console=ttyS0 reboot=k panic=1 pci=off",
         use_initrd=True,
+        huge_pages=huge_pages,
     )
 
     vm.start()
