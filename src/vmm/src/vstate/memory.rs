firecracker-microvm/firecracker

commit d8358055d4b2046fe86095cc11fb840f147417cf

refactor: drop `offset` field from `GuestMemoryRegionState`
In praxis, the way we wrote our snapshot files has always been just
writing all regions in-order. This mean that the offset of a region is
simply the sum of the sizes of the preceding regions. The new
`GuestMemoryMmap::create` code already computes the offsets for mapping
the memory file this way, so drop the explicit calculation at snapshot
creation time (as the calculated value isnt used by the restoration
anymore).

Do not bump the snapshot version number, because we already did so since
the last release.

Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

src/vmm/src/vstate/memory.rs
@@ -122,8 +122,6 @@ pub struct GuestMemoryRegionState {
     pub base_address: u64,
     /// Region size.
     pub size: usize,
-    /// Offset in file/buffer where the region is saved.
-    pub offset: u64,
 }
 
 /// Describes guest memory regions and their snapshot file mappings.
@@ -232,6 +230,7 @@ impl GuestMemoryExtension for GuestMemoryMmap {
         track_dirty_pages: bool,
         huge_pages: HugePageConfig,
     ) -> Result<Self, MemoryError> {
+        let mut offset = 0;
         match file {
             Some(f) => {
                 if huge_pages.is_hugetlbfs() {
@@ -242,10 +241,12 @@ impl GuestMemoryExtension for GuestMemoryMmap {
                     .regions
                     .iter()
                     .map(|r| {
-                        f.try_clone().map(|file_clone| {
-                            let offset = FileOffset::new(file_clone, r.offset);
+                        let fo = f.try_clone().map(|file_clone| {
+                            let offset = FileOffset::new(file_clone, offset);
                             (offset, GuestAddress(r.base_address), r.size)
-                        })
+                        });
+                        offset += r.size as u64;
+                        fo
                     })
                     .collect::<Result<Vec<_>, std::io::Error>>()
                     .map_err(MemoryError::FileError)?;
@@ -266,15 +267,11 @@ impl GuestMemoryExtension for GuestMemoryMmap {
     /// Describes GuestMemoryMmap through a GuestMemoryState struct.
     fn describe(&self) -> GuestMemoryState {
         let mut guest_memory_state = GuestMemoryState::default();
-        let mut offset = 0;
         self.iter().for_each(|region| {
             guest_memory_state.regions.push(GuestMemoryRegionState {
                 base_address: region.start_addr().0,
                 size: u64_to_usize(region.len()),
-                offset,
             });
-
-            offset += region.len();
         });
         guest_memory_state
     }
@@ -536,7 +533,6 @@ mod tests {
             regions: vec![GuestMemoryRegionState {
                 base_address: 0,
                 size: 4096,
-                offset: 0,
             }],
         };
         let file = TempFile::new().unwrap().into_file();
@@ -652,12 +648,10 @@ mod tests {
                 GuestMemoryRegionState {
                     base_address: 0,
                     size: page_size,
-                    offset: 0,
                 },
                 GuestMemoryRegionState {
                     base_address: page_size as u64 * 2,
                     size: page_size,
-                    offset: page_size as u64,
                 },
             ],
         };
@@ -679,12 +673,10 @@ mod tests {
                 GuestMemoryRegionState {
                     base_address: 0,
                     size: page_size * 3,
-                    offset: 0,
                 },
                 GuestMemoryRegionState {
                     base_address: page_size as u64 * 4,
                     size: page_size * 3,
-                    offset: page_size as u64 * 3,
                 },
             ],
         };

src/vmm/src/persist.rs
@@ -584,13 +584,15 @@ fn create_guest_memory(
 ) -> Result<(GuestMemoryMmap, Vec<GuestRegionUffdMapping>), GuestMemoryFromUffdError> {
     let guest_memory = GuestMemoryMmap::from_state(None, mem_state, track_dirty_pages, huge_pages)?;
     let mut backend_mappings = Vec::with_capacity(guest_memory.num_regions());
-    for (mem_region, state_region) in guest_memory.iter().zip(mem_state.regions.iter()) {
+    let mut offset = 0;
+    for mem_region in guest_memory.iter() {
         backend_mappings.push(GuestRegionUffdMapping {
             base_host_virt_addr: mem_region.as_ptr() as u64,
             size: mem_region.size(),
-            offset: state_region.offset,
+            offset,
             page_size_kib: huge_pages.page_size_kib(),
         });
+        offset += mem_region.size() as u64;
     }
 
     Ok((guest_memory, backend_mappings))
@@ -770,7 +772,6 @@ mod tests {
             regions: vec![GuestMemoryRegionState {
                 base_address: 0,
                 size: 0x20000,
-                offset: 0x10000,
             }],
         };
 
@@ -779,7 +780,7 @@ mod tests {
 
         assert_eq!(uffd_regions.len(), 1);
         assert_eq!(uffd_regions[0].size, 0x20000);
-        assert_eq!(uffd_regions[0].offset, 0x10000);
+        assert_eq!(uffd_regions[0].offset, 0);
         assert_eq!(
             uffd_regions[0].page_size_kib,
             HugePageConfig::None.page_size_kib()

commit 1daed3a37bdc61dcbc12f77229dad4e502687f5f

fix: handle overflow when computing mmap offset during restore
Since we dropped the explicit offset field from the snapshot file, we
are implicit computing it as "sum of sizes of all preceding regions". If
the snapshot file is corrupted, it can describe regions whose sum
exceeds u64::MAX. Fix this by adding overflow checks and returning an
error in case of overflows

We also error out if it exceeds i64::MAX as the offset argument to
mmap(2) is a signed 64 bit integer value.

Fixes: d8358055d4b2
Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

src/vmm/src/vstate/memory.rs
@@ -52,6 +52,8 @@ pub enum MemoryError {
     Memfd(memfd::Error),
     /// Cannot resize memfd file: {0}
     MemfdSetLen(std::io::Error),
+    /// Total sum of memory regions exceeds largest possible file offset
+    OffsetTooLarge,
 }
 
 /// Defines the interface for snapshotting memory.
@@ -188,7 +190,13 @@ impl GuestMemoryExtension for GuestMemoryMmap {
                     builder = builder.with_file_offset(file_offset);
                 }
 
-                offset += size as u64;
+                offset = match offset.checked_add(size as u64) {
+                    None => return Err(MemoryError::OffsetTooLarge),
+                    Some(new_off) if new_off >= i64::MAX as u64 => {
+                        return Err(MemoryError::OffsetTooLarge)
+                    }
+                    Some(new_off) => new_off,
+                };
 
                 GuestRegionMmap::new(
                     builder.build().map_err(MemoryError::MmapRegionError)?,