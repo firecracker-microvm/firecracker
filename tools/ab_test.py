d48693e40a772a26bf5f78997801e47f0e965c0a
Patrick Roy <roypat@amazon.co.uk>
2025-03-04 13:42:55 +0000

    fix(ab): its dict.keys(), not dict.key()
    We were calling a non-existing function on an error path. This is why I
    prefer compiled languages smh.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tools/ab_test.py
+++ tools/ab_test.py
@@ -136,7 +136,7 @@ def load_data_series(report_path: Path, tag=None, *, reemit: bool = False):
                     # multiple EMF log messages. We need to reassemble :(
                     assert (
                         processed_emf[dimension_set].keys() == result.keys()
-                    ), f"Found incompatible metrics associated with dimension set {dimension_set}: {processed_emf[dimension_set].key()} in one EMF message, but {result.keys()} in another."
+                    ), f"Found incompatible metrics associated with dimension set {dimension_set}: {processed_emf[dimension_set].keys()} in one EMF message, but {result.keys()} in another."
 
                     for metric, (values, unit) in processed_emf[dimension_set].items():
                         assert result[metric][1] == unit
