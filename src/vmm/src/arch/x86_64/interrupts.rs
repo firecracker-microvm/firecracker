36e33e37d5cf8c4a0637f70b8ffab10a5cdb6b5f
Patrick Roy <roypat@amazon.co.uk>
2025-03-13 14:22:01 +0000

    refactor: stop using loops in byte_order.rs
    Instead of copying the buffers byte-by-byte in a loop, just use
    copy_from_slice, which compiles to a memcpy.
    
    While we're at it, drop some unused function definitions.
    
    Also remove the special snowflake functions for dealing with i8 slices,
    and instead just use zerocopy to safely transmute these into u8 slices,
    on which the normal functions work.
    
    (cherry picked from commit 35d6afd2f6ac11c932e5081b810db82576abe664)
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/arch/x86_64/interrupts.rs
+++ src/vmm/src/arch/x86_64/interrupts.rs
@@ -7,6 +7,7 @@
 
 use kvm_bindings::kvm_lapic_state;
 use kvm_ioctls::VcpuFd;
+use zerocopy::IntoBytes;
 
 use crate::utils::byte_order;
 
@@ -28,13 +29,13 @@ const APIC_MODE_EXTINT: u32 = 0x7;
 fn get_klapic_reg(klapic: &kvm_lapic_state, reg_offset: usize) -> u32 {
     let range = reg_offset..reg_offset + 4;
     let reg = klapic.regs.get(range).expect("get_klapic_reg range");
-    byte_order::read_le_u32_from_i8(reg)
+    byte_order::read_le_u32(reg.as_bytes())
 }
 
 fn set_klapic_reg(klapic: &mut kvm_lapic_state, reg_offset: usize, value: u32) {
     let range = reg_offset..reg_offset + 4;
     let reg = klapic.regs.get_mut(range).expect("set_klapic_reg range");
-    byte_order::write_le_u32_to_i8(reg, value)
+    byte_order::write_le_u32(reg.as_mut_bytes(), value);
 }
 
 fn set_apic_delivery_mode(reg: u32, mode: u32) -> u32 {
