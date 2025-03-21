2136a123e9df8ca374e09d9ca1b9fe781dcfaaff
Patrick Roy <roypat@amazon.co.uk>
2025-03-21 17:14:24 +0000

    add function to VmResources for allocating I/O memory
    If swiotlb is requested through the API, this function will allocate a
    single contiguous region intended for swiotlb use. It needs to be
    continguous because we must be able to describe it using a single memory
    range in FDT, as devices cannot be assigned to reserved memory
    consisting of multiple regions (e.g. FDT assignment is to memory
    regions, not to #reserved-memory nodes).
    
    While we're at it, always use anon memory for the "normal" part of guest
    memory if swiotlb is enabled, as if swiotlb is enabled we know that
    vhost devices will never need to access that part of memory.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/arch/mod.rs
+++ src/vmm/src/arch/mod.rs
@@ -23,7 +23,7 @@ pub use aarch64::{
     ConfigurationError, MMIO_MEM_SIZE, MMIO_MEM_START, arch_memory_regions,
     configure_system_for_boot, get_kernel_start, initrd_load_addr, layout::CMDLINE_MAX_SIZE,
     layout::IRQ_BASE, layout::IRQ_MAX, layout::SYSTEM_MEM_SIZE, layout::SYSTEM_MEM_START,
-    load_kernel,
+    load_kernel, offset_after_last_gap,
 };
 
 /// Module for x86_64 related functionality.
@@ -42,7 +42,7 @@ pub use crate::arch::x86_64::{
     ConfigurationError, MMIO_MEM_SIZE, MMIO_MEM_START, arch_memory_regions,
     configure_system_for_boot, get_kernel_start, initrd_load_addr, layout::APIC_ADDR,
     layout::CMDLINE_MAX_SIZE, layout::IOAPIC_ADDR, layout::IRQ_BASE, layout::IRQ_MAX,
-    layout::SYSTEM_MEM_SIZE, layout::SYSTEM_MEM_START, load_kernel,
+    layout::SYSTEM_MEM_SIZE, layout::SYSTEM_MEM_START, load_kernel, offset_after_last_gap,
 };
 
 /// Types of devices that can get attached to this platform.
