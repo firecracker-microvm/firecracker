26d76f53a991055fdd9d768b5ec9fbe76139bba1
Jack Thomson <jackabt@amazon.com>
2025-03-24 15:56:05 +0000

    test: Add test for kernel build
    Adding a new integration test to assert that the kernel build script
    will succeed.
    
    Signed-off-by: Jack Thomson <jackabt@amazon.com>

---
+++ tests/integration_tests/build/test_hiding_kernel.py
@@ -0,0 +1,29 @@
+# Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""A test which checks that the secret hiding enable kernel builds successfully."""
+
+import pytest
+
+from framework import utils
+
+
+@pytest.mark.timeout(600)
+@pytest.mark.secret_hiding
+def test_build_hiding_kernel():
+    """
+    In the test we will run our kernel build script to check it succeeds and builds the hidden kernel
+    """
+
+    # We have some extra deps for building the kernel that are not in the dev contaner
+    utils.check_output(
+        "apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc dwarves libncurses5-dev kmod fakeroot"
+    )
+
+    # We have to configure git otherwise patch application fails
+    # the git log still credits the original author
+    utils.check_output('git config --global user.name "Firecracker CI"')
+    utils.check_output('git config --global user.email "ci@email.com"')
+
+    utils.check_output(
+        "cd ../resources/hiding_ci; ./build_and_install_kernel.sh --no-install --tidy"
+    )
