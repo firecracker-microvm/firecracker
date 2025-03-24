2cee40b9af9f91b28c4c45b0287e7aad2a80af62
Patrick Roy <roypat@amazon.co.uk>
2025-03-24 11:52:51 +0000

    arm: describe swiotlb region in FDT
    describe the swiotlb region as a restricted-mem node in the flattened
    device tree, and force all virtio devices to use it for their vrings and
    buffers by setting their `memory-region` property.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/arch/aarch64/mod.rs
+++ src/vmm/src/arch/aarch64/mod.rs
@@ -30,7 +30,9 @@ use crate::cpu_config::templates::CustomCpuTemplate;
 use crate::initrd::InitrdConfig;
 use crate::utils::{align_up, usize_to_u64};
 use crate::vmm_config::machine_config::MachineConfig;
-use crate::vstate::memory::{Address, Bytes, GuestAddress, GuestMemory, GuestMemoryMmap};
+use crate::vstate::memory::{
+    Address, Bytes, GuestAddress, GuestMemory, GuestMemoryMmap,
+};
 use crate::vstate::vcpu::KvmVcpuError;
 use crate::{Vcpu, VcpuConfig, Vmm, logger};
 
@@ -138,8 +140,14 @@ pub fn configure_system_for_boot(
         .as_cstring()
         .expect("Cannot create cstring from cmdline string");
 
+    let swiotlb_region = match vmm.vm.swiotlb_regions().num_regions() {
+        0 | 1 => vmm.vm.swiotlb_regions().iter().next(),
+        _ => panic!("Firecracker tried to configure more than one swiotlb region. This is a logic bug.")
+    };
+
     let fdt = fdt::create_fdt(
         vmm.vm.guest_memory(),
+        swiotlb_region,
         vcpu_mpidr,
         cmdline,
         vmm.mmio_device_manager.get_device_info(),
