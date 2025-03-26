5cda4fa4c744872175201fa5e36e1c9300ceaf15
Jack Thomson <jackabt@amazon.com>
2025-03-26 14:43:55 +0000

    tests: Mark kernels newer than 6.12 as next
    To make it easier to track the upstream kernels which may change as we
    rebase, let's mark kernels newer than 6.12 as next for now to make
    dashboarding easier.
    
    Signed-off-by: Jack Thomson <jackabt@amazon.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -139,7 +139,7 @@ def pytest_runtest_logreport(report):
             "test": report.nodeid,
             "instance": global_props.instance,
             "cpu_model": global_props.cpu_model,
-            "host_kernel": "linux-" + global_props.host_linux_version,
+            "host_kernel": "linux-" + global_props.host_linux_version_metrics,
             "phase": report.when,
         },
         # per test
@@ -147,7 +147,7 @@ def pytest_runtest_logreport(report):
             "test": report.nodeid,
             "instance": global_props.instance,
             "cpu_model": global_props.cpu_model,
-            "host_kernel": "linux-" + global_props.host_linux_version,
+            "host_kernel": "linux-" + global_props.host_linux_version_metrics,
         },
         # per coarse-grained test name, dropping parameters and other dimensions to reduce metric count for dashboard
         # Note: noideid is formatted as below
@@ -159,7 +159,7 @@ def pytest_runtest_logreport(report):
         # per phase
         {"phase": report.when},
         # per host kernel
-        {"host_kernel": "linux-" + global_props.host_linux_version},
+        {"host_kernel": "linux-" + global_props.host_linux_version_metrics},
         # per CPU
         {"cpu_model": global_props.cpu_model},
         # and global
