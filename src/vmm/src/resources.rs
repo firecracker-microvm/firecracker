4b9602299a013471d00e1a2ea1b86bb65236fee2
Patrick Roy <roypat@amazon.co.uk>
2025-03-31 14:05:59 +0100

    refactor: generify "these features are incompatible" error variants
    There's be a lot more things that are incompatible going forward (mostly
    related to secret freedom), so instead of adding a ton of error variants
    for each pair of incompatible features, let's just have a single one
    where we can insert arbitrary features via a string argument.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/resources.rs
+++ src/vmm/src/resources.rs
@@ -227,7 +227,9 @@ impl VmResources {
                 self.balloon.set_device(balloon);
 
                 if self.machine_config.huge_pages != HugePageConfig::None {
-                    return Err(ResourcesError::BalloonDevice(BalloonConfigError::HugePages));
+                    return Err(ResourcesError::BalloonDevice(
+                        BalloonConfigError::IncompatibleWith("huge pages"),
+                    ));
                 }
             }
 
@@ -269,7 +271,10 @@ impl VmResources {
         }
 
         if self.balloon.get().is_some() && updated.huge_pages != HugePageConfig::None {
-            return Err(MachineConfigError::BalloonAndHugePages);
+            return Err(MachineConfigError::Incompatible(
+                "balloon device",
+                "huge pages",
+            ));
         }
         self.machine_config = updated;
 
@@ -326,7 +331,7 @@ impl VmResources {
         }
 
         if self.machine_config.huge_pages != HugePageConfig::None {
-            return Err(BalloonConfigError::HugePages);
+            return Err(BalloonConfigError::IncompatibleWith("huge pages"));
         }
 
         self.balloon.set(config)
@@ -1446,7 +1451,7 @@ mod tests {
         assert!(
             matches!(
                 err,
-                ResourcesError::BalloonDevice(BalloonConfigError::HugePages)
+                ResourcesError::BalloonDevice(BalloonConfigError::IncompatibleWith("huge pages"))
             ),
             "{:?}",
             err
--- src/vmm/src/vmm_config/balloon.rs
+++ src/vmm/src/vmm_config/balloon.rs
@@ -28,8 +28,8 @@ pub enum BalloonConfigError {
     CreateFailure(crate::devices::virtio::balloon::BalloonError),
     /// Error updating the balloon device configuration: {0}
     UpdateFailure(std::io::Error),
-    /// Firecracker's huge pages support is incompatible with memory ballooning.
-    HugePages,
+    /// Memory ballooning is incompatible with {0}.
+    IncompatibleWith(&'static str),
 }
 
 /// This struct represents the strongly typed equivalent of the json body
