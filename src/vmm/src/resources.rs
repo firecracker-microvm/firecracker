2753c77d39345dcef5e05fcfa090827c794b75ae
Patrick Roy <roypat@amazon.co.uk>
2025-03-31 14:05:59 +0100

    refactor: generify "these features are incompatible" error variants
    There's be a lot more things that are incompatible going forward (mostly
    related to secret freedom), so instead of adding a ton of error variants
    for each pair of incompatible features, let's just have a single one
    where we can insert arbitrary features via a string argument.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/resources.rs
+++ src/vmm/src/resources.rs
@@ -237,7 +237,9 @@ impl VmResources {
                 self.balloon.set_device(balloon);
 
                 if self.machine_config.huge_pages != HugePageConfig::None {
-                    return Err(ResourcesError::BalloonDevice(BalloonConfigError::HugePages));
+                    return Err(ResourcesError::BalloonDevice(
+                        BalloonConfigError::IncompatibleWith("huge pages"),
+                    ));
                 }
             }
 
@@ -279,7 +281,10 @@ impl VmResources {
         }
 
         if self.balloon.get().is_some() && updated.huge_pages != HugePageConfig::None {
-            return Err(MachineConfigError::BalloonAndHugePages);
+            return Err(MachineConfigError::Incompatible(
+                "balloon device",
+                "huge pages",
+            ));
         }
         self.machine_config = updated;
 
@@ -338,7 +343,7 @@ impl VmResources {
         }
 
         if self.machine_config.huge_pages != HugePageConfig::None {
-            return Err(BalloonConfigError::HugePages);
+            return Err(BalloonConfigError::IncompatibleWith("huge pages"));
         }
 
         self.balloon.set(config)
@@ -1462,7 +1467,7 @@ mod tests {
         assert!(
             matches!(
                 err,
-                ResourcesError::BalloonDevice(BalloonConfigError::HugePages)
+                ResourcesError::BalloonDevice(BalloonConfigError::IncompatibleWith("huge pages"))
             ),
             "{:?}",
             err
