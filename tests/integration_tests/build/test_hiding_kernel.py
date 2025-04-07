2ad777ce61bdbc0cafca171a39c26fa0fce5568d
Babis Chalios <bchalios@amazon.es>
2025-04-07 09:32:59 +0200

    fix: test_hiding_kernel.py
    Add an 'apt update' before `apt install`. Otherwise, we might hold an
    old view of the package versions and installation might fail.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/integration_tests/build/test_hiding_kernel.py
+++ tests/integration_tests/build/test_hiding_kernel.py
@@ -14,7 +14,8 @@ def test_build_hiding_kernel():
     In the test we will run our kernel build script to check it succeeds and builds the hidden kernel
     """
 
-    # We have some extra deps for building the kernel that are not in the dev contaner
+    # We have some extra deps for building the kernel that are not in the dev container
+    utils.check_output("apt update")
     utils.check_output(
         "apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc dwarves libncurses5-dev kmod fakeroot"
     )
