f2360ca05182855111f7f9089b79653568ac933a
Patrick Roy <roypat@amazon.co.uk>
2025-04-11 13:11:14 +0100

    implement userspace bounce buffering support
    Add support to our virtio devices to allow userspace bounce buffering of
    virtio buffers. This is an alternative to swiotlb.
    
    Don't implement it for vhost-user-blk and for virtio-block with async
    engine, because I have no idea how that would even work.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/vmm/src/device_manager/mmio.rs
+++ src/vmm/src/device_manager/mmio.rs
@@ -600,6 +600,14 @@ mod tests {
 
         fn set_acked_features(&mut self, _: u64) {}
 
+        fn force_userspace_bounce_buffers(&mut self) {
+            todo!()
+        }
+
+        fn userspace_bounce_buffers(&self) -> bool {
+            todo!()
+        }
+
         fn device_type(&self) -> u32 {
             0
         }
