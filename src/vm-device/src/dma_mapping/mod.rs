2b43d8fa163148dfbe865a68be0a888653dd191f
Babis Chalios <bchalios@amazon.es>
2025-04-29 14:58:54 +0200

    vm-device: add vm-device crate to repo
    Bring in the vm-device crate from CloudHypervisor. We will be using it
    for adding PCIe support.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

---
+++ src/vm-device/src/dma_mapping/mod.rs
@@ -0,0 +1,18 @@
+// Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright © 2021 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+/// Trait to trigger DMA mapping updates for devices managed by virtio-iommu
+///
+/// Trait meant for triggering the DMA mapping update related to an external
+/// device not managed fully through virtio. It is dedicated to virtio-iommu
+/// in order to trigger the map update anytime the mapping is updated from the
+/// guest.
+pub trait ExternalDmaMapping: Send + Sync {
+    /// Map a memory range
+    fn map(&self, iova: u64, gpa: u64, size: u64) -> std::result::Result<(), std::io::Error>;
+
+    /// Unmap a memory range
+    fn unmap(&self, iova: u64, size: u64) -> std::result::Result<(), std::io::Error>;
+}
