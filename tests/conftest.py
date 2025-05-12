e82f75a397c7468171b384d0dee8e0e13b7c2057
Babis Chalios <bchalios@amazon.es>
2025-05-12 10:46:16 +0200

    pci: add tests for PCIe root bus
    Add an integration test that checks that `lspci` correctly locates the
    PCIe root complex if PCI is enabled for the microVM. Also, add a
    negative test that checks that PCIe root complex doesn't exist when PCI
    is not enabled.
    
    Also, extend coverage of, at least some of, the tests to ensure that
    they run with and without PCI configuration enabled. Do that by
    extending the `uvm_any*` fixtures to yield both variants.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/conftest.py
+++ tests/conftest.py
@@ -569,12 +569,24 @@ def mem_size_mib():
     return 256
 
 
+@pytest.fixture(params=[True, False])
+def pci_enabled(request):
+    """Fixture that allows configuring whether a microVM will have PCI enabled or not"""
+    yield request.param
+
+
 def uvm_booted(
-    microvm_factory, guest_kernel, rootfs, cpu_template, vcpu_count=2, mem_size_mib=256
+    microvm_factory,
+    guest_kernel,
+    rootfs,
+    cpu_template,
+    pci_enabled,
+    vcpu_count=2,
+    mem_size_mib=256,
 ):
     """Return a booted uvm"""
     uvm = microvm_factory.build(guest_kernel, rootfs)
-    uvm.spawn()
+    uvm.spawn(pci=pci_enabled)
     uvm.basic_config(vcpu_count=vcpu_count, mem_size_mib=mem_size_mib)
     uvm.set_cpu_template(cpu_template)
     uvm.add_net_iface()
@@ -582,9 +594,13 @@ def uvm_booted(
     return uvm
 
 
-def uvm_restored(microvm_factory, guest_kernel, rootfs, cpu_template, **kwargs):
+def uvm_restored(
+    microvm_factory, guest_kernel, rootfs, cpu_template, pci_enabled, **kwargs
+):
     """Return a restored uvm"""
-    uvm = uvm_booted(microvm_factory, guest_kernel, rootfs, cpu_template, **kwargs)
+    uvm = uvm_booted(
+        microvm_factory, guest_kernel, rootfs, cpu_template, pci_enabled, **kwargs
+    )
     snapshot = uvm.snapshot_full()
     uvm.kill()
     uvm2 = microvm_factory.build_from_snapshot(snapshot)
@@ -605,6 +621,7 @@ def uvm_any(
     guest_kernel,
     rootfs,
     cpu_template_any,
+    pci_enabled,
     vcpu_count,
     mem_size_mib,
 ):
@@ -614,6 +631,7 @@ def uvm_any(
         guest_kernel,
         rootfs,
         cpu_template_any,
+        pci_enabled,
         vcpu_count=vcpu_count,
         mem_size_mib=mem_size_mib,
     )
@@ -621,7 +639,13 @@ def uvm_any(
 
 @pytest.fixture
 def uvm_any_booted(
-    microvm_factory, guest_kernel, rootfs, cpu_template_any, vcpu_count, mem_size_mib
+    microvm_factory,
+    guest_kernel,
+    rootfs,
+    cpu_template_any,
+    pci_enabled,
+    vcpu_count,
+    mem_size_mib,
 ):
     """Return booted uvms"""
     return uvm_booted(
@@ -629,6 +653,51 @@ def uvm_any_booted(
         guest_kernel,
         rootfs,
         cpu_template_any,
+        pci_enabled,
+        vcpu_count=vcpu_count,
+        mem_size_mib=mem_size_mib,
+    )
+
+
+@pytest.fixture
+def uvm_any_with_pci(
+    uvm_ctor,
+    microvm_factory,
+    guest_kernel_acpi,
+    rootfs,
+    cpu_template_any,
+    vcpu_count,
+    mem_size_mib,
+):
+    """Return booted uvms with PCI enabled"""
+    return uvm_ctor(
+        microvm_factory,
+        guest_kernel_acpi,
+        rootfs,
+        cpu_template_any,
+        True,
+        vcpu_count=vcpu_count,
+        mem_size_mib=mem_size_mib,
+    )
+
+
+@pytest.fixture
+def uvm_any_without_pci(
+    uvm_ctor,
+    microvm_factory,
+    guest_kernel,
+    rootfs,
+    cpu_template_any,
+    vcpu_count,
+    mem_size_mib,
+):
+    """Return booted uvms with PCI disabled"""
+    return uvm_ctor(
+        microvm_factory,
+        guest_kernel,
+        rootfs,
+        cpu_template_any,
+        False,
         vcpu_count=vcpu_count,
         mem_size_mib=mem_size_mib,
     )
