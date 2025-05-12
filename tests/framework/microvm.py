d3572314a0885745bbfeafa345dfa166b4a5cf43
Babis Chalios <bchalios@amazon.es>
2025-05-12 10:46:16 +0200

    pci: add tests for PCIe root bus
    Add an integration test that checks that `lspci` correctly locates the
    PCIe root complex if PCI is enabled for the microVM. Also, add a
    negative test that checks that PCIe root complex doesn't exist when PCI
    is not enabled.
    
    Also, extend coverage of, at least some of, the tests to ensure that
    they run with and without PCI configuration enabled. Do that by
    extending the `uvm_any*` fixtures to yield both variants.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/framework/microvm.py
+++ tests/framework/microvm.py
@@ -633,6 +633,7 @@ class Microvm:
         log_show_origin=False,
         metrics_path="fc.ndjson",
         emit_metrics: bool = False,
+        pci: bool = False,
     ):
         """Start a microVM as a daemon or in a screen session."""
         # pylint: disable=subprocess-run-check
@@ -677,6 +678,9 @@ class Microvm:
         if log_level != "Debug":
             # Checking the timings requires DEBUG level log messages
             self.time_api_requests = False
+
+        if pci:
+            self.jailer.extra_args["enable-pci"] = None
 
         cmd = [
             *self._pre_cmd,
