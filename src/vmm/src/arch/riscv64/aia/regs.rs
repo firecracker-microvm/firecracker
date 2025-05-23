b998eb2a8093d8629341886bb1ebdbbe0b1d1df0
Dimitris Charisis <dchar@cslab.ece.ntua.gr>
2025-05-23 13:28:44 +0300

    vmm(riscv64): introduce `vm`, `fdt`, and `aia` modules
    Add `vm` architecture-specific state module, following the aarch64
    paradigm.
    
    Add support for the in-kernel KVM Advanced Interrupt Architecture (AIA)
    irqchip, which emulates the AIA APLIC and per-hart IMSIC.
    
    Introduce a new `fdt` module to describe the provided hardware platform.
    
    Update `Cargo.toml` to include `vm-fdt` rust-vmm crate for riscv64
    target.
    
    Signed-off-by: Dimitris Charisis <dchar@cslab.ece.ntua.gr>

---
+++ src/vmm/src/arch/riscv64/aia/regs.rs
@@ -0,0 +1,11 @@
+// Copyright © 2025 Computing Systems Laboratory (CSLab), ECE, NTUA. All rights reserved.
+//
+// Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+
+/// Structure used for serializing the state of the AIA registers.
+/// For now, it is just a placeholder.
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct AiaState;
