94660fef7256cac664931243f658a12f55df1ed3
Patrick Roy <roypat@amazon.co.uk>
2025-05-28 14:17:07 +0100

    fix(test): update get_os_version doctest
    With the docker update, we picked up a new ubuntu patch version.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- tests/framework/properties.py
+++ tests/framework/properties.py
@@ -26,7 +26,7 @@ def get_os_version():
     """Get the OS version
 
     >>> get_os_version()
-    'Ubuntu 24.04.1 LTS'
+    'Ubuntu 24.04.2 LTS'
     """
 
     os_release = Path("/etc/os-release").read_text(encoding="ascii")
