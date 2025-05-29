455cf9851cb3c6d86cbad18602ec1a4b155e0787
Takahiro Itazuri <itazur@amazon.com>
2025-05-29 08:00:41 +0000

    chore(vmm): Warn on unexpected subleaf for CPUID leaf 0xB
    KVM no longer returns any subleaves greater than 0. The patch was merged
    in v6.2 and backported to v5.10. So for all our supported kernels,
    subleaves >= 2 should not be included [1].
    
    However, we intentionally leave Firecracker not fail for unsupported
    kernels to keep working. Note that we can detect KVM regression thanks
    to the test that compares a fingerprint with its baseline.
    
    So prints a warning message without bailing out.
    
    [1]: https://github.com/torvalds/linux/commit/45e966fcca03ecdcccac7cb236e16eea38cc18af
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- src/vmm/src/cpu_config/x86_64/cpuid/normalize.rs
+++ src/vmm/src/cpu_config/x86_64/cpuid/normalize.rs
@@ -4,6 +4,7 @@
 use crate::cpu_config::x86_64::cpuid::{
     CpuidEntry, CpuidKey, CpuidRegisters, CpuidTrait, KvmCpuidFlags, cpuid,
 };
+use crate::logger::warn;
 use crate::vmm_config::machine_config::MAX_SUPPORTED_VCPUS;
 
 /// Error type for [`super::Cpuid::normalize`].
@@ -370,8 +371,15 @@ impl super::Cpuid {
                             .map_err(|err| ExtendedTopologyError::DomainType(index, err))?;
                     }
                     _ => {
-                        // We expect here as this is an extremely rare case that is unlikely to ever
-                        // occur.
+                        // KVM no longer returns any subleaves greater than 0. The patch was merged
+                        // in v6.2 and backported to v5.10. So for all our supported kernels,
+                        // subleaves >= 2 should not be included.
+                        // https://github.com/torvalds/linux/commit/45e966fcca03ecdcccac7cb236e16eea38cc18af
+                        //
+                        // However, we intentionally leave Firecracker not fail for unsupported
+                        // kernels to keep working. Note that we can detect KVM regression thanks
+                        // to the test that compares a fingerprint with its baseline.
+                        warn!("Subleaf {index} not expected for CPUID leaf 0xB.");
                         subleaf.result.ecx = index;
                     }
                 }
