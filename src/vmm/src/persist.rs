f8f1437632a3a061c01032ee5be01b69e2199948
Babis Chalios <bchalios@amazon.es>
2025-06-06 13:36:38 +0200

    refactor: VirtIO MMIO persistence logic
    VirtIO MMIO restore logic activates the device the moment we restore the
    device state, if the device was activated when snapshotted. Move the
    activation responsibility to the logic the restores the MMIO transport.
    The reason for this change is that that's how it will be done for the
    PCI transport. Unifying this will allow us reusing the same types for
    restoring the non-transport state of devices.
    
    Note that we needed to change the way Net devices are saved/restored.
    RxBuffer type of Net devices holds RX descriptors that we have parsed
    from the Queue ahead of time. The way we restored this info was
    manipulating the queue to re-parse the RX descriptors during the restore
    phase. However, we need the device to be activated to do so, which now
    isn't. So, instead of storing this info inside the snapshot make sure we
    have flushed everything before taking the snapshot.
    
    Also, simplify a bit the types that we use for serializing/deserializing
    the state of a device.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -166,21 +166,8 @@ pub fn create_snapshot(
     // We need to mark queues as dirty again for all activated devices. The reason we
     // do it here is because we don't mark pages as dirty during runtime
     // for queue objects.
-    // SAFETY:
-    // This should never fail as we only mark pages only if device has already been activated,
-    // and the address validation was already performed on device activation.
     vmm.device_manager
-        .mmio_devices
-        .for_each_virtio_device(|_, _, device| {
-            let mmio_dev_locked = device.inner.lock().expect("Poisoned lock");
-            let d = mmio_dev_locked.locked_device();
-            if d.is_activated() {
-                d.mark_queue_memory_dirty(vmm.vm.guest_memory())
-            } else {
-                Ok(())
-            }
-        })
-        .unwrap();
+        .mark_virtio_queue_memory_dirty(vmm.vm.guest_memory());
 
     Ok(())
 }
