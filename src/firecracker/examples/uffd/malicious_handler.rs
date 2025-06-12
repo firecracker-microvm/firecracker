b1ef8795db709388bb8a40a10be61acb5c9a9369
Nikita Kalyazin <kalyazin@amazon.com>
2025-06-12 12:14:47 +0000

    test(uffd_utils): accept guest_memfd and bitmap memfd
    Accept receiving 3 fds instead of 1, where fds[1] is guest_memfd and
    fds[2] is userfault bitmap memfd.
    
    Also handle the FaultRequest message over the UDS socket by calling a
    new callback in the Runtime and sending a FaultReply.
    
    Co-authored-by: Patrick Roy <roypat@amazon.co.uk>
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/firecracker/examples/uffd/malicious_handler.rs
+++ src/firecracker/examples/uffd/malicious_handler.rs
@@ -21,17 +21,23 @@ fn main() {
     // Get Uffd from UDS. We'll use the uffd to handle PFs for Firecracker.
     let listener = UnixListener::bind(uffd_sock_path).expect("Cannot bind to socket path");
     let (stream, _) = listener.accept().expect("Cannot listen on UDS socket");
+    stream
+        .set_nonblocking(true)
+        .expect("Cannot set non-blocking");
 
     let mut runtime = Runtime::new(stream, file);
-    runtime.run(|uffd_handler: &mut UffdHandler| {
-        // Read an event from the userfaultfd.
-        let event = uffd_handler
-            .read_event()
-            .expect("Failed to read uffd_msg")
-            .expect("uffd_msg not ready");
+    runtime.run(
+        |uffd_handler: &mut UffdHandler| {
+            // Read an event from the userfaultfd.
+            let event = uffd_handler
+                .read_event()
+                .expect("Failed to read uffd_msg")
+                .expect("uffd_msg not ready");
 
-        if let userfaultfd::Event::Pagefault { .. } = event {
-            panic!("Fear me! I am the malicious page fault handler.")
-        }
-    });
+            if let userfaultfd::Event::Pagefault { .. } = event {
+                panic!("Fear me! I am the malicious page fault handler.")
+            }
+        },
+        |_uffd_handler: &mut UffdHandler, _offset: usize| {},
+    );
 }
