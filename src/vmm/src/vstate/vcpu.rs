d59bbdc88da1a0a99799e7c1decc39523177313c
Nikita Kalyazin <kalyazin@amazon.com>
2025-06-18 14:01:57 +0000

    tmp(vmm): do not attempt kvmclock_ctrl if secret free
    kvmclock is currently not supported by Secret Freedom and will always
    fail.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/vmm/src/vstate/vcpu.rs
+++ src/vmm/src/vstate/vcpu.rs
@@ -353,10 +353,13 @@ impl Vcpu {
                 // does not panic on resume, see https://docs.kernel.org/virt/kvm/api.html .
                 // We do not want to fail if the call is not successful, because depending
                 // that may be acceptable depending on the workload.
+                // TODO: once kvmclock is supported with Secret Fredom, remove this condition.
                 #[cfg(target_arch = "x86_64")]
-                if let Err(err) = self.kvm_vcpu.fd.kvmclock_ctrl() {
-                    METRICS.vcpu.kvmclock_ctrl_fails.inc();
-                    warn!("KVM_KVMCLOCK_CTRL call failed {}", err);
+                if self.userfault_channel.is_none() {
+                    if let Err(err) = self.kvm_vcpu.fd.kvmclock_ctrl() {
+                        METRICS.vcpu.kvmclock_ctrl_fails.inc();
+                        warn!("KVM_KVMCLOCK_CTRL call failed {}", err);
+                    }
                 }
 
                 // Move to 'paused' state.
