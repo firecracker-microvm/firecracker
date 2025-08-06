56e46ccf2500da31f11ee635f5451e28a335b5a3
Nikita Kalyazin <kalyazin@amazon.com>
2025-08-06 09:23:14 +0000

    fix(vmm): handle EINTR in process_uffd_socket
    Make sure we continue reading the FaultReply if the syscall was
    interrupted.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/vmm/src/lib.rs
+++ src/vmm/src/lib.rs
@@ -756,6 +756,7 @@ impl Vmm {
                             break;
                         }
                     }
+                    Err(e) if e.kind() == io::ErrorKind::Interrupted => continue,
                     Err(e) => panic!("Read error: {}", e),
                 }
             }
