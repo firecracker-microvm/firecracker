dce075420ad3cbf3939545d296087be0a61e2cf4
Babis Chalios <bchalios@amazon.es>
2025-08-06 14:35:49 +0200

    pci: add unit tests for PCI bus accesses
    Add a few unit tests to check the logic that accesses PCI configuration
    space via the PCI Bus. Ensure that negative cases are being handled
    properly.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- src/pci/src/bus.rs
+++ src/pci/src/bus.rs
@@ -182,11 +182,16 @@ impl PciConfigIo {
             return None;
         }
 
-        let (bus, device, _function, register) =
+        let (bus, device, function, register) =
             parse_io_config_address(self.config_address & !0x8000_0000);
 
         // Only support one bus.
         if bus != 0 {
+            return None;
+        }
+
+        // Don't support multi-function devices.
+        if function > 0 {
             return None;
         }
 
@@ -240,6 +245,16 @@ impl PciConfigIo {
 
 impl BusDevice for PciConfigIo {
     fn read(&mut self, _base: u64, offset: u64, data: &mut [u8]) {
+        // Only allow reads to the register boundary.
+        let start = offset as usize % 4;
+        let end = start + data.len();
+        if end > 4 {
+            for d in data.iter_mut() {
+                *d = 0xff;
+            }
+            return;
+        }
+
         // `offset` is relative to 0xcf8
         let value = match offset {
             0..=3 => self.config_address,
@@ -247,17 +262,8 @@ impl BusDevice for PciConfigIo {
             _ => 0xffff_ffff,
         };
 
-        // Only allow reads to the register boundary.
-        let start = offset as usize % 4;
-        let end = start + data.len();
-        if end <= 4 {
-            for i in start..end {
-                data[i - start] = (value >> (i * 8)) as u8;
-            }
-        } else {
-            for d in data {
-                *d = 0xff;
-            }
+        for i in start..end {
+            data[i - start] = (value >> (i * 8)) as u8;
         }
     }
 
@@ -285,10 +291,15 @@ impl PciConfigMmio {
     }
 
     fn config_space_read(&self, config_address: u32) -> u32 {
-        let (bus, device, _function, register) = parse_mmio_config_address(config_address);
+        let (bus, device, function, register) = parse_mmio_config_address(config_address);
 
         // Only support one bus.
         if bus != 0 {
+            return 0xffff_ffff;
+        }
+
+        // Don't support multi-function devices.
+        if function > 0 {
             return 0xffff_ffff;
         }
 
@@ -307,10 +318,15 @@ impl PciConfigMmio {
             return;
         }
 
-        let (bus, device, _function, register) = parse_mmio_config_address(config_address);
+        let (bus, device, function, register) = parse_mmio_config_address(config_address);
 
         // Only support one bus.
         if bus != 0 {
+            return;
+        }
+
+        // Don't support multi-function devices.
+        if function > 0 {
             return;
         }
 
@@ -415,14 +431,28 @@ fn parse_io_config_address(config_address: u32) -> (usize, usize, usize, usize) 
 
 #[cfg(test)]
 mod tests {
+    use std::sync::atomic::AtomicUsize;
     use std::sync::{Arc, Mutex};
 
     use vm_device::BusDevice;
 
-    use super::{PciBus, PciConfigIo, PciRoot};
-    use crate::DeviceRelocation;
+    use super::{PciBus, PciConfigIo, PciConfigMmio, PciRoot};
+    use crate::bus::{DEVICE_ID_INTEL_VIRT_PCIE_HOST, VENDOR_ID_INTEL};
+    use crate::{
+        DeviceRelocation, PciBarConfiguration, PciBarPrefetchable, PciBarRegionType, PciClassCode,
+        PciConfiguration, PciDevice, PciHeaderType, PciMassStorageSubclass,
+    };
 
-    struct RelocationMock;
+    #[derive(Debug, Default)]
+    struct RelocationMock {
+        reloc_cnt: AtomicUsize,
+    }
+
+    impl RelocationMock {
+        fn cnt(&self) -> usize {
+            self.reloc_cnt.load(std::sync::atomic::Ordering::SeqCst)
+        }
+    }
 
     impl DeviceRelocation for RelocationMock {
         fn move_bar(
@@ -433,13 +463,71 @@ mod tests {
             _pci_dev: &mut dyn crate::PciDevice,
             _region_type: crate::PciBarRegionType,
         ) -> std::result::Result<(), std::io::Error> {
+            self.reloc_cnt
+                .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
             Ok(())
         }
     }
 
+    struct PciDevMock(PciConfiguration);
+
+    impl PciDevMock {
+        fn new() -> Self {
+            let mut config = PciConfiguration::new(
+                0x42,
+                0x0,
+                0x0,
+                PciClassCode::MassStorage,
+                &PciMassStorageSubclass::SerialScsiController,
+                None,
+                PciHeaderType::Device,
+                0x13,
+                0x12,
+                None,
+                None,
+            );
+
+            config
+                .add_pci_bar(&PciBarConfiguration {
+                    addr: 0x1000,
+                    size: 0x1000,
+                    idx: 0,
+                    region_type: PciBarRegionType::Memory32BitRegion,
+                    prefetchable: PciBarPrefetchable::Prefetchable,
+                })
+                .unwrap();
+
+            PciDevMock(config)
+        }
+    }
+
+    impl PciDevice for PciDevMock {
+        fn write_config_register(
+            &mut self,
+            reg_idx: usize,
+            offset: u64,
+            data: &[u8],
+        ) -> Option<Arc<std::sync::Barrier>> {
+            self.0.write_config_register(reg_idx, offset, data);
+            None
+        }
+
+        fn read_config_register(&mut self, reg_idx: usize) -> u32 {
+            self.0.read_reg(reg_idx)
+        }
+
+        fn detect_bar_reprogramming(
+            &mut self,
+            reg_idx: usize,
+            data: &[u8],
+        ) -> Option<crate::BarReprogrammingParams> {
+            self.0.detect_bar_reprogramming(reg_idx, data)
+        }
+    }
+
     #[test]
-    fn test_writing_config_address() {
-        let mock = Arc::new(RelocationMock);
+    fn test_writing_io_config_address() {
+        let mock = Arc::new(RelocationMock::default());
         let root = PciRoot::new(None);
         let mut bus = PciConfigIo::new(Arc::new(Mutex::new(PciBus::new(root, mock))));
 
@@ -480,8 +568,8 @@ mod tests {
     }
 
     #[test]
-    fn test_reading_config_address() {
-        let mock = Arc::new(RelocationMock);
+    fn test_reading_io_config_address() {
+        let mock = Arc::new(RelocationMock::default());
         let root = PciRoot::new(None);
         let mut bus = PciConfigIo::new(Arc::new(Mutex::new(PciBus::new(root, mock))));
 
@@ -526,5 +614,337 @@ mod tests {
         // reading all of it at once
         bus.read(0, 0, &mut buffer);
         assert_eq!(buffer, [0x45, 0x44, 0x43, 0x42]);
+    }
+
+    fn initialize_bus() -> (PciConfigMmio, PciConfigIo, Arc<RelocationMock>) {
+        let mock = Arc::new(RelocationMock::default());
+        let root = PciRoot::new(None);
+        let mut bus = PciBus::new(root, mock.clone());
+        bus.add_device(1, Arc::new(Mutex::new(PciDevMock::new())))
+            .unwrap();
+        let bus = Arc::new(Mutex::new(bus));
+        (PciConfigMmio::new(bus.clone()), PciConfigIo::new(bus), mock)
+    }
+
+    #[test]
+    fn test_invalid_register_boundary_reads() {
+        let (mut mmio_config, mut io_config, _) = initialize_bus();
+
+        // Read crossing register boundaries
+        let mut buffer = [0u8; 4];
+        mmio_config.read(0, 1, &mut buffer);
+        assert_eq!(0xffff_ffff, u32::from_le_bytes(buffer));
+
+        let mut buffer = [0u8; 4];
+        io_config.read(0, 1, &mut buffer);
+        assert_eq!(0xffff_ffff, u32::from_le_bytes(buffer));
+
+        // As well in the config space
+        let mut buffer = [0u8; 4];
+        io_config.read(0, 5, &mut buffer);
+        assert_eq!(0xffff_ffff, u32::from_le_bytes(buffer));
+    }
+
+    // MMIO config addresses are of the form
+    //
+    // | Base address upper bits | Bus Number | Device Number | Function Number | Register number | Byte offset |
+    // |         31-28           |    27-20   |     19-15     |      14-12      |      11-2       |     0-1     |
+    //
+    // Meaning that the offset is built using:
+    //
+    // `bus << 20 | device << 15 | function << 12 | register << 2 | byte`
+    fn mmio_offset(bus: u8, device: u8, function: u8, register: u16, byte: u8) -> u32 {
+        assert!(device < 32);
+        assert!(function < 8);
+        assert!(register < 1024);
+        assert!(byte < 4);
+
+        (bus as u32) << 20
+            | (device as u32) << 15
+            | (function as u32) << 12
+            | (register as u32) << 2
+            | (byte as u32)
+    }
+
+    fn read_mmio_config(
+        config: &mut PciConfigMmio,
+        bus: u8,
+        device: u8,
+        function: u8,
+        register: u16,
+        byte: u8,
+        data: &mut [u8],
+    ) {
+        config.read(
+            0,
+            mmio_offset(bus, device, function, register, byte) as u64,
+            data,
+        );
+    }
+
+    fn write_mmio_config(
+        config: &mut PciConfigMmio,
+        bus: u8,
+        device: u8,
+        function: u8,
+        register: u16,
+        byte: u8,
+        data: &[u8],
+    ) {
+        config.write(
+            0,
+            mmio_offset(bus, device, function, register, byte) as u64,
+            data,
+        );
+    }
+
+    // Similarly, when using the IO mechanism the config addresses have the following format
+    //
+    // | Enabled | zeros | Bus Number | Device Number | Function Number | Register number | zeros |
+    // |    31   | 30-24 |   23-16    |     15-11     |      10-8       |       7-2       |  1-0  |
+    //
+    //
+    // Meaning that the address is built using:
+    //
+    // 0x8000_0000 | bus << 16 | device << 11 | function << 8 | register << 2;
+    //
+    // Only 32-bit aligned accesses are allowed here.
+    fn pio_offset(enabled: bool, bus: u8, device: u8, function: u8, register: u8) -> u32 {
+        assert!(device < 32);
+        assert!(function < 8);
+        assert!(register < 64);
+
+        let offset = if enabled { 0x8000_0000 } else { 0u32 };
+
+        offset
+            | (bus as u32) << 16
+            | (device as u32) << 11
+            | (function as u32) << 8
+            | (register as u32) << 2
+    }
+
+    fn set_io_address(
+        config: &mut PciConfigIo,
+        enabled: bool,
+        bus: u8,
+        device: u8,
+        function: u8,
+        register: u8,
+    ) {
+        let address = u32::to_le_bytes(pio_offset(enabled, bus, device, function, register));
+        config.write(0, 0, &address);
+    }
+
+    fn read_io_config(
+        config: &mut PciConfigIo,
+        enabled: bool,
+        bus: u8,
+        device: u8,
+        function: u8,
+        register: u8,
+        data: &mut [u8],
+    ) {
+        set_io_address(config, enabled, bus, device, function, register);
+        config.read(0, 4, data);
+    }
+
+    fn write_io_config(
+        config: &mut PciConfigIo,
+        enabled: bool,
+        bus: u8,
+        device: u8,
+        function: u8,
+        register: u8,
+        data: &[u8],
+    ) {
+        set_io_address(config, enabled, bus, device, function, register);
+        config.write(0, 4, data);
+    }
+
+    #[test]
+    fn test_mmio_invalid_bus_number() {
+        let (mut mmio_config, _, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        // Asking for Bus 1 should return all 1s
+        read_mmio_config(&mut mmio_config, 1, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+        // Writing the same
+        buffer[0] = 0x42;
+        write_mmio_config(&mut mmio_config, 1, 0, 0, 15, 0, &buffer);
+        read_mmio_config(&mut mmio_config, 1, 0, 0, 15, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 15, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0x0));
+
+        // Asking for Bus 0 should work
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(&buffer[..2], &u16::to_le_bytes(VENDOR_ID_INTEL));
+        assert_eq!(
+            &buffer[2..],
+            &u16::to_le_bytes(DEVICE_ID_INTEL_VIRT_PCIE_HOST)
+        );
+    }
+
+    #[test]
+    fn test_io_invalid_bus_number() {
+        let (_, mut pio_config, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        // Asking for Bus 1 should return all 1s
+        read_io_config(&mut pio_config, true, 1, 0, 0, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+
+        // Asking for Bus 0 should work
+        read_io_config(&mut pio_config, true, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(&buffer[..2], &u16::to_le_bytes(VENDOR_ID_INTEL));
+        assert_eq!(
+            &buffer[2..],
+            &u16::to_le_bytes(DEVICE_ID_INTEL_VIRT_PCIE_HOST)
+        );
+    }
+
+    #[test]
+    fn test_mmio_invalid_function() {
+        let (mut mmio_config, _, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        // Asking for Bus 1 should return all 1s
+        read_mmio_config(&mut mmio_config, 0, 0, 1, 0, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+        // Writing the same
+        buffer[0] = 0x42;
+        write_mmio_config(&mut mmio_config, 0, 0, 1, 15, 0, &buffer);
+        read_mmio_config(&mut mmio_config, 0, 0, 1, 15, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 15, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0x0));
+
+        // Asking for Bus 0 should work
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(&buffer[..2], &u16::to_le_bytes(VENDOR_ID_INTEL));
+        assert_eq!(
+            &buffer[2..],
+            &u16::to_le_bytes(DEVICE_ID_INTEL_VIRT_PCIE_HOST)
+        );
+    }
+
+    #[test]
+    fn test_io_invalid_function() {
+        let (_, mut pio_config, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        // Asking for Bus 1 should return all 1s
+        read_io_config(&mut pio_config, true, 0, 0, 1, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+
+        // Asking for Bus 0 should work
+        read_io_config(&mut pio_config, true, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(&buffer[..2], &u16::to_le_bytes(VENDOR_ID_INTEL));
+        assert_eq!(
+            &buffer[2..],
+            &u16::to_le_bytes(DEVICE_ID_INTEL_VIRT_PCIE_HOST)
+        );
+    }
+
+    #[test]
+    fn test_io_disabled_reads() {
+        let (_, mut pio_config, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        // Trying to read without enabling should return all 1s
+        read_io_config(&mut pio_config, false, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(buffer, u32::to_le_bytes(0xffff_ffff));
+
+        // Asking for Bus 0 should work
+        read_io_config(&mut pio_config, true, 0, 0, 0, 0, &mut buffer);
+        assert_eq!(&buffer[..2], &u16::to_le_bytes(VENDOR_ID_INTEL));
+        assert_eq!(
+            &buffer[2..],
+            &u16::to_le_bytes(DEVICE_ID_INTEL_VIRT_PCIE_HOST)
+        );
+    }
+
+    #[test]
+    fn test_io_disabled_writes() {
+        let (_, mut pio_config, _) = initialize_bus();
+
+        // Try to write the IRQ line used for the root port.
+        let mut buffer = [0u8; 4];
+
+        // First read the current value (use `enabled` bit)
+        read_io_config(&mut pio_config, true, 0, 0, 0, 15, &mut buffer);
+        let irq_line = buffer[0];
+
+        // Write without setting the `enabled` bit.
+        buffer[0] = 0x42;
+        write_io_config(&mut pio_config, false, 0, 0, 0, 15, &buffer);
+
+        // IRQ line shouldn't have changed
+        read_io_config(&mut pio_config, true, 0, 0, 0, 15, &mut buffer);
+        assert_eq!(buffer[0], irq_line);
+
+        // Write with `enabled` bit set.
+        buffer[0] = 0x42;
+        write_io_config(&mut pio_config, true, 0, 0, 0, 15, &buffer);
+
+        // IRQ line should change
+        read_io_config(&mut pio_config, true, 0, 0, 0, 15, &mut buffer);
+        assert_eq!(buffer[0], 0x42);
+    }
+
+    #[test]
+    fn test_mmio_writes() {
+        let (mut mmio_config, _, _) = initialize_bus();
+        let mut buffer = [0u8; 4];
+
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 15, 0, &mut buffer);
+        assert_eq!(buffer[0], 0x0);
+        write_mmio_config(&mut mmio_config, 0, 0, 0, 15, 0, &[0x42]);
+        read_mmio_config(&mut mmio_config, 0, 0, 0, 15, 0, &mut buffer);
+        assert_eq!(buffer[0], 0x42);
+    }
+
+    #[test]
+    fn test_bar_reprogramming() {
+        let (mut mmio_config, _, mock) = initialize_bus();
+        let mut buffer = [0u8; 4];
+        assert_eq!(mock.cnt(), 0);
+
+        read_mmio_config(&mut mmio_config, 0, 1, 0, 0x4, 0, &mut buffer);
+        let old_addr = u32::from_le_bytes(buffer) & 0xffff_fff0;
+        assert_eq!(old_addr, 0x1000);
+        write_mmio_config(
+            &mut mmio_config,
+            0,
+            1,
+            0,
+            0x4,
+            0,
+            &u32::to_le_bytes(0x1312_1110),
+        );
+
+        read_mmio_config(&mut mmio_config, 0, 1, 0, 0x4, 0, &mut buffer);
+        let new_addr = u32::from_le_bytes(buffer) & 0xffff_fff0;
+        assert_eq!(new_addr, 0x1312_1110);
+        assert_eq!(mock.cnt(), 1);
+
+        // BAR1 should not be used, so reading its address should return all 0s
+        read_mmio_config(&mut mmio_config, 0, 1, 0, 0x5, 0, &mut buffer);
+        assert_eq!(buffer, [0x0, 0x0, 0x0, 0x0]);
+
+        // and reprogramming shouldn't have any effect
+        write_mmio_config(
+            &mut mmio_config,
+            0,
+            1,
+            0,
+            0x5,
+            0,
+            &u32::to_le_bytes(0x1312_1110),
+        );
+
+        read_mmio_config(&mut mmio_config, 0, 1, 0, 0x5, 0, &mut buffer);
+        assert_eq!(buffer, [0x0, 0x0, 0x0, 0x0]);
     }
 }
