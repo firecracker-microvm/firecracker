d4b1d635fb03f5b1fa30839535c228441d802465
Riccardo Mancini <mancio@amazon.com>
2025-08-07 14:44:26 +0100

    chore(virtio-mem): create empty virtio/mem module
    Create the new module for the virtio-mem device.
    
    Signed-off-by: Riccardo Mancini <mancio@amazon.com>

---
+++ src/vmm/src/devices/virtio/mem/mod.rs
@@ -0,0 +1,13 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use vm_memory::GuestAddress;
+
+use crate::arch::FIRST_ADDR_PAST_64BITS_MMIO;
+
+pub(crate) const MEM_NUM_QUEUES: usize = 1;
+
+pub(crate) const MEM_QUEUE: usize = 0;
+
+pub const VIRTIO_MEM_DEFAULT_BLOCK_SIZE: usize = 2 << 20; // 2MiB
+pub const VIRTIO_MEM_DEFAULT_SLOT_SIZE: usize = 128 << 20; // 128 MiB
