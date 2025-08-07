ce3f3c513fa574058bff29c98e29f4e817a40394
Riccardo Mancini <mancio@amazon.com>
2025-08-07 18:57:57 +0100

    feat(virtio-mem): add dummy virtio-mem device
    Add a dummy virtio-mem device that is detected by the guest driver.
    The device is configured with total_size, block_size, and slot_size, and
    uses an allocated after the MMIO64 memory zone.
    
    Signed-off-by: Riccardo Mancini <mancio@amazon.com>

--- src/vmm/src/lib.rs
+++ src/vmm/src/lib.rs
@@ -140,6 +140,7 @@ use crate::devices::virtio::balloon::{
 };
 use crate::devices::virtio::block::BlockError;
 use crate::devices::virtio::block::device::Block;
+use crate::devices::virtio::mem::VirtioMemError;
 use crate::devices::virtio::net::Net;
 use crate::logger::{METRICS, MetricsError, error, info, warn};
 use crate::persist::{MicrovmState, MicrovmStateError, VmInfo};
@@ -252,6 +253,8 @@ pub enum VmmError {
     Block(#[from] BlockError),
     /// Balloon: {0}
     Balloon(#[from] BalloonError),
+    /// Failed to create memory hotplug device: {0}
+    VirtioMem(#[from] VirtioMemError),
 }
 
 /// Shorthand type for KVM dirty page bitmap.
