17a4ba103e7d9be0c84dd59ee77ef7e1baed23a8
Riccardo Mancini <mancio@amazon.com>
2025-08-22 12:14:59 +0100

    refactor(virtio-mem): define constants as MiB
    Avoid multiple conversions back and forth from MiB to bytes by just
    storing as MiB.
    
    Signed-off-by: Riccardo Mancini <mancio@amazon.com>

--- src/vmm/src/devices/virtio/mem/mod.rs
+++ src/vmm/src/devices/virtio/mem/mod.rs
@@ -13,7 +13,7 @@ pub(crate) const MEM_NUM_QUEUES: usize = 1;
 
 pub(crate) const MEM_QUEUE: usize = 0;
 
-pub const VIRTIO_MEM_DEFAULT_BLOCK_SIZE: usize = 2 << 20; // 2MiB
-pub const VIRTIO_MEM_DEFAULT_SLOT_SIZE: usize = 128 << 20; // 128 MiB
+pub const VIRTIO_MEM_DEFAULT_BLOCK_SIZE_MIB: usize = 2;
+pub const VIRTIO_MEM_DEFAULT_SLOT_SIZE_MIB: usize = 128;
 
 pub const VIRTIO_MEM_DEV_ID: &str = "mem";
