a02b8a093767b1d25a9fa250b02a14d8ed51f43b
Babis Chalios <bchalios@amazon.es>
2025-11-10 15:54:10 +0100

    vmclock: test snapshot safety features
    Extend VMClock integration tests to also account for the
    vm_generation_counter field and notification support flag.
    
    Signed-off-by: Babis Chalios <bchalios@amazon.es>

--- tests/host_tools/vmclock.c
+++ tests/host_tools/vmclock.c
@@ -1,12 +1,12 @@
 // Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <errno.h>
 #include <stdatomic.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
@@ -16,23 +16,26 @@
 
 const char *VMCLOCK_DEV_PATH = "/dev/vmclock0";
 
-int get_vmclock_handle(struct vmclock_abi **vmclock)
+int open_vmclock(void)
 {
   int fd = open(VMCLOCK_DEV_PATH, 0);
-  if (fd == -1)
-    goto out_err;
+  if (fd == -1) {
+    perror("open");
+    exit(1);
+  }
 
+  return fd;
+}
+
+struct vmclock_abi *get_vmclock_handle(int fd)
+{
   void *ptr = mmap(NULL, sizeof(struct vmclock_abi), PROT_READ, MAP_SHARED, fd, 0);
-  if (ptr == MAP_FAILED)
-    goto out_err_mmap;
+  if (ptr == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
 
-  *vmclock = ptr;
-  return 0;
-
-out_err_mmap:
-  close(fd);
-out_err:
-  return errno;
+  return ptr;
 }
 
 #define READ_VMCLOCK_FIELD_FN(type, field)                 \
@@ -56,15 +59,42 @@ type read##_##field (struct vmclock_abi *vmclock) {        \
 }
 
 READ_VMCLOCK_FIELD_FN(uint64_t, disruption_marker);
+READ_VMCLOCK_FIELD_FN(uint64_t, vm_generation_counter);
 
-int main()
+/*
+ * Read `vmclock_abi` structure using a file descriptor pointing to
+ * `/dev/vmclock0`.
+ */
+void read_vmclock(int fd, struct vmclock_abi *vmclock)
 {
-  struct vmclock_abi *vmclock;
+  int ret;
 
-  int err = get_vmclock_handle(&vmclock);
-  if (err) {
-    printf("Could not mmap vmclock struct: %s\n", strerror(err));
+  /*
+   * Use `pread()`, since the device doesn't implement lseek(), so
+   * we can't reset `fp`.
+   */
+  ret = pread(fd, vmclock, sizeof(*vmclock), 0);
+  if (ret < 0) {
+    perror("read");
     exit(1);
+  } else if (ret < (int) sizeof(*vmclock)) {
+    fprintf(stderr, "We don't handle partial writes (%d). Exiting!\n", ret);
+    exit(1);
+  }
+}
+
+void print_vmclock(struct vmclock_abi *vmclock)
+{
+  if (vmclock->flags & VMCLOCK_FLAG_VM_GEN_COUNTER_PRESENT) {
+    printf("VMCLOCK_FLAG_VM_GEN_COUNTER_PRESENT: true\n");
+  } else {
+    printf("VMCLOCK_FLAG_VM_GEN_COUNTER_PRESENT: false\n");
+  }
+
+  if (vmclock->flags & VMCLOCK_FLAG_NOTIFICATION_PRESENT) {
+    printf("VMCLOCK_FLAG_NOTIFICATION_PRESENT: true\n");
+  } else {
+    printf("VMCLOCK_FLAG_NOTIFICATION_PRESENT: false\n");
   }
 
   printf("VMCLOCK_MAGIC: 0x%x\n", vmclock->magic);
@@ -73,6 +103,92 @@ int main()
   printf("VMCLOCK_CLOCK_STATUS: %u\n", vmclock->clock_status);
   printf("VMCLOCK_COUNTER_ID: %u\n", vmclock->counter_id);
   printf("VMCLOCK_DISRUPTION_MARKER: %lu\n", read_disruption_marker(vmclock));
+  printf("VMCLOCK_VM_GENERATION_COUNTER: %lu\n", read_vm_generation_counter(vmclock));
+  fflush(stdout);
+}
+
+void run_poll(int fd) 
+{
+  struct vmclock_abi vmclock;
+  int epfd, ret, nfds;
+  struct epoll_event ev;
+
+  read_vmclock(fd, &vmclock);
+  print_vmclock(&vmclock);
+
+  epfd = epoll_create(1);
+  if (epfd < 0) {
+    perror("epoll_create");
+    exit(1);
+  }
+
+  ev.events = EPOLLIN | EPOLLRDNORM;
+  ev.data.fd = fd;
+  ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev);
+  if (ret < 0) {
+    perror("epoll_add");
+    exit(1);
+  }
+  
+  while (1) {
+    nfds = epoll_wait(epfd, &ev, 1, -1);
+    if (nfds < 0) {
+      perror("epoll_wait");
+      exit(1);
+    }
+
+    if (ev.data.fd != fd) {
+      fprintf(stderr, "Unknown file descriptor %d\n", ev.data.fd);
+      exit(1);
+    }
+
+    if (ev.events & EPOLLHUP) {
+      fprintf(stderr, "Device does not support notifications. Stop polling\n");
+      exit(1);
+    } else if (ev.events & EPOLLIN) {
+      fprintf(stdout, "Got VMClock notification\n");
+      read_vmclock(fd, &vmclock);
+      print_vmclock(&vmclock);
+    }
+  }
+}
+
+void print_help_message()
+{
+    fprintf(stderr, "usage: vmclock MODE\n");
+    fprintf(stderr, "Available modes:\n");
+    fprintf(stderr, "   -r\tRead vmclock_abi using read()\n");
+    fprintf(stderr, "   -m\tRead vmclock_abi using mmap()\n");
+    fprintf(stderr, "   -p\tPoll VMClock for changes\n");
+}
+
+int main(int argc, char *argv[])
+{
+  int fd;
+  struct vmclock_abi vmclock, *vmclock_ptr;
+
+  if (argc != 2) {
+    print_help_message();
+    exit(1);
+  }
+
+  fd = open_vmclock();
+
+  if (!strncmp(argv[1], "-r", 3)) {
+    printf("Reading VMClock with read()\n");
+    read_vmclock(fd, &vmclock);
+    print_vmclock(&vmclock);
+  } else if (!strncmp(argv[1], "-m", 3)) {
+    printf("Reading VMClock with mmap()\n");
+    vmclock_ptr = get_vmclock_handle(fd);
+    print_vmclock(vmclock_ptr);
+  } else if (!strncmp(argv[1], "-p", 3)) {
+    printf("Polling VMClock\n");
+    run_poll(fd);
+  } else {
+    print_help_message();
+    exit(1);
+  }
 
   return 0;
 }
