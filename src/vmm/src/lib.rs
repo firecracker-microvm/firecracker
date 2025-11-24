168f9e2b506cb741004d1899fee06c46103a7ac0
Nikita Kalyazin <kalyazin@amazon.com>
2025-11-24 17:14:28 +0000

    feat(vmm): configure kvm userfault if secret free is enabled
    This is needed to instruct the kernel to exit to userspace when a vCPU
    fault occurs and the corresponding bit in the userfault bitmap is set.
    
    The userfault bitmap is allocated in a memfd by Firecracker and sent to
    the UFFD handler.
    
    This also sends 3 fds to the UFFD handler in the handshake:
     - UFFD (original)
     - guest_memfd: for the handler to be able to populate guest memory
     - userfault bitmap memfd: for the handler to be able to disable exits
       to userspace for the pages that have already been populated
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- src/vmm/src/lib.rs
+++ src/vmm/src/lib.rs
@@ -119,6 +119,7 @@ pub mod initrd;
 use std::collections::HashMap;
 use std::io;
 use std::os::unix::io::AsRawFd;
+use std::os::unix::net::UnixStream;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
@@ -306,6 +307,8 @@ pub struct Vmm {
 
     /// VM object.
     pub vm: Vm,
+    // Used for userfault communication with the UFFD handler when secret freedom is enabled
+    uffd_socket: Option<UnixStream>,
     // Device manager
     device_manager: DeviceManager,
 }
