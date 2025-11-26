abd0693f0616e22d9412caa7e3b1135d748100a9
Nikita Kalyazin <kalyazin@amazon.com>
2025-11-26 14:54:33 +0000

    test(pmem): add secret free
    This is to make sure we test secret freedom with pmem.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- tests/integration_tests/performance/test_pmem.py
+++ tests/integration_tests/performance/test_pmem.py
@@ -89,6 +89,7 @@ def test_pmem_performance(
     fio_engine,
     metrics,
     results_dir,
+    secret_free,
 ):
     """
     Measure performance of pmem device
@@ -96,7 +97,9 @@ def test_pmem_performance(
     vm = uvm
     vm.memory_monitor = None
     vm.spawn()
-    vm.basic_config(vcpu_count=vcpus, mem_size_mib=GUEST_MEM_MIB)
+    vm.basic_config(
+        vcpu_count=vcpus, mem_size_mib=GUEST_MEM_MIB, secret_free=secret_free
+    )
     vm.add_net_iface()
     # Add a secondary block device for benchmark tests.
     fs = drive_tools.FilesystemFile(
@@ -169,6 +172,7 @@ def test_pmem_first_read(
     fio_block_size,
     metrics,
     results_dir,
+    secret_free,
 ):
     """
     Measure performance of a first full read from the pmem device.
@@ -180,7 +184,7 @@ def test_pmem_first_read(
     for i in range(10):
         vm = microvm_factory.build(guest_kernel, rootfs, pci=True, monitor_memory=False)
         vm.spawn()
-        vm.basic_config(mem_size_mib=GUEST_MEM_MIB)
+        vm.basic_config(mem_size_mib=GUEST_MEM_MIB, secret_free=secret_free)
         vm.add_net_iface()
 
         fs = drive_tools.FilesystemFile(
