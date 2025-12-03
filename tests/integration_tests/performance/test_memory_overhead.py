31bcafdd9745d733c268418630444614f44abbb7
Nikita Kalyazin <kalyazin@amazon.com>
2025-12-03 14:37:52 +0000

    test(perf): add secret_free to test_memory_overhead
    Increasing Secret Freedom test coverage.
    
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- tests/integration_tests/performance/test_memory_overhead.py
+++ tests/integration_tests/performance/test_memory_overhead.py
@@ -37,6 +37,7 @@ def test_memory_overhead(
     mem_size_mib,
     pci_enabled,
     metrics,
+    secret_free,
 ):
     """Track Firecracker memory overhead.
 
@@ -48,7 +49,11 @@ def test_memory_overhead(
             guest_kernel_acpi, rootfs, pci=pci_enabled, monitor_memory=False
         )
         microvm.spawn(emit_metrics=True)
-        microvm.basic_config(vcpu_count=vcpu_count, mem_size_mib=mem_size_mib)
+        microvm.basic_config(
+            vcpu_count=vcpu_count,
+            mem_size_mib=mem_size_mib,
+            secret_free=secret_free,
+        )
         microvm.add_net_iface()
         microvm.start()
         metrics.set_dimensions(
