330a8966343dc95ac543db05149001bdd74a8892
Egor Lazarchuk <yegorlz@amazon.co.uk>
2026-01-07 11:11:49 +0000

    chore(devtool): minor adjustments
    - rename ensure_current_artifacts_are_set_up to
    ensure_current_artifacts to shorten a name a bit
    - rename LOCAL_ARTIFACTS_CURRENT_DIR_FILE to
      LOCAL_CURRENT_ARTIFACTS_FILE to make name more descriptive
    - Simplify logs regarding artifacts status
    - remove ` symbols from cmd_help as bash was interpreting them as actual
      commands to execute
    - other minor renamings
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- .buildkite/pipeline_docker_popular.py
+++ .buildkite/pipeline_docker_popular.py
@@ -28,7 +28,7 @@ pipeline.build_group_per_arch(
 pipeline.build_group(
     "docker-popular-containers",
     [
-        "./tools/devtool ensure_current_artifacts_are_set_up",
+        "./tools/devtool ensure_current_artifacts",
         f'buildkite-agent artifact download "{ROOTFS_TAR}" .',
         f'tar xzf "{ROOTFS_TAR}" -C tools/test-popular-containers',
         './tools/devtool sh "cd ./tools/test-popular-containers; PYTHONPATH=../../tests ./test-docker-rootfs.py"',
