c5c08b984895c2d6a1785e05a0d4790d56bc19c9
aerosouund <aerosound161@gmail.com>
2026-03-24 13:03:13 +0000

    feat: add conn_buffer_size as a persistence param
    - the setting should be persisted across snapshot and restore so the new
      vsock buffers have the same size
    - prevent the caller from passing too large buffers. 256 is also the
      limit set by the linux kernel
    - prevent the caller from passing too small buffers (less than 4096)
      bytes.
    - make the field default to none and add default serialization
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- src/vmm/src/devices/virtio/persist.rs
+++ src/vmm/src/devices/virtio/persist.rs
@@ -483,7 +483,7 @@ mod tests {
         // Remove the file so the path can be used by the socket.
         temp_uds_path.remove().unwrap();
         let uds_path = String::from(temp_uds_path.as_path().to_str().unwrap());
-        let backend = VsockUnixBackend::new(guest_cid, uds_path, VsockType::Stream).unwrap();
+        let backend = VsockUnixBackend::new(guest_cid, uds_path, VsockType::Stream, None).unwrap();
         let device = Vsock::new(guest_cid, backend, &VsockType::Stream).unwrap();
         let vsock = Arc::new(Mutex::new(device));
         let mmio_transport =
