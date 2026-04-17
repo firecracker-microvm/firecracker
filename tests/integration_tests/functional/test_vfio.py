3b44d8b9db3f30854c3ceae50d95879f53db8689
Egor Lazarchuk <yegorlz@amazon.co.uk>
2026-04-17 15:11:30 +0100

    vfio: disallow starting VM with VFIO with incompatible configurations
    Current VFIO implementation has some restrictions:
    - Does not work without PCI since VFIO devices are PCI devices
    - Does not work with virtio-mem device since we don't update DMA
      mappings on hot-plug/unplug
    - Does not work with virtio-balloon since it can `fadvise` on memory
    
    In order to prevent VMs being launched with invalid configurations,
    implement multiple checks for invalid configurations:
    - At API level, prevent adding of incompatible combinations (VFIO after
      balloon/mem or in reverse)
    - At vm creation or snapshot restoraton since they get VmResources from
      other sources.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- tests/integration_tests/functional/test_vfio.py
+++ tests/integration_tests/functional/test_vfio.py
@@ -62,12 +62,12 @@ def uvm_with_vfio(microvm_factory, guest_kernel, rootfs):
     return vm
 
 
-def test_api_vfio(uvm):
+def test_api_vfio(microvm_factory, guest_kernel, rootfs):
     """
     Test VFIO passthrough API commands.
     """
 
-    vm = uvm
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
     vm.spawn()
     vm.basic_config()
 
@@ -87,6 +87,96 @@ def test_api_vfio(uvm):
     expected_msg = re.escape("The ID cannot be empty.")
     with pytest.raises(RuntimeError, match=expected_msg):
         vm.api.vfio.put(id="", sbdf="0000:01:02.03")
+
+
+def test_vfio_incompatible_devices_no_pci(microvm_factory, guest_kernel, rootfs):
+    """
+    Test that adding VFIO device without PCI fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=False)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    expected_msg = re.escape("VFIO devices attached, but PCI disabled")
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.vfio.put(id="nvme0", sbdf="0000:01:02.03")
+
+
+def test_vfio_incompatible_devices_vfio_balloon(microvm_factory, guest_kernel, rootfs):
+    """
+    Test that adding balloon after VFIO fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.vfio.put(id="nvme0", sbdf="0000:01:02.03")
+    expected_msg = re.escape(
+        "VFIO devices are not compatible with memory balloon device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.balloon.put(
+            amount_mib=0, deflate_on_oom=False, stats_polling_interval_s=1
+        )
+
+
+def test_vfio_incompatible_devices_balloon_vfio(microvm_factory, guest_kernel, rootfs):
+    """
+    Test that adding VFIO after balloon fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.balloon.put(amount_mib=0, deflate_on_oom=False, stats_polling_interval_s=1)
+    expected_msg = re.escape(
+        "VFIO devices are not compatible with memory balloon device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.vfio.put(id="nvme0", sbdf="0000:01:02.03")
+
+
+def test_vfio_incompatible_devices_vfio_mem_hot_plug(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding memory hotplug after VFIO fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.vfio.put(id="nvme0", sbdf="0000:01:02.03")
+    expected_msg = re.escape(
+        "VFIO devices are not compatible with memory hot-plugging device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.memory_hotplug.put(
+            total_size_mib=256, slot_size_mib=256, block_size_mib=64
+        )
+
+
+def test_vfio_incompatible_devices_mem_hot_plug_vfio(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding VFIO after memory hotplug fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.memory_hotplug.put(total_size_mib=256, slot_size_mib=256, block_size_mib=64)
+    expected_msg = re.escape(
+        "VFIO devices are not compatible with memory hot-plugging device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.vfio.put(id="nvme0", sbdf="0000:01:02.03")
 
 
 def test_vfio_nvme_not_present_without_config(microvm_factory, guest_kernel, rootfs):
