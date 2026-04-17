8240fd0ff5b8ecc72a912924553cc572b45bb6d6
Egor Lazarchuk <yegorlz@amazon.co.uk>
2026-04-17 15:11:30 +0100

    device passthrough: disallow starting VM in incompatible configurations
    Current device passthrough implementation has some restrictions:
    - Does not work without PCI since VFIO devices are PCI devices
    - Does not work with virtio-mem device since we don't update DMA
      mappings on hot-plug/unplug
    - Does not work with virtio-balloon since it can `fadvise` on memory
    
    In order to prevent VMs being launched with invalid configurations,
    implement multiple checks for invalid configurations:
    - At API level, prevent adding of incompatible combinations (VFIO after
      balloon/mem or in reverse)
    - At vm creation or snapshot restoraton since they get VmResources from
      other sources.
    
    Signed-off-by: Egor Lazarchuk <yegorlz@amazon.co.uk>

--- tests/integration_tests/functional/test_device_passthrough.py
+++ tests/integration_tests/functional/test_device_passthrough.py
@@ -7,9 +7,10 @@ import re
 
 import pytest
 
-from framework.artifacts import GUEST_KERNEL_DEFAULT, pin_guest_kernel
+from framework.artifacts import GUEST_KERNEL_DEFAULT, pin_guest_kernel, pin_pci
 
 
+@pin_pci(True)
 @pin_guest_kernel(GUEST_KERNEL_DEFAULT)
 def test_api_device_passthrough(uvm):
     """
@@ -60,3 +61,104 @@ def test_api_device_passthrough_runtime(uvm):
     )
     with pytest.raises(RuntimeError, match=expected_msg):
         vm.api.device_passthrough.put(id="nvme69", sbdf="01:02.03")
+
+
+@pin_guest_kernel(GUEST_KERNEL_DEFAULT)
+def test_device_passthrough_incompatible_devices_no_pci(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding device without PCI fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=False)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    expected_msg = re.escape("Passthrough devices attached, but PCI disabled")
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.device_passthrough.put(id="nvme0", sbdf="0000:01:02.03")
+
+
+@pin_guest_kernel(GUEST_KERNEL_DEFAULT)
+def test_device_passthrough_incompatible_devices_dp_then_balloon(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding balloon after passthrough device fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.device_passthrough.put(id="nvme0", sbdf="0000:01:02.03")
+    expected_msg = re.escape(
+        "Passthrough devices are not compatible with memory balloon device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.balloon.put(
+            amount_mib=0, deflate_on_oom=False, stats_polling_interval_s=1
+        )
+
+
+@pin_guest_kernel(GUEST_KERNEL_DEFAULT)
+def test_device_passthrough_incompatible_devices_balloon_then_dp(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding passthrough device after balloon fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.balloon.put(amount_mib=0, deflate_on_oom=False, stats_polling_interval_s=1)
+    expected_msg = re.escape(
+        "Passthrough devices are not compatible with memory balloon device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.device_passthrough.put(id="nvme0", sbdf="0000:01:02.03")
+
+
+@pin_guest_kernel(GUEST_KERNEL_DEFAULT)
+def test_device_passthrough_incompatible_devices_dp_then_mem_hot_plug(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding memory hotplug after passthrough device fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.device_passthrough.put(id="nvme0", sbdf="0000:01:02.03")
+    expected_msg = re.escape(
+        "Passthrough devices are not compatible with memory hot-plugging device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.memory_hotplug.put(
+            total_size_mib=256, slot_size_mib=256, block_size_mib=64
+        )
+
+
+@pin_guest_kernel(GUEST_KERNEL_DEFAULT)
+def test_device_passthrough_incompatible_devices_mem_hot_plug_then_dp(
+    microvm_factory, guest_kernel, rootfs
+):
+    """
+    Test that adding passthrough device after memory hotplug fails at API level.
+    """
+    vm = microvm_factory.build(guest_kernel, rootfs, pci=True)
+    vm.jailer.setup()
+    vm.spawn()
+    vm.basic_config()
+
+    vm.api.memory_hotplug.put(total_size_mib=256, slot_size_mib=256, block_size_mib=64)
+    expected_msg = re.escape(
+        "Passthrough devices are not compatible with memory hot-plugging device"
+    )
+    with pytest.raises(RuntimeError, match=expected_msg):
+        vm.api.device_passthrough.put(id="nvme0", sbdf="0000:01:02.03")
