8c1354fe37a9d319000ddb484542d2f177279efa
Ilias Stamatis <ilstam@amazon.com>
2026-06-15 11:10:09 +0100

    Revert "jailer: Use O_NOFOLLOW for cgroup and netns file operations"
    This reverts commit ce2a467895c188c438cba94bd964a9d1ca2b92c4.
    
    It turns out users do want to legitimately use symlinks in these paths.
    Commit 72340caae33 ("docs: Clarify that all jailer inputs must be
    trusted") updated documentation to make it clear that all jailer inputs
    are always considered trusted anyway, so this revert is not a
    regression.
    
    Signed-off-by: Ilias Stamatis <ilstam@amazon.com>

--- src/jailer/src/env.rs
+++ src/jailer/src/env.rs
@@ -519,11 +519,8 @@ impl Env {
 
     fn join_netns(path: &str) -> Result<(), JailerError> {
         // The fd backing the file will be automatically dropped at the end of the scope
-        let netns = OpenOptions::new()
-            .read(true)
-            .custom_flags(libc::O_NOFOLLOW)
-            .open(path)
-            .map_err(|err| JailerError::FileOpen(PathBuf::from(path), err))?;
+        let netns =
+            File::open(path).map_err(|err| JailerError::FileOpen(PathBuf::from(path), err))?;
 
         // SAFETY: Safe because we are passing valid parameters.
         SyscallReturnCode(unsafe { libc::setns(netns.as_raw_fd(), libc::CLONE_NEWNET) })
