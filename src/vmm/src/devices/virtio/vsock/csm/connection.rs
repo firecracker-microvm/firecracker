fc43a8d44a79e91b2acaf4c0dc466cabfcd8d6c6
aerosouund <aerosound161@gmail.com>
2026-06-17 13:40:17 +0000

    refactor: make the vsock TestBackend own metrics creation
    - the muxer is the owner of metrics instantiation in vsock, make the
      TestBacked (muxer equivalent in testing) create VsockDeviceMetrics arc
      during calls to TestBackend::new() so other entities during tests
      (connections and the vsock device) can use that backend's metrics
      instance.
    - make the connection builders take an Option<Arc<VsockDeviceMetrics>>
      as an argument to delegate metrics creation to their caller. Only
      create metrics instances if that caller passed None.
    
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- src/vmm/src/devices/virtio/vsock/csm/connection.rs
+++ src/vmm/src/devices/virtio/vsock/csm/connection.rs
@@ -514,17 +514,19 @@ where
         local_port: u32,
         peer_port: u32,
         peer_buf_alloc: u32,
+        metrics: Option<Arc<VsockDeviceMetrics>>,
     ) -> Self {
-        // If a muxer already added a metrics instance to the map.
+        // If the caller has already supplied us a metrics instance, use that.
         // otherwise if this connection is being initialized separately
         // in a test let it create its own metrics instance
-        let metrics = METRICS
-            .write()
-            .unwrap()
-            .entry(peer_cid)
-            .or_insert_with(|| Arc::new(VsockDeviceMetrics::default()))
-            .clone();
-
+        let metrics = metrics.unwrap_or_else(|| {
+            METRICS
+                .write()
+                .unwrap()
+                .entry(peer_cid)
+                .or_insert_with(|| Arc::new(VsockDeviceMetrics::default()))
+                .clone()
+        });
         Self {
             local_cid,
             peer_cid,
@@ -551,14 +553,19 @@ where
         peer_cid: u64,
         local_port: u32,
         peer_port: u32,
+        metrics: Option<Arc<VsockDeviceMetrics>>,
     ) -> Self {
-        let metrics = METRICS
-            .write()
-            .unwrap()
-            .entry(peer_cid)
-            .or_insert_with(|| Arc::new(VsockDeviceMetrics::default()))
-            .clone();
-
+        // If the caller has already supplied us a metrics instance, use that.
+        // otherwise if this connection is being initialized separately
+        // in a test let it create its own metrics instance
+        let metrics = metrics.unwrap_or_else(|| {
+            METRICS
+                .write()
+                .unwrap()
+                .entry(peer_cid)
+                .or_insert_with(|| Arc::new(VsockDeviceMetrics::default()))
+                .clone()
+        });
         Self {
             local_cid,
             peer_cid,
@@ -884,6 +891,9 @@ mod tests {
 
         fn new(conn_state: ConnState) -> Self {
             let vsock_test_ctx = TestContext::new();
+            // Extract the metrics instance from the test backend to supply to connection
+            // builders
+            let metrics = vsock_test_ctx.device.backend.metrics.clone();
             let mut handler_ctx = vsock_test_ctx.create_event_handler_context();
             let stream = TestStream::new();
             let mut rx_pkt = VsockPacketRx::new().unwrap();
@@ -908,9 +918,15 @@ mod tests {
                     LOCAL_PORT,
                     PEER_PORT,
                     PEER_BUF_ALLOC,
+                    Some(metrics),
                 ),
                 ConnState::LocalInit => VsockConnection::<TestStream>::new_local_init(
-                    stream, LOCAL_CID, PEER_CID, LOCAL_PORT, PEER_PORT,
+                    stream,
+                    LOCAL_CID,
+                    PEER_CID,
+                    LOCAL_PORT,
+                    PEER_PORT,
+                    Some(metrics),
                 ),
                 ConnState::Established => {
                     let mut conn = VsockConnection::<TestStream>::new_peer_init(
@@ -920,6 +936,7 @@ mod tests {
                         LOCAL_PORT,
                         PEER_PORT,
                         PEER_BUF_ALLOC,
+                        Some(metrics),
                     );
                     assert!(conn.has_pending_rx());
                     conn.recv_pkt(&mut rx_pkt).unwrap();
