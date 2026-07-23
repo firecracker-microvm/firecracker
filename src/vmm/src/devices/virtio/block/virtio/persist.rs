b7ffb1382593288b4b45fe85077afb65cc597474
Pierre Bertholom <pbertho@amazon.com>
2026-07-23 15:10:42 +0100

    block: extract inline I/O processing into BlockWorker
    Move queue and completion processing into BlockWorker while keeping
    execution on the VMM thread.
    
    Transfer runtime resources to the worker on activation and return them
    on deactivation. This prepares the data path for worker thread support.
    
    Signed-off-by: Pierre Bertholom <pbertho@amazon.com>

--- src/vmm/src/devices/virtio/block/virtio/persist.rs
+++ src/vmm/src/devices/virtio/block/virtio/persist.rs
@@ -9,14 +9,14 @@ use std::iter;
 use std::sync::{Arc, Mutex};
 use vmm_sys_util::eventfd::EventFd;
 
-use super::device::{BlockResources, DiskProperties};
+use super::device::{BlockResources, BlockState, DiskProperties};
 use super::*;
 use crate::devices::virtio::block::persist::BlockConstructorArgs;
 use crate::devices::virtio::block::virtio::device::{
     FileEngineType, VirtioBlkTopology, VirtioBlockConfig,
 };
 use crate::devices::virtio::block::virtio::metrics::BlockMetricsPerDevice;
-use crate::devices::virtio::device::{DeviceState, VirtioDeviceType};
+use crate::devices::virtio::device::VirtioDeviceType;
 use crate::devices::virtio::generated::virtio_blk::{VIRTIO_BLK_F_DISCARD, VIRTIO_BLK_F_RO};
 use crate::devices::virtio::persist::VirtioDeviceState;
 use crate::rate_limiter::RateLimiter;
@@ -159,10 +159,9 @@ impl Persist<'_> for VirtioBlock {
             config_space,
             activate_evt: EventFd::new(libc::EFD_NONBLOCK).map_err(VirtioBlockError::EventFd)?,
 
-            device_state: DeviceState::Inactive,
             config,
             rate_limiter: Arc::new(Mutex::new(rate_limiter)),
-            resources,
+            state: BlockState::Configuring(resources),
             metrics: BlockMetricsPerDevice::alloc(state.id.clone()),
         })
     }
