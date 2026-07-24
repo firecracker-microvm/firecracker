056e5bb9d30776740753f45390c3f2e1f978bc7b
Pierre Bertholom <pbertho@amazon.com>
2026-07-24 16:33:13 +0100

    block: add runtime updates on worker thread
    Forward disk image updates to the worker.
    Return the updated disk capacity required by the VMM to update config
    space.
    Wake the worker after the VMM handles a rate limiter timer event.
    
    Signed-off-by: Pierre Bertholom <pbertho@amazon.com>

--- src/vmm/src/devices/virtio/block/virtio/device.rs
+++ src/vmm/src/devices/virtio/block/virtio/device.rs
@@ -605,9 +605,7 @@ impl VirtioBlock {
             BlockState::Active(ActiveBlock::Inline(worker)) => {
                 worker.process_queue().unwrap();
             }
-            BlockState::Active(ActiveBlock::Threaded(_)) => {
-                unreachable!("worker control messages are not connected yet")
-            }
+            BlockState::Active(ActiveBlock::Threaded(active)) => active.worker_handle.kick(),
             BlockState::Configuring(_, _) => {}
             BlockState::Placeholder => unreachable!("not a runtime state"),
         }
@@ -631,9 +629,9 @@ impl VirtioBlock {
             BlockState::Active(ActiveBlock::Inline(worker)) => {
                 worker.update_disk_image(disk_image_path, read_only)?
             }
-            BlockState::Active(ActiveBlock::Threaded(_)) => {
-                unreachable!("worker control messages are not connected yet")
-            }
+            BlockState::Active(ActiveBlock::Threaded(active)) => active
+                .worker_handle
+                .update_disk_image(disk_image_path, read_only)?,
             BlockState::Placeholder => unreachable!("not a runtime state"),
         };
         self.config_space.capacity = nsectors.to_le();
