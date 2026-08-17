db3828ea85223f211b487247a185e097ecd1f0ff
PierreBertholom <pbertho@amazon.com>
2026-08-17 15:55:59 +0100

    block: prepare rate limiter for worker sharing
    Rate limiter will be shared between worker threads of a same device.
    Wrap rate limiter in mutex. Extract rate limiter registration to allow
    both inline and threaded paths to register timer_fd on the VMM event
    manager.
    
    Signed-off-by: Pierre Bertholom <pbertho@amazon.com>

--- src/vmm/src/devices/virtio/block/virtio/test_utils.rs
+++ src/vmm/src/devices/virtio/block/virtio/test_utils.rs
@@ -72,11 +72,7 @@ pub fn set_queue(blk: &mut VirtioBlock, idx: usize, q: Queue) {
 }
 
 pub fn set_rate_limiter(blk: &mut VirtioBlock, rl: RateLimiter) {
-    blk.resources_mut().rate_limiter = rl;
-}
-
-pub fn rate_limiter(blk: &mut VirtioBlock) -> &RateLimiter {
-    blk.rate_limiter()
+    *blk.lock_rate_limiter() = rl;
 }
 
 #[cfg(test)]
