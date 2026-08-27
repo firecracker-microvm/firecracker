56d3fdfa1458e9cab2e52378f5ef2ea68aac39ef
Ilias Stamatis <ilstam@amazon.com>
2026-08-27 19:46:05 +0100

    persist: Set pci_enabled from snapshot and warn on mismatch with cli arg
    Due to questionable past API design decisions whether the restored VM
    uses PCI is decided by the snapshot and not by the --enable-pci flag of
    the process doing the restore. Set the option in VmResources based on
    the snapshot state.
    
    Since we're here, log a warning when the two don't match. That's the
    best we can do at this point, since aborting can break customer
    workflows.
    
    VmResources::pci_enabled is not used on snapshot restore at the moment,
    that's why its value didn't matter, but this is going to change in
    subsequent patches.
    
    Signed-off-by: Ilias Stamatis <ilstam@amazon.com>

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -433,6 +433,22 @@ pub fn restore_from_snapshot(
         .map_err(|_| MachineConfigError::InvalidVcpuCount)
         .map_err(BuildMicrovmFromSnapshotError::VmUpdateConfig)?;
 
+    // Due to questionable past API design decisions whether the restored VM
+    // uses PCI is decided by the snapshot and not by the --enable-pci flag of
+    // the process doing the restore. Set the option based on the snapshot
+    // state.
+    let pci_enabled = matches!(
+        microvm_state.device_states.virtio_state,
+        VirtioDevicesState::Pci(_)
+    );
+    if pci_enabled != vm_resources.pci_enabled {
+        warn!(
+            "The snapshot's PCI configuration does not match --enable-pci; \
+             following the snapshot configuration."
+        );
+    }
+    vm_resources.pci_enabled = pci_enabled;
+
     vm_resources
         .update_machine_config(&MachineConfigUpdate {
             vcpu_count: Some(vcpu_count),
