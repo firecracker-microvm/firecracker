6dccbf5ca82bd19735ff0fe7e2e761d7c99778dd
Pierre Bertholom <pbertho@amazon.com>
2026-09-07 10:50:05 +0100

    block: allow drivers to use fewer queues than exposed
    Virtio drivers can use fewer queues than the device exposes. Keep
    unused queues uninitialized while their workers remain available for
    renegotiation.
    
    Signed-off-by: Pierre Bertholom <pbertho@amazon.com>

--- src/vmm/src/devices/virtio/block/virtio/device.rs
+++ src/vmm/src/devices/virtio/block/virtio/device.rs
@@ -828,17 +828,20 @@ impl VirtioDevice for VirtioBlock {
 
         let event_idx = self.has_feature(u64::from(VIRTIO_RING_F_EVENT_IDX));
 
-        let BlockRuntimeState::Configuring(resources, worker_handles) = &mut self.state else {
+        let BlockRuntimeState::Configuring(resources, _) = &mut self.state else {
             unreachable!("inactive device is not configurable");
         };
 
+        // The driver can use fewer queues than the device exposes.
         for resource in resources.iter_mut() {
-            resource
-                .queue
-                .initialize(&mem)
-                .map_err(ActivateError::QueueMemoryError)?;
-            if event_idx {
-                resource.queue.enable_notif_suppression();
+            if resource.queue.config.ready {
+                resource
+                    .queue
+                    .initialize(&mem)
+                    .map_err(ActivateError::QueueMemoryError)?;
+                if event_idx {
+                    resource.queue.enable_notif_suppression();
+                }
             }
         }
 
@@ -968,11 +971,15 @@ impl VirtioDevice for VirtioBlock {
         match &mut self.state {
             BlockRuntimeState::Configuring(resources, _) => {
                 for resource in resources {
-                    resource.queue.initialize(mem)?;
+                    if resource.queue.config.ready {
+                        resource.queue.initialize(mem)?;
+                    }
                 }
             }
             BlockRuntimeState::Inline(worker) => {
-                worker.resources.queue.initialize(mem)?;
+                if worker.resources.queue.config.ready {
+                    worker.resources.queue.initialize(mem)?;
+                }
             }
             BlockRuntimeState::Threaded(active) => {
                 for handle in &active.worker_handles {
